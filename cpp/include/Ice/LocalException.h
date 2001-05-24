918bd075ecb16ceebb876675773539699abad850
Marc Laukien <marc@zeroc.com>
2001-05-24 21:27:14 +0000

    exception stuff

--- cpp/include/Ice/LocalException.h
+++ cpp/include/Ice/LocalException.h
@@ -20,9 +20,21 @@ class ICE_API LocalException
 {
 public:
 
+    LocalException(const char*, int);
     virtual ~LocalException();
+    LocalException(const LocalException&);
+    LocalException& operator=(const LocalException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
+
+protected:
+
+    std::string debugInfo() const;
+
+private:
+
+    const char* file_;
+    int line_;
 };
 
 ICE_API std::ostream& operator<<(std::ostream&, const LocalException&);
@@ -31,22 +43,24 @@ class ICE_API SystemException : public LocalException
 {
 public:    
 
-    SystemException();
+    SystemException(const char*, int);
     SystemException(const SystemException&);
     SystemException& operator=(const SystemException&);
-
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 
 protected:
 
-    int error;
+    int error_;
 };
 
 class ICE_API SocketException : public SystemException
 {
 public:    
 
+    SocketException(const char*, int);
+    SocketException(const SocketException&);
+    SocketException& operator=(const SocketException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
@@ -55,6 +69,9 @@ class ICE_API ConnectFailedException : public SocketException
 {
 public:    
 
+    ConnectFailedException(const char*, int);
+    ConnectFailedException(const ConnectFailedException&);
+    ConnectFailedException& operator=(const ConnectFailedException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
@@ -63,6 +80,9 @@ class ICE_API ConnectionLostException : public SocketException
 {
 public:    
 
+    ConnectionLostException(const char*, int);
+    ConnectionLostException(const ConnectionLostException&);
+    ConnectionLostException& operator=(const ConnectionLostException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
@@ -71,22 +91,30 @@ class ICE_API DNSException : public SystemException
 {
 public:    
 
-    DNSException();
+    DNSException(const char*, int);
     DNSException(const DNSException&);
     DNSException& operator=(const DNSException&);
-
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
 
 class ICE_API ProtocolException : public LocalException
 {
+public:
+
+    ProtocolException(const char*, int);
+    ProtocolException(const ProtocolException&);
+    ProtocolException& operator=(const ProtocolException&);
 };
 
 class ICE_API UnmarshalOutOfBoundsException : public ProtocolException
 {
 public:    
-
+    
+    UnmarshalOutOfBoundsException(const char*, int);
+    UnmarshalOutOfBoundsException(const UnmarshalOutOfBoundsException&);
+    UnmarshalOutOfBoundsException& operator=(
+	const UnmarshalOutOfBoundsException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
@@ -95,6 +123,10 @@ class ICE_API UnsupportedProtocolException : public ProtocolException
 {
 public:    
 
+    UnsupportedProtocolException(const char*, int);
+    UnsupportedProtocolException(const UnsupportedProtocolException&);
+    UnsupportedProtocolException& operator=(
+	const UnsupportedProtocolException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
@@ -103,6 +135,10 @@ class ICE_API UnsupportedEncodingException : public ProtocolException
 {
 public:    
 
+    UnsupportedEncodingException(const char*, int);
+    UnsupportedEncodingException(const UnsupportedEncodingException&);
+    UnsupportedEncodingException& operator=(
+	const UnsupportedEncodingException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
@@ -111,6 +147,9 @@ class ICE_API InvalidMessageException : public ProtocolException
 {
 public:    
 
+    InvalidMessageException(const char*, int);
+    InvalidMessageException(const InvalidMessageException&);
+    InvalidMessageException& operator=(const InvalidMessageException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
@@ -119,6 +158,9 @@ class ICE_API UnknownMessageException : public ProtocolException
 {
 public:    
 
+    UnknownMessageException(const char*, int);
+    UnknownMessageException(const UnknownMessageException&);
+    UnknownMessageException& operator=(const UnknownMessageException&);
     virtual std::string toString() const;
     virtual LocalException* clone() const;
 };
