fcb3e821d5e8bcd42a58b843326e5359069ed2b2
Marc Laukien <marc@zeroc.com>
2001-06-01 14:46:17 +0000

    vector marshaling stuff

--- cpp/include/Ice/Stream.h
+++ cpp/include/Ice/Stream.h
@@ -11,6 +11,7 @@
 #ifndef ICE_STREAM_H
 #define ICE_STREAM_H
 
+
 #include <Ice/InstanceF.h>
 #include <Ice/Buffer.h>
 
@@ -32,28 +33,44 @@ public:
 
     void write(Ice::Byte value) { b.push_back(value); }
     void read(Ice::Byte&);
+    void write(const std::vector<Ice::Byte>&);
+    void read(std::vector<Ice::Byte>&);
 
     void write(bool value) { b.push_back(static_cast<Ice::Byte>(value)); }
     void read(bool&);
+    void write(const std::vector<bool>&);
+    void read(std::vector<bool>&);
 
     void write(Ice::Short);
     void read(Ice::Short&);
+    void write(const std::vector<Ice::Short>&);
+    void read(std::vector<Ice::Short>&);
 
     void write(Ice::Int);
     void read(Ice::Int&);
+    void write(const std::vector<Ice::Int>&);
+    void read(std::vector<Ice::Int>&);
 
     void write(Ice::Long);
     void read(Ice::Long&);
+    void write(const std::vector<Ice::Long>&);
+    void read(std::vector<Ice::Long>&);
 
     void write(Ice::Float);
     void read(Ice::Float&);
+    void write(const std::vector<Ice::Float>&);
+    void read(std::vector<Ice::Float>&);
 
     void write(Ice::Double);
     void read(Ice::Double&);
+    void write(const std::vector<Ice::Double>&);
+    void read(std::vector<Ice::Double>&);
 
     void write(const std::string&);
     void write(const char*); // Optimized version for const char*
     void read(std::string&);
+    void write(const std::vector<std::string>&);
+    void read(std::vector<std::string>&);
 
 private:
 
