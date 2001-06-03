7fbfeb5825a7f89c9a365b7b5c4f31e05d6fc1ac
Marc Laukien <marc@zeroc.com>
2001-06-03 21:13:38 +0000

    fixes

---
+++ cpp/include/Ice/IceStub.h
@@ -0,0 +1,18 @@
+// **********************************************************************
+//
+// Copyright (c) 2002
+// MutableRealms, Inc.
+// Huntsville, AL, USA
+//
+// All Rights Reserved
+//
+// **********************************************************************
+
+#ifndef ICE_ICE_STUB_H
+#define ICE_ICE_STUB_H
+
+#include <Ice/Stub.h>
+#include <Ice/ObjectFactory.h>
+#include <Ice/Outgoing.h>
+
+#endif
