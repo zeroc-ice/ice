17f212005b26952cda4e82e6beeedffdfa2bd43c
Marc Laukien <marc@zeroc.com>
2001-07-30 22:20:31 +0000

    more test stuff

---
+++ cpp/allTests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# **********************************************************************
+#
+# Copyright (c) 2002
+# MutableRealms, Inc.
+# Huntsville, AL, USA
+#
+# All Rights Reserved
+#
+# **********************************************************************
+
+import os
+
+for toplevel in ["", "..", os.path.join("..", ".."), os.path.join("..", "..", "..")]:
+    if os.path.exists(os.path.join(toplevel, "config", "TestUtil.py")):
+        break
+else:
+    raise "can't find toplevel directory!"
+
+for name in ["parser", "operations", "exceptions", "inheritance"]:
+
+    dir = os.path.join(toplevel, "test", name)
+    
+    print
+    print "*** running tests in " + dir + ":"
+    print
+
+    os.system("python " + os.path.join(dir, "run.py"))
+    
