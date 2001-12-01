79e7b827ae3344da08d334f5f2aac4eb1f6ac8dc
Matthew Newhook <matthew@zeroc.com>
2001-12-01 01:16:41 +0000

    Add run.py script.

---
+++ cpp/test/Freeze/cursor/run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# **********************************************************************
+#
+# Copyright (c) 2001
+# MutableRealms, Inc.
+# Huntsville, AL, USA
+#
+# All Rights Reserved
+#
+# **********************************************************************
+
+import os, sys
+
+for toplevel in [".", "..", "../..", "../../..", "../../../.."]:
+    if os.path.exists(os.path.normpath(toplevel + "/config/TestUtil.py")):
+        break
+else:
+    raise "can't find toplevel directory!"
+
+sys.path.append(os.path.normpath(toplevel + "/config"))
+import TestUtil
+
+name = "Freeze/cursor"
+
+testdir = os.path.normpath(toplevel + "/test/" + name)
+client = os.path.normpath(testdir + "/client")
+clientOptions = " --Ice.Config=" + testdir + "/config " + testdir;
+
+print "starting client...",
+clientPipe = os.popen(client + clientOptions)
+output = clientPipe.read().strip()
+if not output:
+    print "failed!"
+    sys.exit(1)
+print "ok"
+print output
+
+sys.exit(0)
