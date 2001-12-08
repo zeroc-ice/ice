078d99150674195e08df7c4f8730e1974511af18
Mark Spruiell <mes@zeroc.com>
2001-12-08 23:12:33 +0000

    adding DispatchProxyRequested

--- java/src/IceInternal/DispatchStatus.java
+++ java/src/IceInternal/DispatchStatus.java
@@ -18,24 +18,26 @@ public final class DispatchStatus
     public static final DispatchStatus DispatchUserException = new DispatchStatus(_DispatchUserException);
     public static final int _DispatchLocationForward = 2;
     public static final DispatchStatus DispatchLocationForward = new DispatchStatus(_DispatchLocationForward);
-    public static final int _DispatchObjectNotExist = 3;
+    public static final int _DispatchProxyRequested = 3;
+    public static final DispatchStatus DispatchProxyRequested = new DispatchStatus(_DispatchProxyRequested);
+    public static final int _DispatchObjectNotExist = 4;
     public static final DispatchStatus DispatchObjectNotExist = new DispatchStatus(_DispatchObjectNotExist);
-    public static final int _DispatchFacetNotExist = 4;
+    public static final int _DispatchFacetNotExist = 5;
     public static final DispatchStatus DispatchFacetNotExist = new DispatchStatus(_DispatchFacetNotExist);
-    public static final int _DispatchOperationNotExist = 5;
+    public static final int _DispatchOperationNotExist = 6;
     public static final DispatchStatus DispatchOperationNotExist = new DispatchStatus(_DispatchOperationNotExist);
-    public static final int _DispatchUnknownLocalException = 6;
+    public static final int _DispatchUnknownLocalException = 7;
     public static final DispatchStatus DispatchUnknownLocalException = new DispatchStatus(_DispatchUnknownLocalException);
-    public static final int _DispatchUnknownUserException = 7;
+    public static final int _DispatchUnknownUserException = 8;
     public static final DispatchStatus DispatchUnknownUserException = new DispatchStatus(_DispatchUnknownUserException);
-    public static final int _DispatchUnknownException = 8;
+    public static final int _DispatchUnknownException = 9;
     public static final DispatchStatus DispatchUnknownException = new DispatchStatus(_DispatchUnknownException);
 
     public static DispatchStatus
     convert(int val)
     {
-	assert val < 9;
-	return __values[val];
+        assert val < 10;
+        return __values[val];
     }
 
     public int
@@ -51,7 +53,7 @@ public final class DispatchStatus
 	__values[val] = this;
     }
 
-    private static DispatchStatus[] __values = new DispatchStatus[9];
+    private static DispatchStatus[] __values = new DispatchStatus[10];
 
     private int __value;
 }
