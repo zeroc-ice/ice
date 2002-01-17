0ab7f17f4b0312ff8f21d68095e1118133111816
Mark Spruiell <mes@zeroc.com>
2002-01-17 20:38:49 +0000

    adding server

---
+++ java/test/Ice/exceptions/Server.java
@@ -0,0 +1,61 @@
+// **********************************************************************
+//
+// Copyright (c) 2001
+// MutableRealms, Inc.
+// Huntsville, AL, USA
+//
+// All Rights Reserved
+//
+// **********************************************************************
+
+public class Server
+{
+    private static int
+    run(String[] args, Ice.Communicator communicator)
+    {
+        Ice.Properties properties = communicator.getProperties();
+        properties.setProperty("Ice.ConnectionWarnings", "0");
+        String endpts = "default -p 12345 -t 2000";
+        Ice.ObjectAdapter adapter =
+            communicator.createObjectAdapterWithEndpoints("TestAdapter",
+                                                          endpts);
+        Ice.Object object = new ThrowerI(adapter);
+        adapter.add(object, Ice.Util.stringToIdentity("thrower"));
+        adapter.activate();
+        communicator.waitForShutdown();
+        return 0;
+    }
+
+    public static void
+    main(String[] args)
+    {
+        int status = 0;
+        Ice.Communicator communicator = null;
+
+        try
+        {
+            communicator = Ice.Util.initialize(args);
+            status = run(args, communicator);
+        }
+        catch(Ice.LocalException ex)
+        {
+            ex.printStackTrace();
+            status = 1;
+        }
+
+        if (communicator != null)
+        {
+            try
+            {
+                communicator.destroy();
+            }
+            catch(Ice.LocalException ex)
+            {
+                ex.printStackTrace();
+                status = 1;
+            }
+        }
+
+        System.exit(status);
+    }
+}
---
+++ java/test/Ice/exceptions/ThrowerI.java
@@ -0,0 +1,140 @@
+// **********************************************************************
+//
+// Copyright (c) 2001
+// MutableRealms, Inc.
+// Huntsville, AL, USA
+//
+// All Rights Reserved
+//
+// **********************************************************************
+
+// Ice version 0.0.1
+
+public final class ThrowerI extends _ThrowerDisp
+{
+    public
+    ThrowerI(Ice.ObjectAdapter adapter)
+    {
+        _adapter = adapter;
+    }
+
+    public void
+    shutdown(Ice.Current current)
+    {
+        _adapter.getCommunicator().shutdown();
+    }
+
+    public boolean
+    supportsUndeclaredExceptions(Ice.Current current)
+    {
+        return false;
+    }
+
+    public void
+    throwAasA(int a, Ice.Current current)
+        throws A
+    {
+        A ex = new A();
+        ex.a = a;
+        throw ex;
+    }
+
+    public void
+    throwAorDasAorD(int a, Ice.Current current)
+        throws A,
+               D
+    {
+        if (a > 0)
+        {
+            A ex = new A();
+            ex.a = a;
+            throw ex;
+        }
+        else
+        {
+            D ex = new D();
+            ex.d = a;
+            throw ex;
+        }
+    }
+
+    public void
+    throwBasA(int a, int b, Ice.Current current)
+        throws A
+    {
+        throwBasB(a, b, current);
+    }
+
+    public void
+    throwBasB(int a, int b, Ice.Current current)
+        throws B
+    {
+        B ex = new B();
+        ex.a = a;
+        ex.b = b;
+        throw ex;
+    }
+
+    public void
+    throwCasA(int a, int b, int c, Ice.Current current)
+        throws A
+    {
+        throwCasC(a, b, c, current);
+    }
+
+    public void
+    throwCasB(int a, int b, int c, Ice.Current current)
+        throws B
+    {
+        throwCasC(a, b, c, current);
+    }
+
+    public void
+    throwCasC(int a, int b, int c, Ice.Current current)
+        throws C
+    {
+        C ex = new C();
+        ex.a = a;
+        ex.b = b;
+        ex.c = c;
+        throw ex;
+    }
+
+    public void
+    throwLocalException(Ice.Current current)
+    {
+        throw new Ice.TimeoutException();
+    }
+
+    public void
+    throwNonIceException(Ice.Current current)
+    {
+        throw new RuntimeException();
+    }
+
+    public void
+    throwUndeclaredA(int a, Ice.Current current)
+        throws B,
+               D
+    {
+        // Not possible in Java
+        throw new Ice.UnknownUserException();
+    }
+
+    public void
+    throwUndeclaredB(int a, int b, Ice.Current current)
+    {
+        // Not possible in Java
+        throw new Ice.UnknownUserException();
+    }
+
+    public void
+    throwUndeclaredC(int a, int b, int c, Ice.Current current)
+        throws D
+    {
+        // Not possible in Java
+        throw new Ice.UnknownUserException();
+    }
+
+    private Ice.ObjectAdapter _adapter;
+}
