b86b7a5d82b954188754c155d24d9d96e15defaa
Mark Spruiell <mes@zeroc.com>
2002-03-27 02:14:13 +0000

    initial check-in

---
+++ java/src/Ice/ServiceManagerAdmin.java
@@ -0,0 +1,104 @@
+// **********************************************************************
+//
+// Copyright (c) 2001
+// MutableRealms, Inc.
+// Huntsville, AL, USA
+//
+// All Rights Reserved
+//
+// **********************************************************************
+
+package Ice;
+
+public class ServiceManagerAdmin
+{
+    private static void
+    usage()
+    {
+        System.err.println(
+            "usage: Ice.ServiceManagerAdmin [options] [command]\n" +
+            "\n" +
+            "Options:\n" +
+            "\n" +
+            "  -h          Display this message." +
+            "\n" +
+            "Commands:\n" +
+            "\n" +
+            "  shutdown    Shutdown the server.");
+    }
+
+    private static int
+    run(String[] args, Ice.Communicator communicator)
+    {
+        Ice.Properties properties = communicator.getProperties();
+        final String refProperty = "ServiceManager";
+        String ref = properties.getProperty(refProperty);
+        if (ref.length() == 0)
+        {
+            System.err.println("property `" + refProperty + "' not set");
+            return 1;
+        }
+
+        if (args.length == 0)
+        {
+            usage();
+            return 1;
+        }
+
+        Ice.ObjectPrx base = communicator.stringToProxy(ref);
+        Ice.ServiceManagerPrx mgr =
+            Ice.ServiceManagerPrxHelper.checkedCast(base.ice_twoway().ice_timeout(-1).ice_secure(false));
+        if (mgr == null)
+        {
+            System.err.println("invalid object reference");
+            return 1;
+        }
+
+        if (args[0].equals("shutdown"))
+        {
+            mgr.shutdown();
+        }
+        else
+        {
+            System.err.println("unknown command `" + args[0] + "'");
+            usage();
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public static void
+    main(String[] args)
+    {
+        int status = 0;
+        Ice.Communicator communicator = null;
+
+        try
+        {
+            StringSeqHolder argsH = new StringSeqHolder(args);
+            communicator = Util.initialize(argsH);
+            status = run(argsH.value, communicator);
+        }
+        catch (Ice.LocalException ex)
+        {
+            ex.printStackTrace();
+            status = 1;
+        }
+
+        if (communicator != null)
+        {
+            try
+            {
+                communicator.destroy();
+            }
+            catch (Ice.LocalException ex)
+            {
+                ex.printStackTrace();
+                status = 1;
+            }
+        }
+
+        System.exit(status);
+    }
+}
