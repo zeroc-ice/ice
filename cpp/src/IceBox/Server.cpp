a4e41edee428a3a82fecd4bf94c88d0614f991e9
Mark Spruiell <mes@zeroc.com>
2002-04-29 22:48:17 +0000

    check command-line options

--- cpp/src/IceBox/Server.cpp
+++ cpp/src/IceBox/Server.cpp
@@ -15,6 +15,17 @@ using namespace std;
 using namespace Ice;
 using namespace IceBox;
 
+static void
+usage(const char* appName)
+{
+    cerr << "Usage: " << appName << " [options]\n";
+    cerr <<     
+        "Options:\n"
+        "-h, --help           Show this message.\n"
+        "-v, --version        Display the Ice version.\n"
+        ;
+}
+
 int
 main(int argc, char* argv[])
 {
@@ -25,6 +36,26 @@ main(int argc, char* argv[])
     try
     {
         communicator = initialize(argc, argv);
+
+        for (int i = 1; i < argc; ++i)
+        {
+            if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+            {
+                usage(argv[0]);
+                return EXIT_SUCCESS;
+            }
+            else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0)
+            {
+                cout << ICE_STRING_VERSION << endl;
+                return EXIT_SUCCESS;
+            }
+            else
+            {
+                cerr << argv[0] << ": unknown option `" << argv[i] << "'" << endl;
+                usage(argv[0]);
+                return EXIT_FAILURE;
+            }
+        }
 
         PropertiesPtr properties = communicator->getProperties();
         StringSeq args = argsToStringSeq(argc, argv);
