c02fe1ebd7663afefd9fb6b2d53ed6066d905902
Anthony Neal <aneal@zeroc.com>
2002-09-13 11:04:54 +0000

    Whoops. Forgot this.

---
+++ cpp/src/IceSSL/OpenSSLPluginIF.h
@@ -0,0 +1,32 @@
+// **********************************************************************
+//
+// Copyright (c) 2001
+// Mutable Realms, Inc.
+// Huntsville, AL, USA
+//
+// All Rights Reserved
+//
+// **********************************************************************
+
+#ifndef ICE_SSL_OPENSSL_PLUGIN_I_F_H
+#define ICE_SSL_OPENSSL_PLUGIN_I_F_H
+
+#include <Ice/Handle.h>
+
+namespace IceSSL
+{
+
+class OpenSSLPluginI;
+typedef IceInternal::Handle<OpenSSLPluginI> OpenSSLPluginIPtr;
+
+}
+
+namespace IceInternal
+{
+
+void incRef(IceSSL::OpenSSLPluginI*);
+void decRef(IceSSL::OpenSSLPluginI*);
+
+}
+
+#endif
