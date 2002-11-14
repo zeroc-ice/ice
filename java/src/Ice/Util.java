5259828a3878a2cfc94c5c49adf7a950a1822852
Mark Spruiell <mes@zeroc.com>
2002-11-14 22:19:56 +0000

    adding proxy comparison methods

--- java/src/Ice/Util.java
+++ java/src/Ice/Util.java
@@ -224,6 +224,59 @@ public final class Util
         return _localAddress + ":" + uid;
     }
 
+    public static int
+    proxyIdentityCompare(ObjectPrx lhs, ObjectPrx rhs)
+    {
+        Identity lhsIdentity = lhs.ice_getIdentity();
+        Identity rhsIdentity = rhs.ice_getIdentity();
+        int n;
+        if((n = lhsIdentity.name.compareTo(rhsIdentity.name)) != 0)
+        {
+            return n;
+        }
+        return lhsIdentity.category.compareTo(rhsIdentity.category);
+    }
+
+    public static int
+    proxyIdentityAndFacetCompare(ObjectPrx lhs, ObjectPrx rhs)
+    {
+        Identity lhsIdentity = lhs.ice_getIdentity();
+        Identity rhsIdentity = rhs.ice_getIdentity();
+        int n;
+        if((n = lhsIdentity.name.compareTo(rhsIdentity.name)) != 0)
+        {
+            return n;
+        }
+        if((n = lhsIdentity.category.compareTo(rhsIdentity.category)) != 0)
+        {
+            return n;
+        }
+
+        String[] lhsFacet = lhs.ice_getFacet();
+        String[] rhsFacet = rhs.ice_getFacet();
+        int i;
+        for(i = 0; i < lhsFacet.length && i < rhsFacet.length; i++)
+        {
+            if((n = lhsFacet[i].compareTo(rhsFacet[i])) != 0)
+            {
+                return n;
+            }
+        }
+
+        if(lhsFacet.length == rhsFacet.length)
+        {
+            return 0;
+        }
+        else if(lhsFacet.length < rhsFacet.length)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
     private static Properties _defaultProperties = null;
     private static String _localAddress = null;
 }
