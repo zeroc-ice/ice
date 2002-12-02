fb880c841ddaf749317b488afabc1900f155d947
Michi Henning <michi@zeroc.com>
2002-12-02 05:40:06 +0000

    Added thread start test.

---
+++ cpp/test/IceUtil/thread/StartTest.cpp
@@ -0,0 +1,65 @@
+// **********************************************************************
+//
+// Copyright (c) 2002
+// ZeroC, Inc.
+// Billerica, MA, USA
+//
+// All Rights Reserved.
+//
+// Ice is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation.
+//
+// **********************************************************************
+
+#include <IceUtil/IceUtil.h>
+
+#include <stdio.h>
+
+#include <StartTest.h>
+#include <TestCommon.h>
+
+using namespace std;
+using namespace IceUtil;
+
+static const string createTestName("thread start");
+
+class StartTestThread : public Thread
+{
+public:
+    
+    StartTestThread()
+    {
+    }
+
+    virtual void run()
+    {
+    }
+};
+
+typedef Handle<StartTestThread> StartTestThreadPtr;
+
+StartTest::StartTest() :
+    TestBase(createTestName)
+{
+}
+
+void
+StartTest::run()
+{
+    //
+    // Check that calling start() more than once raises ThreadStartedException.
+    //
+    StartTestThreadPtr t = new StartTestThread();
+    ThreadControl control = t->start();
+    control.join();
+    bool gotException = false;
+    try {
+	t->start();
+    }
+    catch(const ThreadStartedException&)
+    {
+	gotException = true;
+    }
+    test(gotException);
+}
---
+++ cpp/test/IceUtil/thread/StartTest.h
@@ -0,0 +1,31 @@
+// **********************************************************************
+//
+// Copyright (c) 2002
+// ZeroC, Inc.
+// Billerica, MA, USA
+//
+// All Rights Reserved.
+//
+// Ice is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation.
+//
+// **********************************************************************
+
+#ifndef START_TEST_H
+#define START_TEST_H
+
+#include <TestBase.h>
+
+class StartTest : public TestBase
+{
+public:
+
+    StartTest();
+
+private:
+    
+    virtual void run();
+};
+
+#endif
