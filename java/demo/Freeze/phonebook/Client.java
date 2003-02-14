f9f59e8b653672fe88dc8c03ed1d11c89c571183
Marc Laukien <marc@zeroc.com>
2003-02-14 23:00:37 +0000

    fixes

---
+++ java/demo/Freeze/phonebook/Client.java
@@ -0,0 +1,29 @@
+// **********************************************************************
+//
+// Copyright (c) 2003
+// ZeroC, Inc.
+// Billerica, MA, USA
+//
+// All Rights Reserved.
+//
+// Ice is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation.
+//
+// **********************************************************************
+
+public class Client extends Ice.Application
+{
+    public int
+    run(String[] args)
+    {
+	return RunParser.runParser(appName(), args, communicator());
+    }
+
+    static public void
+    main(String[] args)
+    {
+	Client app = new Client();
+	app.main("demo.Freeze.phonebook.Client", args, "config");
+    }
+}
