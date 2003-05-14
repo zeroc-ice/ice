8fa5c51f196dacc2b2d1a81016156de7cc53e371
Bernard Normier <bernard@zeroc.com>
2003-05-14 15:19:12 +0000

    Updated version to 1.1

--- java/src/IceUtil/Version.java
+++ java/src/IceUtil/Version.java
@@ -2,7 +2,7 @@
 //
 // Copyright (c) 2003
 // ZeroC, Inc.
-// Huntsville, AL, USA
+// Billerica, MA, USA
 //
 // All Rights Reserved.
 //
@@ -19,6 +19,6 @@ public final class Version
     //
     // The Ice version.
     //
-    public final static String ICE_STRING_VERSION = "1.0.1"; // "A.B.C", with A=major, B=minor, C=patch
-    public final static int ICE_INT_VERSION = 10001;         // AABBCC, with AA=major, BB=minor, CC=patch
+    public final static String ICE_STRING_VERSION = "1.1.0"; // "A.B.C", with A=major, B=minor, C=patch
+    public final static int ICE_INT_VERSION = 10100;         // AABBCC, with AA=major, BB=minor, CC=patch
 }
