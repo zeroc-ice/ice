405e66fd74dd016d2dbd9a0dce92de6d200ca12b
Michi Henning <michi@zeroc.com>
2003-07-09 05:41:33 +0000

    Added evictor demo.

---
+++ cpp/demo/book/evictor/EvictorBase.cpp
@@ -0,0 +1,105 @@
+#include <EvictorBase.h>
+#include <Ice/LocalException.h>
+
+EvictorBase::EvictorBase(Ice::Int size)
+    : _size(size)
+{
+    if (_size < 0)
+    {
+        _size = 1000;
+    }
+}
+
+Ice::ObjectPtr
+EvictorBase::locate(const Ice::Current& c, Ice::LocalObjectPtr& cookie)
+{
+    IceUtil::Mutex::Lock lock(_mutex);
+
+    //
+    // Create a cookie.
+    //
+    EvictorCookiePtr ec = new EvictorCookie;
+    cookie = ec;
+
+    //
+    // Check if we have a servant in the map already.
+    //
+    EvictorMap::iterator i = _map.find(c.id);
+    bool newEntry = i == _map.end();
+    if(!newEntry)
+    {
+	//
+	// Got an entry already, dequeue the entry from its current position.
+	//
+	ec->entry = i->second;
+	_queue.erase(ec->entry->pos);
+    }
+    else
+    {
+	//
+	// We do not have an entry. Ask the derived class to
+	// instantiate a servant and add a new entry to the map.
+	//
+	ec->entry = new EvictorEntry;
+	ec->entry->servant = add(c, ec->entry->userCookie); // Down-call
+	if(!ec->entry->servant)
+	{
+	    throw Ice::ObjectNotExistException(__FILE__, __LINE__);
+	}
+	ec->entry->useCount = 0;
+	i = _map.insert(std::make_pair(c.id, ec->entry)).first;
+    }
+
+    //
+    // Increment the use count of the servant and enqueue
+    // the entry at the front, so we get LRU order.
+    //
+    ++(ec->entry->useCount);
+    ec->entry->pos = _queue.insert(_queue.begin(), i);
+
+    return ec->entry->servant;
+}
+
+void
+EvictorBase::finished(const Ice::Current&, const Ice::ObjectPtr&, const Ice::LocalObjectPtr& cookie)
+{
+    IceUtil::Mutex::Lock lock(_mutex);
+
+    EvictorCookiePtr ec = EvictorCookiePtr::dynamicCast(cookie);
+
+    //
+    // Decrement use count and check if there is something to evict.
+    //
+    --(ec->entry->useCount);
+    evictServants();
+}
+
+void
+EvictorBase::deactivate(const std::string& category)
+{
+    IceUtil::Mutex::Lock lock(_mutex);
+
+    _size = 0;
+    evictServants();
+}
+
+void
+EvictorBase::evictServants()
+{
+    //
+    // If the evictor queue has grown larger than the limit,
+    // look at the excess elements to see whether any of them
+    // can be evicted.
+    //
+    for(int i = static_cast<int>(_map.size() - _size); i > 0; --i)
+    {
+	EvictorQueue::reverse_iterator p = _queue.rbegin();
+	if((*p)->second->useCount == 0)
+	{
+	    evict((*p)->second->servant, (*p)->second->userCookie); // Down-call
+	    EvictorMap::iterator pos = *p;
+	    _queue.erase((*p)->second->pos);
+	    _map.erase(pos);
+	}
+    }
+}
---
+++ java/demo/book/evictor/EvictorBase.java
@@ -0,0 +1,157 @@
+// **********************************************************************
+//
+// Copyright (c) 2003
+// ZeroC, Inc.
+// Billerica, MA, USA
+//
+// All Rights Reserved.
+//
+// Ice is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation.
+//
+// **********************************************************************
+
+package Evictor;
+
+public abstract class EvictorBase extends Ice.LocalObjectImpl implements Ice.ServantLocator
+{
+
+    public
+    EvictorBase()
+    {
+	_size = 1000;
+    }
+
+    public
+    EvictorBase(int size)
+    {
+	_size = size < 0 ? 1000 : size;
+    }
+
+    public abstract Ice.Object
+    add(Ice.Current c, Ice.LocalObjectHolder cookie);
+
+    public abstract void
+    evict(Ice.Object servant, Ice.LocalObject cookie);
+
+    synchronized public final Ice.Object
+    locate(Ice.Current c, Ice.LocalObjectHolder cookie)
+    {
+	//
+	// Make a copy of the ID. We need to do this because
+	// Ice.Current.id is the same reference on every call: its
+	// contents change, but it is the same object every time. We
+	// need a copy to insert into the queue and the map.
+	//
+	Ice.Identity idCopy = new Ice.Identity();
+	idCopy.name = c.id.name;
+	idCopy.category = c.id.category;
+
+	//
+	// Create a cookie.
+	//
+	EvictorCookie ec = new EvictorCookie();
+	cookie.value = ec;
+
+	//
+	// Check if we a servant in the map already.
+	//
+	ec.entry = (EvictorEntry)_map.get(idCopy);
+	boolean newEntry = ec.entry == null;
+	if(!newEntry)
+	{
+	    //
+	    // Got an entry already, dequeue the entry from
+	    // its current position.
+	    //
+	    ec.entry.pos.remove();
+	}
+	else
+	{
+	    //
+	    // We do not have entry. Ask the derived class to
+	    // instantiate a servant and add a new entry to the map.
+	    //
+	    ec.entry = new EvictorEntry();
+	    Ice.LocalObjectHolder cookieHolder = new Ice.LocalObjectHolder();
+	    ec.entry.servant = add(c, cookieHolder); // Down-call
+	    if(ec.entry.servant == null)
+	    {
+		throw new Ice.ObjectNotExistException();
+	    }
+	    ec.entry.userCookie = cookieHolder.value;
+	    ec.entry.useCount = 0;
+	    _map.put(idCopy, ec.entry);
+	}
+
+	//
+	// Increment the use count of the servant and enqueue
+	// the entry at the front, so we get LRU order.
+	//
+	++(ec.entry.useCount);
+	_queue.addFirst(idCopy);
+	ec.entry.pos = _queue.iterator();
+	ec.entry.pos.next(); // Position the iterator on the element.
+
+	return ec.entry.servant;
+    }
+
+    synchronized public final void
+    finished(Ice.Current c, Ice.Object o, Ice.LocalObject cookie)
+    {
+	EvictorCookie ec = (EvictorCookie)cookie;
+
+	//
+	// Decrement use count and check if
+	// there is something to evict.
+	//
+	--(ec.entry.useCount);
+	evictServants();
+    }
+
+    synchronized public final void
+    deactivate(String category)
+    {
+	_size = 0;
+	evictServants();
+    }
+
+    private class EvictorEntry
+    {
+	Ice.Object servant;
+	Ice.LocalObject userCookie;
+	java.util.Iterator pos;
+        int useCount;
+    }
+
+    private class EvictorCookie extends Ice.LocalObjectImpl
+    {
+	public EvictorEntry entry;
+    }
+
+    private void evictServants()
+    {
+	//
+	// If the evictor queue has grown larger than the limit,
+	// look at the excess elements to see whether any of them
+	// can be evicted.
+	//
+	for(int i = _map.size() - _size; i > 0; --i)
+	{
+	    java.util.Iterator p = _queue.riterator();
+	    Ice.Identity id = (Ice.Identity)p.next();
+	    EvictorEntry e = (EvictorEntry)_map.get(id);
+	    if(e.useCount == 0)
+	    {
+		evict(e.servant, e.userCookie); // Down-call
+		p.remove();
+		_map.remove(id);
+	    }
+	}
+    }
+
+    private java.util.Map _map = new java.util.HashMap();
+    private Evictor.LinkedList _queue = new Evictor.LinkedList();
+    private int _size;
+}
