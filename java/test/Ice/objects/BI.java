3479093d0d8a59649bb5c8d13d3ea5dee6fbe2ac
Mark Spruiell <mes@zeroc.com>
2004-02-24 13:57:03 +0000

    adding ice_preMarshal/ice_postUnmarshal tests

---
+++ java/test/Ice/objects/BI.java
@@ -0,0 +1,42 @@
+// **********************************************************************
+//
+// Copyright (c) 2003
+// ZeroC, Inc.
+// Billerica, MA, USA
+//
+// All Rights Reserved.
+//
+// Ice is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation.
+//
+// **********************************************************************
+
+public final class BI extends B
+{
+    public
+    BI()
+    {
+        _postUnmarshalInvoked = false;
+    }
+
+    public boolean
+    postUnmarshalInvoked(Ice.Current current)
+    {
+        return _postUnmarshalInvoked;
+    }
+
+    public void
+    ice_preMarshal()
+    {
+        preMarshalInvoked = true;
+    }
+
+    public void
+    ice_postUnmarshal()
+    {
+        _postUnmarshalInvoked = true;
+    }
+
+    private boolean _postUnmarshalInvoked;
+}
