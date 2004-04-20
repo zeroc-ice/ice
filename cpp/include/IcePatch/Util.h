bf9657eb1fbd77b0fb287e1bb2a386159bf4d74c
Mark Spruiell <mes@zeroc.com>
2004-04-20 21:25:37 +0000

    adding support for download totals

--- cpp/include/IcePatch/Util.h
+++ cpp/include/IcePatch/Util.h
@@ -59,6 +59,25 @@ ICE_PATCH_API Ice::ByteSeq calcPartialMD5(const std::string&, Ice::Int, const Ic
 ICE_PATCH_API Ice::ByteSeq getBZ2(const std::string&, Ice::Int, Ice::Int);
 ICE_PATCH_API void createBZ2(const std::string&, const Ice::LoggerPtr&);
 
+typedef std::map<Ice::ByteSeq, Ice::Long> TotalMap;
+
+ICE_PATCH_API TotalMap getTotalMap(const Ice::CommunicatorPtr&, const std::string&);
+ICE_PATCH_API void putTotalMap(const Ice::CommunicatorPtr&, const std::string&, const TotalMap&);
+
+#ifdef _WIN32
+
+//
+// Function object to do case-insensitive string comparison.
+//
+class ICE_PATCH_API CICompare : public std::binary_function<std::string, std::string, bool>
+{
+public:
+
+    bool operator()(const std::string&, const std::string&) const;
+};
+
+#endif
+
 }
 
 #endif
