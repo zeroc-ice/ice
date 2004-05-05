068a79dbfa1ad04ec37a532764baaa35b7dff572
Michi Henning <michi@zeroc.com>
2004-05-05 01:20:14 +0000

    Removed PreBuildEvent.bat.

--- cs/bin/PreBuildEvent.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-C:\cygwin\home\michi\src\icicle\compileSlice.bat C:\cygwin\home\michi\src\icicle\ Ice
-if errorlevel 1 goto CSharpReportError
-goto CSharpEnd
-:CSharpReportError
-echo Project error: A tool returned an error code from the build event
-exit 1
-:CSharpEnd
