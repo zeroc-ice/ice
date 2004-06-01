b404c69f475bad47a1a9bae3942e7e3de714fd62
Michi Henning <michi@zeroc.com>
2004-06-01 01:54:40 +0000

    Add AMI fault tolerance tests.

--- cs/test/Ice/faultTolerance/AllTests.cs
+++ cs/test/Ice/faultTolerance/AllTests.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Diagnostics;
+using System.Threading;
 
 public class AllTests
 {
@@ -19,6 +20,170 @@ public class AllTests
         {
             throw new System.Exception();
         }
+    }
+
+    private class Callback
+    {
+	public Callback()
+	{
+	    _called = false;
+	}
+
+	public bool check()
+	{
+	    lock(this)
+	    {
+		while(!_called)
+		{
+		    Monitor.Wait(this, TimeSpan.FromMilliseconds(30000));
+
+		    if(!_called)
+		    {
+			return false; // Must be timeout.
+		    }
+		}
+		
+		_called = false;
+		return true;
+	    }
+	}
+	
+	public void called()
+	{
+	    lock(this)
+	    {
+		Debug.Assert(!_called);
+		_called = true;
+		Monitor.Pulse(this);
+	    }
+	}
+
+	private bool _called;
+    }
+
+    private class AMI_Test_pidI : AMI_Test_pid
+    {
+	public override void ice_response(int pid)
+	{
+	    _pid = pid;
+	    callback.called();
+	}
+	
+	public override void ice_exception(Ice.Exception ex)
+	{
+	    test(false);
+	}
+	
+	public int pid()
+	{
+	    return _pid;
+	}
+	
+	public bool check()
+	{
+	    return callback.check();
+	}
+
+	private int _pid;
+	
+	private Callback callback = new Callback();
+    }
+    
+    private class AMI_Test_shutdownI : AMI_Test_shutdown
+    {
+	public override void ice_response()
+	{
+	    callback.called();
+	}
+	
+	public override void ice_exception(Ice.Exception ex)
+	{
+	    test(false);
+	}
+	
+	public bool check()
+	{
+	    return callback.check();
+	}
+
+	private Callback callback = new Callback();
+    }
+    
+    private class AMI_Test_abortI : AMI_Test_abort
+    {
+	public override void ice_response()
+	{
+	    test(false);
+	}
+	
+	public override void ice_exception(Ice.Exception ex)
+	{
+	    try
+	    {
+		throw ex;
+	    }
+	    catch(Ice.ConnectionLostException )
+	    {
+	    }
+	    catch(Ice.ConnectFailedException)
+	    {
+	    }
+	    catch(Ice.SocketException)
+	    {
+	    }
+	    catch(Exception)
+	    {
+		test(false);
+	    }
+	    callback.called();
+	}
+	
+	public bool check()
+	{
+	    return callback.check();
+	}
+
+	private Callback callback = new Callback();
+    }
+    
+    private class AMI_Test_idempotentAbortI : AMI_Test_idempotentAbort
+    {
+	public override void ice_response()
+	{
+	    test(false);
+	}
+	
+	public override void ice_exception(Ice.Exception ex)
+	{
+	    @delegate.ice_exception(ex);
+	}
+	
+	public bool check()
+	{
+	    return @delegate.check();
+	}
+
+	private AMI_Test_abortI @delegate = new AMI_Test_abortI();
+    }
+    
+    private class AMI_Test_nonmutatingAbortI : AMI_Test_nonmutatingAbort
+    {
+	public override void ice_response()
+	{
+	    test(false);
+	}
+	
+	public override void ice_exception(Ice.Exception ex)
+	{
+	    @delegate.ice_exception(ex);
+	}
+	
+	public bool check()
+	{
+	    return @delegate.check();
+	}
+
+	private AMI_Test_abortI @delegate = new AMI_Test_abortI();
     }
     
     public static void allTests(Ice.Communicator communicator, System.Collections.ArrayList ports)
@@ -62,7 +227,6 @@ public class AllTests
 	    }
 	    else
 	    {
-                /*
 		Console.Out.Write("testing server #" + i + " with AMI... ");
 		Console.Out.Flush();
 		AMI_Test_pidI cb = new AMI_Test_pidI();
@@ -72,7 +236,6 @@ public class AllTests
 		test(pid != oldPid);
 		Console.Out.WriteLine("ok");
 		oldPid = pid;
-                */
 	    }
             
             if(j == 0)
@@ -86,13 +249,11 @@ public class AllTests
 		}
 		else
 		{
-                    /*
 		    Console.Out.Write("shutting down server #" + i + " with AMI... ");
-		    AMI_Test_shutdownI cb = new AMI_Test_shtudownI();
-		    obj.shtudown_async(cb);
+		    AMI_Test_shutdownI cb = new AMI_Test_shutdownI();
+		    obj.shutdown_async(cb);
 		    test(cb.check());
 		    Console.Out.WriteLine("ok");
-                    */
 		}
             }
             else if(j == 1 || i + 1 > ports.Count)
@@ -121,14 +282,12 @@ public class AllTests
 		}
 		else
 		{
-                    /*
 		    Console.Out.Write("aborting server #" + i + " with AMI... ");
 		    Console.Out.Flush();
 		    AMI_Test_abortI cb = new AMI_Test_abortI();
 		    obj.abort_async(cb);
 		    test(cb.check());
 		    Console.Out.WriteLine("ok");
-                    */
 		}
             }
             else if(j == 2)
@@ -157,14 +316,12 @@ public class AllTests
 		}
 		else
 		{
-                    /*
 		    Console.Out.Write("aborting server #" + i + " and #" + (i + 1) + " with idempotent AMI call... ");
 		    Console.Out.Flush();
 		    AMI_Test_idempotentAbortI cb = new AMI_Test_idempotentAbortI();
 		    obj.idempotentAbort_async(cb);
 		    test(cb.check());
 		    Console.Out.WriteLine("ok");
-                    */
 		}
                 ++i;
             }
@@ -194,14 +351,12 @@ public class AllTests
 		}
 		else
 		{
-                    /*
 		    Console.Out.Write("aborting server #" + i + " and #" + (i + 1) + " with nonmutating AMI call... ");
 		    Console.Out.Flush();
 		    AMI_Test_nonmutatingAbortI cb = new AMI_Test_nonmutatingAbortI();
 		    obj.nonmutatingAbort_async(cb);
 		    test(cb.check());
 		    Console.Out.WriteLine("ok");
-                    */
 		}
                 ++i;
             }
