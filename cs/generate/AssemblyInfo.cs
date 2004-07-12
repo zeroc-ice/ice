e22dd6478a427e9ea3b4234c7fd7b50521de6c3a
Michi Henning <michi@zeroc.com>
2004-07-12 05:50:21 +0000

    Icicle -> Icecs renaming.

--- cs/generate/AssemblyInfo.cs
+++ cs/generate/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("Make-like program to compile Slice files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ZerocC, Inc.")]
-[assembly: AssemblyProduct("Icicle")]
+[assembly: AssemblyProduct("Ice for C# Pre-Build Event Program")]
 [assembly: AssemblyCopyright("2004")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
--- cs/src/Ice/AssemblyInfo.cs
+++ cs/src/Ice/AssemblyInfo.cs
@@ -11,19 +11,19 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Icicle")]
+[assembly: AssemblyTitle("Icecs")]
 [assembly: AssemblyDescription("Ice core run-time support")]
 [assembly: AssemblyCompany("ZeroC, Inc.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Ice")]
+[assembly: AssemblyProduct("Ice for C#")]
 [assembly: AssemblyCopyright("Copyright (c) 2003-2004, ZeroC, Inc.")]
 [assembly: AssemblyTrademark("Ice")]
 [assembly: AssemblyCulture("")]		
 [assembly: AssemblyVersion("1.5.0")]
 [assembly: AssemblyDelaySign(false)]
 #if __MonoCS__
-[assembly: AssemblyKeyFile("IcicleKey.snk")] // mcs uses different search algorithm
+[assembly: AssemblyKeyFile("IcecsKey.snk")] // mcs uses different search algorithm
 #else
-[assembly: AssemblyKeyFile(@"..\..\IcicleKey.snk")]
+[assembly: AssemblyKeyFile(@"..\..\IcecsKey.snk")]
 #endif
 [assembly: AssemblyKeyName("")]
