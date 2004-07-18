a33f6da11eb6dc712cb30371649bc0ba2e4fd9ba
Benoit Foucher <benoit@zeroc.com>
2004-07-18 15:14:23 +0000

    Lazy initialize the dictionary to fix out of order of static initialization     problem.

--- cpp/src/Ice/SliceChecksums.cpp
+++ cpp/src/Ice/SliceChecksums.cpp
@@ -13,21 +13,42 @@
 using namespace std;
 using namespace Ice;
 
-static SliceChecksumDict _sliceChecksums;
+static SliceChecksumDict* _sliceChecksums = 0;
 static IceUtil::StaticMutex _mutex = ICE_STATIC_MUTEX_INITIALIZER;
+
+class SliceChecksumDictDestroyer
+{
+public:
+    
+    ~SliceChecksumDictDestroyer()
+    {
+	delete _sliceChecksums;
+	_sliceChecksums = 0;
+    }
+};
+static SliceChecksumDictDestroyer destroyer;
 
 SliceChecksumDict
 Ice::sliceChecksums()
 {
     IceUtil::StaticMutex::Lock lock(_mutex);
-    return _sliceChecksums;
+    if(_sliceChecksums == 0)
+    {
+	_sliceChecksums = new SliceChecksumDict();
+    }
+    return *_sliceChecksums;
 }
 
 IceInternal::SliceChecksumInit::SliceChecksumInit(const char* checksums[])
 {
     IceUtil::StaticMutex::Lock lock(_mutex);
+    if(_sliceChecksums == 0)
+    {
+	_sliceChecksums = new SliceChecksumDict();
+    }
+
     for(int i = 0; checksums[i] != 0; i += 2)
     {
-        _sliceChecksums.insert(SliceChecksumDict::value_type(checksums[i], checksums[i + 1]));
+        _sliceChecksums->insert(SliceChecksumDict::value_type(checksums[i], checksums[i + 1]));
     }
 }
