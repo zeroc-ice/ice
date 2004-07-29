773edf31c292f7fc9c682ceed0654635f60ba27a
Michi Henning <michi@zeroc.com>
2004-07-29 02:43:57 +0000

    Removed AssemblyInformationalVersion attribute again because Visual C# is     broken and can't handle it.

--- cs/src/Ice/AssemblyInfo.cs
+++ cs/src/Ice/AssemblyInfo.cs
@@ -19,7 +19,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright (c) 2003-2004, ZeroC, Inc.")]
 [assembly: AssemblyTrademark("Ice")]
 [assembly: AssemblyCulture("")]		
-[assembly: AssemblyInformationalVersion("Ice for C#, v1.5.0")]
 [assembly: AssemblyVersion("1.5.0")]
 [assembly: AssemblyDelaySign(false)]
 #if __MonoCS__
