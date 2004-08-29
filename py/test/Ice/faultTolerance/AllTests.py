be7e4d3b41af93968bc837c33c8c9fb24922f322
Mark Spruiell <mes@zeroc.com>
2004-08-29 20:12:16 +0000

    initial check-in

---
+++ py/test/Ice/faultTolerance/AllTests.py
@@ -0,0 +1,154 @@
+# **********************************************************************
+#
+# Copyright (c) 2003-2004 ZeroC, Inc. All rights reserved.
+#
+# This copy of Ice is licensed to you under the terms described in the
+# ICE_LICENSE file included in this distribution.
+#
+# **********************************************************************
+
+import Ice, Test, _Top
+
+def test(b):
+    if not b:
+        raise RuntimeError('test assertion failed')
+
+class CallbackBase:
+    def __init__(self):
+        self._called = False
+        self._cond = threading.Condition()
+
+    def check(self):
+        self._cond.acquire()
+        try:
+            while not self._called:
+                self._cond.wait(5.0)
+            if self._called:
+                self._called = False
+                return True;
+            else:
+                return False
+        finally:
+            self._cond.release()
+
+    def called(self):
+        self._cond.acquire()
+        _called = True
+        self._cond.notify()
+        self._cond.release()
+
+def allTests(communicator, ports):
+    print "testing stringToProxy... ",
+    ref = "test"
+    for p in ports:
+        ref = ref + ":default -t 60000 -p " + str(p)
+    base = communicator.stringToProxy(ref)
+    test(base)
+    print "ok"
+
+    print "testing checked cast... ",
+    obj = _Top.TestPrx.checkedCast(base)
+    test(obj)
+    test(obj == base)
+    print "ok"
+
+    oldPid = 0
+    ami = False
+    i = 1
+    j = 0
+    while i <= len(ports):
+        if j > 3:
+            j = 0
+            #ami = not ami
+
+        if not ami:
+            print "testing server #%d... " % i,
+            pid = obj.pid()
+            test(pid != oldPid)
+            print "ok"
+            oldPid = pid
+        else:
+            print "testing server #%d with AMI... " % i,
+            cb = AMI_Test_pidI()
+            obj.pid_async(cb)
+            test(cb.check())
+            pid = cb.pid()
+            test(pid != oldPid)
+            print "ok"
+            oldPid = pid
+
+        if j == 0:
+            if not ami:
+                print "shutting down server #%d... " % i,
+                obj.shutdown()
+                print "ok"
+            else:
+                print "shutting down server #%d with AMI... " % i,
+                cb = AMI_Test_shutdownI()
+                obj.shutdown_async(cb)
+                test(cb.check())
+                print "ok"
+        elif j == 1 or i + 1 > len(ports):
+            if not ami:
+                print "aborting server #%d... " % i,
+                try:
+                    obj.abort()
+                    test(False)
+                except Ice.ConnectionLostException:
+                    print "ok"
+                except Ice.ConnectFailedException:
+                    print "ok"
+            else:
+                print "aborting server #%d with AMI... " % i,
+                cb = AMI_Test_abortI()
+                obj.abort_async(cb)
+                test(cb.check())
+                print "ok"
+        elif j == 2:
+            if not ami:
+                print "aborting server #%d and #%d with idempotent call... " % (i, i + 1),
+                try:
+                    obj.idempotentAbort()
+                    test(False)
+                except Ice.ConnectionLostException:
+                    print "ok"
+                except Ice.ConnectFailedException:
+                    print "ok"
+            else:
+                print "aborting server #%d and #%d with idempotent AMI call... " % (i, i + 1),
+                cb = AMI_Test_idempotentAbortI()
+                obj.idempotentAbort_async(cb)
+                test(cb.check())
+                print "ok"
+
+            i = i + 1
+        elif j == 3:
+            if not ami:
+                print "aborting server #%d and #%d with nonmutating call... " % (i, i + 1),
+                try:
+                    obj.nonmutatingAbort()
+                    test(False)
+                except Ice.ConnectionLostException:
+                    print "ok"
+                except Ice.ConnectFailedException:
+                    print "ok"
+            else:
+                print "aborting server #%d and #%d with nonmutating AMI call... " % (i, i + 1),
+                cb = AMI_Test_nonmutatingAbortI()
+                obj.nonmutatingAbort_async(cb)
+                test(cb.check())
+                print "ok"
+
+            i = i + 1
+        else:
+            assert(False)
+
+        i = i + 1
+        j = j + 1
+
+    print "testing whether all servers are gone... ",
+    try:
+        obj.ice_ping()
+        test(False)
+    except Ice.LocalException:
+        print "ok"
---
+++ py/test/Ice/faultTolerance/Server.py
@@ -0,0 +1,85 @@
+# **********************************************************************
+#
+# Copyright (c) 2003-2004 ZeroC, Inc. All rights reserved.
+#
+# This copy of Ice is licensed to you under the terms described in the
+# ICE_LICENSE file included in this distribution.
+#
+# **********************************************************************
+
+import sys, os, Ice, Test, _Top
+
+def usage(n):
+    sys.stderr.write("Usage: " + n + " port\n")
+
+class TestI(_Top.Test):
+    def __init__(self, adapter):
+        self._adapter = adapter
+
+    def shutdown(self, current=None):
+        self._adapter.getCommunicator().shutdown()
+
+    def abort(self, current=None):
+        print "aborting..."
+        sys.exit(0)
+
+    def idempotentAbort(self, current=None):
+        sys.exit(0)
+
+    def nonmutatingAbort(self, current=None):
+        sys.exit(0)
+
+    def pid(self, current=None):
+        return os.getpid()
+
+def run(args, communicator):
+    port = 0
+    for arg in args[1:]:
+        if arg[0] == '-':
+            print >> sys.stderr, args[0] + ": unknown option `" + arg + "'"
+            usage(args[0])
+            return False
+        if port > 0:
+            print >> sys.stderr, args[0] + ": only one port can be specified"
+            usage(args[0])
+            return False
+
+        port = int(arg)
+
+    if port <= 0:
+        print >> sys.stderr, args[0] + ": no port specified"
+        usage(args[0])
+        return False
+
+    endpts = "default -p " + str(port)
+    communicator.getProperties().setProperty("TestAdapter.Endpoints", endpts)
+    adapter = communicator.createObjectAdapter("TestAdapter")
+    object = TestI(adapter)
+    adapter.add(object, Ice.stringToIdentity("test"))
+    adapter.activate()
+    communicator.waitForShutdown()
+    return True
+
+try:
+    #
+    # In this test, we need a longer server idle time, otherwise
+    # our test servers may time out before they are used in the
+    # test.
+    #
+    properties = Ice.getDefaultProperties(sys.argv)
+    properties.setProperty("Ice.ServerIdleTime", "120") # Two minutes.
+
+    communicator = Ice.initialize(sys.argv)
+    status = run(sys.argv, communicator)
+except Ice.Exception, ex:
+    print ex
+    status = False
+
+if communicator:
+    try:
+        communicator.destroy()
+    except Ice.Exception, ex:
+        print ex
+        status = False
+
+sys.exit(not status)
