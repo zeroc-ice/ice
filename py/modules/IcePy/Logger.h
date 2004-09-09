fd30162cffde14212676f7b0f9e3fbbc97e4bc4a
Mark Spruiell <mes@zeroc.com>
2004-09-09 23:43:22 +0000

    initial check-in

---
+++ py/modules/IcePy/Logger.h
@@ -0,0 +1,27 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2004 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#ifndef ICEPY_LOGGER_H
+#define ICEPY_LOGGER_H
+
+#include <Python.h>
+#include <Ice/LoggerF.h>
+
+namespace IcePy
+{
+
+bool initLogger(PyObject*);
+
+PyObject* createLogger(const Ice::LoggerPtr&);
+
+Ice::LoggerPtr wrapLogger(PyObject*);
+
+}
+
+#endif
