bdfb2a59e99463463996bd11b661bbdeb678add4
Michi Henning <michi@zeroc.com>
2004-09-15 00:57:09 +0000

    *** empty log message ***

---
+++ vb/demo/Ice/callback/Client.vb
@@ -0,0 +1,18 @@
+'  **********************************************************************
+' 
+'  Copyright (c) 2003-2004 ZeroC, Inc. All rights reserved.
+' 
+'  This copy of Ice is licensed to you under the terms described in the
+'  ICE_LICENSE file included in this distribution.
+' 
+'  **********************************************************************
+
+Module callbackC
+
+    Public Sub Main(ByVal args() As String)
+	Dim app As CallbackClient = New CallbackClient
+	Dim status As Integer = app.main(args, "config")
+	System.Environment.Exit(status)
+    End Sub
+
+End Module
