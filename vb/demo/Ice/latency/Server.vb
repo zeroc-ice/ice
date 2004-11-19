ce8c7b00813d05ed11a47739cb0c85f5fe2c999b
Michi Henning <michi@zeroc.com>
2004-11-19 06:08:50 +0000

    Added demo projects for binary distribution.

--- vb/demo/Ice/latency/Server.vb
+++ vb/demo/Ice/latency/Server.vb
@@ -13,38 +13,38 @@ Imports Demo
 Module Server
 
     Private Function run(ByVal args() As String, ByVal communicator As Ice.Communicator) As Integer
-	Dim adapter As Ice.ObjectAdapter = communicator.createObjectAdapter("Latency")
-	Dim [object] As Ice.Object = New Ping
-	adapter.add([object], Ice.Util.stringToIdentity("ping"))
-	adapter.activate()
-	communicator.waitForShutdown()
-	Return 0
+        Dim adapter As Ice.ObjectAdapter = communicator.createObjectAdapter("Latency")
+        Dim [object] As Ice.Object = New Ping
+        adapter.add([object], Ice.Util.stringToIdentity("ping"))
+        adapter.activate()
+        communicator.waitForShutdown()
+        Return 0
     End Function
 
     Sub Main(ByVal args() As String)
-	Dim status As Integer = 0
-	Dim communicator As Ice.Communicator = Nothing
+        Dim status As Integer = 0
+        Dim communicator As Ice.Communicator = Nothing
 
-	Try
-	    Dim properties As Ice.Properties = Ice.Util.createProperties()
-	    properties.load("config")
-	    communicator = Ice.Util.initializeWithProperties(args, properties)
-	    status = run(args, communicator)
-	Catch ex As System.Exception
-	    System.Console.Error.WriteLine(ex)
-	    status = 1
-	End Try
+        Try
+            Dim properties As Ice.Properties = Ice.Util.createProperties()
+            properties.load("config")
+            communicator = Ice.Util.initializeWithProperties(args, properties)
+            status = run(args, communicator)
+        Catch ex As System.Exception
+            System.Console.Error.WriteLine(ex)
+            status = 1
+        End Try
 
-	If Not communicator Is Nothing Then
-	    Try
-		communicator.destroy()
-	    Catch ex As System.Exception
-		System.Console.Error.WriteLine(ex)
-		status = 1
-	    End Try
-	End If
+        If Not communicator Is Nothing Then
+            Try
+                communicator.destroy()
+            Catch ex As System.Exception
+                System.Console.Error.WriteLine(ex)
+                status = 1
+            End Try
+        End If
 
-	System.Environment.Exit(status)
+        System.Environment.Exit(status)
     End Sub
 
 End Module
