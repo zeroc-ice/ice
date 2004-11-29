384011a545d5a200dd5dc20b1053ebbf248f56a9
Michi Henning <michi@zeroc.com>
2004-11-29 03:52:15 +0000

    Changed code to use generated PropertyNames.{cpp,h} file.

--- cpp/src/Ice/PropertiesI.cpp
+++ cpp/src/Ice/PropertiesI.cpp
@@ -10,6 +10,7 @@
 #include <Ice/PropertiesI.h>
 #include <Ice/Initialize.h>
 #include <Ice/LocalException.h>
+#include <Ice/PropertyNames.h>
 #include <fstream>
 
 using namespace std;
@@ -90,366 +91,14 @@ Ice::PropertiesI::getPropertiesForPrefix(const string& prefix)
     return result;
 }
 
-//
-// Valid properties for each application.
-// A '*' character is a wildcard. If used, it must appear at the end of the string.
-// Examples: "Ice.Foo.*" allows all properties with that prefix, such as "Ice.Foo.Bar".
-//           "Ice.Foo*" allows properties such as "Ice.Foo.Bar" and "Ice.FooBar".
-//
-static const string iceProps[] =
-{
-    "ChangeUser",
-    "Config",
-    "ConnectionIdleTime",
-    "Default.Host",
-    "Default.Locator",
-    "Default.Package",
-    "Default.Protocol", // Not used by C++ but left here to suppress warnings.
-    "Default.Router",
-    "GC.Interval",
-    "Logger.Timestamp",
-    "MessageSizeMax",
-    "MonitorConnections",
-    "Nohup",
-    "NullHandleAbort",
-    "Override.Compress",
-    "Override.ConnectTimeout",
-    "Override.Timeout",
-    "Package.*", // Not used by C++ but left here to suppress warnings.
-    "Plugin.*",
-    "PrintAdapterReady",
-    "PrintProcessId",
-    "ProgramName",
-    "RetryIntervals",
-    "ServerId",
-    "ServerIdleTime",
-    "StdErr",
-    "StdOut",
-    "ThreadPool.Client.Size",
-    "ThreadPool.Client.SizeMax",
-    "ThreadPool.Client.SizeWarn",
-    "ThreadPool.Client.StackSize",
-    "ThreadPool.Server.Size",
-    "ThreadPool.Server.SizeMax",
-    "ThreadPool.Server.SizeWarn",
-    "ThreadPool.Server.StackSize",
-    "ThreadPerConnection",
-    "ThreadPerConnection.StackSize",
-    "Trace.GC",
-    "Trace.Location",
-    "Trace.Network",
-    "Trace.Protocol",
-    "Trace.Retry",
-    "Trace.Slicing",
-    "UDP.RcvSize",
-    "UDP.SndSize",
-    "UseEventLog",
-    "UseSyslog",
-    "Warn.AMICallback",
-    "Warn.Connections",
-    "Warn.Datagrams",
-    "Warn.Dispatch",
-    "Warn.Leaks"
-};
-
-static const string iceBoxProps[] =
-{
-    "LoadOrder",
-    "PrintServicesReady",
-    "Service.*",
-    "ServiceManager.AdapterId",
-    "ServiceManager.Endpoints",
-    "ServiceManager.PublishedEndpoints",
-    "ServiceManager.Identity",
-    "ServiceManager.RegisterProcess",
-    "ServiceManager.ThreadPool.Size",
-    "ServiceManager.ThreadPool.SizeMax",
-    "ServiceManager.ThreadPool.SizeWarn",
-    "ServiceManager.ThreadPool.StackSize",
-    "UseSharedCommunicator.*"
-};
-
-static const string icePackProps[] =
-{
-    "Node.AdapterId",
-    "Node.CollocateRegistry",
-    "Node.Data",
-    "Node.Endpoints",
-    "Node.PublishedEndpoints",
-    "Node.Name",
-    "Node.Output",
-    "Node.PrintServersReady",
-    "Node.PropertiesOverride",
-    "Node.RedirectErrToOut",
-    "Node.ThreadPool.Size",
-    "Node.ThreadPool.SizeMax",
-    "Node.ThreadPool.SizeWarn",
-    "Node.ThreadPool.StackSize",
-    "Node.Trace.Activator",
-    "Node.Trace.Adapter",
-    "Node.Trace.Server",
-    "Node.WaitTime",
-    "Registry.Admin.AdapterId",
-    "Registry.Admin.Endpoints",
-    "Registry.Admin.PublishedEndpoints",
-    "Registry.Admin.ThreadPool.Size",
-    "Registry.Admin.ThreadPool.SizeMax",
-    "Registry.Admin.ThreadPool.SizeWarn",
-    "Registry.Admin.ThreadPool.StackSize",
-    "Registry.Client.AdapterId",
-    "Registry.Client.Endpoints",
-    "Registry.Client.PublishedEndpoints",
-    "Registry.Client.ThreadPool.Size",
-    "Registry.Client.ThreadPool.SizeMax",
-    "Registry.Client.ThreadPool.SizeWarn",
-    "Registry.Client.ThreadPool.StackSize",
-    "Registry.Data",
-    "Registry.DynamicRegistration",
-    "Registry.Internal.AdapterId",
-    "Registry.Internal.Endpoints",
-    "Registry.Internal.PublishedEndpoints",
-    "Registry.Internal.ThreadPool.Size",
-    "Registry.Internal.ThreadPool.SizeMax",
-    "Registry.Internal.ThreadPool.SizeWarn",
-    "Registry.Internal.ThreadPool.StackSize",
-    "Registry.Server.AdapterId",
-    "Registry.Server.Endpoints",
-    "Registry.Server.PublishedEndpoints",
-    "Registry.Server.ThreadPool.Size",
-    "Registry.Server.ThreadPool.SizeMax",
-    "Registry.Server.ThreadPool.SizeWarn",
-    "Registry.Server.ThreadPool.StackSize",
-    "Registry.Trace.AdapterRegistry",
-    "Registry.Trace.NodeRegistry",
-    "Registry.Trace.ObjectRegistry",
-    "Registry.Trace.ServerRegistry"
-};
-
-static const string icePatchProps[] =
-{
-    "BusyTimeout",
-    "Directory",
-    "Dynamic",
-    "Endpoints",
-    "PublishedEndpoints",
-    "RemoveOrphaned",
-    "Thorough",
-    "Trace.Files",
-    "UpdatePeriod"
-};
-
-static const string icePatch2Props[] =
-{
-    "AdapterId",
-    "Admin.AdapterId",
-    "Admin.Endpoints",
-    "Admin.PublishedEndpoints"
-    "Admin.ThreadPool.Size",
-    "Admin.ThreadPool.SizeMax",
-    "Admin.ThreadPool.SizeWarn",
-    "Admin.ThreadPool.StackSize",
-    "Directory",
-    "Endpoints",
-    "PublishedEndpoints"
-    "ThreadPool.Size",
-    "ThreadPool.SizeMax",
-    "ThreadPool.SizeWarn",
-    "ThreadPool.StackSize"
-};
-
-static const string iceSSLProps[] =
-{
-    "Client.CertPath*",
-    "Client.Config",
-    "Client.Handshake.Retries",
-    "Client.IgnoreValidPeriod",
-    "Client.Overrides.CACertificate",
-    "Client.Overrides.DSA.Certificate",
-    "Client.Overrides.DSA.PrivateKey",
-    "Client.Overrides.RSA.Certificate",
-    "Client.Overrides.RSA.PrivateKey",
-    "Client.Passphrase.Retries",
-    "Server.CertPath*",
-    "Server.Config",
-    "Server.IgnoreValidPeriod",
-    "Server.Overrides.CACertificate",
-    "Server.Overrides.DSA.Certificate",
-    "Server.Overrides.DSA.PrivateKey",
-    "Server.Overrides.RSA.Certificate",
-    "Server.Overrides.RSA.PrivateKey",
-    "Server.Passphrase.Retries",
-    "Trace.Security"
-};
-
-static const string iceStormProps[] =
-{
-    "Flush.Timeout",
-    "Publish.AdapterId",
-    "Publish.Endpoints",
-    "Publish.PublishedEndpoints",
-    "Publish.RegisterProcess",
-    "Publish.ThreadPool.Size",
-    "Publish.ThreadPool.SizeMax",
-    "Publish.ThreadPool.SizeWarn",
-    "Publish.ThreadPool.StackSize",
-    "TopicManager.AdapterId",
-    "TopicManager.Endpoints",
-    "TopicManager.PublishedEndpoints",
-    "TopicManager.Proxy",
-    "TopicManager.RegisterProcess",
-    "TopicManager.ThreadPool.Size",
-    "TopicManager.ThreadPool.SizeMax",
-    "TopicManager.ThreadPool.SizeWarn",
-    "TopicManager.ThreadPool.StackSize",
-    "Trace.Flush",
-    "Trace.Subscriber",
-    "Trace.Topic",
-    "Trace.TopicManager"
-};
-
-static const string glacierProps[] =
-{
-    "Router.AcceptCert",
-    "Router.AdapterId",
-    "Router.AllowCategories",
-    "Router.Client.AdapterId",
-    "Router.Client.Endpoints",
-    "Router.Client.PublishedEndpoints",
-    "Router.Client.ForwardContext",
-    "Router.Client.SleepTime",
-    "Router.Client.ThreadPool.Size",
-    "Router.Client.ThreadPool.SizeMax",
-    "Router.Client.ThreadPool.SizeWarn",
-    "Router.Client.ThreadPool.StackSize",
-    "Router.Client.Throttle.Twoways",
-    "Router.Endpoints",
-    "Router.PublishedEndpoints",
-    "Router.ThreadPool.Size",
-    "Router.ThreadPool.SizeMax",
-    "Router.ThreadPool.SizeWarn",
-    "Router.ThreadPool.StackSize",
-    "Router.Identity",
-    "Router.PrintProxyOnFd",
-    "Router.Server.AdapterId",
-    "Router.Server.Endpoints",
-    "Router.Server.PublishedEndpoints",
-    "Router.Server.ForwardContext",
-    "Router.Server.SleepTime",
-    "Router.Server.ThreadPool.Size",
-    "Router.Server.ThreadPool.SizeMax",
-    "Router.Server.ThreadPool.SizeWarn",
-    "Router.Server.ThreadPool.StackSize",
-    "Router.Server.Throttle.Twoways",
-    "Router.SessionManager",
-    "Router.Trace.Client",
-    "Router.Trace.RoutingTable",
-    "Router.Trace.Server",
-    "Router.Trace.Throttle",
-    "Router.UserId",
-    "Starter.AdapterId",
-    "Starter.AddUserToAllowCategories",
-    "Starter.Certificate.BitStrength",
-    "Starter.Certificate.CommonName",
-    "Starter.Certificate.Country",
-    "Starter.Certificate.IssuedAdjust",
-    "Starter.Certificate.Locality",
-    "Starter.Certificate.Organization",
-    "Starter.Certificate.OrganizationalUnit",
-    "Starter.Certificate.SecondsValid",
-    "Starter.Certificate.StateProvince",
-    "Starter.CryptPasswords",
-    "Starter.Endpoints",
-    "Starter.PublishedEndpoints",
-    "Starter.PermissionsVerifier",
-    "Starter.PropertiesOverride",
-    "Starter.RouterPath",
-    "Starter.StartupTimeout",
-    "Starter.ThreadPool.Size",
-    "Starter.ThreadPool.SizeMax",
-    "Starter.ThreadPool.SizeWarn",
-    "Starter.ThreadPool.StackSize",
-    "Starter.Trace"
-};
-
-static const string glacier2Props[] =
-{
-    "AddUserToAllowCategories",
-    "Admin.Endpoints",
-    "Admin.PublishedEndpoints",
-    "AdminIdentity",
-    "AllowCategories",
-    "Client.AlwaysBatch",
-    "Client.Endpoints",
-    "Client.PublishedEndpoints",
-    "Client.ForwardContext",
-    "Client.SleepTime",
-    "Client.Trace.Override",
-    "Client.Trace.Reject",
-    "Client.Trace.Request",
-    "Client.Buffered",
-    "CryptPasswords",
-    "PermissionsVerifier",
-    "RouterIdentity",
-    "Server.AlwaysBatch",
-    "Server.Endpoints",
-    "Server.PublishedEndpoints",
-    "Server.ForwardContext",
-    "Server.SleepTime",
-    "Server.Trace.Override",
-    "Server.Trace.Request",
-    "Server.Buffered",
-    "SessionManager",
-    "SessionTimeout",
-    "Trace.RoutingTable",
-    "Trace.Session"
-};
-
-static const string freezeProps[] =
-{
-    "DbEnv.*",
-    "Evictor.*",
-    "Trace.DbEnv",
-    "Trace.Evictor",
-    "Trace.Map",
-    "Trace.Transaction",
-    "Warn.CloseInFinalize",
-    "Warn.Deadlocks"
-};
-
-struct PropertyValues
-{
-    string prefix;
-    const string* props;
-    size_t propsSize;
-
-    PropertyValues(string pf, const string* p, size_t s) :
-	prefix(pf), props(p), propsSize(s)
-    {
-    }
-};
-
-//
-// Array of valid properties for each application.
-//
-static const PropertyValues validProps[] =
-{
-    PropertyValues("Freeze", freezeProps, sizeof(freezeProps) / sizeof(freezeProps[0])),
-    PropertyValues("Glacier", glacierProps, sizeof(glacierProps) / sizeof(glacierProps[0])),
-    PropertyValues("Glacier2", glacier2Props, sizeof(glacier2Props) / sizeof(glacier2Props[0])),
-    PropertyValues("IceBox", iceBoxProps, sizeof(iceBoxProps) / sizeof(iceBoxProps[0])),
-    PropertyValues("Ice", iceProps, sizeof(iceProps) / sizeof(iceProps[0])),
-    PropertyValues("IcePack", icePackProps, sizeof(icePackProps) / sizeof(icePackProps[0])),
-    PropertyValues("IcePatch", icePatchProps, sizeof(icePatchProps) / sizeof(icePatchProps[0])),
-    PropertyValues("IcePatch2", icePatch2Props, sizeof(icePatch2Props) / sizeof(icePatch2Props[0])),
-    PropertyValues("IceSSL", iceSSLProps, sizeof(iceSSLProps) / sizeof(iceSSLProps[0])),
-    PropertyValues("IceStorm", iceStormProps, sizeof(iceStormProps) / sizeof(iceStormProps[0]))
-};
-
-static const size_t validPropsSize = sizeof(validProps) / sizeof(validProps[0]);
-
 void
 Ice::PropertiesI::setProperty(const string& key, const string& value)
 {
+    if(key.empty())
+    {
+	return;
+    }
+
     //
     // Check if the property is legal. (We write to cerr instead of
     // using a logger because no logger may be established at the time
@@ -459,28 +108,34 @@ Ice::PropertiesI::setProperty(const string& key, const string& value)
     if(dotPos != string::npos)
     {
 	string prefix = key.substr(0, dotPos);
-        bool found = false;
-	for(size_t i = 0; i != validPropsSize && !found; ++i)
+	for(const char* const** i = IceInternal::PropertyNames::validProps; *i != 0; ++i)
 	{
-	    if(validProps[i].prefix == prefix)
+	    string property(*i[0]);
+	    dotPos = property.find('.');
+	    assert(dotPos != string::npos);
+	    string propPrefix = property.substr(0, dotPos);
+	    if(propPrefix != prefix)
 	    {
-		string suffix = key.substr(++dotPos, string::npos);
-		for(size_t j = 0; j != validProps[i].propsSize && !found; ++j)
+		continue;
+	    }
+
+	    bool found = false;
+	    for(const char* const* j = *i; *j != 0 && !found; ++j)
+	    {
+		property = *j;
+		string::size_type starPos = property.find('*');
+		if(starPos == string::npos)
 		{
-		    string::size_type starPos = validProps[i].props[j].find('*');
-		    if(starPos == string::npos)
-		    {
-			found = validProps[i].props[j].compare(suffix) == 0;
-		    }
-		    else
-		    {
-			found = validProps[i].props[j].compare(0, starPos - 1, suffix.substr(0, starPos - 1)) == 0;
-		    }
+		    found = property == key;
 		}
-		if(!found)
+		else
 		{
-		    cerr << "warning: unknown property: " << key << endl;
+		    found = property.compare(0, starPos, key.substr(0, starPos)) == 0;
 		}
+	    }
+	    if(!found)
+	    {
+		cerr << "warning: unknown property: " << key << endl;
 	    }
 	}
     }
@@ -490,16 +145,13 @@ Ice::PropertiesI::setProperty(const string& key, const string& value)
     //
     // Set or clear the property.
     //
-    if(!key.empty())
+    if(!value.empty())
     {
-	if(!value.empty())
-	{
-	    _properties[key] = value;
-	}
-	else
-	{
-	    _properties.erase(key);
-	}
+	_properties[key] = value;
+    }
+    else
+    {
+	_properties.erase(key);
     }
 }
 
