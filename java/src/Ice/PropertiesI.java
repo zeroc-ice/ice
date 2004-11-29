04fb06cac6541ce236f01c74ecb38a481a4dd518
Michi Henning <michi@zeroc.com>
2004-11-29 03:54:15 +0000

    Changed code to use generated PropertyNames.java file.

--- java/src/Ice/PropertiesI.java
+++ java/src/Ice/PropertiesI.java
@@ -96,27 +96,43 @@ public final class PropertiesI extends LocalObjectImpl implements Properties
 	// a logger because no logger may be established at the time the property
 	// is parsed.)
 	//
+	if(key == null || key.length == 0)
+	{
+	    return;
+	}
+
         int dotPos = key.indexOf('.');
 	if(dotPos != -1)
 	{
-	    String prefix = key.substring(0, dotPos);
-	    String[] validProps = (String[])_vp._validProps.get(prefix);
-	    if(validProps != null)
+	    String prefix = key.substring(0, dotPos - 1);
+	    for(int i = 0; IceInternal.PropertyNames.validProps[i] != null; ++i)
 	    {
-	        String suffix = key.substring(dotPos + 1, key.length());
-		boolean found = false;
-		for(int j = 0; j < validProps.length && !found; ++j)
+	        String property = IceInternal.PropertyNames.validProps[i][0];
+		dotPos = property.indexOf('.');
+		assert(dotPos != -1);
+		String propPrefix = property.substring(0, dotPos - 1);
+		if(!propPrefix.equals(prefix))
 		{
-		    int starPos = validProps[j].indexOf('*');
+		    continue;
+		}
+
+		boolean found = false;
+		for(int j = 0; IceInternal.PropertyNames.validProps[i][j] != null && !found; ++j)
+		{
+		    property = IceInternal.PropertyNames.validProps[i][j];
+		    int starPos = property.indexOf('*');
 		    if(starPos == -1)
 		    {
-		        found = validProps[j].equals(suffix);
+		        found = property.equals(key);
 		    }
 		    else
 		    {
-		        int max = java.lang.Math.min(suffix.length(), starPos);
-			String s1 = suffix.substring(0, max);
-			String s2 = validProps[j].substring(0, max);
+			if(key.length() < starPos)
+			{
+			    continue;
+			}
+			String s1 = property.substring(0, starPos);
+			String s2 = key.substring(0, starPos);
 			found = s1.equals(s2);
 		    }
 		}
@@ -130,16 +146,13 @@ public final class PropertiesI extends LocalObjectImpl implements Properties
 	//
 	// Set or clear the property.
 	//
-	if(key != null && key.length() > 0)
+	if(value != null && value.length() > 0)
 	{
-	    if(value != null && value.length() > 0)
-	    {
-		_properties.put(key, value);
-	    }
-	    else
-	    {
-		_properties.remove(key);
-	    }
+	    _properties.put(key, value);
+	}
+	else
+	{
+	    _properties.remove(key);
 	}
     }
 
@@ -358,331 +371,4 @@ public final class PropertiesI extends LocalObjectImpl implements Properties
     }
 
     private java.util.HashMap _properties = new java.util.HashMap();
-
-    //
-    // Valid properties for each application.
-    // A '*' character is a wildcard. If used, it must appear at the end of the string.
-    // Examples: "Ice.Foo.*" allows all properties with that prefix, such as "Ice.Foo.Bar".
-    //           "Ice.Foo*" allows properties such as "Ice.Foo.Bar" and "Ice.FooBar".
-    //
-    private static final String _iceProps[] =
-    {
-	"ChangeUser",
-	"Config",
-	"ConnectionIdleTime",
-	"Default.Host",
-	"Default.Locator",
-	"Default.Package",
-	"Default.Protocol",
-	"Default.Router",
-	"GC.Interval",
-	"Logger.Timestamp",
-	"MessageSizeMax",
-	"MonitorConnections",
-	"Nohup",
-	"NullHandleAbort",
-	"Override.Compress",
-	"Override.ConnectTimeout",
-	"Override.Timeout",
-	"Package.*",
-	"Plugin.*",
-	"PrintAdapterReady",
-	"PrintProcessId",
-	"ProgramName",
-	"RetryIntervals",
-	"StdErr",
-	"StdOut",
-	"ServerId",
-	"ServerIdleTime",
-	"ThreadPool.Client.Size",
-	"ThreadPool.Client.SizeMax",
-	"ThreadPool.Client.SizeWarn",
-	"ThreadPool.Client.StackSize",
-	"ThreadPool.Server.Size",
-	"ThreadPool.Server.SizeMax",
-	"ThreadPool.Server.SizeWarn",
-	"ThreadPool.Server.StackSize",
-	"ThreadPerConnection",
-	"ThreadPerConnection.StackSize",
-	"Trace.GC",
-	"Trace.Location",
-	"Trace.Network",
-	"Trace.Protocol",
-	"Trace.Retry",
-	"Trace.Slicing",
-	"UDP.RcvSize",
-	"UDP.SndSize",
-	"UseEventLog",
-	"UseSyslog",
-	"Warn.AMICallback",
-	"Warn.Connections",
-	"Warn.Datagrams",
-	"Warn.Dispatch",
-	"Warn.Leaks"
-    };
-    
-    private static final String _iceBoxProps[] =
-    {
-	"LoadOrder",
-	"PrintServicesReady",
-	"Service.*",
-	"ServiceManager.AdapterId",
-	"ServiceManager.Endpoints",
-	"ServiceManager.PublishedEndpoints",
-	"ServiceManager.Identity",
-	"ServiceManager.RegisterProcess",
-	"ServiceManager.ThreadPool.Size",
-	"ServiceManager.ThreadPool.SizeMax",
-	"ServiceManager.ThreadPool.SizeWarn",
-	"ServiceManager.ThreadPool.StackSize",
-	"UseSharedCommunicator.*"
-    };
-
-    private static final String _icePackProps[] =
-    {
-	"Node.AdapterId",
-	"Node.CollocateRegistry",
-	"Node.Data",
-	"Node.Endpoints",
-	"Node.PublishedEndpoints",
-	"Node.Name",
-	"Node.Output",
-	"Node.PrintServersReady",
-	"Node.PropertiesOverride",
-	"Node.RedirectErrToOut",
-	"Node.ThreadPool.Size",
-	"Node.ThreadPool.SizeMax",
-	"Node.ThreadPool.SizeWarn",
-	"Node.ThreadPool.StackSize",
-	"Node.Trace.Activator",
-	"Node.Trace.Adapter",
-	"Node.Trace.Server",
-	"Node.WaitTime",
-	"Registry.Admin.AdapterId",
-	"Registry.Admin.Endpoints",
-	"Registry.Admin.PublishedEndpoints",
-	"Registry.Admin.ThreadPool.Size",
-	"Registry.Admin.ThreadPool.SizeMax",
-	"Registry.Admin.ThreadPool.SizeWarn",
-	"Registry.Admin.ThreadPool.StackSize",
-	"Registry.Client.AdapterId",
-	"Registry.Client.Endpoints",
-	"Registry.Client.PublishedEndpoints",
-	"Registry.Client.ThreadPool.Size",
-	"Registry.Client.ThreadPool.SizeMax",
-	"Registry.Client.ThreadPool.SizeWarn",
-	"Registry.Client.ThreadPool.StackSize",
-	"Registry.Data",
-	"Registry.DynamicRegistration",
-	"Registry.Internal.AdapterId",
-	"Registry.Internal.Endpoints",
-	"Registry.Internal.PublishedEndpoints",
-	"Registry.Internal.ThreadPool.Size",
-	"Registry.Internal.ThreadPool.SizeMax",
-	"Registry.Internal.ThreadPool.SizeWarn",
-	"Registry.Internal.ThreadPool.StackSize",
-	"Registry.Server.AdapterId",
-	"Registry.Server.Endpoints",
-	"Registry.Server.PublishedEndpoints",
-	"Registry.Server.ThreadPool.Size",
-	"Registry.Server.ThreadPool.SizeMax",
-	"Registry.Server.ThreadPool.SizeWarn",
-	"Registry.Server.ThreadPool.StackSize",
-	"Registry.Trace.AdapterRegistry",
-	"Registry.Trace.NodeRegistry",
-	"Registry.Trace.ObjectRegistry",
-	"Registry.Trace.ServerRegistry"
-    };
-
-    private static final String _icePatchProps[] =
-    {
-	"BusyTimeout",
-	"Directory",
-	"Dynamic",
-	"Endpoints",
-	"PublishedEndpoints",
-	"RemoveOrphaned",
-	"Thorough",
-	"Trace.Files",
-	"UpdatePeriod"
-    };
-    
-    private static final String _iceSSLProps[] =
-    {
-	"Client.CertPath*",
-	"Client.Config",
-	"Client.Handshake.Retries",
-	"Client.IgnoreValidPeriod",
-	"Client.Overrides.CACertificate",
-	"Client.Overrides.DSA.Certificate",
-	"Client.Overrides.DSA.PrivateKey",
-	"Client.Overrides.RSA.Certificate",
-	"Client.Overrides.RSA.PrivateKey",
-	"Client.Passphrase.Retries",
-	"Server.CertPath*",
-	"Server.Config",
-	"Server.IgnoreValidPeriod",
-	"Server.Overrides.CACertificate",
-	"Server.Overrides.DSA.Certificate",
-	"Server.Overrides.DSA.PrivateKey",
-	"Server.Overrides.RSA.Certificate",
-	"Server.Overrides.RSA.PrivateKey",
-	"Server.Passphrase.Retries",
-	"Trace.Security"
-    };
-
-    private static final String _iceStormProps[] =
-    {
-	"Flush.Timeout",
-	"Publish.AdapterId",
-	"Publish.Endpoints",
-	"Publish.PublishedEndpoints",
-	"Publish.RegisterProcess",
-	"Publish.ThreadPool.Size",
-	"Publish.ThreadPool.SizeMax",
-	"Publish.ThreadPool.SizeWarn",
-	"Publish.ThreadPool.StackSize",
-	"TopicManager.AdapterId",
-	"TopicManager.Endpoints",
-	"TopicManager.PublishedEndpoints",
-	"TopicManager.Proxy",
-	"TopicManager.RegisterProcess",
-	"TopicManager.ThreadPool.Size",
-	"TopicManager.ThreadPool.SizeMax",
-	"TopicManager.ThreadPool.SizeWarn",
-	"TopicManager.ThreadPool.StackSize",
-	"Trace.Flush",
-	"Trace.Subscriber",
-	"Trace.Topic",
-	"Trace.TopicManager"
-    };
-
-    private static final String _glacierProps[] =
-    {
-	"Router.AcceptCert",
-	"Router.AdapterId",
-	"Router.AllowCategories",
-	"Router.Client.AdapterId",
-	"Router.Client.Endpoints",
-	"Router.Client.PublishedEndpoints",
-	"Router.Client.ForwardContext",
-	"Router.Client.SleepTime",
-	"Router.Client.ThreadPool.Size",
-	"Router.Client.ThreadPool.SizeMax",
-	"Router.Client.ThreadPool.SizeWarn",
-	"Router.Client.ThreadPool.StackSize",
-	"Router.Client.Throttle.Twoways",
-	"Router.Endpoints",
-	"Router.PublishedEndpoints",
-	"Router.ThreadPool.Size",
-	"Router.ThreadPool.SizeMax",
-	"Router.ThreadPool.SizeWarn",
-	"Router.ThreadPool.StackSize",
-	"Router.Identity",
-	"Router.PrintProxyOnFd",
-	"Router.Server.AdapterId",
-	"Router.Server.Endpoints",
-	"Router.Server.PublishedEndpoints",
-	"Router.Server.ForwardContext",
-	"Router.Server.SleepTime",
-	"Router.Server.ThreadPool.Size",
-	"Router.Server.ThreadPool.SizeMax",
-	"Router.Server.ThreadPool.SizeWarn",
-	"Router.Server.ThreadPool.StackSize",
-	"Router.Server.Throttle.Twoways",
-	"Router.SessionManager",
-	"Router.Trace.Client",
-	"Router.Trace.RoutingTable",
-	"Router.Trace.Server",
-	"Router.Trace.Throttle",
-	"Router.UserId",
-	"Starter.AdapterId",
-	"Starter.AddUserToAllowCategories",
-	"Starter.Certificate.BitStrength",
-	"Starter.Certificate.CommonName",
-	"Starter.Certificate.Country",
-	"Starter.Certificate.IssuedAdjust",
-	"Starter.Certificate.Locality",
-	"Starter.Certificate.Organization",
-	"Starter.Certificate.OrganizationalUnit",
-	"Starter.Certificate.SecondsValid",
-	"Starter.Certificate.StateProvince",
-	"Starter.CryptPasswords",
-	"Starter.Endpoints",
-	"Starter.PublishedEndpoints",
-	"Starter.PermissionsVerifier",
-	"Starter.PropertiesOverride",
-	"Starter.RouterPath",
-	"Starter.StartupTimeout",
-	"Starter.ThreadPool.Size",
-	"Starter.ThreadPool.SizeMax",
-	"Starter.ThreadPool.SizeWarn",
-	"Starter.ThreadPool.StackSize",
-	"Starter.Trace"
-    };
-
-    private static final String _glacier2Props[] =
-    {
-	"AddUserToAllowCategories",
-	"Admin.Endpoints",
-	"Admin.PublishedEndpoints",
-	"AdminIdentity",
-	"AllowCategories",
-	"Client.AlwaysBatch",
-	"Client.Endpoints",
-	"Client.PublishedEndpoints",
-	"Client.ForwardContext",
-	"Client.SleepTime",
-	"Client.Trace.Override",
-	"Client.Trace.Reject",
-	"Client.Trace.Request",
-	"Client.Buffered",
-	"CryptPasswords",
-	"PermissionsVerifier",
-	"RouterIdentity",
-	"Server.AlwaysBatch",
-	"Server.Endpoints",
-	"Server.PublishedEndpoints",
-	"Server.ForwardContext",
-	"Server.SleepTime",
-	"Server.Trace.Override",
-	"Server.Trace.Request",
-	"Server.Buffered",
-	"SessionManager",
-	"SessionTimeout",
-	"Trace.RoutingTable",
-	"Trace.Session"
-    };
-
-    private static final String _freezeProps[] =
-    {
-	"DbEnv.*",
-	"Evictor.*",
-	"Trace.DbEnv",
-	"Trace.Evictor",
-	"Trace.Map",
-        "Trace.Transaction",
-	"Warn.CloseInFinalize",
-	"Warn.Deadlocks"
-    };
-
-    private static final class ValidProps
-    {
-        ValidProps()
-	{
-	    _validProps.put("Freeze", _freezeProps);
-	    _validProps.put("Glacier", _glacierProps);
-	    _validProps.put("Glacier2", _glacier2Props);
-	    _validProps.put("IceBox", _iceBoxProps);
-	    _validProps.put("Ice", _iceProps);
-	    _validProps.put("IcePack", _icePackProps);
-	    _validProps.put("IcePatch", _icePatchProps);
-	    _validProps.put("IceSSL", _iceSSLProps);
-	    _validProps.put("IceStorm", _iceStormProps);
-	}
-
-        public static java.util.HashMap _validProps = new java.util.HashMap();
-    };
-
-    private static final ValidProps _vp = new ValidProps();
 }
