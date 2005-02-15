cb461e28d4e7bb09aa4a7cd60b39ba7892bbe89a
Michi Henning <michi@zeroc.com>
2005-02-15 04:26:43 +0000

    Added bidir demo projects. Added IcePatch2 and IceStorm slice libraries.

---
+++ cs/src/IcePatch2/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2004 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("icepatch2cs")]
+[assembly: AssemblyDescription("IcePatch2 run-time support")]
+[assembly: AssemblyCompany("ZeroC, Inc.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("IcePatch2 for C#")]
+[assembly: AssemblyCopyright("Copyright (c) 2003-2004, ZeroC, Inc.")]
+[assembly: AssemblyTrademark("Ice")]
+[assembly: AssemblyCulture("")]		
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyDelaySign(false)]
+#if __MonoCS__
+[assembly: AssemblyKeyFile("IcePatch2csKey.snk")] // mcs uses different search algorithm.
+#else
+[assembly: AssemblyKeyFile(@"..\..\IcePatch2csKey.snk")]
+#endif
+[assembly: AssemblyKeyName("")]
