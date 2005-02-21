9f135a026c8656973ded1dc02e7ca4987c208e36
Brent Eagles <brent@zeroc.com>
2005-02-21 19:37:07 +0000

    reduce code duplication in getVersion()

--- cpp/newmakebindist.py
+++ cpp/newmakebindist.py
@@ -350,9 +350,7 @@ def getVersion(cvsTag, buildDir):
     os.chdir(buildDir)
     if getPlatform() == "aix":
         os.environ["LIBPATH"] = ""
-        os.system("cvs -d cvs.mutablerealms.com:/home/cvsroot export -r " + cvsTag + " ice/include/IceUtil/Config.h")
-    else:
-        os.system("cvs -d cvs.mutablerealms.com:/home/cvsroot export -r " + cvsTag + " ice/include/IceUtil/Config.h")
+    os.system("cvs -d cvs.mutablerealms.com:/home/cvsroot export -r " + cvsTag + " ice/include/IceUtil/Config.h")
 
     result = [ getIceVersion("ice/include/IceUtil/Config.h"), getIceSoVersion("ice/include/IceUtil/Config.h")]
     os.remove("ice/include/IceUtil/Config.h")
