f7e067e1fd1087fe9a9793b96669328fe3264f00
Michi Henning <michi@zeroc.com>
2005-02-24 05:51:33 +0000

    Fixed the UUID generator such that it would try to use /dev/urandom when     running with Mono under Windows.

--- cs/src/Ice/Util.cs
+++ cs/src/Ice/Util.cs
@@ -10,12 +10,11 @@
 namespace Ice
 {
 
+    using IceInternal;
     using System.Runtime.InteropServices;
-#if __MonoCS__
     using System.Diagnostics;
     using System.IO;
     using System.Text;
-#endif
 
     public sealed class Util
     {
@@ -145,74 +144,6 @@ namespace Ice
 	    }
 	}
 	
-#if __MonoCS__
-
-	public static string generateUUID()
-	{
-	    //
-	    // On Linux, /dev/random, even when used in blocking mode, sometimes 
-	    // fails or returns fewer bytes.
-	    // Maybe we should use a combination of version 4 UUIDs (with /dev/random),
-	    // and version 1 UUIDs (MAC address + time), when /dev/random is exhausted?
-	    //
-	    FileStream f = File.OpenRead("/dev/urandom");
-
-	    const int uuidSize = 16;
-	    byte[] buf = new byte[uuidSize];
-
-	    //
-	    // Limit the number of attempts to 20 reads to avoid an infinite loop.
-	    //
-	    int reads = 0;
-	    int index = 0;
-	    while(reads < 20 && index != uuidSize)
-	    {
-		int bytesRead = f.Read(buf, index, uuidSize - index);
-		index += bytesRead;
-		reads++;
-	    }
-	    f.Close();
-	    if(index != uuidSize)
-	    {
-	         throw new System.ApplicationException("generateUUID(): could not get 16 random bytes");
-	    }
-
-	    //
-	    // Adjust the bits that say "version 4" UUUID
-	    //
-	    buf[6] &= 0x0F;
-	    buf[6] |= (4 << 4);
-	    buf[8] &= 0x3F;
-	    buf[8] |= 0x80;
-
-	    StringBuilder sb = new StringBuilder();
-	    for(int i = 0; i < 4; ++i)
-	    {
-	    	sb.AppendFormat("{0:X2}", buf[i]);
-	    }
-	    sb.Append("-");
-	    for(int i = 4; i < 6; ++i)
-	    {
-	    	sb.AppendFormat("{0:X2}", buf[i]);
-	    }
-	    sb.Append("-");
-	    for(int i = 6; i < 8; ++i)
-	    {
-	    	sb.AppendFormat("{0:X2}", buf[i]);
-	    }
-	    sb.Append("-");
-	    for(int i = 8; i < 10; ++i)
-	    {
-	    	sb.AppendFormat("{0:X2}", buf[i]);
-	    }
-	    sb.Append("-");
-	    for(int i = 10; i < 16; ++i)
-	    {
-	    	sb.AppendFormat("{0:X2}", buf[i]);
-	    }
-	    return sb.ToString();
-	}
-#else
 	[StructLayout(LayoutKind.Sequential)]
 	private struct UUID
 	{
@@ -239,52 +170,119 @@ namespace Ice
 
 	public static string generateUUID()
 	{
-	    int rc;
-	    UUID uuid = new UUID();
-	    rc = UuidCreate(ref uuid);
-	    if(rc != RPC_S_OK && rc != RPC_S_UUID_LOCAL_ONLY)
+	    if(AssemblyUtil._platform == AssemblyUtil.Platform.NonWindows)
 	    {
-		Ice.SyscallException ex = new Ice.SyscallException("UuidCreate() failed");
-		ex.error = rc;
-		throw ex;
+		//
+		// On Linux, /dev/random, even when used in blocking mode, sometimes 
+		// fails or returns fewer bytes.
+		// Maybe we should use a combination of version 4 UUIDs (with /dev/random),
+		// and version 1 UUIDs (MAC address + time), when /dev/random is exhausted?
+		//
+		FileStream f = File.OpenRead("/dev/urandom");
+
+		const int uuidSize = 16;
+		byte[] buf = new byte[uuidSize];
+
+		//
+		// Limit the number of attempts to 20 reads to avoid an infinite loop.
+		//
+		int reads = 0;
+		int index = 0;
+		while(reads < 20 && index != uuidSize)
+		{
+		    int bytesRead = f.Read(buf, index, uuidSize - index);
+		    index += bytesRead;
+		    reads++;
+		}
+		f.Close();
+		if(index != uuidSize)
+		{
+		     throw new System.ApplicationException("generateUUID(): could not get 16 random bytes");
+		}
+
+		//
+		// Adjust the bits that say "version 4" UUUID
+		//
+		buf[6] &= 0x0F;
+		buf[6] |= (4 << 4);
+		buf[8] &= 0x3F;
+		buf[8] |= 0x80;
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 4; ++i)
+		{
+		    sb.AppendFormat("{0:X2}", buf[i]);
+		}
+		sb.Append("-");
+		for(int i = 4; i < 6; ++i)
+		{
+		    sb.AppendFormat("{0:X2}", buf[i]);
+		}
+		sb.Append("-");
+		for(int i = 6; i < 8; ++i)
+		{
+		    sb.AppendFormat("{0:X2}", buf[i]);
+		}
+		sb.Append("-");
+		for(int i = 8; i < 10; ++i)
+		{
+		    sb.AppendFormat("{0:X2}", buf[i]);
+		}
+		sb.Append("-");
+		for(int i = 10; i < 16; ++i)
+		{
+		    sb.AppendFormat("{0:X2}", buf[i]);
+		}
+		return sb.ToString();
 	    }
-	    System.IntPtr str = new System.IntPtr();
-	    rc = UuidToString(ref uuid, ref str);
-	    switch(rc)
+	    else
 	    {
-		case RPC_S_OK:
+		int rc;
+		UUID uuid = new UUID();
+		rc = UuidCreate(ref uuid);
+		if(rc != RPC_S_OK && rc != RPC_S_UUID_LOCAL_ONLY)
 		{
-		    break;
-		}
-		case RPC_S_OUT_OF_MEMORY:
-		{
-		    throw new Ice.MemoryLimitException("No memory for UUID string");
-		}
-		default:
-		{
-		    Ice.SyscallException ex = new Ice.SyscallException("UuidToString() failed");
+		    Ice.SyscallException ex = new Ice.SyscallException("UuidCreate() failed");
 		    ex.error = rc;
 		    throw ex;
 		}
+		System.IntPtr str = new System.IntPtr();
+		rc = UuidToString(ref uuid, ref str);
+		switch(rc)
+		{
+		    case RPC_S_OK:
+		    {
+			break;
+		    }
+		    case RPC_S_OUT_OF_MEMORY:
+		    {
+			throw new Ice.MemoryLimitException("No memory for UUID string");
+		    }
+		    default:
+		    {
+			Ice.SyscallException ex = new Ice.SyscallException("UuidToString() failed");
+			ex.error = rc;
+			throw ex;
+		    }
+		}
+		string result;
+		try 
+		{
+		    result = Marshal.PtrToStringAnsi(str);
+		}
+		catch(System.Exception ex)
+		{
+		    throw new Ice.SyscallException(ex);
+		}
+		if((rc = RpcStringFree(ref str)) != RPC_S_OK)
+		{
+		    Ice.SyscallException ex = new Ice.SyscallException("Cannot deallocate UUID");
+		    ex.error = rc;
+		    throw ex;
+		}
+		return result;
 	    }
-	    string result;
-	    try 
-	    {
-		result = Marshal.PtrToStringAnsi(str);
-	    }
-	    catch(System.Exception ex)
-	    {
-		throw new Ice.SyscallException(ex);
-	    }
-	    if((rc = RpcStringFree(ref str)) != RPC_S_OK)
-	    {
-		Ice.SyscallException ex = new Ice.SyscallException("Cannot deallocate UUID");
-		ex.error = rc;
-		throw ex;
-	    }
-	    return result;
 	}
-#endif
 	
 	public static int proxyIdentityCompare(ObjectPrx lhs, ObjectPrx rhs)
 	{
