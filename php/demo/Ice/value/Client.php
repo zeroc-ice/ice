11574381632adcbb08c1f0f36f9fcf004df1210d
Mark Spruiell <mes@zeroc.com>
2005-03-02 18:47:45 +0000

    adding copyright

--- php/demo/Ice/value/Client.php
+++ php/demo/Ice/value/Client.php
@@ -1,4 +1,13 @@
 <?php
+// **********************************************************************
+//
+// Copyright (c) 2003-2005 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
 Ice_loadProfile();
 
 class PrinterI extends Demo_Printer
