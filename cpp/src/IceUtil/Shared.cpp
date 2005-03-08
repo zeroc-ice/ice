6359b3671c0c235167defafcb56f7931593d4058
Marc Laukien <marc@zeroc.com>
2005-03-08 18:10:56 +0000

    fix

---
+++ cpp/src/IceUtil/Shared.cpp
@@ -0,0 +1,58 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2005 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#include <IceUtil/Shared.h>
+
+using namespace IceUtil;
+
+IceUtil::SimpleShared::SimpleShared() :
+    _ref(0),
+    _noDelete(false)
+{
+}
+
+IceUtil::SimpleShared::~SimpleShared()
+{
+}
+
+IceUtil::Shared::Shared() :
+#ifndef ICE_HAS_ATOMIC_FUNCTIONS
+    _ref(0),
+#endif
+    _noDelete(false)
+{
+#ifdef ICE_HAS_ATOMIC_FUNCTIONS
+    ice_atomic_set(&_ref, 0);
+#endif
+}
+
+IceUtil::Shared::~Shared()
+{
+}
+
+int
+IceUtil::Shared::__getRef() const
+{
+#if defined(_WIN32)
+    return InterlockedExchangeAdd(const_cast<LONG*>(&_ref), 0);
+#elif defined(ICE_HAS_ATOMIC_FUNCTIONS)
+    return ice_atomic_exchange_add(0, const_cast<ice_atomic_t*>(&_ref));
+#else
+    _mutex.lock();
+    int ref = _ref;
+    _mutex.unlock();
+    return ref;
+#endif
+}
+
+void
+IceUtil::Shared::__setNoDelete(bool b)
+{
+    _noDelete = b;
+}
