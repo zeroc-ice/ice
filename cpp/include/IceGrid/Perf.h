109a925901021305a9e84fbd829bff323a052d78
Bernard Normier <bernard@zeroc.com>
2005-03-25 21:10:02 +0000

    Adding IcePerf as part of IceGrid

---
+++ cpp/include/IceGrid/Perf.h
@@ -0,0 +1,26 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2005 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#ifndef ICE_GRID_PERF_H
+#define ICE_GRID_PERF_H
+
+#include <IceGrid/PerfTypes.h>
+#include <Ice/Ice.h>
+
+namespace IcePerf
+{
+
+//
+// Obtain performance data for the local system/processes
+//
+ICE_PERF_API PerfDataSeq getPerfData(const Ice::StringSeq&);
+
+}
+
+#endif
