bfe68e2593c64eaf029b7a2f7112e51ba06dd0d3
Marc Laukien <marc@zeroc.com>
2005-04-13 19:47:08 +0000

    fixes

--- cpp/demo/Ice/session/SessionManagerI.h
+++ cpp/demo/Ice/session/SessionManagerI.h
@@ -36,33 +36,35 @@ typedef IceUtil::Handle<ReapThread> ReapThreadPtr;
 
 //
 // Users wanting to create their own session manager specialization
-// should inherit off this class and implement create. Create must
-// call add with the creation session proxy.
+// should inherit off this class and implement create(). create() must
+// call add() with the creation session proxy.
+// XXX Not nice! It shouldn't be necessary for the derived class to do this. Instead, create() should be implemented here, and call another virtual function. XXX
 //
-class SessionManagerI : public ::Demo::SessionManager, public IceUtil::Mutex
+class SessionManagerI : public Demo::SessionManager, public IceUtil::Mutex
 {
 public:
 
     SessionManagerI(const Ice::CommunicatorPtr&);
-    ~SessionManagerI();
+    ~SessionManagerI(); // XXX virtual XXX
 
-    virtual ::Demo::SessionPrx create(const ::Ice::Current&) = 0;
+    virtual Demo::SessionPrx create(const Ice::Current&) = 0;
     virtual void shutdown(const Ice::Current&);
 
     void destroy();
-    void add(const ::Demo::SessionPrx&);
-    void remove(const ::Ice::Identity&);
-    void refresh(const ::Ice::Identity&);
+    void add(const Demo::SessionPrx&);
+    void remove(const Ice::Identity&);
+    void refresh(const Ice::Identity&);
     void reap();
 
 private:
 
     const IceUtil::Time _timeout;
     ReapThreadPtr _reapThread;
-    const ::Ice::LoggerPtr _logger;
-    std::map<Ice::Identity, std::pair< IceUtil::Time, ::Demo::SessionPrx> > _sessions;
+    const Ice::LoggerPtr _logger;
+    std::map<Ice::Identity, std::pair<IceUtil::Time, Demo::SessionPrx> > _sessions;
     bool _destroy;
 };
+
 typedef IceUtil::Handle<SessionManagerI> SessionManagerIPtr;
 
 #endif
