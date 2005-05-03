caa684e3c3e09a15c7f6657a026d57c9f98857ad
Michi Henning <michi@zeroc.com>
2005-05-03 04:45:28 +0000

    Removed PreBuildEvent.bat.

--- cs/demo/Ice/session/PreBuildEvent.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-C:\cygwin\home\michi\src\icecs\\bin\generatecs.exe C:\cygwin\home\michi\src\icecs\ C:\cygwin\home\michi\src\icecs\demo\Ice\session\ sessionS
-if errorlevel 1 goto CSharpReportError
-goto CSharpEnd
-:CSharpReportError
-echo Project error: A tool returned an error code from the build event
-exit 1
-:CSharpEnd
