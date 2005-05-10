45d140acbe2b1886456b40f1da46e69ea3e98484
Mark Spruiell <mes@zeroc.com>
2005-05-10 17:21:56 +0000

    remove generated directories; add -t option to skip building translators

--- java/makedist.py
+++ java/makedist.py
@@ -19,6 +19,7 @@ def usage():
     print "Options:"
     print "-h    Show this message."
     print "-d    Skip SGML documentation conversion."
+    print "-t    Skip building translators and use the ones in PATH."
     print "-v    Be verbose."
     print
     print "If no tag is specified, HEAD is used."
@@ -31,10 +32,10 @@ def find(path, patt):
     files = os.listdir(path)
     for x in files:
         fullpath = os.path.join(path, x);
+        if fnmatch.fnmatch(x, patt):
+            result.append(fullpath)
         if os.path.isdir(fullpath) and not os.path.islink(fullpath):
             result.extend(find(fullpath, patt))
-        elif fnmatch.fnmatch(x, patt):
-            result.append(fullpath)
     return result
 
 #
@@ -64,6 +65,7 @@ if sys.platform.startswith("win") or sys.platform.startswith("cygwin"):
 #
 tag = "-rHEAD"
 skipDocs = 0
+skipTranslators = 0
 verbose = 0
 for x in sys.argv[1:]:
     if x == "-h":
@@ -71,6 +73,8 @@ for x in sys.argv[1:]:
         sys.exit(0)
     elif x == "-d":
         skipDocs = 1
+    elif x == "-t":
+        skipTranslators = 1
     elif x == "-v":
         verbose = 1
     elif x.startswith("-"):
@@ -150,40 +154,41 @@ if not skipDocs:
 #
 # Build slice2java and slice2freezej.
 #
-print "Building translators..."
-cwd = os.getcwd()
-os.chdir(os.path.join("ice", "src", "icecpp"))
-os.system("gmake")
-os.chdir(cwd)
-os.chdir(os.path.join("ice", "src", "IceUtil"))
-os.system("gmake")
-os.chdir(cwd)
-os.chdir(os.path.join("ice", "src", "Slice"))
-os.system("gmake")
-os.chdir(cwd)
-os.chdir(os.path.join("ice", "src", "slice2java"))
-os.system("gmake")
-os.chdir(cwd)
-os.chdir(os.path.join("ice", "src", "slice2freezej"))
-os.system("gmake")
-os.chdir(cwd)
+if not skipTranslators:
+    print "Building translators..."
+    cwd = os.getcwd()
+    os.chdir(os.path.join("ice", "src", "icecpp"))
+    os.system("gmake")
+    os.chdir(cwd)
+    os.chdir(os.path.join("ice", "src", "IceUtil"))
+    os.system("gmake")
+    os.chdir(cwd)
+    os.chdir(os.path.join("ice", "src", "Slice"))
+    os.system("gmake")
+    os.chdir(cwd)
+    os.chdir(os.path.join("ice", "src", "slice2java"))
+    os.system("gmake")
+    os.chdir(cwd)
+    os.chdir(os.path.join("ice", "src", "slice2freezej"))
+    os.system("gmake")
+    os.chdir(cwd)
 
-sys.path.append(os.path.join("ice", "config"))
-import TestUtil
+    sys.path.append(os.path.join("ice", "config"))
+    import TestUtil
 
-os.environ["PATH"] = os.path.join(cwd, "ice", "bin") + ":" + os.getenv("PATH", "")
+    os.environ["PATH"] = os.path.join(cwd, "ice", "bin") + ":" + os.getenv("PATH", "")
 
-if TestUtil.isHpUx():
-    os.environ["SHLIB_PATH"] = os.path.join(cwd, "ice", "lib") + ":" + os.getenv("SHLIB_PATH", "")
-elif TestUtil.isDarwin():
-    os.environ["DYLD_LIBRARY_PATH"] = os.path.join(cwd, "ice", "lib") + ":" + os.getenv("DYLD_LIBRRARY_PATH", "")
-elif TestUtil.isAIX():
-    os.environ["LIBPATH"] = os.path.join(cwd, "ice", "lib") + ":" + os.getenv("LIBPATH", "")
-else:
-    os.environ["LD_LIBRARY_PATH"] = os.path.join(cwd, "ice", "lib") + ":" + os.getenv("LD_LIBRARY_PATH", "")
+    if TestUtil.isHpUx():
+	os.environ["SHLIB_PATH"] = os.path.join(cwd, "ice", "lib") + ":" + os.getenv("SHLIB_PATH", "")
+    elif TestUtil.isDarwin():
+	os.environ["DYLD_LIBRARY_PATH"] = os.path.join(cwd, "ice", "lib") + ":" + os.getenv("DYLD_LIBRRARY_PATH", "")
+    elif TestUtil.isAIX():
+	os.environ["LIBPATH"] = os.path.join(cwd, "ice", "lib") + ":" + os.getenv("LIBPATH", "")
+    else:
+	os.environ["LD_LIBRARY_PATH"] = os.path.join(cwd, "ice", "lib") + ":" + os.getenv("LD_LIBRARY_PATH", "")
 
-if os.environ.has_key("ICE_HOME"):
-    del os.environ["ICE_HOME"]
+    if os.environ.has_key("ICE_HOME"):
+	del os.environ["ICE_HOME"]
 
 #
 # Remove files.
@@ -215,6 +220,13 @@ os.rename(os.path.join("lib", "Ice.jar"), "Ice.jar")
 shutil.rmtree("lib")
 os.mkdir("lib")
 os.rename("Ice.jar", os.path.join("lib", "Ice.jar"))
+
+#
+# Remove "generated" subdirectories.
+#
+filesToRemove = find(".", "*generated") # generated, cgenerated, sgenerated
+for x in filesToRemove:
+    shutil.rmtree(x)
 
 os.chdir(cwd)
 
