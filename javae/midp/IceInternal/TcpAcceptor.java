79e113a616367b43c340c9016c5906929a395643
Brent Eagles <brent@zeroc.com>
2005-07-16 14:41:58 +0000

    edit trace statement

--- javae/midp/IceInternal/TcpAcceptor.java
+++ javae/midp/IceInternal/TcpAcceptor.java
@@ -150,7 +150,7 @@ class TcpAcceptor implements Acceptor
         {
 	    if(_traceLevels.network >= 2)
 	    {
-		String s = "attempting to bind to tcp socket <unknown:" + port + ">";
+		String s = "attempting to bind to tcp socket on port " + port;
 		_logger.trace(_traceLevels.networkCat, s);
 	    }
 	    _connection =
