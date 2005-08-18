3be555dfbc48bbf2b5f3b4d9ea93510552aeb70c
Brent Eagles <brent@zeroc.com>
2005-08-18 13:53:10 +0000

    adding missing copyright headers

--- javae/demo/IceE/midp/chat/Console.java
+++ javae/demo/IceE/midp/chat/Console.java
@@ -1,3 +1,12 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2005 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice-E is licensed to you under the terms described in the
+// ICEE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
 import javax.microedition.lcdui.*;
 import java.util.Vector;
 
