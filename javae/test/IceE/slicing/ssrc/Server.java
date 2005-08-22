bb207146d7edcb5ccfdf7207835ee92e726cd55b
Brent Eagles <brent@zeroc.com>
2005-08-22 13:34:59 +0000

    Adding comments to test config files to aide in users wanting to use the     MIDlet form of the tests.

--- javae/test/IceE/slicing/ssrc/Server.java
+++ javae/test/IceE/slicing/ssrc/Server.java
@@ -14,7 +14,11 @@ public class Server
     {
         Ice.Properties properties = communicator.getProperties();
         properties.setProperty("Ice.Warn.Dispatch", "0");
-        properties.setProperty("TestAdapter.Endpoints", "default -p 12345 -t 2000");
+	if(properties.getProperty("TestAdapter.Endpoints").length() == 0)
+	{
+	    properties.setProperty("TestAdapter.Endpoints", "default -p 12345 -t 2000");
+	}
+
         Ice.ObjectAdapter adapter = communicator.createObjectAdapter("TestAdapter");
         Ice.Object object = new TestI(adapter);
         adapter.add(object, Ice.Util.stringToIdentity("Test"));
