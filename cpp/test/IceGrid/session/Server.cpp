ca868e30d5967f464f448130991ee3175e2f9ba3
Benoit Foucher <benoit@zeroc.com>
2005-09-01 09:21:27 +0000

    Fix

---
+++ cpp/test/IceGrid/session/Server.cpp
@@ -0,0 +1,45 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2005 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#include <Ice/Ice.h>
+#include <TestCommon.h>
+
+using namespace std;
+
+class Server : public Ice::Application
+{
+public:
+
+    virtual int run(int argc, char* argv[]);
+};
+
+int
+Server::run(int argc, char* argv[])
+{
+    Ice::ObjectAdapterPtr adapter = communicator()->createObjectAdapter("Server");
+    shutdownOnInterrupt();
+    try
+    {
+        adapter->activate();
+    }
+    catch(const Ice::ObjectAdapterDeactivatedException&)
+    {
+    }
+    communicator()->waitForShutdown();
+    ignoreInterrupt();
+    return EXIT_SUCCESS;
+}
+
+int
+main(int argc, char* argv[])
+{
+    Server app;
+    int rc = app.main(argc, argv);
+    return rc;
+}
