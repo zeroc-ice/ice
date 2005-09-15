d818b43bc4a5b083b87bf1d283de9b125eae5b65
Matthew Newhook <matthew@zeroc.com>
2005-09-15 01:49:00 +0000

    http://bugzilla.zeroc.com/bugzilla/show_bug.cgi?id=335

--- vb/demo/Glacier2/callback/CallbackServer.vb
+++ vb/demo/Glacier2/callback/CallbackServer.vb
@@ -7,17 +7,3 @@
 ' 
 '  **********************************************************************
 
-Imports glacier2demo.Demo
-
-Class CallbackServer
-    Inherits Ice.Application
-
-    Public Overloads Overrides Function run(ByVal args() As String) As Integer
-        Dim adapter As Ice.ObjectAdapter = communicator().createObjectAdapter("Callback.Server")
-        adapter.add(New CallbackI, Ice.Util.stringToIdentity("callback"))
-        adapter.activate()
-        communicator().waitForShutdown()
-        Return 0
-    End Function
-
-End Class
