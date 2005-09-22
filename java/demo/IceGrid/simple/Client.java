bfdd9834dafd44a84753591220657ca1cc6a3098
Benoit Foucher <benoit@zeroc.com>
2005-09-22 12:53:39 +0000

    Added IceGrid demo.

---
+++ java/demo/IceGrid/simple/Client.java
@@ -0,0 +1,99 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2005 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+import Demo.*;
+
+public class Client extends Ice.Application
+{
+    private void
+    menu()
+    {
+        System.out.println(
+            "usage:\n" +
+            "t: send greeting as twoway\n" +
+            "s: shutdown server\n" +
+            "x: exit\n" +
+            "?: help\n");
+    }
+
+    public int
+    run(String[] args)
+    {
+        Ice.Properties properties = communicator().getProperties();
+
+	properties.parseCommandLineOptions("", args);
+
+	Ice.ObjectPrx base = communicator().stringToProxy(properties.getPropertyWithDefault("Identity", "hello"));
+	HelloPrx twoway = HelloPrxHelper.checkedCast(base);
+	if(twoway == null)
+	{
+            System.err.println("invalid proxy");
+	    return 1;
+	}
+
+        menu();
+
+        java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+
+        String line = null;
+        do
+        {
+            try
+            {
+                System.out.print("==> ");
+                System.out.flush();
+                line = in.readLine();
+                if(line == null)
+                {
+                    break;
+                }
+                if(line.equals("t"))
+                {
+                    twoway.sayHello();
+                }
+                else if(line.equals("s"))
+                {
+                    twoway.shutdown();
+                }
+                else if(line.equals("x"))
+                {
+                    // Nothing to do
+                }
+                else if(line.equals("?"))
+                {
+                    menu();
+                }
+                else
+                {
+                    System.out.println("unknown command `" + line + "'");
+                    menu();
+                }
+            }
+            catch(java.io.IOException ex)
+            {
+                ex.printStackTrace();
+            }
+            catch(Ice.LocalException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+        while(!line.equals("x"));
+
+        return 0;
+    }
+
+    public static void
+    main(String[] args)
+    {
+	Client app = new Client();
+	int status = app.main("Client", args, "config");
+	System.exit(status);
+    }
+}
