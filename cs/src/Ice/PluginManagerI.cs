0193083e24fd011dbedcb3f98f5f084af42d3df1
Matthew Newhook <matthew@zeroc.com>
2005-10-13 04:42:39 +0000

    http://bugzilla.zeroc.com/bugzilla/show_bug.cgi?id=491

--- cs/src/Ice/PluginManagerI.cs
+++ cs/src/Ice/PluginManagerI.cs
@@ -96,6 +96,12 @@ namespace Ice
 	    //
 	    // Ice.Plugin.name=entry_point [args]
 	    //
+	    // The code below is different from the Java/C++ algorithm
+	    // because C# must support full assembly names such as:
+	    //
+	    // Ice.Plugin.Logger=logger, Version=0.0.0.0, Culture=neutral:LoginPluginFactory
+
+	    //
 	    string prefix = "Ice.Plugin.";
 	    Ice.Properties properties = _communicator.getProperties();
 	    PropertyDict plugins = properties.getPropertiesForPrefix(prefix);
@@ -105,83 +111,121 @@ namespace Ice
 		string val = (string)entry.Value;
 		
 		//
-		// Separate the entry point from the arguments.
+		// Separate the entry point from the arguments. First
+		// look for the :, then for the next whitespace. This
+		// represents the end of the entry point.
 		//
-		string className;
-		string[] args;
-		int pos = val.IndexOf(' ');
-		if(pos == -1)
+		// The remainder of the configuration line represents
+		// the arguments.
+		//
+		string entryPoint = val;
+		string[] args = new string[0];
+		int start = val.IndexOf(':');
+		if(start != -1)
 		{
-		    pos = val.IndexOf('\t');
-		}
-		if(pos == -1)
-		{
-		    pos = val.IndexOf('\n');
-		}
-		if(pos == -1)
-		{
-		    className = val;
-		    args = new string[0];
-		}
-		else
-		{
-		    className = val.Substring(0, pos);
-		    char[] delims = { ' ', '\t', '\n' };
-		    args = val.Substring(pos).Trim().Split(delims, pos);
+		    //
+		    // Find the whitespace.
+		    //
+		    int pos = val.IndexOf(' ', start);
+		    if(pos == -1)
+		    {
+			pos = val.IndexOf('\t', start);
+		    }
+		    if(pos == -1)
+		    {
+			pos = val.IndexOf('\n', start);
+		    }
+		    if(pos != -1)
+		    {
+			entryPoint = val.Substring(0, pos);
+			char[] delims = { ' ', '\t', '\n' };
+			args = val.Substring(pos).Trim().Split(delims, pos);
+		    }
 		}
 		
 		//
-		// Convert command-line options into properties. First we
-		// convert the options from the plug-in configuration, then
-		// we convert the options from the application command-line.
+		// Convert command-line options into properties. First
+		// we convert the options from the plug-in
+		// configuration, then we convert the options from the
+		// application command-line.
 		//
 		args = properties.parseCommandLineOptions(name, args);
 		cmdArgs = properties.parseCommandLineOptions(name, cmdArgs);
 		
-		loadPlugin(name, className, args);
+		loadPlugin(name, entryPoint, args);
 	    }
 	}
 	
-	private void loadPlugin(string name, string className, string[] args)
+	private void loadPlugin(string name, string entryPoint, string[] args)
 	{
 	    Debug.Assert(_communicator != null);
+	    
+	    //
+	    // Retrieve the assembly name and the type.
+	    //
+	    string err = "unable to load plugin '" + entryPoint + "': ";
+	    int sepPos = entryPoint.IndexOf(':');
+	    if (sepPos == -1)
+	    {
+		PluginInitializationException e = new PluginInitializationException();
+		e.reason = err + "invalid entry point format";
+		throw e;
+	    }
+	    
+	    System.Reflection.Assembly pluginAssembly = null;
+	    string assemblyName = entryPoint.Substring(0, sepPos);
+	    try
+	    {
+		if (System.IO.File.Exists(assemblyName))
+		{
+		    pluginAssembly = System.Reflection.Assembly.LoadFrom(assemblyName);
+		}
+		else
+		{
+		    pluginAssembly = System.Reflection.Assembly.Load(assemblyName);
+		}
+	    }
+	    catch(System.Exception ex)
+	    {
+		PluginInitializationException e = new PluginInitializationException();
+		e.reason = err + "unable to load assembly: '" + assemblyName + "': " + ex.ToString();
+		throw e;
+	    }
 	    
 	    //
 	    // Instantiate the class.
 	    //
 	    PluginFactory factory = null;
-            System.Type c = System.Type.GetType(className);
+	    string className = entryPoint.Substring(sepPos + 1);
+	    System.Type c = pluginAssembly.GetType(className);
             if(c == null)
             {
                 PluginInitializationException e = new PluginInitializationException();
-                e.reason = "cannot locate metadata for class " + className;
+                e.reason = err + "GetType failed for '" + className + "'";
                 throw e;
             }
+
 	    try
 	    {
 
-                System.Object obj = IceInternal.AssemblyUtil.createInstance(c);
-		try
-		{
-		    factory = (PluginFactory) obj;
-		}
-		catch(System.InvalidCastException ex)
-		{
-		    PluginInitializationException e = new PluginInitializationException(ex);
-		    e.reason = "class " + className + " does not implement Ice.PluginFactory";
-		    throw e;
-		}
+		factory = (PluginFactory)IceInternal.AssemblyUtil.createInstance(c);
+	    }
+	    catch(System.InvalidCastException ex)
+	    {
+		PluginInitializationException e = new PluginInitializationException(ex);
+		e.reason = err + "InvalidCastException to Ice.PluginFactory";
+		throw e;
 	    }
 	    catch(System.UnauthorizedAccessException ex)
 	    {
 		PluginInitializationException e = new PluginInitializationException(ex);
-		e.reason = "unable to access default constructor in class " + className;
+		e.reason = err + "UnauthorizedAccessException: " + ex.ToString();
 		throw e;
 	    }
 	    catch(System.Exception ex)
 	    {
 		PluginInitializationException e = new PluginInitializationException(ex);
-		e.reason = "unable to instantiate class " + className;
+		e.reason = err + "System.Exception: " + ex.ToString();
 		throw e;
 	    }
 	    
@@ -195,19 +239,20 @@ namespace Ice
 	    }
 	    catch(PluginInitializationException ex)
 	    {
+		ex.reason = err + ex.reason;
 		throw ex;
 	    }
 	    catch(System.Exception ex)
 	    {
 		PluginInitializationException e = new PluginInitializationException(ex);
-		e.reason = "exception in factory " + className;
+		e.reason = err + "System.Exception in factory.create: " + ex.ToString();
 		throw e;
 	    }
 	    
 	    if(plugin == null)
 	    {
 		PluginInitializationException ex = new PluginInitializationException();
-		ex.reason = "failure in factory " + className;
+		ex.reason = err + "factory.create returned null plug-in";
 		throw ex;
 	    }
 
