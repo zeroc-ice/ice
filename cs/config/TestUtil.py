78112992e35408520f690380fed9d278ff20ed03
Dwayne Boone <dwayne@zeroc.com>
2005-11-15 17:24:38 +0000

    Fix getAdapterReady()

--- cs/config/TestUtil.py
+++ cs/config/TestUtil.py
@@ -102,7 +102,7 @@ def getServerPid(serverPipe):
 def getAdapterReady(serverPipe):
 
     output = serverPipe.readline().strip()
-    if compress and output == "warning: bzip2 support not available, Ice.Override.Compress ignored\n":
+    if compress and output.strip() == "warning: bzip2 support not available, Ice.Override.Compress ignored":
         output = serverPipe.readline()
 
     if not output:
