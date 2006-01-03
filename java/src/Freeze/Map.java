774cde90f98743c58eda51a2c3ee3991706fb219
Bernard Normier <bernard@zeroc.com>
2006-01-03 18:24:51 +0000

    Fixed range search bug

--- java/src/Freeze/Map.java
+++ java/src/Freeze/Map.java
@@ -1429,7 +1429,7 @@ public abstract class Map extends java.util.AbstractMap
 			    if(_fromKey != null)
 			    {
 				dbIKey.setData(_fromKey);
-				status = c.getSearchKey(dbIKey, dbKey, dbValue, null);
+				status = c.getSearchKeyRange(dbIKey, dbKey, dbValue, null);
 			    }
 			    else
 			    {
