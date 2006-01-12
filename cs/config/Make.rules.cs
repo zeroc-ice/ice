10c53163f4976de3a5ef41d80cd449f20ffa1da8
Mark Spruiell <mes@zeroc.com>
2006-01-12 02:58:19 +0000

    Clean up, adding no_gac

--- cs/config/Make.rules.cs
+++ cs/config/Make.rules.cs
@@ -8,6 +8,14 @@
 # **********************************************************************
 
 #
+# If you are building an Ice for C# source distribution, define
+# src_build = yes. Disable src_build if you've installed a binary
+# distribution (e.g., an RPM).
+#
+
+src_build		= yes
+
+#
 # Set this to the root of the Ice for C++ source tree if you want to
 # build using the Slice definitions from the C++ source tree.
 # (You must set slice_home to a pathname that is relative to $(top_srcdir),
@@ -15,7 +23,7 @@
 # source tree around, the setting of slice_home does not matter.
 #
 
-slice_home=$(top_srcdir)/../ice
+slice_home		= $(top_srcdir)/../ice
 
 #
 # Select an installation base directory. The directory will be created
@@ -25,11 +33,21 @@ slice_home=$(top_srcdir)/../ice
 prefix			= /opt/icecs-$(VERSION)
 
 #
+# The default behavior of 'make install' attempts to add the Ice for C#
+# libraries to the Global Assembly Cache (GAC). If you would prefer not
+# to install these libraries to the GAC, or if you do not have sufficient
+# priveleges to do so, then enable no_gac and the libraries will be
+# copied to $(prefix)/bin instead.
+#
+
+#no_gac			= 1
+
+#
 # Define DEBUG as yes if you want to build with debug information and
 # assertions enabled.
 #
 
-DEBUG		= yes
+DEBUG			= yes
 
 #
 # If libbzip2 is not installed in a standard location where the compiler 
@@ -69,13 +87,6 @@ KERBEROS_HOME		?= /usr/kerberos
 
 #EXPAT_HOME		?= /opt/expat
 
-#
-# If you are building from a CVS source tree, define cvs_build = yes.
-# Otherwise, for an Ice for C# distribution, leave it commented out.
-#
-
-cvs_build=yes
-
 # ----------------------------------------------------------------------
 # Don't change anything below this line!
 # ----------------------------------------------------------------------
@@ -95,10 +106,14 @@ install_bindir		= $(prefix)/bin
 install_libdir		= $(prefix)/lib
 install_slicedir	= $(prefix)/slice
 
-ifdef cvs_build
+ifdef src_build
 ref = -r:$(bindir)/$(1).dll
 else
 ref = -pkg:$(1)
+endif
+
+ifdef no_gac
+NOGAC			= ${no_gac}
 endif
 
 INSTALL			= cp -fp
