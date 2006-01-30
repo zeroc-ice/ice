da13388290f99cbc064acc1ca531213e319f6544
Bernard Normier <bernard@zeroc.com>
2006-01-30 23:23:43 +0000

    Removed obsolete stuff

--- cpp/test/IceUtil/unicode/run.py
+++ cpp/test/IceUtil/unicode/run.py
@@ -26,28 +26,11 @@ testdir = os.path.join(toplevel, "test", name)
 client = os.path.join(testdir, "client")
 clientOptions = ' ' + testdir;
 
-print "creating random utf-8 data...",
-import random, string
-values = range(32, 500) + range(2000, 2500) + range(40000, 40100)
-random.shuffle(values)
-characters = string.join(map(unichr, values), u"")
-file = open("numeric.txt", "w")
-for w in values:
-    file.write(str(w) + "\n")
-file.close();
-file = open("utf8.txt", "wb")
-file.write(characters.encode("utf-8"))
-file.close();
-print "ok"
-
 print "starting client...",
 clientPipe = os.popen(client + clientOptions + " 2>&1")
 print "ok"
 
 TestUtil.printOutputFromPipe(clientPipe);
-    
-os.remove("numeric.txt")
-os.remove("utf8.txt")
 
 clientStatus = clientPipe.close()
 
