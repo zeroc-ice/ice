1717123066ebe0b4c9aa8ec19cf99adfe8f5cde4
Benoit Foucher <benoit@zeroc.com>
2006-02-14 15:01:52 +0000

    Removed workaround for http://bugzilla.ximian.com/show_bug.cgi?id=76496

--- cs/src/Ice/Network.cs
+++ cs/src/Ice/Network.cs
@@ -399,32 +399,22 @@ namespace IceInternal
 			closeSocketNoThrow(socket);
 
 			//
-			// BUGFIX: Mono 1.9.1 appears to be returning native error numbers, not the WSA
-			// variety. http://bugzilla.ximian.com/show_bug.cgi?id=76496
-			//
-			// Unfortunately the native code for ECONNREFUSED under MacOS is 61. Under Linux
-			// its 111 -- they don't match. Therefore we assume ConnectFailedException.
-			//
-			throw new Ice.ConnectFailedException("Connect failed");
-
-			//
 			// Create a Win32Exception out of this error value. Check the for refused, and
 			// failed. Otherwise its a plain old socket exception.
 			//
-			//Win32Exception sockEx = new Win32Exception(val);
-			//if(connectionRefused(sockEx))
-			//{
-			    //throw new Ice.ConnectionRefusedException("Connect refused", sockEx);
-			//}
-			//else if(connectFailed(sockEx))
-			//{
-			    //throw new Ice.ConnectFailedException("Connect failed", sockEx);
-			//}
-			//else
-			//{
-			    //throw new Ice.SocketException(sockEx);
-			//}
-			
+			Win32Exception sockEx = new Win32Exception(val);
+			if(connectionRefused(sockEx))
+			{
+			    throw new Ice.ConnectionRefusedException("Connect refused", sockEx);
+			}
+			else if(connectFailed(sockEx))
+			{
+			    throw new Ice.ConnectFailedException("Connect failed", sockEx);
+			}
+			else
+			{
+			    throw new Ice.SocketException(sockEx);
+			}			
 		    }
 		    
 		    //
