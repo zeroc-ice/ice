e534e714be519a8a4bea702529b7ee40a4cc91a0
Benoit Foucher <benoit@zeroc.com>
2006-03-15 12:55:02 +0000

    Fix

--- cpp/test/Ice/binding/AllTests.cpp
+++ cpp/test/Ice/binding/AllTests.cpp
@@ -164,9 +164,9 @@ allTests(const Ice::CommunicatorPtr& communicator)
 	    for(i = 0; i < nRetry &&  test->getAdapterName() == name; i++);
 	    test(i == nRetry);
 
-	    for(vector<RemoteObjectAdapterPrx>::const_iterator p = adapters.begin(); p != adapters.end(); ++p)
+	    for(vector<RemoteObjectAdapterPrx>::const_iterator q = adapters.begin(); q != adapters.end(); ++q)
 	    {
-		(*p)->getTestIntf()->ice_connection()->close(false);
+		(*q)->getTestIntf()->ice_connection()->close(false);
 	    }
 	}	    
 
