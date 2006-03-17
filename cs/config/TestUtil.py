383ea1fb02d29ce522e573a0315bf77aec2d5ef7
Dwayne Boone <dwayne@zeroc.com>
2006-03-17 14:33:37 +0000

    Bug 758

--- cs/config/TestUtil.py
+++ cs/config/TestUtil.py
@@ -29,8 +29,8 @@ compress = 1
 # thread per connection mode.
 #
 
-threadPerConnection = 0
-#threadPerConnection = 1
+#threadPerConnection = 0
+threadPerConnection = 1
 
 if protocol == "ssl":
     threadPerConnection = 1
