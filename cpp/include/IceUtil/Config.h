ff33ec17dfa848a3353d4b881942df2f5bf758ef
Benoit Foucher <benoit@zeroc.com>
2006-03-28 14:12:52 +0000

    Fix

--- cpp/include/IceUtil/Config.h
+++ cpp/include/IceUtil/Config.h
@@ -65,7 +65,7 @@
 #   define ICE_DECLSPEC_IMPORT /**/
 #endif
 
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && (_MSC_VER >= 1300)
 #   define ICE_DEPRECATED_API __declspec(deprecated)
 #elif defined(__GNUC__)
 #   define ICE_DEPRECATED_API __attribute__((deprecated))
