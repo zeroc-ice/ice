b50d1de068d16043f8da8caae51baddac1aa755d
Dwayne Boone <dwayne@zeroc.com>
2006-04-19 15:17:19 +0000

    Added method to destroy Glkacier2 session from backend

---
+++ cpp/test/Glacier2/sessionControl/SessionI.cpp
@@ -0,0 +1,44 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2006 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#include <Ice/Ice.h>
+#include <SessionI.h>
+
+using namespace std;
+using namespace Test;
+
+Glacier2::SessionPrx
+SessionManagerI::create(const string&, const Glacier2::SessionControlPrx& sessionControl, const Ice::Current& current)
+{
+    return Glacier2::SessionPrx::uncheckedCast(current.adapter->addWithUUID(new SessionI(sessionControl)));
+}
+
+void
+SessionManagerI::shutdown(const Ice::Current& current)
+{
+    current.adapter->getCommunicator()->shutdown();
+}
+
+SessionI::SessionI(const Glacier2::SessionControlPrx& sessionControl) :
+    _sessionControl(sessionControl)
+{
+    assert(sessionControl);
+}
+
+void
+SessionI::destroySession(const Ice::Current& current)
+{
+    _sessionControl->destroy();
+}
+
+void
+SessionI::destroy(const Ice::Current& current)
+{
+    current.adapter->remove(current.id);
+}
