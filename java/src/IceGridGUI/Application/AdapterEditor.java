12971b7779ccff6292c87d9e8d7c0b266081bdf5
Benoit Foucher <benoit@zeroc.com>
2006-04-25 18:12:47 +0000

    Added param for allocatable in ObjectDescriptor

--- java/src/IceGridGUI/Application/AdapterEditor.java
+++ java/src/IceGridGUI/Application/AdapterEditor.java
@@ -549,7 +549,7 @@ class AdapterEditor extends CommunicatorChildEditor
 	    Ice.Identity id = 
 		Ice.Util.stringToIdentity((String)entry.getKey());
 	    String type = (String)entry.getValue();
-	    result.add(new ObjectDescriptor(id, type));
+	    result.add(new ObjectDescriptor(id, type, false));
 	}
 	return result;
     }
