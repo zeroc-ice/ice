0a51329a22de1b70841684bcfe290c6ad56baa68
Dwayne Boone <dwayne@zeroc.com>
2006-05-03 13:41:37 +0000

    Fixed windows compile error

--- cpp/include/Ice/BasicStream.h
+++ cpp/include/Ice/BasicStream.h
@@ -476,7 +476,7 @@ public:
     void write(const std::string& v, bool convert = true)
     {
         Ice::Int sz = static_cast<Ice::Int>(v.size());
-        if(convert && sz > 0 && _stringConverter)
+        if(convert && sz > 0 && _stringConverter != 0)
 	{
 	    writeConverted(v);
 	}
@@ -502,7 +502,7 @@ public:
             {
                 throwUnmarshalOutOfBoundsException(__FILE__, __LINE__);
             }
-	    if(convert && _stringConverter)
+	    if(convert && _stringConverter != 0)
 	    {
 		_stringConverter->fromUTF8(i, i + sz, v);
 	    }
