a7724018c51b51563775577f84071498410a744c
Mark Spruiell <mes@zeroc.com>
2006-05-19 19:39:51 +0000

    adding createCertificate

--- java/ssl/jdk1.5/IceSSL/Util.java
+++ java/ssl/jdk1.5/IceSSL/Util.java
@@ -46,6 +46,54 @@ public final class Util
 	}
     }
 
+    //
+    // Create a certificate from a PEM-encoded string.
+    //
+    public static java.security.cert.X509Certificate
+    createCertificate(String certPEM)
+	throws java.security.cert.CertificateException
+    {
+	final String header = "-----BEGIN CERTIFICATE-----";
+	final String footer = "-----END CERTIFICATE-----";
+
+	//
+	// The generateCertificate method requires that its input begin
+	// with the PEM header.
+	//
+	int pos = certPEM.indexOf(header);
+	if(pos == -1)
+	{
+	    certPEM = header + "\n" + certPEM;
+	}
+	else if(pos > 0)
+	{
+	    certPEM = certPEM.substring(pos);
+	}
+
+	//
+	// Add the footer if necessary.
+	//
+	if(certPEM.indexOf(footer) == -1)
+	{
+	    certPEM = certPEM + footer;
+	}
+
+	byte[] bytes = null;
+	try
+	{
+	    bytes = certPEM.getBytes("UTF8");
+	}
+	catch(java.io.UnsupportedEncodingException ex)
+	{
+	    assert(false);
+	    return null;
+	}
+
+	java.io.ByteArrayInputStream in = new java.io.ByteArrayInputStream(bytes);
+	java.security.cert.CertificateFactory cf = java.security.cert.CertificateFactory.getInstance("X.509");
+	return (java.security.cert.X509Certificate)cf.generateCertificate(in);
+    }
+
     static ConnectionInfo
     populateConnectionInfo(javax.net.ssl.SSLSession session, java.net.Socket fd)
     {
