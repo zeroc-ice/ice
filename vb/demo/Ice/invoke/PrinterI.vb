e69f5644451043bfaef8aa1d66dae7343ebf9266
Mark Spruiell <mes@zeroc.com>
2006-06-28 05:56:03 +0000

    fixing cast bug

--- vb/demo/Ice/invoke/PrinterI.vb
+++ vb/demo/Ice/invoke/PrinterI.vb
@@ -63,7 +63,7 @@ Public Class PrinterI
             Return True
         ElseIf current.operation.Equals("printStruct") Then
             Dim s As [Structure] = New [Structure]
-            s.read__(inStream)
+            s.ice_read(inStream)
             inStream.destroy()
             Console.WriteLine("Printing struct: name=" & s.name & ", value=" & s.value)
             Return True
