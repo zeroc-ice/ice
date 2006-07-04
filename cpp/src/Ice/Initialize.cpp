54ad0632613b01c01d7418736725749f4053662b
Mark Spruiell <mes@zeroc.com>
2006-07-04 13:25:30 +0000

    reverting change in 1.42

--- cpp/src/Ice/Initialize.cpp
+++ cpp/src/Ice/Initialize.cpp
@@ -140,7 +140,7 @@ Ice::initialize(int& argc, char* argv[], const InitializationData& initializatio
 }
 
 CommunicatorPtr
-Ice::initialize(const InitializationData& initializationData, Int version)
+Ice::initialize(const InitializationData& initData, Int version)
 {
     //
     // We can't simply call the other initialize() because this one does NOT read
@@ -148,14 +148,10 @@ Ice::initialize(const InitializationData& initializationData, Int version)
     //
     checkIceVersion(version);
 
-    int argc = 0;
-    char* argv[] = { 0 };
-
-    InitializationData initData = initializationData;
-    initData.properties = createProperties(argc, argv, initData.properties, initData.stringConverter);
-
     CommunicatorI* communicatorI = new CommunicatorI(initData);
     CommunicatorPtr result = communicatorI; // For exception safety.
+    int argc = 0;
+    char* argv[] = { 0 };
     communicatorI->finishSetup(argc, argv);
     return result;
 }
