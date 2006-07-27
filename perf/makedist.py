ae2ebb8624ff61b4e5b25d674e40b9bcdf368b97
Brent Eagles <brent@zeroc.com>
2006-07-27 17:23:45 +0000

    adding a distribution creation script

---
+++ perf/makedist.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# **********************************************************************
+#
+# Copyright (c) 2003-2006 ZeroC, Inc. All rights reserved.
+#
+# This copy of Ice is licensed to you under the terms described in the
+# ICE_LICENSE file included in this distribution.
+#
+# **********************************************************************
+
+import os, sys, shutil, fnmatch, re, glob
+
+# TODO:
+#
+# - Find a way to include an IcePERF version with the sources and have
+# makedist.py extract that version number.
+#
+
+#
+# Program usage.
+#
+def usage():
+    print "Usage: " + sys.argv[0] + " [options] [tag]"
+    print
+    print "Options:"
+    print "-h    Show this message."
+    print "-v    Be verbose."
+    print
+    print "If no tag is specified, HEAD is used."
+
+#
+# Find files matching a pattern.
+#
+def find(path, patt):
+    result = [ ]
+    files = os.listdir(path)
+    for x in files:
+        fullpath = os.path.join(path, x);
+        if os.path.isdir(fullpath) and not os.path.islink(fullpath):
+            result.extend(find(fullpath, patt))
+        elif fnmatch.fnmatch(x, patt):
+            result.append(fullpath)
+    return result
+
+#
+# Comment out rules in a Makefile.
+#
+def fixMakefile(file, target):
+    origfile = file + ".orig"
+    os.rename(file, origfile)
+    oldMakefile = open(origfile, "r")
+    newMakefile = open(file, "w")
+    origLines = oldMakefile.readlines()
+
+    doComment = 0
+    doCheck = 0
+    newLines = []
+    for x in origLines:
+        #
+        # If the rule contains the target string, then
+        # comment out this rule.
+        #
+        if not x.startswith("\t") and x.find(target) != -1 and x.find(target + ".o") == -1:
+            doComment = 1
+        #
+        # If the line starts with "clean::", then check
+        # the following lines and comment out any that
+        # contain the target string.
+        #
+        elif x.startswith("clean::"):
+            doCheck = 1
+        #
+        # Stop when we encounter an empty line.
+        #
+        elif len(x.strip()) == 0:
+            doComment = 0
+            doCheck = 0
+
+        if doComment or (doCheck and x.find(target) != -1):
+            x = "#" + x
+        newLines.append(x)
+
+    newMakefile.writelines(newLines)
+    newMakefile.close()
+    oldMakefile.close()
+    os.remove(origfile)
+
+#
+# Remove lines containing a keyword from a file.
+#
+def editFile(file, target):
+    origfile = file + ".orig"
+    os.rename(file, origfile)
+    oldFile = open(origfile, "r")
+    newFile = open(file, "w")
+    origLines = oldFile.readlines()
+
+    newLines = []
+    for x in origLines:
+        if x.find(target) == -1:
+            newLines.append(x)
+
+    newFile.writelines(newLines)
+    newFile.close()
+    oldFile.close()
+    os.remove(origfile)
+
+#
+# Comment out implicit parser/scanner rules in config/Make.rules.
+#
+def fixMakeRules(file):
+    # 
+    # We currently don't have any requirements here.
+    #
+    pass
+
+#
+# Fix version in README, INSTALL files
+#
+def fixVersion(files, version):
+
+    for file in files:
+        origfile = file + ".orig"
+        os.rename(file, origfile)
+        oldFile = open(origfile, "r")
+        newFile = open(file, "w")
+        newFile.write(re.sub("@ver@", version, oldFile.read()))
+        newFile.close()
+        oldFile.close()
+        os.remove(origfile)
+
+#
+# main()
+#
+
+#
+# Check arguments
+#
+tag = "-rHEAD"
+verbose = 0
+for x in sys.argv[1:]:
+    if x == "-h":
+        usage()
+        sys.exit(0)
+    elif x == "-v":
+        verbose = 1
+    elif x.startswith("-"):
+        print sys.argv[0] + ": unknown option `" + x + "'"
+        print
+        usage()
+        sys.exit(1)
+    else:
+        tag = "-r" + x
+
+#
+# Remove any existing "dist" directory and create a new one.
+#
+distdir = "dist"
+if os.path.exists(distdir):
+    shutil.rmtree(distdir)
+os.mkdir(distdir)
+os.chdir(distdir)
+
+#
+# Export sources from CVS.
+#
+print "Checking out CVS tag " + tag + "..."
+if verbose:
+    quiet = ""
+else:
+    quiet = "-Q"
+os.system("cvs " + quiet + " -d cvs.zeroc.com:/home/cvsroot export " + tag + " iceperf")
+
+#
+# Remove files.
+#
+print "Removing unnecessary files..."
+filesToRemove = [ \
+    os.path.join("ice", "makedist.py"), \
+    ]
+filesToRemove.extend(find("iceperf", ".dummy"))
+for x in filesToRemove:
+    if os.path.exists(x):
+        os.remove(x)
+
+#
+# omniORB is not a part of the public performance tests.
+#
+shutil.rmtree(os.path.join("iceperf", "src", "omniORB"))
+
+
+#
+# Comment out the implicit parser and scanner rules in
+# config/Make.rules.
+#
+print "Fixing makefiles..."
+fixMakeRules(os.path.join("iceperf", "config", "Make.rules"))
+
+#
+# Get Ice version.
+#
+
+#
+# XXX - tie version onto something!
+#
+version="0.1"
+
+#
+# Create archives.
+#
+print "Creating distribution..."
+icever = "IcePERF-" + version
+os.rename("iceperf", icever)
+if verbose:
+    quiet = "v"
+else:
+    quiet = ""
+os.system("chmod -R u+rw,go+r-w . " + icever)
+os.system("find " + icever + " \\( -name \"*.h\" -or -name \"*.cpp\" -or -name \"*.ice\" \\) -exec chmod a-x {} \\;")
+os.system("find " + icever + " \\( -name \"README*\" -or -name \"INSTALL*\" \\) -exec chmod a-x {} \\;")
+os.system("find " + icever + " \\( -name \"Makefile\" -or -name \"*.dsp\" \\) -exec chmod a-x {} \\;")
+os.system("find " + icever + " -type d -exec chmod a+x {} \\;")
+os.system("find " + icever + " -perm +111 -exec chmod a+x {} \\;")
+os.system("tar c" + quiet + "f " + icever + ".tar " + icever)
+os.system("gzip -9 " + icever + ".tar")
+if verbose:
+    quiet = ""
+else:
+    quiet = "q"
+os.system("zip -9r" + quiet + " " + icever + ".zip " + icever)
+
+#
+# Copy files (README, etc.).
+#
+# shutil.copyfile(os.path.join(icever, "CHANGES"), "IcePERF-" + version + "-CHANGES")
+
+#
+# Done.
+#
+print "Cleaning up..."
+shutil.rmtree(icever)
+print "Done."
