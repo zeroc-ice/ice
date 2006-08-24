1db34489ac15b74b45fc8367ecead89fdf0ca03a
Mark Spruiell <mes@zeroc.com>
2006-08-24 20:01:52 +0000

    adding debug

--- cpp/test/Glacier2/router/run.py
+++ cpp/test/Glacier2/router/run.py
@@ -36,6 +36,8 @@ command = router + TestUtil.clientServerOptions + \
           r' --Glacier2.CryptPasswords="' + toplevel + r'/test/Glacier2/router/passwords"'
 
 print "starting router...",
+if TestUtil.debug:
+    print "(" + command + ")",
 starterPipe = os.popen(command + " 2>&1")
 TestUtil.getServerPid(starterPipe)
 #
