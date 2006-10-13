3ea4712c07990cf0435da196ce7a796758c6645c
Brent Eagles <brent@zeroc.com>
2006-10-13 13:13:15 +0000

    Add strong naming and assembly information.

---
+++ cs/src/IceSSL/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("icesslcs")]
+[assembly: AssemblyDescription("IceSSL run-time support")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ZeroC, Inc")]
+[assembly: AssemblyProduct("IceSSL for C#")]
+[assembly: AssemblyCopyright("Copyright (c) 2003-2006, ZeroC, Inc")]
+[assembly: AssemblyTrademark("Ice")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7bce56e0-9332-4002-8e71-ad59227d916a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
