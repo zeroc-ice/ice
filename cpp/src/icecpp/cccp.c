516a814eb09bfee142d5dc31bbeb1dbe433d9db5
Brent Eagles <brent@zeroc.com>
2006-11-07 16:42:49 +0000

    Updating FSF address

--- cpp/src/icecpp/cccp.c
+++ cpp/src/icecpp/cccp.c
@@ -13,10 +13,10 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA. */
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, 51
+Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 #include "config.h"
 #include <sys/types.h>
--- cpp/src/icecpp/gansidecl.h
+++ cpp/src/icecpp/gansidecl.h
@@ -13,10 +13,10 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  */
+You should have received a copy of the GNU General Public License along
+with GNU CC; see the file COPYING.  If not, write to the Free Software
+Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 /* This file mimics some of the support provided by include/ansidecl.h
    in binutils and gdb releases.
