e5ae0f16283c293efb0ac5e8176d7dc0aa779342
Dwayne Boone <dwayne@zeroc.com>
2007-01-02 16:04:59 +0000

    Bug 1629 - Logger synchronization

--- java/src/Ice/LoggerI.java
+++ java/src/Ice/LoggerI.java
@@ -19,6 +19,7 @@ public final class LoggerI extends LocalObjectImpl implements Logger
 	    _prefix = prefix + ": ";
 	}
 
+    	_lineSeparator = System.getProperties().getProperty("line.separator");
 	_date = java.text.DateFormat.getDateInstance(java.text.DateFormat.SHORT);
 	_time = new java.text.SimpleDateFormat(" HH:mm:ss:SSS");
     }
@@ -26,70 +27,58 @@ public final class LoggerI extends LocalObjectImpl implements Logger
     public void
     print(String message)
     {
-	synchronized(_globalMutex)
-	{
-	    System.err.println(message);
-	}
+	System.err.print(message + _lineSeparator);
     }
 
     public void
     trace(String category, String message)
     {
-	synchronized(_globalMutex)
-	{
-            StringBuffer s = new StringBuffer("[ ");
-	    s.append(_date.format(new java.util.Date()));
-	    s.append(_time.format(new java.util.Date()));
-	    s.append(' ');
-	    s.append(_prefix);
-	    s.append(category);
-	    s.append(": ");
-	    s.append(message);
-	    s.append(" ]");
-            int idx = 0;
-            while((idx = s.indexOf("\n", idx)) != -1)
-            {
-                s.insert(idx + 1, "  ");
-                ++idx;
-            }
-            System.err.println(s.toString());
-	}
+        StringBuffer s = new StringBuffer("[ ");
+	s.append(_date.format(new java.util.Date()));
+	s.append(_time.format(new java.util.Date()));
+	s.append(' ');
+	s.append(_prefix);
+	s.append(category);
+	s.append(": ");
+	s.append(message);
+	s.append(" ]");
+        int idx = 0;
+        while((idx = s.indexOf("\n", idx)) != -1)
+        {
+            s.insert(idx + 1, "  ");
+            ++idx;
+        }
+        System.err.print(s.toString() + _lineSeparator);
     }
 
     public void
     warning(String message)
     {
-	synchronized(_globalMutex)
-	{
-	    StringBuffer s = new StringBuffer();
-	    s.append(_date.format(new java.util.Date()));
-	    s.append(_time.format(new java.util.Date()));
-	    s.append(' ');
-	    s.append(_prefix);
-	    s.append("warning: ");
-	    s.append(message);
-	    System.err.println(s.toString());
-	}
+	StringBuffer s = new StringBuffer();
+	s.append(_date.format(new java.util.Date()));
+	s.append(_time.format(new java.util.Date()));
+	s.append(' ');
+	s.append(_prefix);
+	s.append("warning: ");
+	s.append(message);
+	System.err.print(s.toString() + _lineSeparator);
     }
 
     public void
     error(String message)
     {
-	synchronized(_globalMutex)
-	{
-	    StringBuffer s = new StringBuffer();
-	    s.append(_date.format(new java.util.Date()));
-	    s.append(_time.format(new java.util.Date()));
-	    s.append(' ');
-	    s.append(_prefix);
-	    s.append("error: ");
-	    s.append(message);
-	    System.err.println(s.toString());
-	}
+	StringBuffer s = new StringBuffer();
+	s.append(_date.format(new java.util.Date()));
+	s.append(_time.format(new java.util.Date()));
+	s.append(' ');
+	s.append(_prefix);
+	s.append("error: ");
+	s.append(message);
+	System.err.print(s.toString() + _lineSeparator);
     }
 
     String _prefix = "";
-    static java.lang.Object _globalMutex = new java.lang.Object();
-    java.text.DateFormat _date = null;
-    java.text.SimpleDateFormat _time = null;
+    String _lineSeparator;
+    java.text.DateFormat _date;
+    java.text.SimpleDateFormat _time;
 }
