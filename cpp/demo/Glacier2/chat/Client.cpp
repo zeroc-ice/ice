7bb0489661e02b2638b35ed14de6b7459f0043b7
Bernard Normier <bernard@zeroc.com>
2007-01-16 16:37:02 +0000

    Fixed build failures

--- cpp/demo/Glacier2/chat/Client.cpp
+++ cpp/demo/Glacier2/chat/Client.cpp
@@ -8,6 +8,7 @@
 // **********************************************************************
 
 #include <Ice/Application.h>
+#include <IceUtil/IceUtil.h>
 #include <Glacier2/Router.h>
 #include <Chat.h>
 
