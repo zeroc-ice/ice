1341e9981af82d148cc99b20827c85a2cdcfbe48
Dwayne Boone <dwayne@zeroc.com>
2007-01-26 16:34:14 +0000

    Synced with other assemby files

--- cs/src/IceSSL/AssemblyInfo.cs
+++ cs/src/IceSSL/AssemblyInfo.cs
@@ -1,33 +1,25 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+// **********************************************************************
+//
+// Copyright (c) 2003-2007 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: CLSCompliant(true)]
+
 [assembly: AssemblyTitle("icesslcs")]
 [assembly: AssemblyDescription("IceSSL run-time support")]
+[assembly: AssemblyCompany("ZeroC, Inc.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ZeroC, Inc")]
 [assembly: AssemblyProduct("IceSSL for C#")]
-[assembly: AssemblyCopyright("Copyright (c) 2003-2007, ZeroC, Inc")]
+[assembly: AssemblyCopyright("Copyright (c) 2003-2007, ZeroC, Inc.")]
 [assembly: AssemblyTrademark("Ice")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7bce56e0-9332-4002-8e71-ad59227d916a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
+[assembly: AssemblyCulture("")]		
 [assembly: AssemblyVersion("3.2.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDelaySign(false)]
