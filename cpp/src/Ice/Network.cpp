5249960fc757312e7eb9b935ed08c2b107940db9
Benoit Foucher <benoit@zeroc.com>
2007-02-13 10:20:18 +0000

    Bug 1739

--- cpp/src/Ice/Network.cpp
+++ cpp/src/Ice/Network.cpp
@@ -307,7 +307,12 @@ IceInternal::shutdownSocketReadWrite(SOCKET fd)
         //
 #if defined(_WIN32)
         int error = WSAGetLastError();
-        if(error == WSAENOTCONN)
+	//
+	// Under Vista its possible to get a WSAECONNRESET. See
+	// http://bugzilla.zeroc.com/bugzilla/show_bug.cgi?id=1739 for
+	// some details.
+	//
+        if(error == WSAENOTCONN || error == WSAECONNRESET)
         {
             return;
         }
