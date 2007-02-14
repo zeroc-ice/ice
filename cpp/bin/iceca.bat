4f72eba5c94c1f0062d62e2927f4f93a07ca12c2
Bernard Normier <bernard@zeroc.com>
2007-02-14 20:12:32 +0000

    .bat file for iceca python script

---
+++ cpp/bin/iceca.bat
@@ -0,0 +1,13 @@
+@echo off
+REM **********************************************************************
+REM
+REM Copyright (c) 2003-2007 ZeroC, Inc. All rights reserved.
+REM
+REM This copy of Ice is licensed to you under the terms described in the
+REM ICE_LICENSE file included in this distribution.
+REM
+REM **********************************************************************
+
+REM .bat wrapper for iceca python script. Assumes python is in your PATH.
+
+@python %~dp0iceca  %*
