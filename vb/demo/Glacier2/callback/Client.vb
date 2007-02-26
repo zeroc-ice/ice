9a8da9506b74dd149557f94b0bdb401d34e47acf
Dwayne Boone <dwayne@zeroc.com>
2007-02-26 13:42:00 +0000

    Bug 1938

--- vb/demo/Glacier2/callback/Client.vb
+++ vb/demo/Glacier2/callback/Client.vb
@@ -97,7 +97,7 @@ Module Glacier2callbackC
                     Console.Out.Flush()
                     line = Console.In.ReadLine()
                     If line Is Nothing Then
-                        Exit Try
+                        Exit Do
                     End If
                     If line.Equals("t") Then
                         Dim context As Ice.Context = New Ice.Context
