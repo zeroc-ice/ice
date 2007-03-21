a962b9afd980164a68104c5f422b0c9c51886f4c
Michi Henning <michi@zeroc.com>
2007-03-21 04:06:14 +0000

    Added a bunch of missing #include directives.

--- cpp/include/IceUtil/IceUtil.h
+++ cpp/include/IceUtil/IceUtil.h
@@ -11,29 +11,41 @@
 #define ICE_UTIL_ICE_UTIL_H
 
 //
-// This file must include *all* other headers of IceUtil.
+// This file must include *all* other headers of IceUtil, except
+// for DisableWarnings.h.
 //
 
-#include <IceUtil/ArgVector.h>
-#include <IceUtil/Functional.h>
-#include <IceUtil/Shared.h>
-#include <IceUtil/Unicode.h>
-#include <IceUtil/UUID.h>
+#include <IceUtil/Config.h>
 #include <IceUtil/AbstractMutex.h>
-#include <IceUtil/Mutex.h>
-#include <IceUtil/RecMutex.h>
-#include <IceUtil/RWRecMutex.h>
-#include <IceUtil/StaticMutex.h>
-#include <IceUtil/Monitor.h>
-#include <IceUtil/Thread.h>
-#include <IceUtil/CountDownLatch.h>
+#include <IceUtil/Algorithm.h>
+#include <IceUtil/ArgVector.h>
 #include <IceUtil/Base64.h>
-#include <IceUtil/Time.h>
-#include <IceUtil/StringUtil.h>
-#include <IceUtil/InputUtil.h>
-#include <IceUtil/OutputUtil.h>
-#include <IceUtil/Options.h>
-#include <IceUtil/Random.h>
+#include <IceUtil/Cache.h>
+#include <IceUtil/Cond.h>
+#include <IceUtil/CountDownLatch.h>
 #include <IceUtil/CtrlCHandler.h>
+#include <IceUtil/Exception.h>
+#include <IceUtil/Functional.h>
+#include <IceUtil/Handle.h>
+#include <IceUtil/InputUtil.h>
+#include <IceUtil/Iterator.h>
+#include <IceUtil/Lock.h>
+#include <IceUtil/MD5.h>
+#include <IceUtil/Monitor.h>
+#include <IceUtil/Mutex.h>
+#include <IceUtil/Options.h>
+#include <IceUtil/OutputUtil.h>
+#include <IceUtil/RWRecMutex.h>
+#include <IceUtil/Random.h>
+#include <IceUtil/RecMutex.h>
+#include <IceUtil/ScopedArray.h>
+#include <IceUtil/Shared.h>
+#include <IceUtil/StaticMutex.h>
+#include <IceUtil/StringUtil.h>
+#include <IceUtil/Thread.h>
+#include <IceUtil/ThreadException.h>
+#include <IceUtil/Time.h>
+#include <IceUtil/UUID.h>
+#include <IceUtil/Unicode.h>
 
 #endif
