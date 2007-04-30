9abc54c676c6434fcabc053924acb276034952f3
Bernard Normier <bernard@zeroc.com>
2007-04-30 18:29:55 +0000

    Fixe GCC 4.1.1 build failure

--- cppe/include/IceE/ProxyF.h
+++ cppe/include/IceE/ProxyF.h
@@ -11,38 +11,7 @@
 #ifndef ICEE_PROXY_F_H
 #define ICEE_PROXY_F_H
 
-#include <IceE/Shared.h>
-#include <IceE/Config.h>
 #include <IceE/ProxyHandle.h>
 
-namespace IceProxy
-{
-
-namespace Ice
-{
-
-class Object;
-
-}
-
-}
-
-namespace IceInternal
-{
-
-inline ::IceProxy::Ice::Object* 
-upCast(::IceProxy::Ice::Object* o)
-{
-    return o;
-}
-
-}
-
-namespace Ice
-{
-
-typedef IceInternal::ProxyHandle< ::IceProxy::Ice::Object> ObjectPrx;
-
-}
 
 #endif
