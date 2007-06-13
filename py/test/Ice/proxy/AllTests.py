ccf44c0a55d9c5a0028628c2ea71eae070c4c33f
Michi Henning <michi@zeroc.com>
2007-06-13 15:19:24 +1000

    Bug 2225, proxy tests don't run with --protocol ssl.

--- py/test/Ice/proxy/AllTests.py
+++ py/test/Ice/proxy/AllTests.py
@@ -522,7 +522,9 @@ def allTests(communicator, collocated):
     test(pstr == "test -t:tcp -h 127.0.0.1 -p 12010 -t 10000");
     
     # Working?
-    p1.ice_ping();
+    ssl = communicator.getProperties().getProperty("Ice.Default.Protocol") == "ssl";
+    if not ssl:
+        p1.ice_ping();
 
     # Two legal TCP endpoints expressed as opaque endpoints
     p1 = communicator.stringToProxy("test:opaque -t 1 -v CTEyNy4wLjAuMeouAAAQJwAAAA==:opaque -t 1 -v CTEyNy4wLjAuMusuAAAQJwAAAA==");
@@ -535,17 +537,40 @@ def allTests(communicator, collocated):
     #
     p1 = communicator.stringToProxy("test:opaque -t 2 -v CTEyNy4wLjAuMREnAAD/////AA==:opaque -t 99 -v abch");
     pstr = communicator.proxyToString(p1);
-    test(pstr == "test -t:opaque -t 2 -v CTEyNy4wLjAuMREnAAD/////AA==:opaque -t 99 -v abch");
+    if not ssl:
+        test(pstr == "test -t:opaque -t 2 -v CTEyNy4wLjAuMREnAAD/////AA==:opaque -t 99 -v abch");
+    else:
+        test(pstr == "test -t:ssl -h 127.0.0.1 -p 10001:opaque -t 99 -v abch");
 
     #
     # Try to invoke on the SSL endpoint to verify that we get a
-    # NoEndpointException.
+    # NoEndpointException (or ConnectionRefusedException when
+    # running with SSL).
     #
     try:
         p1.ice_ping();
         test(false);
     except Ice.NoEndpointException:
-        pass
+        test(not ssl);
+    except Ice.ConnectionRefusedException:
+        test(ssl);
+
+    #
+    # Test that the proxy with an SSL endpoint and a nonsense
+    # endpoint (which the server doesn't understand either) can be
+    # sent over the wire and returned by the server without losing
+    # the opaque endpoints.
+    #
+    p2 = derived.echo(p1);
+    pstr = communicator.proxyToString(p2);
+    if not ssl:
+        test(pstr == "test -t:opaque -t 2 -v CTEyNy4wLjAuMREnAAD/////AA==:opaque -t 99 -v abch");
+    else:
+        test(pstr == "test -t:ssl -h 127.0.0.1 -p 10001:opaque -t 99 -v abch");
+
+    print "ok"
+
+    return cl
 
     #
     # Test that the proxy with an SSL endpoint and a nonsense
