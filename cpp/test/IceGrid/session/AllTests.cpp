73b9fef5be77b2c99e736e1f5de852b3dcae1a31
Benoit Foucher <benoit@zeroc.com>
2007-09-25 11:35:42 +0200

    Fixed VC8 compilation error

--- cpp/test/IceGrid/session/AllTests.cpp
+++ cpp/test/IceGrid/session/AllTests.cpp
@@ -619,7 +619,7 @@ allTests(const Ice::CommunicatorPtr& communicator)
             adminRouter1->ice_ping();
             break;
         }
-        catch(const Ice::LocalException& ex)
+        catch(const Ice::LocalException&)
         {
             IceUtil::ThreadControl::sleep(IceUtil::Time::milliSeconds(100));
         }
