0afc8682df16c5698810d55b7d6a853ea8829339
Brent Eagles <brent@zeroc.com>
2007-11-08 21:55:17 -0330

    Fix a problem when building in a git repository or source distribution with ICE_HOME set to root/cpp.

--- cs/config/Make.rules.cs
+++ cs/config/Make.rules.cs
@@ -13,6 +13,8 @@
 #
 USE_ICE_RPM ?= 0
 
+USE_SRC_DIST = 0
+
 #
 # Checks for ICE_HOME environment variable.  If it isn't present,
 # attempt to find an Ice installation in /usr or the default install
@@ -112,7 +114,11 @@ libdir			= $(top_srcdir)/lib
 ifneq ($(USE_ICE_RPM),0)
     slicedir		= /usr/share/Ice-$(VERSION)/slice
 else
-    slicedir		= $(ICE_DIR)/slice
+    ifeq ($(shell test -d $(ICE_DIR)/slice && echo 0),0)
+        slicedir		= $(ICE_DIR)/slice
+    else
+        slicedir		= $(ICE_DIR)/../slice
+    endif
 endif
 
 
