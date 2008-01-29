d93d7b26c4fd882c9ae7126d20d6e67ba28ec3e1
Dwayne Boone <dwayne@zeroc.com>
2008-01-29 10:44:04 -0330

    Fixed couplr of Ice-E compile problems

--- cppe/include/IceE/Iterator.h
+++ cppe/include/IceE/Iterator.h
@@ -12,22 +12,24 @@
 
 #include <iterator>
 
+namespace IceUtilInternal
+{
 
-// Work-around for a limitation in the standard library provided 
-// with the Sun C++ 5.x compilers
-#if defined(__SUNPRO_CC) && defined(_RWSTD_NO_CLASS_PARTIAL_SPEC)
 
 template<class ForwardIterator>
 inline typename ForwardIterator::difference_type
-ice_distance(ForwardIterator first, ForwardIterator last)
+distance(ForwardIterator first, ForwardIterator last)
 {
+// Work-around for a limitation in the standard library provided 
+// with the Sun C++ 5.x compilers
+#if defined(__SUNPRO_CC) && defined(_RWSTD_NO_CLASS_PARTIAL_SPEC)
     ForwardIterator::difference_type result = 0;
     std::distance(first, last, result);
     return result;
+#else
+    return ::std::distance(first, last);
+#endif
 }
 
-#else
-#define ice_distance(x,y) std::distance(x,y)
-#endif
-
+}
 #endif
