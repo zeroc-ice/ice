b9b3e3f2b10ac504607cfdc90e9309f27ad2511c
Dwayne Boone <dwayne@zeroc.com>
2008-03-26 08:42:16 -0230

    Bug 2929 - Fixed incorrect check for ipv6

--- config/TestUtil.py
+++ config/TestUtil.py
@@ -875,9 +875,11 @@ def runTests(configs, tests, num = 0):
             #
             # Skip tests not supported with IPv6 if necessary
             #
-            if args.find("--ipv6"):
+
+            print args
+            if args.find("ipv6") != -1:
+                match = False
                 for j in ipv6Skip:
-                    match = False
                     if i.endswith(j):
                         match = True
                         break
