c8dbc5fe3ee8f8f329ba3f8b9f433a83b0acefa9
Mark Spruiell <mes@zeroc.com>
2008-04-29 16:04:13 -0700

    Minor edits to these files:   CHANGES   cpp/INSTALL.WINDOWS   cpp/include/Ice/LoggerUtil.h

--- cpp/include/Ice/LoggerUtil.h
+++ cpp/include/Ice/LoggerUtil.h
@@ -150,8 +150,8 @@ template<class Y>
 ICE_API Trace& operator<<(Trace&, std::ios_base& (*)(std::ios_base&));
 
 //
-// A special plugin that sets logger during construction. Both initialize and
-// destroy are no-op. See Ice::InitializationData.
+// A special plugin that installs a logger during a communicator's initialization.
+// Both initialize and destroy are no-op. See Ice::InitializationData.
 //
 
 class ICE_API LoggerPlugin : public Ice::Plugin
