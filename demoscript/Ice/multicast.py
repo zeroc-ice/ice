002df23d2c1052b30110e9db01f268f8c756ff87
Matthew Newhook <matthew@zeroc.com>
2008-05-13 17:02:21 +0800

    remove bogus change to multicast.py and removed reference to 1.4.2 from RELEASE_NOTES.

--- demoscript/Ice/multicast.py
+++ demoscript/Ice/multicast.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python:
+#!/usr/bin/env python
 # **********************************************************************
 #
 # Copyright (c) 2003-2008 ZeroC, Inc. All rights reserved.
