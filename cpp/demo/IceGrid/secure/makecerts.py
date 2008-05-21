423958609ec81617f7094d10b7091bee63742921
Matthew Newhook <matthew@zeroc.com>
2008-05-21 10:33:08 +0800

    http://bugzilla/bugzilla/show_bug.cgi?id=3198 - drop use of ICE_HOME in makecerts.py.

--- cpp/demo/IceGrid/secure/makecerts.py
+++ cpp/demo/IceGrid/secure/makecerts.py
@@ -11,13 +11,7 @@
 import os, sys, shutil, glob
 
 def iceca(args):
-
-    if(os.environ.has_key("ICE_HOME")):
-        cmd = os.path.join(os.environ["ICE_HOME"], "bin", "iceca") + " " + args
-    else:
-        cmd = "iceca " + args
-
-    if os.system(cmd):
+    if os.system("iceca " + args):
         sys.exit(1)
 
 def createCertificate(filename, cn):
