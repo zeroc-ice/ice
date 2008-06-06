2f0ea2ab15b5a50f1c2d666a657469a6241c3f4b
Benoit Foucher <benoit@zeroc.com>
2008-06-06 13:16:55 +0200

    - Fixed bug 3242
    - Cleaned up makesldist.py and makedist.py (moved common code
      to distribution/lib/DistUtils.py)
    - Added -c option to makedist.py and makesldist.py to allow
      compartion distribution with previous one.
    
    Conflicts:
    
    	makesldist.py

--- distribution/lib/DistUtils.py
+++ distribution/lib/DistUtils.py
@@ -8,7 +8,7 @@
 #
 # **********************************************************************
 
-import os, sys, shutil, glob, fnmatch, string, re
+import os, sys, shutil, glob, fnmatch, string, re, fileinput, time
 from stat import *
 
 #
@@ -88,6 +88,360 @@ proguard = { \
 #
 # Some utility methods
 # 
+
+#
+# Remove file or directory, warn if it doesn't exist.
+#
+def remove(path, recurse = True):
+
+    if not os.path.exists(path):
+        print "warning: " + path + " doesn't exist"
+        return
+
+    if os.path.isdir(path):
+        if recurse:
+            shutil.rmtree(path)
+        else:
+            try:
+                os.rmdir(path)
+            except:
+                pass
+    else:
+        os.remove(path)
+
+#
+# Copy src to dest
+# 
+def copy(src, dest, warnDestExists = True):
+
+    if not os.path.exists(src):
+        print "warning: can't copy `" + src + "': file doesn't exist"
+        return
+
+    if not os.path.isdir(src) and os.path.isdir(dest):
+        dest = os.path.join(dest, os.path.basename(src))
+
+    if os.path.exists(dest):
+        if warnDestExists:
+            print "warning: overwritting " + dest
+        remove(dest)
+
+    if os.path.dirname(dest) and not os.path.exists(os.path.dirname(dest)):
+        os.makedirs(os.path.dirname(dest))
+
+    if os.path.isdir(src):
+        shutil.copytree(src, dest, True)
+    elif os.path.islink(src):
+        os.symlink(os.readlink(src), dest)
+    else:
+        shutil.copy(src, dest)
+
+    fixPermission(dest)
+
+#
+# Move src to dest
+#
+def move(src, dest, warnDestExists = True):
+
+    if not os.path.exists(src):
+        print "warning: can't move `" + src + "': file doesn't exist"
+        return
+
+    if os.path.exists(dest):
+        if warnDestExists:
+            print "warning: overwritting " + dest
+        shutil.rmtree(dest)
+
+    if os.path.dirname(dest) and not os.path.exists(os.path.dirname(dest)):
+        os.makedirs(os.path.dirname(dest))
+
+    shutil.move(src, dest)
+    fixPermission(dest)
+
+#
+# Copy files from srcpath and matching the given patterns to destpath
+#
+def copyMatchingFiles(srcpath, destpath, patterns):
+    for p in patterns:
+        for f in glob.glob(os.path.join(srcpath, p)):
+            copy(f, os.path.join(destpath, os.path.basename(f)))
+
+#
+# Get the language mapping directory for a given suffix.
+#
+def getMappingDir(suffix, mapping):
+    if mapping == "cpp":
+        return suffix
+    elif mapping == "java":
+        return suffix + "j"
+    else:
+        return suffix + mapping
+
+#
+# Comment out rules in a Makefile.
+#
+def fixMakefile(file, base, ext):
+
+    origfile = file + ".orig"
+    os.rename(file, origfile)
+    oldMakefile = open(origfile, "r")
+    newMakefile = open(file, "w")
+    origLines = oldMakefile.readlines()
+
+    doComment = 0
+    doCheck = 0
+    newLines = []
+    for x in origLines:
+        #
+        # If the rule contains the target string, then
+        # comment out this rule.
+        #
+        if not x.startswith("\t") and x.find(base + ext) != -1:
+            doComment = 1
+        #
+        # If the line starts with "clean::", then check
+        # the following lines and comment out any that
+        # contain the target string.
+        #
+        elif x.startswith("clean::"):
+            doCheck = 1
+        #
+        # Stop when we encounter an empty line.
+        #
+        elif len(x.strip()) == 0:
+            doComment = 0
+            doCheck = 0
+
+        if doComment or (doCheck and x.find(base) != -1):
+            x = "#" + x
+        newLines.append(x)
+
+    newMakefile.writelines(newLines)
+    newMakefile.close()
+    oldMakefile.close()
+    os.remove(origfile)
+
+#
+# Comment out rules in VC project.
+#
+def fixProject(file, target):
+    origfile = file + ".orig"
+    os.rename(file, origfile)
+    oldProject = open(origfile, "r")
+    newProject = open(file, "w")
+    origLines = oldProject.readlines()
+
+    #
+    # Find a Source File declaration containing SOURCE=<target>
+    # and comment out the entire declaration.
+    #
+    expr = re.compile("SOURCE=.*" + target.replace(".", "\\.") + ".*")
+    inSource = 0
+    doComment = 0
+    newLines = []
+    source = []
+    for x in origLines:
+        if x.startswith("# Begin Source File"):
+            inSource = 1
+
+        if inSource:
+            if not doComment and expr.match(x) != None:
+                doComment = 1
+            source.append(x)
+        else:
+            newLines.append(x)
+
+        if x.startswith("# End Source File"):
+            inSource = 0
+            for s in source:
+                if doComment:
+                    newLines.append('#xxx#' + s)
+                else:
+                    newLines.append(s)
+            doComment = 0
+            source = []
+
+    newProject.writelines(newLines)
+    newProject.close()
+    oldProject.close()
+    os.remove(origfile)
+
+#
+# Comment out implicit parser/scanner rules in config/Make.rules.
+#
+def fixMakeRules(file):
+    origfile = file + ".orig"
+    os.rename(file, origfile)
+    oldFile = open(origfile, "r")
+    newFile = open(file, "w")
+    origLines = oldFile.readlines()
+
+    doComment = 0
+    newLines = []
+    for x in origLines:
+        if x.find("%.y") != -1 or x.find("%.l") != -1:
+            doComment = 1
+        #
+        # Stop when we encounter an empty line.
+        #
+        elif len(x.strip()) == 0:
+            doComment = 0
+
+        if doComment:
+            x = "#" + x
+        newLines.append(x)
+
+    newFile.writelines(newLines)
+    newFile.close()
+    oldFile.close()
+    os.remove(origfile)
+
+#
+# Fix version in given file.
+#
+def fixVersion(file, version, mmversion = None, libversion = None):
+
+    origfile = file + ".orig"
+    os.rename(file, origfile)
+    oldFile = open(origfile, "r")
+    newFile = open(file, "w")
+    line = oldFile.read();
+    line = re.sub("@ver@", version, line)
+    if mmversion:
+        line = re.sub("@mmver@", mmversion, line)
+    if libversion:
+        line = re.sub("@libver@", libversion, line)
+    newFile.write(line)
+    newFile.close()
+    oldFile.close()
+
+    # Preserve the executable permission
+    st = os.stat(origfile)
+    if st.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH): 
+        os.chmod(file, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) # rwxr-xr-x
+    os.remove(origfile)
+
+#
+# Generate bison files and comment out the Makefile rule
+#
+def generateBisonFile(x, verbose = False):
+
+    #
+    # Change to the directory containing the file.
+    #
+    (dir,file) = os.path.split(x)
+    cwd = os.getcwd()
+    os.chdir(dir)
+    (base,ext) = os.path.splitext(file)
+
+    #
+    # Run gmake to create the output files.
+    #
+    if verbose:
+        quiet = ""
+    else:
+        quiet = "-s"
+    if file == "cexp.y":
+        os.system("gmake " + quiet + " cexp.c")
+    else:
+        os.system("gmake " + quiet + " " + base + ".cpp")
+
+    #
+    # Edit the Makefile to comment out the grammar rules.
+    #
+    fixMakefile("Makefile", base, ext)
+    fixMakefile("Makefile.mak", base, ext)
+
+    #
+    # Edit the project file(s) to comment out the grammar rules.
+    #
+    for p in glob.glob("*.dsp"):
+        fixProject(p, file)
+    os.chdir(cwd)
+
+#
+# Generate flex files and comment out the Makefile rule
+#
+def generateFlexFile(x, verbose = False):
+
+    #
+    # Change to the directory containing the file.
+    #
+    (dir,file) = os.path.split(x)
+    cwd = os.getcwd()
+    os.chdir(dir)
+    (base,ext) = os.path.splitext(file)
+
+    #
+    # Run gmake to create the output files.
+    #
+    if verbose:
+        quiet = ""
+    else:
+        quiet = "-s"
+    os.system("gmake " + quiet + " " + base + ".cpp")
+
+    #
+    # Edit the Makefile to comment out the flex rules.
+    #
+    fixMakefile("Makefile", base, ext)
+    fixMakefile("Makefile.mak", base, ext)
+
+    #
+    # Edit the project file(s) to comment out the flex rules.
+    #
+    for p in glob.glob("*.dsp"):
+        fixProject(p, file)
+
+    os.chdir(cwd)
+
+def regexpEscape(expr):
+    escaped = ""
+    for c in expr:
+        # TODO: escape more characters?
+        if c in ".\\/":
+            escaped += "\\" + c
+        else:
+            escaped += c
+    return escaped            
+
+def substitute(file, regexps):
+    for line in fileinput.input(file, True):
+        for (expr, text) in regexps:
+	    if not expr is re:
+		expr = re.compile(expr)
+            line = expr.sub(text, line)
+        print line,
+
+def fixFilePermission(file, verbose = False):
+
+    patterns = [ \
+        "*.h", \
+        "*.cpp", \
+        "*.ice", \
+        "README*", \
+        "INSTALL*", \
+        "*.xml", \
+        "*.mc", \
+        "Makefile", \
+        "Makefile.mak", \
+        "*.dsp", \
+        ]
+
+    st = os.stat(file)
+
+    if st.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH):
+        for p in patterns:
+            if fnmatch.fnmatch(file, p):
+                if verbose:
+                    print "removing exec permissions on: " + file
+                break
+        else:
+            os.chmod(file, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) # rwxr-xr-x
+            return
+
+    os.chmod(file, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) # rw-r--r--
+
 def fixPermission(dest):
 
     if os.path.isdir(dest):
@@ -95,34 +449,156 @@ def fixPermission(dest):
         for f in os.listdir(dest):
             fixPermission(os.path.join(dest, f))
     else:
-        if os.stat(dest).st_mode & (S_IXUSR | S_IXGRP | S_IXOTH):
-            os.chmod(dest, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) # rwxr-xr-x
-        else:
-            os.chmod(dest, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) # rw-r--r--
+        fixFilePermission(dest)
 
-def copy(src, dest):
 
-    if not os.path.exists(src):
-        print "warning: can't copy `" + src + "': file doesn't exist"
-        return
+def tarArchive(dir, verbose = False, archiveDir = None):
 
-    # Create the directories if necessary.
-    if not os.path.exists(os.path.dirname(dest)):
-        os.makedirs(os.path.dirname(dest))
-        
-    # Copy the directory, link or file.
-    if os.path.isdir(src):
-        shutil.copytree(src, dest)
-    elif os.path.islink(src):
-        if os.path.exists(dest):
-            os.remove(dest)
-        os.symlink(os.readlink(src), dest)
+    dist = os.path.basename(dir)
+    print "   creating " + dist + ".tar.gz ...",
+    sys.stdout.flush()
+
+    cwd = os.getcwd()
+    os.chdir(os.path.dirname(dir))
+
+    if verbose:
+        quiet = "v"
     else:
-        if os.path.exists(dest):
-            os.remove(dest)
-        shutil.copy(src, dest)
+        quiet = ""
 
-    fixPermission(dest)
+    if archiveDir:
+        os.mkdir("tmp")
+        os.rename(dist, os.path.join("tmp", archiveDir))
+        os.chdir("tmp")
+        os.system("tar c" + quiet + "f - " + archiveDir + " | gzip -9 - > " + os.path.join("..", dist) + ".tar.gz")
+        os.chdir("..")
+        os.rename(os.path.join("tmp", archiveDir), dir)
+        os.rmdir("tmp")
+    else:
+        os.system("tar c" + quiet + "f - " + dist + " | gzip -9 - > " + dist + ".tar.gz")
+
+    os.chdir(cwd)
+    print "ok"
+
+def untarArchive(archive, verbose = False, archiveDir = None):
+
+    if not os.path.exists(archive):
+        print "couldn't find " + archive
+        return False
+
+    if verbose:
+        quiet = "v"
+    else:
+        quiet = ""
+
+    if archiveDir:
+        os.mkdir("tmp")
+        os.chdir("tmp")
+        os.system("gunzip -c " + os.path.join("..", archive) + " | tar x" + quiet + "f -")
+        os.rename(os.listdir(".")[0], os.path.join("..", archiveDir))
+        os.chdir("..")
+        os.rmdir("tmp")
+    else:
+        os.system("gunzip -c " + archive + " | tar x" + quiet + "f -")
+
+    return True
+
+def zipArchive(dir, verbose = False):
+
+    dist = os.path.basename(dir)
+    print "   creating " + dist + ".zip ...",
+    sys.stdout.flush()
+
+    cwd = os.getcwd()
+    os.chdir(os.path.dirname(dir))
+
+    if verbose:
+        os.system("zip -9r " + dist + ".zip " + dist)
+    else:
+        os.system("zip -9rq " + dist +".zip " + dist)
+
+    os.chdir(cwd)
+    print "ok"
+
+def compareDirs(orig, new):
+
+    added = [ ]
+    updated = [ ]
+    removed = [ ]
+    cwd = os.getcwd()
+
+    for root, dirnames, filenames in os.walk(orig):
+        for f in filenames:
+            filepath = os.path.join(root[len(orig) + 1:], f)
+            if not os.path.exists(os.path.join(new, filepath)):
+                removed.append(os.path.join(new, filepath))
+            else:
+                pipe = os.popen("diff -q " + os.path.join(orig, filepath) + " " + os.path.join(new, filepath))
+                if len(pipe.readlines()) > 0:
+                    updated.append(os.path.join(new, filepath))
+                pipe.close()
+
+    for root, dirnames, filenames in os.walk(new):
+        for f in filenames:
+            filepath = os.path.join(root[len(new) + 1:], f)
+            if not os.path.exists(os.path.join(orig, filepath)):
+                added.append(os.path.join(new, filepath))
+
+    return (added, updated, removed)
+
+def writeSrcDistReport(product, version, compareToDir, distributions):
+
+    cwd = os.getcwd()
+    os.chdir(cwd)
+
+    print "Writing report in README...",
+    readme = open("README", "w")
+    print >>readme, "This directory contains the source distributions of " + product + " " + version + ".\n"
+    print >>readme, "Creation time: " + time.strftime("%a %b %d %Y, %I:%M:%S %p (%Z)")
+    (sysname, nodename, release, ver, machine) = os.uname();
+    print >>readme, "Host: " + nodename
+    print >>readme, "Platform: " + sysname + " " + release
+    if os.path.exists("/etc/redhat-release"):
+        f = open("/etc/redhat-release")
+        print >>readme, "Linux distribution: " + f.readline().strip()
+        f.close()
+    else:
+        print >>readme, "Not created on a Linux distribution"
+    print >>readme, "User: " + os.environ["USER"]
+    print >>readme, ""
+
+    if compareToDir:
+        print
+        print >>readme, "Comparison with", compareToDir
+        modifications = ([], [], [])
+        for dist in distributions:
+            dist = os.path.basename(dist)
+            print "   comparing " + dist + " ...",
+            sys.stdout.flush()
+            if untarArchive(os.path.join(compareToDir, dist) + ".tar.gz", False, dist + "-orig"):
+                n = compareDirs(dist + "-orig", dist)
+                modifications = [ modifications[i] + n[i]  for i in range(len(modifications))]
+                if n != ([], [], []):
+                    os.system("diff -r -N " + dist + "-orig " + dist + " > patch-" + dist)
+                remove(dist + "-orig")
+            print "ok"
+
+        (added, updated, removed) = modifications
+        for (desc, list) in [("Added", added), ("Removed", removed), ("Updated", updated)]:
+            if len(list) > 0:
+                list.sort()
+                print >>readme
+                print >>readme, desc, "files:"
+                print >>readme, string.join(["=" for c in range(len(desc + " files:"))], '')
+                for f in list:
+                    print >>readme, f
+
+    else:
+        print "ok"
+        
+    readme.close()
+
+    os.chdir(cwd)
 
 #
 # Third-party helper base class
@@ -215,7 +691,7 @@ class ThirdParty :
             for src in files:
                 copy(src, os.path.join(buildDir, src[len(self.location) + 1::]))
             print "ok"
-
+    
 #
 # Platform helper classes
 #
