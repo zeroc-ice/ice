5e6ed83d0799af93fccd7447ace4c80bb68fe946
Benoit Foucher <benoit@zeroc.com>
2008-12-12 18:32:51 +0100

    Use AMI for MFC demo, fixed bogus self-connect on Linux

--- cpp/demo/Ice/MFC/client/Resource.h
+++ cpp/demo/Ice/MFC/client/Resource.h
@@ -1,5 +1,5 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by HelloClient.rc
 //
 #define IDD_HELLOCLIENT_DIALOG          101
@@ -13,6 +13,9 @@
 #define IDC_SHUTDOWN                    1006
 #define IDC_STATUSBAR                   1007
 #define IDC_DELAY                       1008
+#define IDC_HOST                        1009
+#define IDC_HOSTNAME                    1010
+#define IDC_HOSTLABEL                   1010
 
 // Next default values for new objects
 // 
@@ -20,7 +23,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
