afd79f8bcef1dfadbb9aeb8d5d4b6667fcae8c14
Matthew Newhook <matthew@zeroc.com>
2009-03-11 17:34:49 -0230

    removed copyright header for subprocess.py./

--- scripts/subprocess.py
+++ scripts/subprocess.py
@@ -1,12 +1,3 @@
-# **********************************************************************
-#
-# Copyright (c) 2003-2009 ZeroC, Inc. All rights reserved.
-#
-# This copy of Ice is licensed to you under the terms described in the
-# ICE_LICENSE file included in this distribution.
-#
-# **********************************************************************
-
 r"""subprocess - Subprocesses with accessible I/O streams
 
 This module allows you to spawn processes, connect to their
