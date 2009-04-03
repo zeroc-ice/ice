91d0fd30a8e4e6e8ac8ad72ff3b1422f6f09748d
Dwayne Boone <dwayne@zeroc.com>
2009-04-03 14:36:47 -0230

    Fixed compile error

--- java/test/Util/Application.java
+++ java/test/Util/Application.java
@@ -89,7 +89,7 @@ public abstract class Application
         //
         if(Util.getProcessLogger() instanceof LoggerI)
         {
-            Util.setProcessLogger(new LoggerI(initData.properties.getProperty("Ice.ProgramName")));
+            Util.setProcessLogger(new LoggerI(initData.properties.getProperty("Ice.ProgramName"), ""));
         }
 
         int status = 0;
