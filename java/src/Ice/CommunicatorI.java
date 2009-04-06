e2b56ab0b2bfe9c963f08d3e2b42cc92f54f68ed
Mark Spruiell <mes@zeroc.com>
2009-04-06 07:27:29 -0700

    restoring getDefaultContext/setDefaultContext in Java

--- java/src/Ice/CommunicatorI.java
+++ java/src/Ice/CommunicatorI.java
@@ -137,6 +137,24 @@ public final class CommunicatorI implements Communicator
         _instance.setDefaultLocator(locator);
     }
 
+    /**
+     * @deprecated
+     **/
+    public java.util.Map<String, String>
+    getDefaultContext()
+    {
+        return _instance.getDefaultContext();
+    }
+
+    /**
+     * @deprecated
+     **/
+    public void
+    setDefaultContext(java.util.Map<String, String> ctx)
+    {
+        _instance.setDefaultContext(ctx);
+    }
+
     public ImplicitContext
     getImplicitContext()
     {
