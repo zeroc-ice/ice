14b25ce02b15cc031d0b620f41c7ff50571e3e4d
Benoit Foucher <benoit@zeroc.com>
2010-06-30 12:11:18 +0200

    Fixed bug 4795 - None is now recognized as a keyword

--- cpp/src/Slice/PythonUtil.cpp
+++ cpp/src/Slice/PythonUtil.cpp
@@ -183,7 +183,7 @@ lookupKwd(const string& name)
     //
     static const string keywordList[] =
     {
-        "and", "assert", "break", "class", "continue", "def", "del", "elif", "else", "except", "exec",
+        "None", "and", "assert", "break", "class", "continue", "def", "del", "elif", "else", "except", "exec",
         "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "not", "or", "pass",
         "print", "raise", "return", "try", "while", "yield"
     };
