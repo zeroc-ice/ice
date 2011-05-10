6bde970f13850ff180155133a542ac027b57444c
Mark Spruiell <mes@zeroc.com>
2011-05-10 14:42:55 -0700

    bug 5090 - add .NET CF demo

---
+++ cs/demo/Ice/compact/MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace client
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.btnHello = new System.Windows.Forms.Button();
+            this.btnShutdown = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.hostname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnHello
+            // 
+            this.btnHello.Location = new System.Drawing.Point(14, 79);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(212, 20);
+            this.btnHello.TabIndex = 0;
+            this.btnHello.Text = "Say Hello";
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // btnShutdown
+            // 
+            this.btnShutdown.Location = new System.Drawing.Point(14, 109);
+            this.btnShutdown.Name = "btnShutdown";
+            this.btnShutdown.Size = new System.Drawing.Size(212, 20);
+            this.btnShutdown.TabIndex = 1;
+            this.btnShutdown.Text = "Shutdown server";
+            this.btnShutdown.Click += new System.EventHandler(this.btnShutdown_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(14, 139);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(212, 40);
+            // 
+            // lblHost
+            // 
+            this.lblHost.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold);
+            this.lblHost.Location = new System.Drawing.Point(14, 18);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(212, 20);
+            this.lblHost.Text = "Hostname:";
+            // 
+            // hostname
+            // 
+            this.hostname.Location = new System.Drawing.Point(14, 48);
+            this.hostname.Name = "hostname";
+            this.hostname.Size = new System.Drawing.Size(212, 21);
+            this.hostname.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 294);
+            this.Controls.Add(this.hostname);
+            this.Controls.Add(this.lblHost);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnShutdown);
+            this.Controls.Add(this.btnHello);
+            this.Name = "MainForm";
+            this.Text = "Ice Hello Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.Button btnShutdown;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.TextBox hostname;
+    }
+}
+
---
+++ cs/demo/Ice/compact/README
@@ -0,0 +1,63 @@
+This demo shows how to write a simple C# client application using Ice
+for .NET Compact Framework (CF).
+
+This demo requires a hello server. You can use the C# server located
+in ..\hello, or you can use a server from any other language mapping.
+
+You must use Visual Studio 2008 to build this client (Visual Studio
+2010 does not support Compact Framework development). Follow these
+instructions to build and run the client:
+
+1) Open the project in Visual Studio 2008. This is a Smart Device
+   project and therefore Visual Studio will compile it against the
+   Compact Framework libraries. Furthermore, the Ice Visual Studio
+   Add-in detects a Smart Device project and automatically
+   configures the project to compile against the Ice for .NET CF
+   DLL in <Ice installation directory>\bin\cf\Ice.dll.
+
+2) Build the client by right-clicking on the project and choosing
+   "Build".
+
+3) Start a device emulator. Select Tools -> Device Emulator Manager,
+   highlight "USA Windows Mobile 5.0 Pocket PC R2 Emulator", and
+   select Actions -> Connect.
+
+4) In the emulator window, select File -> Configure, open the Network
+   tab, and check "Enable NE2000 PCMCIA network adapter". Open the
+   drop-down menu and select the appropriate network adapter, then
+   press OK.
+
+   If you get an error dialog that mentions Virtual PC 2007, you will
+   need to install this product to enable network access for the
+   emulator. You can download Virtual PC 2007 at the link below:
+
+   http://www.microsoft.com/downloads/en/details.aspx?FamilyID=04d26402-3199-48a3-afa2-2dc0b40a73b6
+
+   Close the emulator, install Virtual PC 2007, and restart the
+   emulator.
+
+5) After successfully enabling the network adapter, you may need to
+   activate the network connection. Click on the symbol resembling an
+   antenna in the device's status bar and choose Connect to start the
+   connection. If you wish, start Internet Explorer and verify that
+   you can successfully access the Internet.
+
+6) In Visual Studio, right-click on the project, choose Deploy, select
+   the "USA Windows Mobile 5.0 Pocket PC R2 Emulator" device, and
+   click Deploy. This action causes Visual Studio to install the .NET
+   CF run time, the client executable, and the Ice for .NET CF run
+   time in the emulator.
+
+7) In a command window, start the hello server.
+
+8) In the emulator, choose Start -> Programs and select File Explorer.
+   Click on My Documents and choose My Device. Select Program Files,
+   then select "client".
+
+9) Finally, select "client" to start the program. In the Hostname
+   field, enter the IP address or host name of the machine on which
+   the hello server is running. Click the Say Hello button to send a
+   request to the server.
+
+   Note that the initial request takes a little longer to complete
+   while the connection is being established.
