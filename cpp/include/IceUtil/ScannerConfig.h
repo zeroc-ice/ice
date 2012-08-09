9786742d68d702425a65f06f80ff272c337fa6db
Jose <jose@zeroc.com>
2012-08-09 20:11:45 +0200

    Add ScannerConfig.h

---
+++ cpp/include/IceUtil/ScannerConfig.h
@@ -0,0 +1,23 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2012 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#pragma once
+
+//
+// COMPILERFIX: VC compilers does not provide stdint.h header until VC100
+// the header must be included before that macros for integral types
+// in flex generated Scanners are defined.
+//
+// in C99 conformant compilers we don't need to include it because the
+// header is included by inttypes.h, that is included by the gernated 
+// Scanners.
+//
+#if defined(_MSC_VER) && (_MSC_VER >= 1600)
+#   include <stdint.h>
+#endif
