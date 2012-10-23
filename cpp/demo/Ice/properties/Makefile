4ad319537fd99343089b51349116a4139cc80116
Bernard Normier <bernard@zeroc.com>
2012-10-23 19:16:27 +0000

    Updated copyright in various files

--- cpp/demo/Ice/properties/Makefile
+++ cpp/demo/Ice/properties/Makefile
@@ -1,6 +1,6 @@
 # **********************************************************************
 #
-# Copyright (c) 2003-2009 ZeroC, Inc. All rights reserved.
+# Copyright (c) 2003-2012 ZeroC, Inc. All rights reserved.
 #
 # This copy of Ice is licensed to you under the terms described in the
 # ICE_LICENSE file included in this distribution.
--- cs/src/PolicyServer/PolicyServer.cs
+++ cs/src/PolicyServer/PolicyServer.cs
@@ -1,6 +1,6 @@
 // **********************************************************************
 //
-// Copyright (c) 2003-2008 ZeroC, Inc. All rights reserved.
+// Copyright (c) 2003-2012 ZeroC, Inc. All rights reserved.
 //
 // This copy of Ice for Silverlight  is licensed to you under the terms
 // described in the ICESL_LICENSE file included in this distribution.
