b941cf7f00a4c37aaece47cf0133f0216d9d74f4
Bernard Normier <bernard@zeroc.com>
2014-06-17 14:09:14 +0000

    Test properties escapes in Python

--- py/test/Ice/properties/Client.py
+++ py/test/Ice/properties/Client.py
@@ -44,10 +44,41 @@ print("ok")
 
 sys.stdout.write("testing using Ice.Config with multiple config files... ")
 sys.stdout.flush()
-properties = Ice.createProperties(["--Ice.Config=config/config.1, config/config.2, config/config.3"]);
-test(properties.getProperty("Config1") == "Config1");
-test(properties.getProperty("Config2") == "Config2");
-test(properties.getProperty("Config3") == "Config3");
+properties = Ice.createProperties(["--Ice.Config=config/config.1, config/config.2, config/config.3"])
+test(properties.getProperty("Config1") == "Config1")
+test(properties.getProperty("Config2") == "Config2")
+test(properties.getProperty("Config3") == "Config3")
+print("ok")
+
+sys.stdout.write("testing configuration file escapes... ")
+sys.stdout.flush()
+properties = Ice.createProperties(["--Ice.Config=config/escapes.cfg"])
+
+props = { "Foo\tBar":"3",
+          "Foo\\tBar":"4",
+          "Escape\\ Space":"2",
+          "Prop1":"1",
+          "Prop2":"2",
+          "Prop3":"3",
+          "My Prop1":"1",
+          "My Prop2":"2",
+          "My.Prop1":"a property",
+          "My.Prop2":"a     property",
+          "My.Prop3":"  a     property  ",
+          "My.Prop4":"  a     property  ",
+          "My.Prop5":"a \\ property",
+          "foo=bar":"1",
+          "foo#bar":"2",
+          "foo bar":"3",
+          "A":"1",
+          "B":"2 3 4",
+          "C":"5=#6",
+          "AServer":"\\\\server\\dir",
+          "BServer":"\\server\\dir"};
+
+for k in props.keys():
+    test(properties.getProperty(k) == props[k])
+
 print("ok")
 
 sys.exit(0)
