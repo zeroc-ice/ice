1380a03134e077c28181568ca9d0ac5ee58a17d6
Jose <jose@zeroc.com>
2014-09-03 18:49:15 +0200

    IceSSL configuration test build problem in OS X

--- cpp/test/IceSSL/configuration/Util.h
+++ cpp/test/IceSSL/configuration/Util.h
@@ -3,8 +3,24 @@
 
 #ifdef ICE_USE_SECURE_TRANSPORT
 
-#include <IceUtil/FileUtil.h>
 #include <Security/Security.h>
+
+namespace Test
+{
+
+int
+getcwd(std::string& cwd)
+{
+    char cwdbuf[PATH_MAX];
+    if(::getcwd(cwdbuf, PATH_MAX) == NULL)
+    {
+        return -1;
+    }
+    cwd = cwdbuf;
+    return 0;
+}
+
+}
 
 void
 removeKeychain(const std::string& keychainPath, const std::string& password)
@@ -13,10 +29,10 @@ removeKeychain(const std::string& keychainPath, const std::string& password)
     // KeyChain path is relative to the current working directory.
     //
     std::string path = keychainPath;
-    if(!IceUtilInternal::isAbsolutePath(keychainPath))
+    if(path.find("/") != 0)
     {
         std::string cwd;
-        if(IceUtilInternal::getcwd(cwd) == 0)
+        if(Test::getcwd(cwd) == 0)
         {
             path = std::string(cwd) + '/' + path;
         }
