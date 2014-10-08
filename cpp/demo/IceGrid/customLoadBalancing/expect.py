448010e0dca8602105a149f707b019b86429de4e
Dwayne Boone <dwayne@zeroc.com>
2014-10-08 15:19:12 -0230

    Added expect script for IceGRid/customLoadBalac=ncing demo

--- cpp/demo/IceGrid/customLoadBalancing/expect.py
+++ cpp/demo/IceGrid/customLoadBalancing/expect.py
@@ -20,6 +20,69 @@ if len(path) == 0:
 sys.path.append(path[0])
 
 from demoscript import Util
-from demoscript.IceGrid import simple
 
-simple.run('./client')
+sys.stdout.write("cleaning databases... ")
+sys.stdout.flush()
+nodeDir = os.path.join("db", "node")
+if not os.path.exists(nodeDir):
+    os.mkdir(nodeDir)
+else:
+    Util.cleanDbDir(nodeDir)
+regDir = os.path.join("db", "registry")
+if not os.path.exists(regDir):
+    os.mkdir(regDir)
+else:
+    Util.cleanDbDir(regDir)
+print("ok")
+
+if Util.defaultHost:
+    args = ' --IceGrid.Node.PropertiesOverride="Ice.Default.Host=127.0.0.1"'
+else:
+    args = ''
+
+sys.stdout.write("starting icegridnode... ")
+sys.stdout.flush()
+node = Util.spawn(Util.getIceGridNode() + ' --Ice.Config=config.grid --Ice.PrintAdapterReady %s' % (args))
+node.expect('IceGrid.Registry.Internal ready')
+node.expect('IceGrid.Registry.Server ready')
+node.expect('IceGrid.Registry.Client ready')
+node.expect('IceGrid.Node ready')
+print("ok")
+
+sys.stdout.write("deploying application... ")
+sys.stdout.flush()
+admin = Util.spawn(Util.getIceGridAdmin() + ' --Ice.Config=config.grid')
+admin.expect('>>>')
+admin.sendline("application add \'application.xml\'")
+admin.expect('>>>')
+print("ok")
+
+sys.stdout.write("testing preferred currency EUR... ")
+sys.stdout.flush()
+client = Util.spawn('./client --Ice.Default.Locator.Context.currency=EUR')
+client.expect('Preferred currency configured for the client: EUR')
+client.expect('Preferred currencies of the server: .*EUR.*')
+client.waitTestSuccess(timeout=1)
+print("ok")
+
+sys.stdout.write("testing preferred currency GBP... ")
+sys.stdout.flush()
+client = Util.spawn('./client --Ice.Default.Locator.Context.currency=GBP')
+client.expect('Preferred currency configured for the client: GBP')
+client.expect('Preferred currencies of the server: .*GBP.*')
+client.waitTestSuccess(timeout=1)
+print("ok")
+
+sys.stdout.write("testing preferred currency JPY... ")
+sys.stdout.flush()
+client = Util.spawn('./client --Ice.Default.Locator.Context.currency=JPY')
+client.expect('Preferred currency configured for the client: JPY')
+client.expect('Preferred currencies of the server: .*JPY.*')
+client.waitTestSuccess(timeout=1)
+print("ok")
+
+admin.sendline('registry shutdown Master')
+admin.sendline('exit')
+admin.waitTestSuccess()
+node.waitTestSuccess()
+
