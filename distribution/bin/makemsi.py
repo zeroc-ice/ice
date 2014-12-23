f106d2060c1e09943d1d0acddc49154e310e27db
Jose <jose@zeroc.com>
2014-12-23 18:01:16 +0100

    Update PHP version for Windows installer

--- distribution/bin/makemsi.py
+++ distribution/bin/makemsi.py
@@ -211,7 +211,7 @@ def usage():
     print(r"                              is C:\proguard")
     print("")
     print(r"  --php-home=<path>           PHP source location, default location")
-    print(r"                              is C:\php-5.6.3")
+    print(r"                              is C:\php-5.6.4")
     print("")
     print(r"  --php-bin-home=<path>       PHP binaries location, default location")
     print(r"                              is C:\Program Files (x86)\PHP")
@@ -687,10 +687,10 @@ if not skipBuild:
 
                     if lang == "php":
                         if phpHome is None:
-                            if not os.path.exists(r"C:\php-5.6.3"):
+                            if not os.path.exists(r"C:\php-5.6.4"):
                                 print("PHP source distribution not found")
                                 sys.exit(1)
-                            phpHome = r"C:\php-5.6.3"
+                            phpHome = r"C:\php-5.6.4"
 
                         if phpBinHome is None:
                             if not os.path.exists(r"C:\Program Files (x86)\PHP"):
