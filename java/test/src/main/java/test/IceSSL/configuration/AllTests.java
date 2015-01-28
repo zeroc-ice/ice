b7afc385e270343453490bc659efa4833fb0bfdc
Jose <jose@zeroc.com>
2015-01-28 23:30:12 +0100

    SSLv3 is disabled by default in latest JDK updates

--- java/test/src/main/java/test/IceSSL/configuration/AllTests.java
+++ java/test/src/main/java/test/IceSSL/configuration/AllTests.java
@@ -771,34 +771,38 @@ public class AllTests
             comm.destroy();
 
             //
-            // SSLv3 is disabled by default in the IBM JDK.
+            // SSLv3 has been disabled by default in latests JDK updates.
             //
-            if(System.getProperty("java.vendor").toLowerCase().indexOf("ibm") == -1)
-            {
-                //
-                // This should succeed.
-                //
-                comm = Ice.Util.initialize(args, initData);
-                fact = ServerFactoryPrxHelper.checkedCast(comm.stringToProxy(factoryRef));
-                test(fact != null);
-                d = createServerProps(defaultProperties, defaultDir, defaultHost);
-                d.put("IceSSL.Keystore", "s_rsa_ca1.jks");
-                d.put("IceSSL.Password", "password");
-                d.put("IceSSL.Truststore", "cacert1.jks");
-                d.put("IceSSL.VerifyPeer", "2");
-                d.put("IceSSL.Protocols", "tls1, ssl3");
-                server = fact.createServer(d);
-                try
-                {
-                    server.ice_ping();
-                }
-                catch(Ice.LocalException ex)
-                {
-                    test(false);
-                }
-                fact.destroyServer(server);
-                comm.destroy();
-            }
+//             //
+//             // SSLv3 is disabled by default in the IBM JDK.
+//             //
+//             if(System.getProperty("java.vendor").toLowerCase().indexOf("ibm") == -1)
+//             {
+//                 //
+//                 // This should succeed.
+//                 //
+//                 comm = Ice.Util.initialize(args, initData);
+//                 fact = ServerFactoryPrxHelper.checkedCast(comm.stringToProxy(factoryRef));
+//                 test(fact != null);
+//                 d = createServerProps(defaultProperties, defaultDir, defaultHost);
+//                 d.put("IceSSL.Keystore", "s_rsa_ca1.jks");
+//                 d.put("IceSSL.Password", "password");
+//                 d.put("IceSSL.Truststore", "cacert1.jks");
+//                 d.put("IceSSL.VerifyPeer", "2");
+//                 d.put("IceSSL.Protocols", "tls1, ssl3");
+//                 server = fact.createServer(d);
+//                 try
+//                 {
+//                     server.ice_ping();
+//                 }
+//                 catch(Ice.LocalException ex)
+//                 {
+//                     ex.printStackTrace();
+//                     test(false);
+//                 }
+//                 fact.destroyServer(server);
+//                 comm.destroy();
+//             }
         }
 
         {
