7dacf7bd2e4ddfc27e95a98d78b318c9f8b4b818
Benoit Foucher <benoit@zeroc.com>
2015-02-09 11:18:20 +0100

    Removed bogus tracing from Ice/admin Java test

--- java/test/src/main/java/test/Ice/admin/Server.java
+++ java/test/src/main/java/test/Ice/admin/Server.java
@@ -27,7 +27,6 @@ public class Server extends test.Util.Application
         Ice.InitializationData initData = createInitializationData();
         initData.properties = Ice.Util.createProperties(argsH);
         initData.properties.setProperty("Ice.Package.Test", "test.Ice.admin");
-        initData.properties.setProperty("Ice.Trace.Protocol", "1");
         initData.properties.setProperty("TestAdapter.Endpoints", "default -p 12010:udp");
         initData.properties.setProperty("Ice.Warn.Dispatch", "0");
         return initData;
