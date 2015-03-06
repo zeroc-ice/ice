eb7cba7c4a5f05754ca6d1acb006482cee38de40
Joe George <joe@zeroc.com>
2015-03-06 15:55:00 +0000

    Patch 5 - Fix IceGrid registry issue with shared repicla groups

--- cpp/src/IceGrid/AdapterCache.h
+++ cpp/src/IceGrid/AdapterCache.h
@@ -62,7 +62,7 @@ protected:
 
     AdapterCache& _cache;
     const std::string _id;
-    const std::string _application;
+    std::string _application;
 };
 typedef IceUtil::Handle<AdapterEntry> AdapterEntryPtr;
 
@@ -105,9 +105,9 @@ public:
     virtual AdapterPrx getProxy(const std::string&, bool) const { return 0; }
 
     void addReplica(const std::string&, const ServerAdapterEntryPtr&);
-    void removeReplica(const std::string&);
+    bool removeReplica(const std::string&);
 
-    void update(const LoadBalancingPolicyPtr&);
+    void update(const std::string&, const LoadBalancingPolicyPtr&);
     bool hasAdaptersFromOtherApplications() const;
 
 private:
