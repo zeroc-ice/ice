1b8d9e18f823303906bf8ca02fb503f1d7db0c81
Jose <jose@zeroc.com>
2016-01-12 16:40:59 +0100

    remove unused isKind template

--- cpp/include/Ice/Traits.h
+++ cpp/include/Ice/Traits.h
@@ -1,3 +1,4 @@
+
 #ifndef ICE_TRAITS_H
 #define ICE_TRAITS_H
 
@@ -13,31 +14,6 @@ class Value;
 
 template<class T> using IsProxy = ::std::is_base_of<::Ice::ObjectPrx, T>;
 template<class T> using IsValue = ::std::is_base_of<::Ice::Value, T>;
-
-enum class Kind { Value, Proxy };
-
-template<typename T>
-struct TypeTraits
-{
-    constexpr static bool isKind(Kind kind)
-    {
-        switch(kind)
-        {
-            case Kind::Value:
-            {
-                return std::is_base_of<Value, T>::value;
-            }
-            case Kind::Proxy:
-            {
-                return std::is_base_of<ObjectPrx, T>::value;
-            }
-            default:
-            {
-                return false;
-            }
-        }
-    }
-};
 
 }
 #endif
