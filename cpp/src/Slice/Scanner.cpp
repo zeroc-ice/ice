392c578a56e0e14d5310ab56c3699ba689ed5a0f
Joe George <joe@zeroc.com>
2016-03-01 09:49:47 -0500

    Update copyright headers

--- cpp/src/Slice/Scanner.cpp
+++ cpp/src/Slice/Scanner.cpp
@@ -547,7 +547,7 @@ char *slice_text;
 
 // **********************************************************************
 //
-// Copyright (c) 2003-2015 ZeroC, Inc. All rights reserved.
+// Copyright (c) 2003-2016 ZeroC, Inc. All rights reserved.
 //
 // This copy of Ice is licensed to you under the terms described in the
 // ICE_LICENSE file included in this distribution.
