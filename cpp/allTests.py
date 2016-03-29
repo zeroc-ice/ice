9012487f2f6a2374c4938fff67d7145d6fdff446
Jose <jose@zeroc.com>
2016-03-29 11:18:55 +0200

    Mingw build fixes

--- cpp/allTests.py
+++ cpp/allTests.py
@@ -78,7 +78,7 @@ tests = [
     ("Ice/enums", ["once"]),
     ("Ice/logger", ["once"]),
     ("Ice/networkProxy", ["core", "noipv6", "nosocks"]),
-    ("Ice/services", ["once"]),
+    ("Ice/services", ["once", "nomingw"]),
     ("IceSSL/configuration", ["once", "novalgrind"]), # valgrind doesn't work well with openssl
     ("IceBox/configuration", ["core", "noipv6", "novc100", "nomingw", "nomx"]),
     ("IceBox/admin", ["core", "noipv6", "novc100", "nomingw", "nomx"]),
