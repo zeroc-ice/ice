8cbf8db38daf78d06095df89a4210490dfb4b609
Benoit Foucher <benoit@zeroc.com>
2016-05-11 12:43:00 +0200

    Added support for setting version/soversion on a per project/component basis

--- cpp/test/IceBox/configuration/Makefile.mk
+++ cpp/test/IceBox/configuration/Makefile.mk
@@ -9,7 +9,9 @@
 
 $(test)_libraries := $(test)_TestService
 
-$(test)_TestService_sources         = TestI.cpp Service.cpp Test.ice
-$(test)_TestService_dependencies    = IceBox
+$(test)_TestService_sources         	= TestI.cpp Service.cpp Test.ice
+$(test)_TestService_dependencies    	= IceBox
+$(test)_TestService_version		=
+$(test)_TestService_soversion		=
 
 tests += $(test)
--- cpp/test/IceGrid/noRestartUpdate/Makefile.mk
+++ cpp/test/IceGrid/noRestartUpdate/Makefile.mk
@@ -11,6 +11,8 @@ $(test)_libraries := $(test)_TestService
 
 $(test)_TestService_sources 	 = Service.cpp TestI.cpp Test.ice
 $(test)_TestService_dependencies = IceBox
+$(test)_TestService_version	 =
+$(test)_TestService_soversion	 =
 
 $(test)_client_dependencies = IceGrid Glacier2
 
