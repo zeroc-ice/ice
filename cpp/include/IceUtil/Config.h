c52382e22826e246d3256fabae377fb6be2339e9
Bernard Normier <bernard@zeroc.com>
2016-05-31 17:46:40 -0400

    Fixed ICE_SO_VERSION

--- cpp/include/IceUtil/Config.h
+++ cpp/include/IceUtil/Config.h
@@ -194,7 +194,7 @@
 //
 #define ICE_STRING_VERSION "3.7a2" // "A.B.C", with A=major, B=minor, C=patch
 #define ICE_INT_VERSION 30752      // AABBCC, with AA=major, BB=minor, CC=patch
-#define ICE_SO_VERSION "37a0"      // "ABC", with A=major, B=minor, C=patch
+#define ICE_SO_VERSION "37a2"      // "ABC", with A=major, B=minor, C=patch
 
 #if !defined(ICE_BUILDING_ICE) && defined(ICE_API_EXPORTS)
 #   define ICE_BUILDING_ICE
