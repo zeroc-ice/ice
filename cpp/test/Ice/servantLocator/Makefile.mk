50c12db56b74eebcd4977aedf9824346f7ca7407
Bernard Normier <bernard@zeroc.com>
2016-06-10 09:21:36 -0400

    Restored serveramd on Linux/OSX

--- cpp/test/Ice/servantLocator/Makefile.mk
+++ cpp/test/Ice/servantLocator/Makefile.mk
@@ -9,6 +9,6 @@
 
 $(test)_client_sources = $(test-client-sources) ServantLocatorI.cpp
 $(test)_server_sources = $(test-server-sources) ServantLocatorI.cpp
-$(test)_serveramd_sources = $(test-server-sources) ServantLocatorI.cpp
+$(test)_serveramd_sources = $(test-serveramd-sources) ServantLocatorI.cpp
 
 tests += $(test)
