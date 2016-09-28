551177ba4f528a9070830218a0b0a6341d0fc7a4
Jose <jose@zeroc.com>
2016-09-28 21:29:51 +0200

    check UTF-8 locale is used with unicodePaths test

--- python/test/Slice/unicodePaths/run.py
+++ python/test/Slice/unicodePaths/run.py
@@ -26,7 +26,7 @@ def test(b):
         print("failed!")
         sys.exit(1)
 
-if TestUtil.isAIX():
+if TestUtil.isAIX() or TestUtil.isLinux():
     encoding = locale.getdefaultlocale()[1]
     if encoding != "UTF-8":
         print("Please set LC_ALL to xx_xx.UTF-8, for example FR_FR.UTF-8")
