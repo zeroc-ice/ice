5e6e85c810828f25174699ac69953714275b2d5f
Bernard Normier <bernard@zeroc.com>
2017-01-20 19:09:25 -0500

    Fixed build failure with static libs

--- cpp/test/Ice/adapterDeactivation/Client.cpp
+++ cpp/test/Ice/adapterDeactivation/Client.cpp
@@ -7,7 +7,7 @@
 //
 // **********************************************************************
 
-#include <Ice/Application.h>
+#include <Ice/Ice.h>
 #include <TestCommon.h>
 #include <Test.h>
 #include <string>
