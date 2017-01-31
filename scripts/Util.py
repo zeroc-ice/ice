a0cb898540196d1bf89dbea99de06b75c5dafd2d
Benoit Foucher <benoit@zeroc.com>
2017-01-31 10:37:33 +0100

    Bumped Windows connect timeout for testing

--- scripts/Util.py
+++ scripts/Util.py
@@ -613,7 +613,7 @@ class Mapping:
                                                       "Ice/faultTolerance",
                                                       "Ice/services",
                                                       "IceDiscovery/simple"]:
-                        props["Ice.Override.ConnectTimeout"] = "200"
+                        props["Ice.Override.ConnectTimeout"] = "400"
 
                 # Additional properties specified on the command line with --cprops or --sprops
                 additionalProps = []
