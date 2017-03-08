922f5c31e3b3d1d417263fc04263b078b41b3425
Jose <jose@zeroc.com>
2017-03-08 10:18:31 +0100

    Fixed (ICE-6962) - add test for slice compiler --impl option

---
+++ csharp/test/Ice/impl/Server.cs
@@ -0,0 +1,54 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyTitle("IceTest")]
+[assembly: AssemblyDescription("Ice test")]
+[assembly: AssemblyCompany("ZeroC, Inc.")]
+
+public class Server : TestCommon.Application
+{
+    public override int run(string[] args)
+    {
+        //
+        // We don't want connection warnings because of the timeout test.
+        //
+        communicator().getProperties().setProperty("Ice.Warn.Connections", "0");
+
+        communicator().getProperties().setProperty("TestAdapter.Endpoints", getTestEndpoint(0) + ":udp");
+        Ice.ObjectAdapter adapter = communicator().createObjectAdapter("TestAdapter");
+        adapter.add(new Test.MyDerivedClassI(), Ice.Util.stringToIdentity("test"));
+        adapter.activate();
+
+        communicator().waitForShutdown();
+        return 0;
+    }
+
+    protected override Ice.InitializationData getInitData(ref string[] args)
+    {
+        Ice.InitializationData initData = base.getInitData(ref args);
+        //
+        // Its possible to have batch oneway requests dispatched
+        // after the adapter is deactivated due to thread
+        // scheduling so we supress this warning.
+        //
+        initData.properties.setProperty("Ice.Warn.Dispatch", "0");
+        return initData;
+    }
+
+    public static int Main(string[] args)
+    {
+        Server app = new Server();
+        return app.runmain(args);
+    }
+}
---
+++ java-compat/test/src/main/java/test/Ice/impl/AMDServer.java
@@ -0,0 +1,48 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+package test.Ice.impl;
+import test.Ice.impl.AMD.Test.MyDerivedClassI;
+
+public class AMDServer extends test.Util.Application
+{
+    @Override
+    public int run(String[] args)
+    {
+        communicator().getProperties().setProperty("TestAdapter.Endpoints", getTestEndpoint(0) + ":udp");
+        Ice.ObjectAdapter adapter = communicator().createObjectAdapter("TestAdapter");
+        adapter.add(new MyDerivedClassI(), Ice.Util.stringToIdentity("test"));
+        adapter.activate();
+        return WAIT;
+    }
+
+    @Override
+    protected Ice.InitializationData getInitData(Ice.StringSeqHolder argsH)
+    {
+        Ice.InitializationData initData = super.getInitData(argsH);
+        //
+        // It's possible to have batch oneway requests dispatched
+        // after the adapter is deactivated due to thread
+        // scheduling so we suppress this warning.
+        //
+        initData.properties.setProperty("Ice.Warn.Dispatch", "0");
+        initData.properties.setProperty("Ice.Package.Test", "test.Ice.impl.AMD");
+
+        return initData;
+    }
+
+    public static void main(String[] args)
+    {
+        AMDServer c = new AMDServer();
+        int status = c.main("AMDServer", args);
+
+        System.gc();
+        System.exit(status);
+    }
+}
