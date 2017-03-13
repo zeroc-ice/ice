104a2f6be4f90ddc8bf9e938c4cee17e17137b70
Benoit Foucher <benoit@zeroc.com>
2017-03-13 11:38:39 +0100

    Fixed ICE-7665 - fixed test/Ice/imple GNU make warning

--- cpp/test/Ice/impl/Makefile.mk
+++ cpp/test/Ice/impl/Makefile.mk
@@ -22,11 +22,19 @@ $5_cppflags 	+= -I$$($5_objdir)
 
 $$($5_objects): $$($5_objdir)/$$($2_impl)I.cpp
 
+ifeq ($$(filter $$($5_objdir)/$$($2_impl)I.cpp,$$($1_impls)),)
+
 $$($5_objdir)/$$($2_impl)I.cpp: $1/$$($2_impl).ice $(slice2cpp_path)
 	$(E) "Compiling $$< implementation"
 	$(Q)$(MKDIR) -p $$(*D)
 	$(Q)$(RM) $$(*).h $$(*).cpp
 	$(Q)$(slice2cpp_path) $$($2_sliceflags) $$(if $$(filter cpp11-%,$4),--impl-c++11,--impl-c++98) --output-dir $$(*D) $$<
+
+# Prevents warning if multiple configurations refer to $$($5_objdir)/$$($2_impl)I.cpp
+$1_impls += $$($5_objdir)/$$($2_impl)I.cpp
+
+endif
+
 endif
 
 endef
