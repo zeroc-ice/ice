9e91632d8c3842bdc78db19b32176c43c0574218
Bernard Normier <bernard@zeroc.com>
2017-06-02 09:59:59 -0400

    Fixed missing or mangled copyright notice

--- java-compat/src/IceBT/src/main/java/android/bluetooth/BluetoothDevice.java
+++ java-compat/src/IceBT/src/main/java/android/bluetooth/BluetoothDevice.java
@@ -1,3 +1,12 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
 //
 // This is a placeholder for the Android API. It is not included in the IceBT JAR file.
 //
--- java/test/Ice/translator/Test/CustomList.java
+++ java/test/Ice/translator/Test/CustomList.java
@@ -1,3 +1,12 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
 package Test;
 
 public class CustomList<E> extends java.util.ArrayList<E>
