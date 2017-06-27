7fcf7518cd11649570eb02052b1efbd827b442c1
Benoit Foucher <benoit@zeroc.com>
2017-06-27 16:47:42 +0200

    Fixed ICE-8159 - increased number of retries, also fixed warning suppression code to not supress lines for tracing when enabled

--- scripts/tests/IceDiscovery/simple.py
+++ scripts/tests/IceDiscovery/simple.py
@@ -14,16 +14,30 @@ domainId = uuid.uuid4() # Ensures each test uses a unique domain ID
 
 props = lambda process, current: {
     "IceDiscovery.Timeout": 50,
-    "IceDiscovery.RetryCount": 5,
+    "IceDiscovery.RetryCount": 10,
     "IceDiscovery.DomainId": domainId,
     "IceDiscovery.Interface": "" if isinstance(platform, Linux) else "::1" if current.config.ipv6 else "127.0.0.1",
     "IceDiscovery.Port": current.driver.getTestPort(10),
     "Ice.Plugin.IceDiscovery": current.getPluginEntryPoint("IceDiscovery", process)
 }
 
+#
+# Suppress the warning lines
+#
+suppressDiscoveryWarning=False
+def suppressWarning(x):
+    global suppressDiscoveryWarning
+    if re.search("-! .* warning: .*failed to lookup adapter.*\n", x):
+        suppressDiscoveryWarning = True
+        return re.sub("-! .* warning: .*failed to lookup adapter.*\n", "", x)
+    elif suppressDiscoveryWarning and re.search("^   .*\n", x):
+        return ""
+    else:
+        suppressDiscoveryWarning = False
+        return x
+
 # Filter-out the warning about invalid lookup proxy
-outfilters = [ lambda x: re.sub("-! .* warning: .*failed to lookup adapter.*\n", "", x),
-               lambda x: re.sub("^   .*\n", "", x) ]
+outfilters = [ lambda x: suppressWarning(x) ]
 
 TestSuite(__name__, [
    ClientServerTestCase(client=Client(args=[3], props=props, outfilters=outfilters),
