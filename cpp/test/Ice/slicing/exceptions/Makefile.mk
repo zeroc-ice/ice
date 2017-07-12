b99d8e6f279cba87ba8baf37a80dc9db811509c3
Jose <jose@zeroc.com>
2017-07-12 19:52:29 +0200

    ICE-8291 - warning: variable tracking size limit exceeded with -fvar-tracking-assignments

--- cpp/test/Ice/slicing/exceptions/Makefile.mk
+++ cpp/test/Ice/slicing/exceptions/Makefile.mk
@@ -12,4 +12,8 @@ $(test)_client_sources          = $(test-client-sources) ClientPrivate.ice
 $(test)_server_sources          = $(test-server-sources) ServerPrivate.ice
 $(test)_serveramd_sources       = $(test-serveramd-sources) ServerPrivateAMD.ice
 
+ifneq ($(linux_id),)
+    $(test)_cppflags += $(if $(filter yes,$(OPTIMIZE)),-fno-var-tracking-assignments)
+endif
+
 tests += $(test)
