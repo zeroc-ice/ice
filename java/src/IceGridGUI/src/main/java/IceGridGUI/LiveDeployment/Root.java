64ddee30a6f14a22b25d100db710dc7cc7c02127
Jose <jose@zeroc.com>
2017-08-24 11:00:01 +0200

    [BACKPORT] [ICE-8191] Fix IceGridGUI default settings for log window

--- java/src/IceGridGUI/src/main/java/IceGridGUI/LiveDeployment/Root.java
+++ java/src/IceGridGUI/src/main/java/IceGridGUI/LiveDeployment/Root.java
@@ -1205,10 +1205,10 @@ public class Root extends ListArrayTreeNode
     private void loadLogPrefs()
     {
         Preferences logPrefs = _coordinator.getPrefs().node("Log");
-        _logMaxLines = logPrefs.getInt("maxLines", 500);
-        _logMaxSize = logPrefs.getInt("maxSize", 20000);
-        _logInitialLines = logPrefs.getInt("initialLines", 10);
-        _logMaxReadSize = logPrefs.getInt("maxReadSize", 10000);
+        _logMaxLines = logPrefs.getInt("maxLines", 1000);
+        _logMaxSize = logPrefs.getInt("maxSize", 1000000);
+        _logInitialLines = logPrefs.getInt("initialLines", 1000);
+        _logMaxReadSize = logPrefs.getInt("maxReadSize", 1000000);
         _logPeriod = logPrefs.getInt("period", 300);
 
         if(_logMaxReadSize + 512 > _messageSizeMax)
