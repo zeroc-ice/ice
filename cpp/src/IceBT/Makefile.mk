c0fbd394b1db9929f0c722aa158a58fa34eeec68
Benoit Foucher <benoit@zeroc.com>
2017-09-12 17:06:39 +0200

    Build system improvements and fixes
    - Fixed static build failures (ICE-8509)
    - Improved check for IceBT build (ICE-8353)
    - Validate configs/platforms (ICE-8096)
    - Removed duplicated system libs from linker flags (ICE-7670)

--- cpp/src/IceBT/Makefile.mk
+++ cpp/src/IceBT/Makefile.mk
@@ -7,22 +7,16 @@
 #
 # **********************************************************************
 
-ifneq ($(filter debian ubuntu yocto poky,$(linux_id)),)
-
 #
-# Only build the IceBT plugin if pkg-config, BlueZ and D-Bus requirements are
-# installed.
+# Only build the IceBT plugin if Bluetooth system libraries are available (Bluez/DBus)
 #
-ifeq ($(shell pkg-config --exists bluez dbus-1 2> /dev/null && echo yes),yes)
+ifneq ($(IceBT_system_libs),)
 $(project)_libraries    := IceBT
 
 IceBT_targetdir         := $(libdir)
 IceBT_dependencies      := Ice
 IceBT_cppflags          := -DICEBT_API_EXPORTS $(shell pkg-config --cflags dbus-1)
 IceBT_sliceflags        := --include-dir IceBT
-IceBT_system_libs       = $(IceSSL_system_libs) $(shell pkg-config --libs dbus-1)
 
 projects += $(project)
-endif
-
 endif
