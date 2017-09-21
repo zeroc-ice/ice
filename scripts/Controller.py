57f774594e1ccfe549bcea8997d028b4ef5e2b14
Jose <jose@zeroc.com>
2017-09-21 08:39:33 +0200

    Fix Windows C++ compiler detection in test scripts
    Visual Studio 2017 15.3.5 comes with VC++ 19.11
    our script only handle up to VC++ 19.10. Fixed
    the code to handle this version and throw a
    proper exception if a unknown version is present

--- scripts/Controller.py
+++ scripts/Controller.py
@@ -95,7 +95,7 @@ class ControllerDriver(Driver):
                     return self.current.serverTestCase._startServerSide(self.current)
                 except Exception as ex:
                     self.serverSideRunning = False
-                    raise Test.Common.TestCaseFailedException(self.current.result.getOutput() + "\n" + str(ex))
+                    raise Test.Common.TestCaseFailedException(self.current.result.getOutput() + "\n" + traceback.format_exc())
 
             def stopServerSide(self, success, c):
                 if self.serverSideRunning:
