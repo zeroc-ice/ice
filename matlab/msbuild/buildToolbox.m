367a15e034cf6d80f397689a0ff416d35034ec63
Jose <jose@zeroc.com>
2017-10-12 16:41:24 +0200

    MATLAB packaging fixes

--- matlab/msbuild/buildToolbox.m
+++ matlab/msbuild/buildToolbox.m
@@ -2,21 +2,18 @@ function buildToolbox(iceVersion)
   rootDir = fileparts(mfilename('fullpath'));
   packageDir = fullfile(rootDir, 'toolbox');
   sourceDir = fullfile(packageDir, 'ice');
-  projectFile = fullfile(rootDir, 'ice.prj');
-  oldPath = path();
-  path(oldPath, sourceDir);
+  projectFile = fullfile(rootDir, 'toolbox.prj');
   try
     fprintf(1, 'Building Ice toolbox package... ');
     f = fopen(fullfile(sourceDir, 'Contents.m'), 'w+');
     fprintf(f, '%% ice\r\n');
     fprintf(f, '%% Version %s (R%s) %s\r\n', iceVersion, version('-release'), date);
     fclose(f);
-    matlab.addons.toolbox.packageToolbox(projectFile);
-    path(oldPath);
-    fprintf(1, 'ok\n');
+    matlab.addons.toolbox.packageToolbox(projectFile, sprintf('ice-%s-R%s.mltbx', iceVersion, version('-release')))
+    fprintf(1, 'ok\r\n');
+    exit(0);
   catch e
-    path(oldPath);
-    fprintf(1, 'failed\n');
-    e.rethrow();
+    fprintf(1, 'failed\r\n%s', getReport(e));
+    exit(1);
   end
 end
