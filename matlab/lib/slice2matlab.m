85779ace6b1989ae6e56cd6c380dfa74fe16b7d6
Jose <jose@zeroc.com>
2017-10-13 22:23:05 +0200

    Add slice2matlab help comment

--- matlab/lib/slice2matlab.m
+++ matlab/lib/slice2matlab.m
@@ -1,35 +1,37 @@
-%{
-**********************************************************************
-Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
-This copy of Ice is licensed to you under the terms described in the
-ICE_LICENSE file included in this distribution.
-**********************************************************************
-%}
 function [status, out] = slice2matlab(args)
-  rootDir = fileparts(mfilename('fullpath'));
+    % slice2matlab   Summary of slice2matlab
+    %
+    % A wrapper function to call Slice to MATLAB compiler slice2matlab.exe.
+    %
+    % Parameters:
+    %   args - arguments passed to slice2matlab.exe.
 
-  candidates = {'..\slice2matlab.exe'
-                '..\..\cpp\bin\x64\Release\slice2matlab.exe'
-                '..\..\cpp\bin\x64\Debug\slice2matlab.exe'
-                '..\msbuild\packages\zeroc.ice.v140\build\native\tools\slice2matlab.exe'};
+    % Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
 
-  path = '';
-  for i = 1:length(candidates)
-    f = fullfile(rootDir, candidates{i});
-    if exist(f, 'file') == 2
-      path = f;
-      break;
+    rootDir = fileparts(mfilename('fullpath'));
+
+    candidates = {'..\slice2matlab.exe'
+                  '..\..\cpp\bin\x64\Release\slice2matlab.exe'
+                  '..\..\cpp\bin\x64\Debug\slice2matlab.exe'
+                  '..\msbuild\packages\zeroc.ice.v140\build\native\tools\slice2matlab.exe'};
+
+    path = '';
+    for i = 1:length(candidates)
+        f = fullfile(rootDir, candidates{i});
+        if exist(f, 'file') == 2
+            path = f;
+            break;
+        end
     end
-  end
 
-  if isempty(path)
-    status = 1;
-    out = 'Cannot locate slice2matlab.exe\n';
-  else
-    [status, out] = system(sprintf('%s %s', path, args));
-  end
+    if isempty(path)
+        status = 1;
+        out = 'Cannot locate slice2matlab.exe\n';
+    else
+        [status, out] = system(sprintf('%s %s', path, args));
+    end
 
-  if ~isempty(out)
-    fprintf(1, '%s', out);
-  end
+    if ~isempty(out)
+        fprintf(1, '%s', out);
+    end
 end
