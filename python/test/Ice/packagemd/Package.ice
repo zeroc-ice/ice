a2d0c97dffa640c7359bf7f5fb824ee84bf4020a
Mark Spruiell <mes@zeroc.com>
2017-10-24 13:59:59 -0700

    ICE-8545 - add test for python package metadata

---
+++ python/test/Ice/packagemd/Package.ice
@@ -0,0 +1,57 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#pragma once
+
+[["python:package:testpkg"]]
+module Test2
+{
+class C1
+{
+    int i;
+}
+
+class C2 extends C1
+{
+    long l;
+}
+
+exception E1
+{
+    int i;
+}
+
+exception E2 extends E1
+{
+    long l;
+}
+}
+
+module Test3
+{
+class C1
+{
+    int i;
+}
+
+class C2 extends C1
+{
+    long l;
+}
+
+exception E1
+{
+    int i;
+}
+
+exception E2 extends E1
+{
+    long l;
+}
+}
---
+++ python/test/Ice/packagemd/Server.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# **********************************************************************
+#
+# Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+#
+# This copy of Ice is licensed to you under the terms described in the
+# ICE_LICENSE file included in this distribution.
+#
+# **********************************************************************
+
+import os, sys, traceback
+
+import Ice
+Ice.loadSlice('--all -I. Test.ice')
+import Test, Test1, testpkg
+
+class InitialI(Test.Initial):
+
+    def getTest1C2AsObject(self, current):
+        return Test1.C2()
+
+    def getTest1C2AsC1(self, current):
+        return Test1.C2()
+
+    def getTest1C2AsC2(self, current):
+        return Test1.C2()
+
+    def throwTest1E2AsE1(self, current):
+        raise Test1.E2()
+
+    def throwTest1E2AsE2(self, current):
+        raise Test1.E2()
+
+    def throwTest1Def(self, current):
+        raise Test1._def()
+
+    def getTest2C2AsObject(self, current):
+        return testpkg.Test2.C2()
+
+    def getTest2C2AsC1(self, current):
+        return testpkg.Test2.C2()
+
+    def getTest2C2AsC2(self, current):
+        return testpkg.Test2.C2()
+
+    def throwTest2E2AsE1(self, current):
+        raise testpkg.Test2.E2()
+
+    def throwTest2E2AsE2(self, current):
+        raise testpkg.Test2.E2()
+
+    def getTest3C2AsObject(self, current):
+        return testpkg.Test3.C2()
+
+    def getTest3C2AsC1(self, current):
+        return testpkg.Test3.C2()
+
+    def getTest3C2AsC2(self, current):
+        return testpkg.Test3.C2()
+
+    def throwTest3E2AsE1(self, current):
+        raise testpkg.Test3.E2()
+
+    def throwTest3E2AsE2(self, current):
+        raise testpkg.Test3.E2()
+
+    def shutdown(self, current):
+        current.adapter.getCommunicator().shutdown()
+
+def run(args, communicator):
+    communicator.getProperties().setProperty("TestAdapter.Endpoints", "default -p 12010")
+    adapter = communicator.createObjectAdapter("TestAdapter")
+    initial = InitialI()
+    adapter.add(initial, Ice.stringToIdentity("initial"))
+    adapter.activate()
+
+    communicator.waitForShutdown()
+    return True
+
+try:
+    with Ice.initialize(sys.argv) as communicator:
+         status = run(sys.argv, communicator)
+except:
+    traceback.print_exc()
+    status = False
+
+sys.exit(not status)
