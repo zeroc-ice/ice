4b61dbc06d1436d7531878f257cedcf7f08e97e2
Jose <jose@zeroc.com>
2018-03-26 14:38:33 +0200

    Bogus uuid v4 format
    See https://tools.ietf.org/rfc/rfc4122.txt

--- js/src/Ice/UUID.js
+++ js/src/Ice/UUID.js
@@ -13,7 +13,7 @@ function generateUUID()
     let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
         let r = (d + Math.random() * 16) % 16 | 0;
         d = Math.floor(d / 16);
-        return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
     return uuid;
 }
