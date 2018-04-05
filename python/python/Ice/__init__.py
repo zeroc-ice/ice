aef4492fbadf9ba1ff86a8ee54e5fe9837044fd6
Jose <jose@zeroc.com>
2018-04-05 16:02:30 +0200

    Move Ice/IceFuture.py -> to Ice/Future/WrapFuture.py
    Using an Ice submodule for this file allow us to exclude it from
    python < 3.5 pip packages, python bytecode compile will fail with
    python < 3.5 for this file and break pip package.
    
    See ICE-8794

--- python/python/Ice/__init__.py
+++ python/python/Ice/__init__.py
@@ -80,7 +80,7 @@ def Python35():
     return sys.version_info[:2] >= (3, 5)
 
 if Python35():
-    from Ice.IceFuture import FutureBase, wrap_future
+    from Ice.Future.WrapFuture import FutureBase, wrap_future
 else:
     FutureBase = object
 
