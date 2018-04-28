6f82bd8178f5dded058be82040ab9c0dd60e3ad2
mdorner <michael.dorner@tum.de>
2018-04-28 20:53:03 +0200

    Register FileParser on IceGridAdmin.Server adapter
    - Properly create IceGridAdmin.Server OA instead of using
    createObjectAdapterwithEndpoints for FileParser
    - Default IceGridAdmin.Server.Endpoints to old endpoints
    - Add IceGridAdmin.Server to the list of acceptable properties
    - Property files regenerated

--- cpp/src/IceGrid/Client.cpp
+++ cpp/src/IceGrid/Client.cpp
@@ -241,8 +241,10 @@ Client::main(StringSeq& args)
     try
     {
         _appName = args[0];
+        PropertiesPtr defaultProps = createProperties();
+        defaultProps->setProperty("IceGridAdmin.Server.Endpoints", "tcp -h localhost");
         InitializationData id;
-        id.properties = createProperties(args);
+        id.properties = createProperties(args, defaultProps);
         id.properties->setProperty("Ice.Warn.Endpoints", "0");
         _communicator = initialize(id);
 
@@ -371,10 +373,8 @@ Client::run(StringSeq& originalArgs)
 
     if(opts.isSet("server"))
     {
-        string endpoints =
-            communicator()->getProperties()->getPropertyWithDefault("FileParser.Endpoints", "tcp -h localhost");
         ObjectAdapterPtr adapter =
-            communicator()->createObjectAdapterWithEndpoints("FileParser", endpoints);
+            communicator()->createObjectAdapter("IceGridAdmin.Server");
         adapter->activate();
         ObjectPrx proxy = adapter->add(new FileParserI, stringToIdentity("FileParser"));
         consoleOut << proxy << endl;
