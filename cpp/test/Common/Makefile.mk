ed9368faca877dd587ccbf25b2cde956f4164def
Jose <jose@zeroc.com>
2018-06-05 14:38:30 +0200

    Fix TestCommon libdir

--- cpp/test/Common/Makefile.mk
+++ cpp/test/Common/Makefile.mk
@@ -14,7 +14,7 @@ $(project)_caninstall   := no
 # Put the shared TestCommon library in the lib directory for convenience on platforms
 # which don't support something like @loader_path.
 #
-TestCommon[shared]_targetdir    := lib
+TestCommon[shared]_targetdir    := $(libdir)
 TestCommon_dependencies         := IceSSL Ice
 TestCommon_cppflags             := -DTEST_API_EXPORTS -I$(includedir) -I$(project) -Itest/include
 
