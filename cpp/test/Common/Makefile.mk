7919c21dc8f936f7e99650f55b27dff33c4d1605
Benoit Foucher <benoit@zeroc.com>
2018-06-06 10:24:12 +0200

    Fixed bug where binary distribution testing would fail to build the TestCommon library

--- cpp/test/Common/Makefile.mk
+++ cpp/test/Common/Makefile.mk
@@ -12,9 +12,11 @@ $(project)_caninstall   := no
 
 #
 # Put the shared TestCommon library in the lib directory for convenience on platforms
-# which don't support something like @loader_path.
+# which don't support something like @loader_path. We don't use lib or $(libdir) here
+# because TestCommon isn't installed with the binary distribution and it might also
+# be used by a component that has Ice as a sub-module (e.g. Freeze).
 #
-TestCommon[shared]_targetdir    := $(libdir)
+TestCommon[shared]_targetdir    := $(call mappingdir,$(currentdir),lib)
 TestCommon_dependencies         := IceSSL Ice
 TestCommon_cppflags             := -DTEST_API_EXPORTS -I$(includedir) -I$(project) -Itest/include
 
