8db3aa7ea80ce0680788a65d1aaec2355a389f0f
Jose <jose@zeroc.com>
2019-03-07 19:01:53 +0100

    Do not build Slice compilers it can cause problems with Carthage iOS
    When building iOS packages Carthage cannot build Slice compilers,
    seems related to the xcodebuild settings set by Carthage, as it
    was working fine with plain xcode or xcodebuild.

--- swift/config/xcode-slice2cpp.sh
+++ swift/config/xcode-slice2cpp.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if [ -f /usr/local/opt/ice/bin/slice2cpp ]; then
+    SLICE2CPP=/usr/local/bin/slice2cpp
+else
+    SLICE2CPP=$SRCROOT/../cpp/bin/slice2cpp
+fi
+
 BASENAME=$(basename -- "$INPUT_FILE_PATH")
 BASENAME="${BASENAME%.*}"
 mkdir -p "$DERIVED_FILE_DIR/$1"
-echo $SYMROOT/$CONFIGURATION/slice2cpp "$DERIVED_FILE_DIR/$1"
-$SYMROOT/$CONFIGURATION/slice2cpp -I"$SRCROOT/../slice" --include-dir $1 --output-dir "$DERIVED_FILE_DIR/$1" "$INPUT_FILE_PATH"
+$SLICE2CPP -I"$SRCROOT/../slice" --include-dir $1 --output-dir "$DERIVED_FILE_DIR/$1" "$INPUT_FILE_PATH"
 mkdir -p "$SYMROOT/$PLATFORM_NAME/include/$1"
 mv "$DERIVED_FILE_DIR/$1/$BASENAME.h" "$SYMROOT/$PLATFORM_NAME/include/$1/$BASENAME.h"
--- swift/config/xcode-slice2swift.sh
+++ swift/config/xcode-slice2swift.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+if [ -f /usr/local/opt/ice/bin/slice2swift ]; then
+    SLICE2SWIFT=/usr/local/bin/slice2swift
+else
+    SLICE2SWIFT=$SRCROOT/../cpp/bin/slice2swift
+fi
+
 if [ -z "$1" ]; then
-    $SYMROOT/$CONFIGURATION/slice2swift -I"$SRCROOT/../slice" -I"$INPUT_FILE_DIR" --output-dir "$DERIVED_FILE_DIR" "$INPUT_FILE_PATH"
+    $SLICE2SWIFT -I"$SRCROOT/../slice" -I"$INPUT_FILE_DIR" --output-dir "$DERIVED_FILE_DIR" "$INPUT_FILE_PATH"
 else
     BASENAME=$(basename -- "$INPUT_FILE_PATH")
     BASENAME="${BASENAME%.*}"
     mkdir -p "$DERIVED_FILE_DIR/$1"
-    $SYMROOT/$CONFIGURATION/slice2swift -I"$SRCROOT/../slice" -I"$INPUT_FILE_DIR" --output-dir "$DERIVED_FILE_DIR/$1" "$INPUT_FILE_PATH"
+    $SLICE2SWIFT -I"$SRCROOT/../slice" -I"$INPUT_FILE_DIR" --output-dir "$DERIVED_FILE_DIR/$1" "$INPUT_FILE_PATH"
     mv "$DERIVED_FILE_DIR/$1/$BASENAME.swift" "$DERIVED_FILE_DIR/$1_$BASENAME.swift"
 fi
