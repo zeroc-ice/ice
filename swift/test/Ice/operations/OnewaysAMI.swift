c29f20c0c78335866c986baec9b7ec205ebb3846
Jose <jose@zeroc.com>
2019-03-21 10:28:25 +0100

    Oneways async testing

--- swift/test/Ice/operations/OnewaysAMI.swift
+++ swift/test/Ice/operations/OnewaysAMI.swift
@@ -13,9 +13,67 @@ public class OnewaysAMI {
         PromiseKit.conf.Q.return = .global()
         PromiseKit.conf.logHandler = { _ in }
 
-        let p = uncheckedCast(prx: proxy.ice_oneway(), type: MyClassPrx.self)
-        
-        p?.ice_pingAsync()
+        let p = uncheckedCast(prx: proxy.ice_oneway(), type: MyClassPrx.self)!
+
+        do {
+            let group = DispatchGroup()
+            group.enter()
+            _ = p.ice_pingAsync(sent: { _ in
+                group.leave()
+            })
+            group.wait()
+        }
+
+        do {
+            try firstly {
+                p.ice_isAAsync(id: ice_staticId(MyClassPrx.self))
+            }.done { _ in
+                try test(false)
+            }.wait()
+        } catch is Ice.TwowayOnlyException {}
+
+        do {
+            try firstly {
+                p.ice_idsAsync()
+            }.done { _ in
+                try test(false)
+            }.wait()
+        } catch is Ice.TwowayOnlyException {}
+
+        do {
+            let group = DispatchGroup()
+            group.enter()
+            _ = p.opVoidAsync(sent: { _ in
+                group.leave()
+            })
+            group.wait()
+        }
+
+        do {
+            let group = DispatchGroup()
+            group.enter()
+            _ = p.opIdempotentAsync(sent: { _ in
+                group.leave()
+            })
+            group.wait()
+        }
+
+        do {
+            let group = DispatchGroup()
+            group.enter()
+            _ = p.opNonmutatingAsync(sent: { _ in
+                group.leave()
+            })
+            group.wait()
+        }
+
+        do {
+            try firstly {
+                p.opByteAsync(p1: 0xff, p2: 0x0f)
+            }.done { _ in
+                try test(false)
+            }.wait()
+        } catch is Ice.TwowayOnlyException {}
     }
 
     public static func test(_ value: Bool, file: String = #file, line: Int = #line) throws {
