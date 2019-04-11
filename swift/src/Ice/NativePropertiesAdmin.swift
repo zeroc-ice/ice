26f9775b5c1ceba0f0ea4c197d94df349d0b47bf
Jose <jose@zeroc.com>
2019-04-11 18:43:58 +0200

    Make NativePropertiesAdmin public

--- swift/src/Ice/NativePropertiesAdmin.swift
+++ swift/src/Ice/NativePropertiesAdmin.swift
@@ -12,14 +12,14 @@
  * @param PropertyDict A dictionary containing the properties that were added, changed or removed,
  * with a removed property denoted by an entry whose value is an empty string.
  */
-typealias PropertiesAdminUpdateCallback = (PropertyDict) -> Void
+public typealias PropertiesAdminUpdateCallback = (PropertyDict) -> Void
 
-typealias PropertiesAdminRemoveallback = () -> Void
+public typealias PropertiesAdminRemoveallback = () -> Void
 
 /**
  * Base protocol for the Properties admin facet.
  */
-protocol NativePropertiesAdmin {
+public protocol NativePropertiesAdmin {
     /**
      * Register an update callback that will be invoked when property updates occur.
      * @param cb The callback.
