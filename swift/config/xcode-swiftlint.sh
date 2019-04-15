302b6156c969d3a3c62fbb46909e603c51dcdb94
Joe George <joe@zeroc.com>
2019-04-15 14:23:59 -0400

    Remove swiftformat from xcode script

--- swift/config/xcode-swiftlint.sh
+++ swift/config/xcode-swiftlint.sh
@@ -3,12 +3,6 @@
 # Copyright (c) ZeroC, Inc. All rights reserved.
 #
 
-if which swiftformat >/dev/null; then
-    swiftformat "$1"
-else
-    echo "warning: SwiftFormat not installed, download from https://github.com/nicklockwood/SwiftFormat"
-fi
-
 if which swiftlint >/dev/null; then
     swiftlint --path "$1" --config "$2"
 else
