0397c60cf78bbe06d167d6e5f886d4d252b7b144
Bernard Normier <bernard@zeroc.com>
2019-04-24 09:04:02 -0400

    Fixed copyright notice

--- swift/src/Ice/ServantManager.swift
+++ swift/src/Ice/ServantManager.swift
@@ -1,8 +1,6 @@
-// **********************************************************************
 //
-// Copyright (c) 2003-2018 ZeroC, Inc. All rights reserved.
+// Copyright (c) ZeroC, Inc. All rights reserved.
 //
-// **********************************************************************
 
 class ServantManager {
     let adapterName: String
