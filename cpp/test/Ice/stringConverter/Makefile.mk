d74cfb53a4037dff90ea92bfe89ceb3d4d52c234
Bernard Normier <bernard@zeroc.com>
2019-07-15 04:10:33 -0500

    Improved computation of libpath

--- cpp/test/Ice/stringConverter/Makefile.mk
+++ cpp/test/Ice/stringConverter/Makefile.mk
@@ -2,8 +2,6 @@
 # Copyright (c) ZeroC, Inc. All rights reserved.
 #
 
-$(test)_ldflags      = $(iconv_ldflags)
-
-$(test)_system_libs  = $(iconv_program_system_libs)
+$(test)_libs      = iconv
 
 tests += $(test)
