07b8b37de0776378907ef68a6ed56c768e2a5248
Jose <jose@zeroc.com>
2019-10-10 10:32:33 +0200

    Fix for Ice/acm timeout - Close #558

--- java/test/src/main/java/test/Ice/acm/AllTests.java
+++ java/test/src/main/java/test/Ice/acm/AllTests.java
@@ -430,7 +430,7 @@ public class AllTests
         public CloseOnIdleAndInvocationTest(TestHelper helper, RemoteCommunicatorPrx com, java.io.PrintWriter out)
         {
             super(helper, "close on idle and invocation", com, out);
-            setClientACM(1, 3, 0); // Only close on idle and invocation
+            setClientACM(3, 3, 0); // Only close on idle and invocation
         }
 
         public void runTestCase(RemoteObjectAdapterPrx adapter, TestIntfPrx proxy)
@@ -443,7 +443,7 @@ public class AllTests
             adapter.hold();
             try
             {
-                Thread.sleep(3000); // Idle for 3 seconds
+                Thread.sleep(5000); // Idle for 5 seconds
             }
             catch(java.lang.InterruptedException ex)
             {
