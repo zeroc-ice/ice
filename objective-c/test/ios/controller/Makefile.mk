00e14ca4a5f6e119995beb0b3b7c7e0c886c8b8b
Benoit Foucher <benoit@zeroc.com>
2019-10-24 10:26:51 +0200

    No longer systematically clean C++/Objective-C test iOS controller

--- objective-c/test/ios/controller/Makefile.mk
+++ objective-c/test/ios/controller/Makefile.mk
@@ -10,6 +10,7 @@ $(project)_project              = Objective-C Test Controller.xcodeproj
 $(project)_scheme               = $(if $(filter arc-xcodesdk,$2),Objective-C ARC Test Controller,Objective-C Test Controller)
 # Always build debug configuration to prevent symbols from being stripped
 $(project)_configuration        = Debug
+$(project)_slicecompiler        = slice2objc
 
 projects += $(project)
 tests:: $(project)
