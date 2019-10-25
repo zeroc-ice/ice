739790df9e1df6a36402233905b051afc741ec57
Bernard Normier <bernard@zeroc.com>
2019-10-25 16:07:46 -0400

    Bug fixes

--- cpp/src/IceGrid/IceGridNode.cpp
+++ cpp/src/IceGrid/IceGridNode.cpp
@@ -359,6 +359,31 @@ NodeService::startImpl(int argc, char* argv[], int& status)
         {
             dataPath += "/";
         }
+
+        createDirectory(dataPath + "servers");
+        createDirectory(dataPath + "tmp");
+
+#ifdef _WIN32
+        //
+        // Make sure these directories are not indexed by the Windows
+        // indexing service (which can cause random "Access Denied"
+        // errors if indexing runs at the same time as the node is
+        // creating/deleting files).
+        //
+        try
+        {
+            setNoIndexingAttribute(dataPath + "servers");
+            setNoIndexingAttribute(dataPath + "tmp");
+        }
+        catch(const FileException& ex)
+        {
+            if(!nowarn)
+            {
+                Warning out(communicator()->getLogger());
+                out << "couldn't disable file indexing:\n" << ex;
+            }
+        }
+#endif
     }
 
     //
