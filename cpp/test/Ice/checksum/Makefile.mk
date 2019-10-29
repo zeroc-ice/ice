ccd0afad21f63d7b5a9b3c46f2f37f9971d19fe8
Austin Henriksen <austin@zeroc.com>
2019-10-29 14:41:15 -0400

    Removed checksum tests from cpp

--- cpp/test/Ice/checksum/Makefile.mk
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) ZeroC, Inc. All rights reserved.
-#
-
-$(test)_sliceflags      = --checksum
-$(test)_client_sources  = $(test-client-sources) ClientTypes.ice
-$(test)_server_sources  = $(test-server-sources) ServerTypes.ice
-
-tests += $(test)
