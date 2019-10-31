ef78eae6104c397b5f4a20bffd730b18079c48b4
Benoit Foucher <benoit@zeroc.com>
2019-10-31 09:31:01 +0100

    Added logging for IceGrid/replication test to investigate client failure

--- cpp/test/IceGrid/replication/test.py
+++ cpp/test/IceGrid/replication/test.py
@@ -5,5 +5,15 @@
 
 clientProps = lambda process, current: { "ServerDir" : current.getBuildDir("server") }
 
+# Enable some tracing to allow investigating test failures
+traceProps = {
+    "Ice.Trace.Network" : 2,
+    "Ice.Trace.Retry" : 1,
+    "Ice.Trace.Protocol" : 1
+}
+
 if isinstance(platform, Windows) or os.getuid() != 0:
-    TestSuite(__file__, [ IceGridTestCase(client=IceGridClient(props=clientProps)) ], runOnMainThread=True, multihost=False)
+    TestSuite(__file__,
+              [ IceGridTestCase(client=IceGridClient(props=clientProps, traceProps=traceProps)) ],
+              runOnMainThread=True,
+              multihost=False)
