17559e6a29c8892ae4c83279c789211279617de4
Austin Henriksen <austin@zeroc.com>
2019-11-07 20:40:28 -0500

    Fixed test issues.

--- cpp/test/IceGrid/session/Client.cpp
+++ cpp/test/IceGrid/session/Client.cpp
@@ -20,7 +20,7 @@ Client::run(int argc, char** argv)
     auto properties = createTestProperties(argc, argv);
     properties->parseCommandLineOptions("", Ice::argsToStringSeq(argc, argv));
     properties->setProperty("Ice.Warn.Connections", "0");
-    initialize(argc, argv, properties);
+    Ice::CommunicatorHolder communicatorHolder = initialize(argc, argv, properties);
     void allTests(Test::TestHelper*);
     allTests(this);
 }
