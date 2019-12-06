5eb014fc486ece05199e126655c5e48f560cc86f
Bernard Normier <bernard@zeroc.com>
2019-12-06 15:38:29 -0500

    Build Slice compilers with -std=c++17

--- cpp/src/Slice/JavaUtil.cpp
+++ cpp/src/Slice/JavaUtil.cpp
@@ -848,7 +848,7 @@ Slice::JavaGenerator::fixKwd(const string& name) const
         return lookupKwd(name);
     }
     StringList ids = splitScopedName(name);
-    transform(ids.begin(), ids.end(), ids.begin(), ptr_fun(lookupKwd));
+    transform(ids.begin(), ids.end(), ids.begin(), lookupKwd);
     stringstream result;
     for(StringList::const_iterator i = ids.begin(); i != ids.end(); ++i)
     {
