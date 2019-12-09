8bc77551402bd787535c38353fc52514b12d9578
Jose <jose@zeroc.com>
2019-12-09 16:44:22 +0100

    Fix MATLAB build failure

--- matlab/src/Util.h
+++ matlab/src/Util.h
@@ -42,8 +42,6 @@ mxArray* createByteArray(const Ice::Byte*, const Ice::Byte*);
 mxArray* createByteList(const std::vector<Ice::Byte>&);
 mxArray* createCertificateList(const std::vector<IceSSL::CertificatePtr>&);
 
-std::string idToClass(const std::string&);
-
 template<typename T>
 std::shared_ptr<T> deref(void* p)
 {
