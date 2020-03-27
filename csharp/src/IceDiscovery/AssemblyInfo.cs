ddbcbf8efe1fc568a030b392a51f784f215d1182
Jose <jose@zeroc.com>
2020-03-27 13:14:36 +0100

    Update C# version to 3.7.3.2

--- csharp/src/IceDiscovery/AssemblyInfo.cs
+++ csharp/src/IceDiscovery/AssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Ice")]
 [assembly: AssemblyCopyright("© ZeroC, Inc.")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.7.3.1")]
+[assembly: AssemblyVersion("3.7.3.2")]
 
 [assembly: ComVisible(false)]
