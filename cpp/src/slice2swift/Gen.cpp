aa8f11b1e1c1024ef16b3c83767e4be3257cc594
Jose <jose@zeroc.com>
2020-04-28 23:48:25 +0200

    Fix swift dangling pointer warnings - Close #821

--- cpp/src/slice2swift/Gen.cpp
+++ cpp/src/slice2swift/Gen.cpp
@@ -836,15 +836,20 @@ Gen::TypesVisitor::visitDictionary(const DictionaryPtr& p)
         string keyParam = "let key: " + keyType;
         writeMarshalUnmarshalCode(out, p->keyType(), p, keyParam, false);
         out << nl << "v[key] = nil as " << valueType;
+        out << nl << "Swift.withUnsafeMutablePointer(to: &v[key, default:nil])";
+        out << sb;
         out << nl << "e.values[i] = Ice.DictEntry<" << keyType << ", " << valueType << ">("
             << "key: key, "
-            << "value: UnsafeMutablePointer<" << valueType << ">(&v[key, default:nil]))";
+            << "value: $0)";
+        out << eb;
         writeMarshalUnmarshalCode(out, p->valueType(), p, "e.values[i].value.pointee", false);
         out << eb;
 
         out << nl << "for i in 0..<sz" << sb;
-        out << nl << "e.values[i].value = Swift.UnsafeMutablePointer<" << valueType
-            << ">(&v[e.values[i].key, default:nil])";
+        out << nl << "Swift.withUnsafeMutablePointer(to: &v[e.values[i].key, default:nil])";
+        out << sb;
+        out << nl << "e.values[i].value = $0";
+        out << eb;
         out << eb;
     }
     else
