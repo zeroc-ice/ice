cf383ff3122b097b637d3108a5b691c568e07b4d
Bernard Normier <bernard@zeroc.com>
2020-05-01 16:29:45 -0400

    Added doc-comments

--- csharp/src/Ice/IClassFactory.cs
+++ csharp/src/Ice/IClassFactory.cs
@@ -4,8 +4,13 @@
 
 namespace Ice
 {
+    /// <summary>IClassFactory is a publicly visible Ice internal interface used by InputStream and implemented by
+    /// the generated code.</summary>
     public interface IClassFactory
     {
+        /// <summary>Creates a default instance of the associated class (mapped from a Slice class). A default instance
+        /// is constructed using the parameterless constructor of the class.</summary>
+        /// <returns>A new instance of the class.</returns>
         AnyClass CreateDefaultInstance();
     }
 }
--- csharp/src/Ice/IRemoteExceptionFactory.cs
+++ csharp/src/Ice/IRemoteExceptionFactory.cs
@@ -4,8 +4,14 @@
 
 namespace Ice
 {
+    /// <summary>IRemoteExceptionFactory is a publicly visible Ice internal interface used by InputStream and
+    /// implemented by the generated code.</summary>
     public interface IRemoteExceptionFactory
     {
+        /// <summary>Creates a new instance of the associated remote exception and reads its fields from the stream.
+        /// </summary>
+        /// <param name="istr">The input stream.</param>
+        /// <returns>The new remote exception.</returns>
         RemoteException Read(InputStream istr);
     }
 }
