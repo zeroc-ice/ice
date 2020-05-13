dd925e5718a1c84368a4e6b63ce23e5c10745e41
Jose <jose@zeroc.com>
2020-05-13 14:40:14 +0200

    Use thread local for Random instead of using a lock

--- csharp/src/Ice/EnumerableExtensions.cs
+++ csharp/src/Ice/EnumerableExtensions.cs
@@ -3,25 +3,26 @@
 //
 
 using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 
 namespace Ice
 {
     public static class EnumerableExtensions
     {
+        private static readonly ThreadLocal<Random> _rand = new ThreadLocal<Random>(() => new Random());
+
         /// <summary>Shuffle the elements of a sequence.</summary>
         /// <typeparam name="TSource">The type of the elements of source.</typeparam>
         /// <param name="source">A sequence of values to shuffle.</param>
         /// <returns>An IEnumerable&lt;TSouce&gt; whose elements are shuffled.</returns>
         public static IEnumerable<TSource> Shuffle<TSource>(this IEnumerable<TSource> source)
         {
-            lock (_rand)
-            {
-                return source.OrderBy(element => _rand.Next());
-            }
+            Random? rand = _rand.Value;
+            Debug.Assert(rand != null);
+            return source.OrderBy(element => rand.Next());
         }
-
-        private static readonly Random _rand = new Random();
     }
 }
