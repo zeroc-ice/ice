4a13ef7072eebb6013af90b2ec062223730f69c9
Joe George <joe@zeroc.com>
2020-06-10 12:04:52 -0400

    Add __main__ logic to slice2py.py (Fixes #893)

--- python/scripts/slice2py.py
+++ python/scripts/slice2py.py
@@ -3,11 +3,19 @@
 # Copyright (c) ZeroC, Inc. All rights reserved.
 #
 
-import sys, Ice, IcePy
+import sys
+import Ice
+import IcePy
+
+
 def main():
     sliceDir = Ice.getSliceDir()
     # Automatically add the slice dir.
-    if not sliceDir is None:
+    if sliceDir is not None:
         sys.argv.append('-I' + sliceDir)
     val = IcePy.compile(sys.argv)
     sys.exit(int(val))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
