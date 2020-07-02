05d292fd0d4271e7ba4b391191b0b708718b9f3b
Jose <jose@zeroc.com>
2020-07-02 23:53:35 +0200

    Make logger classes internal

--- csharp/src/Ice/Logger.cs
+++ csharp/src/Ice/Logger.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace ZeroC.Ice
 {
-    public abstract class Logger : ILogger
+    internal abstract class Logger : ILogger
     {
         public string Prefix { get; }
 
@@ -58,40 +58,40 @@ namespace ZeroC.Ice
     }
 
     /// <summary>Represents a logger that writes messages to the standard error output stream.</summary>
-    public sealed class ConsoleLogger : Logger
+    internal sealed class ConsoleLogger : Logger
     {
-        /// <summary>Creates a new console logger.</summary>
-        /// <param name="prefix">The prefix to perpend to messages write by this logger.</param>
-        public ConsoleLogger(string prefix)
-            : base(prefix)
-        {
-        }
-
         public override ILogger CloneWithPrefix(string prefix) => new ConsoleLogger(prefix);
 
         protected override void Write(string message) => Console.Error.WriteLine(message);
+
+        /// <summary>Creates a new console logger.</summary>
+        /// <param name="prefix">The prefix to perpend to messages write by this logger.</param>
+        internal ConsoleLogger(string prefix)
+            : base(prefix)
+        {
+        }
     }
 
     /// <summary>Represents a logger that writes messages to a file.</summary>
-    public sealed class FileLogger : Logger, IDisposable
+    internal sealed class FileLogger : Logger, IDisposable
     {
         private readonly string _file;
         private readonly TextWriter _writer;
 
+        public override ILogger CloneWithPrefix(string prefix) => new FileLogger(prefix, _file, _writer);
+
+        public void Dispose() => _writer.Close();
+
         /// <summary>Creates a new file logger.</summary>
         /// <param name="prefix">The prefix to perpend to messages write by this logger.</param>
         /// <param name="file">The file path to write to.</param>
-        public FileLogger(string prefix, string file)
+        internal FileLogger(string prefix, string file)
             : base(prefix)
         {
             _file = file;
             _writer = TextWriter.Synchronized(
                 new StreamWriter(new FileStream(file, FileMode.Append, FileAccess.Write, FileShare.Read)));
         }
-
-        public override ILogger CloneWithPrefix(string prefix) => new FileLogger(prefix, _file, _writer);
-
-        public void Dispose() => _writer.Close();
 
         protected override void Write(string message)
         {
@@ -110,24 +110,10 @@ namespace ZeroC.Ice
 
     /// <summary>Creates a logger that trace messages using <see cref="System.Diagnostics.Trace"/> API,
     /// this is the default logger.</summary>
-    public sealed class TraceLogger : Logger
+    internal sealed class TraceLogger : Logger
     {
         private static readonly ConsoleListener _consoleListener = new ConsoleListener();
         private readonly bool _addConsoleListener;
-
-        /// <summary>Creates a new trace logger.</summary>
-        /// <param name="prefix">The prefix to perpend to messages write by this logger.</param>
-        /// <param name="addConsoleListener">If true a listener that writes messages to the standard error is added
-        /// to trace listeners <see cref="Trace.Listeners"/> this is only done once per process.</param>
-        public TraceLogger(string prefix, bool addConsoleListener)
-            : base(prefix)
-        {
-            _addConsoleListener = addConsoleListener;
-            if (addConsoleListener && !System.Diagnostics.Trace.Listeners.Contains(_consoleListener))
-            {
-                System.Diagnostics.Trace.Listeners.Add(_consoleListener);
-            }
-        }
 
         public override ILogger CloneWithPrefix(string prefix) => new TraceLogger(prefix, _addConsoleListener);
         public override void Error(string message)
@@ -146,6 +132,20 @@ namespace ZeroC.Ice
         {
             System.Diagnostics.Trace.TraceWarning(Format("warning", message));
             System.Diagnostics.Trace.Flush();
+        }
+
+        /// <summary>Creates a new trace logger.</summary>
+        /// <param name="prefix">The prefix to perpend to messages write by this logger.</param>
+        /// <param name="addConsoleListener">If true a listener that writes messages to the standard error is added
+        /// to trace listeners <see cref="Trace.Listeners"/> this is only done once per process.</param>
+        internal TraceLogger(string prefix, bool addConsoleListener)
+            : base(prefix)
+        {
+            _addConsoleListener = addConsoleListener;
+            if (addConsoleListener && !System.Diagnostics.Trace.Listeners.Contains(_consoleListener))
+            {
+                System.Diagnostics.Trace.Listeners.Add(_consoleListener);
+            }
         }
 
         protected override void Write(string message)
