42d388a9e3c22276e08d0d15a37d13f8750eb175
Joe George <joe@externl.com>
2020-07-07 11:25:39 -0400

    Update swift/src/Ice/InputStream.swift
    Co-authored-by: Jose  <pepone@users.noreply.github.com>

--- swift/src/Ice/InputStream.swift
+++ swift/src/Ice/InputStream.swift
@@ -1017,7 +1017,7 @@ extension EncapsDecoder {
         // If circular references are not allowed we insert null (for cycle detection) and add
         // the object to the map once it has been fully unmarshaled.
         //
-        unmarshaledMap[index] = stream.acceptClassCycles ? v : nil
+        unmarshaledMap.updateValue(stream.acceptClassCycles ? v : nil, forKey:index)
 
         //
         // Read the instance.
