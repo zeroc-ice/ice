6e2e110f5c47dff60394b83caefc97467e078ad3
Jose <pepone@users.noreply.github.com>
2020-08-18 17:34:47 +0200

    Update csharp/src/Ice/IPlugin.cs
    Co-authored-by: Joe George <joe@externl.com>

--- csharp/src/Ice/IPlugin.cs
+++ csharp/src/Ice/IPlugin.cs
@@ -9,7 +9,7 @@ namespace ZeroC.Ice
     public interface IPlugin : IAsyncDisposable
     {
         /// <summary>Perform any necessary initialization steps.</summary>
-        /// <param name="context">The plug-in initialization context allows to register invocation and dispatch
+        /// <param name="context">The plug-in initialization context enables the registration of invocation and dispatch
         /// interceptors, it can also be use to set the communicator logger.</param>
         void Initialize(PluginInitializationContext context);
     }
