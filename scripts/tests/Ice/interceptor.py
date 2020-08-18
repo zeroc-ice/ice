25c18a1a3609bce1625bd0a59990a2b1c431352f
Jose <jose@zeroc.com>
2020-08-18 17:54:21 +0200

    Add TODO comment

--- scripts/tests/Ice/interceptor.py
+++ scripts/tests/Ice/interceptor.py
@@ -2,6 +2,7 @@
 # Copyright (c) ZeroC, Inc. All rights reserved.
 #
 
+# TODO review this once the new interceptor test is ported to all mappings
 if isinstance(Mapping.getByPath(__name__), CSharpMapping):
     TestSuite(__name__, [ClientServerTestCase(), ClientAMDServerTestCase(), CollocatedTestCase()])
 else:
