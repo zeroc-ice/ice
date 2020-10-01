f0d979187004a476e68c8f62f3dda51e5d3239cb
Austin Henriksen <austin@zeroc.com>
2020-09-30 22:14:32 -0400

    Fixed logic for checking empty enums.

--- cpp/src/Slice/Grammar.cpp
+++ cpp/src/Slice/Grammar.cpp
@@ -3578,7 +3578,7 @@ yyreduce:
     if(en)
     {
         EnumeratorListTokPtr enumerators = EnumeratorListTokPtr::dynamicCast(yyvsp[-1]);
-        if(enumerators->v.empty() && !en->underlying() && en->isUnchecked())
+        if(enumerators->v.empty() && (!en->underlying() || !en->isUnchecked()))
         {
             unit->error("enum `" + en->name() + "' must have at least one enumerator");
         }
