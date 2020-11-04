0a2e907f48b9d85d020b17a828c5e9b82222dc33
Jose <jose@zeroc.com>
2020-11-04 20:24:42 +0100

    Remove interceptor colocated test as it was not using colocation

--- scripts/tests/Ice/interceptor.py
+++ scripts/tests/Ice/interceptor.py
@@ -4,6 +4,6 @@
 
 # TODO review this once the new interceptor test is ported to all mappings
 if isinstance(Mapping.getByPath(__name__), CSharpMapping):
-    TestSuite(__name__, [ClientServerTestCase(), ClientAMDServerTestCase(), CollocatedTestCase()])
+    TestSuite(__name__, [ClientServerTestCase(), ClientAMDServerTestCase()])
 else:
     TestSuite(__name__, [ClientTestCase(client=Client(props={"Ice.Warn.Dispatch" : 0}))], libDirs=["interceptortest"])
