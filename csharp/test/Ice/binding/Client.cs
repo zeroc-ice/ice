d4bc84ec246c3ecf395c4ad65ed74830c4c7e817
Austin Henriksen <austin@zeroc.com>
2020-12-21 10:52:23 -0500

    Finished binding test.

--- csharp/test/Ice/binding/Client.cs
+++ csharp/test/Ice/binding/Client.cs
@@ -2,25 +2,24 @@
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Test;
+using ZeroC.Test;
 
 namespace ZeroC.Ice.Test.Binding
 {
     public class Client : TestHelper
     {
-        public override async Task RunAsync(string[] args)
+        public override Task RunAsync(string[] args) => AllTests.RunAsync(this);
+
+        public static async Task<int> Main(string[] args)
         {
             Dictionary<string, string> properties = CreateTestProperties(ref args);
-
             // Under out-of-FDs conditions, the server might close the connection after it accepted it and
             // sent the connection validation message (the failure typically occurs when calling StartRead on
             // the transport).
             properties["Ice.Warn.Connections"] = "0";
 
-            await using Communicator communicator = Initialize(properties);
-            AllTests.Run(this);
+            await using var communicator = CreateCommunicator(properties);
+            return await RunTestAsync<Client>(communicator, args);
         }
-
-        public static Task<int> Main(string[] args) => TestDriver.RunTestAsync<Client>(args);
     }
 }
