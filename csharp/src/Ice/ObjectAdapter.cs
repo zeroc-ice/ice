5dcfabcdea6676978be3418b631910bbcaeda2ea
Jose <jose@zeroc.com>
2020-12-28 16:22:01 +0100

    Cleanup unused using directives

--- csharp/src/Ice/ObjectAdapter.cs
+++ csharp/src/Ice/ObjectAdapter.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
--- csharp/src/Ice/TaskExtensions.cs
+++ csharp/src/Ice/TaskExtensions.cs
@@ -1,7 +1,5 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-using System;
-using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
