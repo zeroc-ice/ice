f9024ccb994273053492ec1b61890a1a3503313d
Jose <jose@zeroc.com>
2020-12-30 17:57:24 +0100

    Fix exception message

--- scripts/Util.py
+++ scripts/Util.py
@@ -807,7 +807,8 @@ class Mapping(object):
     @classmethod
     def getByName(self, name):
         if not name in self.mappings:
-            raise RuntimeError("unknown mapping `{0}', available mappings `{1}'".format(name, self.mappings))
+            raise RuntimeError("unknown mapping: `{0}', known mappings: `{1}'".format(
+                name, list(self.mappings)))
         return self.mappings.get(name)
 
     @classmethod
