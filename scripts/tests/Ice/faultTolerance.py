bfb82ed043bfcadd8a9cae63cc79a3665696a541
Benoit Foucher <benoit@zeroc.com>
2021-01-20 10:27:24 +0100

    Added tracing to Ice/faultTolerance test, fixes #1251

--- scripts/tests/Ice/faultTolerance.py
+++ scripts/tests/Ice/faultTolerance.py
@@ -4,6 +4,18 @@
 
 from Util import *
 
+# This is used for the trace file
+props = lambda process, current: {
+    "Ice.ProgramName": "server{}".format(process.args[0])
+}
+
+# Enable some tracing to allow investigating test failures
+traceProps = {
+    "Ice.Trace.Network": 3,
+    "Ice.Trace.Retry": 1,
+    "Ice.Trace.Protocol": 1,
+}
+
 #
 # Start 12 servers
 #
@@ -11,5 +23,6 @@ servers=range(1, 13)
 
 TestSuite(__name__, [
     ClientServerTestCase(client=Client(args=[i for i in servers]),
-                         servers=[Server(args=[i], waitForShutdown=False, quiet=True) for i in servers])
+                         servers=[Server(args=[i], waitForShutdown=False, props=props, quiet=True) for i in servers],
+                         traceProps=traceProps)
 ])
