zeroc-ice/ice

commit 884258479b0243853d3aa958aa9171703abd0fd4

Update "ice" JavaScript npm module to work with browser bundlers (#1345)

js/src/Ice/ModuleRegistry.js
@@ -2,54 +2,120 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 //
 
-const modules = {};
+let Ice = {};
 
-class _ModuleRegistry
+if (typeof process !== 'undefined')
 {
-    static module(name)
-    {
-        let m = modules[name];
-        if(m === undefined)
-        {
-            m = {};
-            modules[name] = m;
-        }
-        return m;
-    }
+    const modules = {};
 
-    static require(m, paths)
+    class _ModuleRegistry
     {
-        let o;
-        paths.forEach(path =>
+        static module(name)
+        {
+            let m = modules[name];
+            if(m === undefined)
             {
-                o = m.require(path);
-            });
-        return o;
-    }
-
-    static type(scoped)
-    {
-        if(scoped === undefined)
-        {
-            return undefined;
+                m = {};
+                modules[name] = m;
+            }
+            return m;
         }
 
-        const components = scoped.split(".");
-        let T = modules;
-
-        for(let i = 0; i < components.length; ++i)
+        static require(m, paths)
         {
-            T = T[components[i]];
-            if(T === undefined)
+            let o;
+            paths.forEach(path =>
+                {
+                    o = m.require(path);
+                });
+            return o;
+        }
+
+        static type(scoped)
+        {
+            if(scoped === undefined)
             {
                 return undefined;
             }
+
+            const components = scoped.split(".");
+            let type = modules;
+
+            for(let i = 0; i < components.length; ++i)
+            {
+                type = T[components[i]];
+                if(type === undefined)
+                {
+                    return undefined;
+                }
+            }
+            return type;
         }
-        return T;
     }
+
+    Ice = _ModuleRegistry.module("Ice");
+    Ice.Slice = Ice.Slice || {};
+    Ice._ModuleRegistry = _ModuleRegistry;
+}
+else
+{
+    /* global
+        self : false
+    */
+    const root = typeof window !== "undefined" ? window :
+        typeof global !== "undefined" ? global :
+        typeof self !== "undefined" ? self : {};
+    /* global
+        self : true
+    */
+    class _ModuleRegistry
+    {
+        static module(name)
+        {
+            let m = root[name];
+            if(m === undefined)
+            {
+                m = {};
+                root[name] = m;
+            }
+            return m;
+        }
+
+        static require(name)
+        {
+            return root;
+        }
+
+        static type(scoped)
+        {
+            if(scoped === undefined)
+            {
+                return undefined;
+            }
+            const components = scoped.split(".");
+            let T = root;
+
+            for(let i = 0, length = components.length; i < length; ++i)
+            {
+                T = T[components[i]];
+                if(T === undefined)
+                {
+                    return undefined;
+                }
+            }
+            return T;
+        }
+    }
+
+    Ice = _ModuleRegistry.module("Ice");
+
+    Ice._require = function()
+    {
+        return root;
+    };
+
+    Ice.Slice = Ice.Slice || {};
+    Ice._ModuleRegistry = _ModuleRegistry;
 }
 
-const Ice = _ModuleRegistry.module("Ice");
-Ice.Slice = Ice.Slice || {};
-Ice._ModuleRegistry = _ModuleRegistry;
 exports.Ice = Ice;

js/src/Ice/browser/ModuleRegistry.js
@@ -1,62 +1,0 @@
-//
-// Copyright (c) ZeroC, Inc. All rights reserved.
-//
-
-/* global
-    self : false
-*/
-const root = typeof window !== "undefined" ? window :
-             typeof global !== "undefined" ? global :
-             typeof self !== "undefined" ? self : {};
-/* global
-    self : true
-*/
-
-class _ModuleRegistry
-{
-    static module(name)
-    {
-        let m = root[name];
-        if(m === undefined)
-        {
-            m = {};
-            root[name] = m;
-        }
-        return m;
-    }
-
-    static require(name)
-    {
-        return root;
-    }
-
-    static type(scoped)
-    {
-        if(scoped === undefined)
-        {
-            return undefined;
-        }
-        const components = scoped.split(".");
-        let T = root;
-
-        for(let i = 0, length = components.length; i < length; ++i)
-        {
-            T = T[components[i]];
-            if(T === undefined)
-            {
-                return undefined;
-            }
-        }
-        return T;
-    }
-}
-
-const Ice = _ModuleRegistry.module("Ice");
-
-Ice._require = function()
-{
-    return root;
-};
-
-Ice.Slice = Ice.Slice || {};
-Ice._ModuleRegistry = _ModuleRegistry;

commit 01c0c424c13b4017151f29f7a0a1c20a1f8010e6

Fix JavaScript type resolution bug introduced with #1345

js/src/Ice/ModuleRegistry.js
@@ -40,10 +40,9 @@ if (typeof process !== 'undefined')
 
             const components = scoped.split(".");
             let type = modules;
-
             for(let i = 0; i < components.length; ++i)
             {
-                type = T[components[i]];
+                type = type[components[i]];
                 if(type === undefined)
                 {
                     return undefined;
@@ -93,17 +92,16 @@ else
                 return undefined;
             }
             const components = scoped.split(".");
-            let T = root;
-
+            let type = root;
             for(let i = 0, length = components.length; i < length; ++i)
             {
-                T = T[components[i]];
-                if(T === undefined)
+                type = type[components[i]];
+                if(type === undefined)
                 {
                     return undefined;
                 }
             }
-            return T;
+            return type;
         }
     }
 