fd6d6d4c4408e34c880e58ec22b3d6664291666b
Jose <jose@zeroc.com>
2022-07-26 17:27:36 +0200

    Update android test controller emulator checks

--- java/test/android/controller/src/main/java/com/zeroc/testcontroller/ControllerApp.java
+++ java/test/android/controller/src/main/java/com/zeroc/testcontroller/ControllerApp.java
@@ -172,14 +172,15 @@ public class ControllerApp extends Application
 
     public static boolean isEmulator()
     {
-        return Build.FINGERPRINT.startsWith("generic") ||
-               Build.FINGERPRINT.startsWith("unknown") ||
-               Build.MODEL.contains("google_sdk") ||
-               Build.MODEL.contains("Emulator") ||
-               Build.MODEL.contains("Android SDK built for x86") ||
-               Build.MANUFACTURER.contains("Genymotion") ||
-               (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")) ||
-               Build.PRODUCT.equals("google_sdk");
+        return Build.FINGERPRINT.startsWith("google/sdk_gphone") ||
+            Build.FINGERPRINT.startsWith("generic") ||
+            Build.FINGERPRINT.startsWith("unknown") ||
+            Build.MODEL.contains("google_sdk") ||
+            Build.MODEL.contains("Emulator") ||
+            Build.MODEL.contains("Android SDK built for x86") ||
+            Build.MANUFACTURER.contains("Genymotion") ||
+            (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")) ||
+            Build.PRODUCT.equals("google_sdk");
     }
 
     class ControllerI
