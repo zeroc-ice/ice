f38b3a63413763cf2930fa5cf0ac288de6cdc60e
Jose <jose@zeroc.com>
2023-03-07 19:35:24 +0100

    Fix build failure

--- cpp/src/IceUtil/StringConverter.cpp
+++ cpp/src/IceUtil/StringConverter.cpp
@@ -2,18 +2,18 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 //
 
+#include <IceUtil/StringConverter.h>
+#include <IceUtil/MutexPtrLock.h>
+#include <IceUtil/Mutex.h>
+#include <IceUtil/StringUtil.h>
+
 // TODO codecvt was deprecated in C++17 and cause build failures in C++17 mode
-#if (_cplusplus >= 201703L)
+#if (ICE_CPLUSPLUS >= 201703L)
 #   include <IceUtil/DisableWarnings.h>
 #elif defined(_MSC_VER) && (_MSVC_LANG >= 201703L)
     // For VC++ we should replace this code with MultiByteToWideChar() and WideCharToMultiByte()
 #   define  _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
 #endif
-
-#include <IceUtil/StringConverter.h>
-#include <IceUtil/MutexPtrLock.h>
-#include <IceUtil/Mutex.h>
-#include <IceUtil/StringUtil.h>
 
 #ifdef ICE_HAS_CODECVT_UTF8
 #include <codecvt>
