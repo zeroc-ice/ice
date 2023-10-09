143d504bd8ced1fe4405707bc476dff41b634c99
Joe George <joe@externl.com>
2023-10-09 11:54:12 -0400

    Update cpp/src/slice2java/GenCompat.cpp
    Co-authored-by: Austin Henriksen <austin.r.henriksen79@gmail.com>

--- cpp/src/slice2java/GenCompat.cpp
+++ cpp/src/slice2java/GenCompat.cpp
@@ -211,7 +211,7 @@ writeParamList(Output& out, vector<string> params, bool end = true, bool newLine
 }
 
 // Returns the "length" of the constructor parameter list from the given data member list.
-// All types are counted as 1, except for long and double which are counted as 2.
+// All types are counted as 1 unit, except for long and double which are counted as 2 units.
 // See https://docs.oracle.com/javase/specs/jvms/se20/html/jvms-4.html#jvms-4.3.3
 int
 constructorParameterLength(const DataMemberList& members)
