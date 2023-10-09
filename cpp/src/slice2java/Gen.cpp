c8a75133cc47363522bc61b1c1e3b023c7cf16e3
Joe George <joe@externl.com>
2023-10-09 11:54:19 -0400

    Update cpp/src/slice2java/Gen.cpp
    Co-authored-by: Austin Henriksen <austin.r.henriksen79@gmail.com>

--- cpp/src/slice2java/Gen.cpp
+++ cpp/src/slice2java/Gen.cpp
@@ -137,7 +137,7 @@ string ofFactory(const TypePtr& type)
 }
 
 // Returns the "length" of the constructor parameter list from the given data member list.
-// All types are counted as 1, except for long and double which are counted as 2.
+// All types are counted as 1 unit, except for long and double which are counted as 2 units.
 // See https://docs.oracle.com/javase/specs/jvms/se20/html/jvms-4.html#jvms-4.3.3
 int
 constructorParameterLength(const DataMemberList& members)
