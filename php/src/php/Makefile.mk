debc1c008a7dd9d3e28fc670613ba52d1d8f43a8
Joe George <joe@zeroc.com>
2023-12-18 13:41:53 -0500

    Fix PHP library build to include missing functions from Slice

--- php/src/php/Makefile.mk
+++ php/src/php/Makefile.mk
@@ -11,7 +11,11 @@ IcePHP_installdir       := $(install_phplibdir)
 IcePHP_cppflags         := -I$(project) -I$(top_srcdir)/cpp/src $(ice_cpp_cppflags) $(php_cppflags)
 IcePHP_ldflags          := $(php_ldflags)
 IcePHP_dependencies     := IceDiscovery IceLocatorDiscovery IceSSL Ice
-IcePHP_extra_sources    := $(top_srcdir)/cpp/src/Slice/PHPUtil.cpp
+IcePHP_extra_sources    := $(filter-out %Util.cpp %Ruby.cpp,\
+                           $(wildcard $(top_srcdir)/cpp/src/Slice/*.cpp)) \
+                           $(top_srcdir)/cpp/src/Slice/SliceUtil.cpp \
+                           $(top_srcdir)/cpp/src/Slice/PHPUtil.cpp \
+                           $(top_srcdir)/cpp/src/Slice/StringLiteralUtil.cpp \
 
 projects += $(project)
 srcs:: $(project)
