8ba87349efb1d2c48a82d8aca2dabf3ba7d85018
Bernard Normier <bernard@zeroc.com>
2023-12-23 15:57:26 -0500

    Fix IceIAP build

--- cpp/src/IceIAP/Makefile.mk
+++ cpp/src/IceIAP/Makefile.mk
@@ -9,7 +9,10 @@ IceIAP_platforms        := iphoneos iphonesimulator
 
 IceIAP_targetdir        := $(libdir)
 IceIAP_dependencies     := Ice
-IceIAP_sliceflags       := --include-dir IceIAP
 IceIAP_cppflags         := -DICEIAP_API_EXPORTS
+
+# We exclude the following Slice files that contain only local definitions, as we don't want two header files with the
+# same name.
+IceIAP_excludes         += $(wildcard ../slice/IceIAP/*.ice)
 
 projects += $(project)
--- cpp/src/IceSSL/Makefile.mk
+++ cpp/src/IceSSL/Makefile.mk
@@ -7,7 +7,6 @@ $(project)_libraries    := IceSSL
 IceSSL_targetdir        := $(libdir)
 IceSSL_dependencies     := Ice
 IceSSL_cppflags         := -DICESSL_API_EXPORTS
-IceSSL_sliceflags       := --include-dir IceSSL
 
 #
 # Exclude sources that are not build with the default
@@ -17,9 +16,7 @@ IceSSL_excludes         += $(wildcard src/IceSSL/SChannel*.cpp)
 
 # We exclude the following Slice files that contain only local definitions, as we don't want two header files with the
 # same name.
-IceSSL_excludes            += ../slice/IceSSL/ConnectionInfo.ice \
-                              ../slice/IceSSL/ConnectionInfoF.ice \
-                              ../slice/IceSSL/EndpointInfo.ice \
+IceSSL_excludes         += $(wildcard ../slice/IceSSL/*.ice)
 
 ifeq ($(os),Darwin)
 IceSSL_excludes         += $(wildcard src/IceSSL/OpenSSL*.cpp)
