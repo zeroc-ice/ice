ac98b4fcb9979f0709f1524fde28937cc521fe3d
Bernard Normier <bernard@zeroc.com>
2024-01-26 10:12:25 -0500

    Remove LocalObject from php

--- php/lib/Ice.php
+++ php/lib/Ice.php
@@ -116,13 +116,11 @@ namespace Ice
 
     global $Ice__t_Value;
     global $Ice__t_ObjectSeq;
-    global $Ice__t_LocalObject;
     global $Ice__t_ObjectPrx;
     global $Ice__t_ObjectProxySeq;
 
     $Ice__t_Value = IcePHP_defineClass('::Ice::Object', "\\Ice\\Value", -1, false, false, null, null);
     $Ice__t_ObjectSeq = IcePHP_defineSequence('::Ice::ObjectSeq', $Ice__t_Value);
-    $Ice__t_LocalObject = IcePHP_defineClass('::Ice::LocalObject', "\\Ice\\LocalObject", -1, false, false, null, null);
     $Ice__t_ObjectPrx = IcePHP_defineProxy('::Ice::Object', null, null);
     $Ice__t_ObjectProxySeq = IcePHP_defineSequence('::Ice::ObjectProxySeq', $Ice__t_ObjectPrx);
 
