bf46886bfff17422601b056d47ecd6d072bc9f74
Jose <jose@zeroc.com>
2024-02-02 14:07:26 +0100

    PHP and Ruby build fixes

--- ruby/src/IceRuby/Makefile.mk
+++ ruby/src/IceRuby/Makefile.mk
@@ -11,7 +11,7 @@ IceRuby_installdir      := $(install_rubylibdir)
 IceRuby_cppflags        := -I$(project) $(ice_cpp_cppflags) -I$(top_srcdir)/cpp/src -I$(top_srcdir)/cpp/src/slice2rb \
     $(ruby_cppflags)
 IceRuby_system_libs     := $(ruby_ldflags)
-IceRubydependencies     := IceDiscovery IceLocatorDiscovery IceSSL Ice
+IceRuby_dependencies     := IceDiscovery IceLocatorDiscovery IceSSL Ice
 IceRuby_libs            := mcpp
 IceRuby_extra_sources   := $(wildcard $(top_srcdir)/cpp/src/Slice/*.cpp) \
                            $(top_srcdir)/cpp/src/slice2rb/RubyUtil.cpp   \
