5c75038d6ffeaa480ade5a6927c1bc7db1cb2b21
Bernard Normier <bernard@zeroc.com>
2024-03-07 14:44:14 -0500

    Merge Properties and PropertiesI

--- cpp/include/Ice/Properties.h
+++ cpp/include/Ice/Properties.h
@@ -8,9 +8,11 @@
 #include "Config.h"
 #include "Ice/BuiltinSequences.h"
 #include "Ice/PropertyDict.h"
+#include "StringConverter.h"
 
+#include <set>
+#include <string>
 #include <string_view>
-#include <optional>
 
 namespace Ice
 {
@@ -19,13 +21,33 @@ namespace Ice
  * A property set used to configure Ice and Ice applications. Properties are key/value pairs, with both keys and
  * values being strings. By convention, property keys should have the form
  * <em>application-name</em>[.<em>category</em>[.<em>sub-category</em>]].<em>name</em>.
+ * This class is thread-safe: multiple threads can safely read and write the properties without their own
+ * synchronization.
  * \headerfile Ice/Ice.h
  */
-class ICE_API Properties
+class ICE_API Properties final
 {
 public:
 
-    virtual ~Properties() = default;
+    /**
+     * Default constructor.
+    */
+    Properties() = default;
+
+    /**
+     * Copy constructor.
+     * @param source The property set to copy.
+     */
+    Properties(const Properties& source);
+
+    /**
+     * Constructs a property from command-line arguments and a default property set.
+     * @param args The command-line arguments. Property arguments are removed from this sequence.
+     * @param defaults The default property set.
+     */
+    Properties(StringSeq& args, const std::shared_ptr<Properties>& defaults);
+
+    Properties& operator=(const Properties& rhs) = delete;
 
     /**
      * Get a property by key. If the property is not set, an empty string is returned.
@@ -33,7 +55,7 @@ public:
      * @return The property value.
      * @see #setProperty
      */
-    virtual std::string getProperty(std::string_view key) noexcept = 0;
+    std::string getProperty(std::string_view key) noexcept;
 
     /**
      * Get a property by key. If the property is not set, the given default value is returned.
@@ -42,7 +64,7 @@ public:
      * @return The property value or the default value.
      * @see #setProperty
      */
-    virtual std::string getPropertyWithDefault(std::string_view key, std::string_view value) noexcept = 0;
+    std::string getPropertyWithDefault(std::string_view key, std::string_view value) noexcept;
 
     /**
      * Get a property as an integer. If the property is not set, 0 is returned.
@@ -50,7 +72,7 @@ public:
      * @return The property value interpreted as an integer.
      * @see #setProperty
      */
-    virtual int getPropertyAsInt(std::string_view key) noexcept = 0;
+    int getPropertyAsInt(std::string_view key) noexcept;
 
     /**
      * Get a property as an integer. If the property is not set, the given default value is returned.
@@ -59,7 +81,7 @@ public:
      * @return The property value interpreted as an integer, or the default value.
      * @see #setProperty
      */
-    virtual int getPropertyAsIntWithDefault(std::string_view key, int value) noexcept = 0;
+    int getPropertyAsIntWithDefault(std::string_view key, int value) noexcept;
 
     /**
      * Get a property as a list of strings. The strings must be separated by whitespace or comma. If the property is
@@ -71,7 +93,7 @@ public:
      * @return The property value interpreted as a list of strings.
      * @see #setProperty
      */
-    virtual StringSeq getPropertyAsList(std::string_view key) noexcept = 0;
+    StringSeq getPropertyAsList(std::string_view key) noexcept;
 
     /**
      * Get a property as a list of strings.  The strings must be separated by whitespace or comma. If the property is
@@ -84,7 +106,7 @@ public:
      * @return The property value interpreted as list of strings, or the default value.
      * @see #setProperty
      */
-    virtual StringSeq getPropertyAsListWithDefault(std::string_view key, const StringSeq& value) noexcept = 0;
+    StringSeq getPropertyAsListWithDefault(std::string_view key, const StringSeq& value) noexcept;
 
     /**
      * Get all properties whose keys begins with <em>prefix</em>. If <em>prefix</em> is an empty string, then all
@@ -92,7 +114,7 @@ public:
      * @param prefix The prefix to search for (empty string if none).
      * @return The matching property set.
      */
-    virtual PropertyDict getPropertiesForPrefix(std::string_view prefix) noexcept = 0;
+    PropertyDict getPropertiesForPrefix(std::string_view prefix) noexcept;
 
     /**
      * Set a property. To unset a property, set it to the empty string.
@@ -100,14 +122,14 @@ public:
      * @param value The property value.
      * @see #getProperty
      */
-    virtual void setProperty(std::string_view key, std::string_view value) = 0;
+    void setProperty(std::string_view key, std::string_view value);
 
     /**
      * Get a sequence of command-line options that is equivalent to this property set. Each element of the returned
      * sequence is a command-line option of the form <code>--<em>key</em>=<em>value</em></code>.
      * @return The command line options for this property set.
      */
-    virtual StringSeq getCommandLineOptions() noexcept = 0;
+    StringSeq getCommandLineOptions() noexcept;
 
     /**
      * Convert a sequence of command-line options into properties. All options that begin with
@@ -117,7 +139,7 @@ public:
      * @param options The command-line options.
      * @return The command-line options that do not start with the specified prefix, in their original order.
      */
-    virtual StringSeq parseCommandLineOptions(std::string_view prefix, const StringSeq& options) = 0;
+    StringSeq parseCommandLineOptions(std::string_view prefix, const StringSeq& options);
 
     /**
      * Convert a sequence of command-line options into properties. All options that begin with one of the following
@@ -126,19 +148,50 @@ public:
      * @param options The command-line options.
      * @return The command-line options that do not start with one of the listed prefixes, in their original order.
      */
-    virtual StringSeq parseIceCommandLineOptions(const StringSeq& options) = 0;
+    StringSeq parseIceCommandLineOptions(const StringSeq& options);
 
     /**
      * Load properties from a file.
      * @param file The property file.
      */
-    virtual void load(std::string_view file) = 0;
+    void load(std::string_view file);
 
     /**
      * Create a copy of this property set.
      * @return A copy of this property set.
      */
-    virtual std::shared_ptr<Properties> clone() noexcept = 0;
+    std::shared_ptr<Properties> clone() { return std::make_shared<Properties>(*this); }
+
+    /**
+     * Get the properties that were never read.
+     * @return A list of unused properties.
+     */
+    std::set<std::string> getUnusedProperties();
+
+private:
+
+    void parseLine(std::string_view, const StringConverterPtr&);
+
+    void loadConfig();
+
+    struct PropertyValue
+    {
+        PropertyValue() :
+            used(false)
+        {
+        }
+
+        PropertyValue(std::string_view v, bool u) :
+            value(std::string{v}),
+            used(u)
+        {
+        }
+
+        std::string value;
+        bool used;
+    };
+    std::map<std::string, PropertyValue, std::less<>> _properties;
+    std::mutex _mutex;
 };
 
 using PropertiesPtr = std::shared_ptr<Properties>;
