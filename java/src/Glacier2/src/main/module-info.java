c1053899703158ccda9294dccd4f43a595f4648c
Jose <jose@zeroc.com>
2024-03-18 18:47:18 +0100

    Avoid creating Java split packages - Fix #425

---
+++ java/src/Glacier2/src/main/module-info.java
@@ -0,0 +1,9 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+module com.zeroc.glacier2 {
+    exports com.zeroc.Glacier2;
+    exports com.zeroc.Glacier2.IceMX;
+    requires transitive com.zeroc.ice;
+}
