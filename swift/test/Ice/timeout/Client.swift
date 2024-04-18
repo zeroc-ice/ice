b965be1017914cdd59e275f21d7299df41f3d4ff
Bernard Normier <bernard@zeroc.com>
2024-04-18 23:07:22 +0200

    Fix Swift test

--- swift/test/Ice/timeout/Client.swift
+++ swift/test/Ice/timeout/Client.swift
@@ -8,7 +8,10 @@ import TestCommon
 class Client: TestHelperI {
   override public func run(args: [String]) throws {
     do {
-      let communicator = try initialize(args)
+      let properties = try createTestProperties(args)
+      properties.setProperty(key: "Ice.Connection.ConnectTimeout", value: "1")
+      properties.setProperty(key: "Ice.Connection.CloseTimeout", value: "1")
+      let communicator = try initialize(properties)
       defer {
         communicator.destroy()
       }
