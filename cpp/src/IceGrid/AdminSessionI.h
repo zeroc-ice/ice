9139133ec5d868a96c73a1a3423af7013cccdcff
Bernard Normier <bernard@zeroc.com>
2024-04-26 11:43:51 -0400

    Remove IceGrid SessionTimeout properties

--- cpp/src/IceGrid/AdminSessionI.h
+++ cpp/src/IceGrid/AdminSessionI.h
@@ -26,7 +26,7 @@ namespace IceGrid
 
         Ice::ObjectPrx _register(const std::shared_ptr<SessionServantManager>&, const Ice::ConnectionPtr&);
 
-        void keepAlive(const Ice::Current& current) override { BaseSessionI::keepAlive(current); }
+        void keepAlive(const Ice::Current&) override {} // no-op
 
         std::optional<AdminPrx> getAdmin(const Ice::Current&) const override;
         std::optional<Ice::ObjectPrx> getAdminCallbackTemplate(const Ice::Current&) const override;
