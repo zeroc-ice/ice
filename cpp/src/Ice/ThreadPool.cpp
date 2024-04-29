4df2cf7a74583374e6a95cadce962446ed9936d0
Benoit Foucher <benoit@zeroc.com>
2024-04-29 15:56:42 +0200

    Format fix

--- cpp/src/Ice/ThreadPool.cpp
+++ cpp/src/Ice/ThreadPool.cpp
@@ -866,9 +866,7 @@ IceInternal::ThreadPool::ioCompleted(ThreadPoolCurrent& current)
         {
             Warning out(_instance->initializationData().logger);
             out << "thread pool `" << _prefix << "' is running low on threads\n"
-                << "Size=" << _size << ", "
-                << "SizeMax=" << _sizeMax << ", "
-                << "SizeWarn=" << _sizeWarn;
+                << "Size=" << _size << ", " << "SizeMax=" << _sizeMax << ", " << "SizeWarn=" << _sizeWarn;
         }
 
         if (!_destroyed)
