ff4c91757d9f48e0f696f591a9749b1a86967eb6
Joe George <joe@zeroc.com>
2024-05-02 12:37:10 -0400

    Fix MATLAB

--- matlab/test/Ice/properties/Client.m
+++ matlab/test/Ice/properties/Client.m
@@ -3,7 +3,6 @@
 %
 
 function client(args)
-    addpath('generated');
     if ~libisloaded('ice')
         loadlibrary('ice', @iceproto)
     end
@@ -11,28 +10,28 @@ function client(args)
     props = Ice.createProperties()
 
     fprintf('testing ice properties with set default values...');
-    toStringMode = props.getIceProperty("Ice.ToStringMode")
-    assert(strcmp(toStringMode, "Unicode"))
-    closeTimeout = props.getIcePropertyAsInt("Ice.Connection.CloseTimeout")
-    assert(closeTimeout == 10)
-    retryIntervals = props.getIcePropertyAsList("Ice.RetryIntervals")
+    toStringMode = props.getIceProperty('Ice.ToStringMode');
+    assert(strcmp(toStringMode, 'Unicode'));
+    closeTimeout = props.getIcePropertyAsInt('Ice.Connection.CloseTimeout');
+    assert(closeTimeout == 10);
+    retryIntervals = props.getIcePropertyAsList('Ice.RetryIntervals');
     assert(length(retryIntervals) == 1);
-    assert(strcmp(retryIntervals{1}, "0"))
+    assert(strcmp(retryIntervals{1}, '0'));
     fprintf('ok\n');
 
     fprintf('testing ice properties with unset default values...');
-    stringValue = props.getIceProperty("Ice.Admin.Router")
-    assert(strcmp(stringValue, ""))
-    intValue = props.getIcePropertyAsInt("Ice.Admin.Router")
-    assert(intValue == 0)
-    listValue = props.getIcePropertyAsList("Ice.Admin.Router")
+    stringValue = props.getIceProperty('Ice.Admin.Router');
+    assert(strcmp(stringValue, ''));
+    intValue = props.getIcePropertyAsInt('Ice.Admin.Router');
+    assert(intValue == 0);
+    listValue = props.getIcePropertyAsList('Ice.Admin.Router');
     assert(length(listValue) == 0);
     fprintf('ok\n');
 
     fprintf('testing that getting an unknown ice property throws an exception...');
     try
-        props.getIceProperty("Ice.UnknownProperty")
-        assert(false)
+        props.getIceProperty('Ice.UnknownProperty');
+        assert(false);
     catch ex
     end
     fprintf('ok\n');
