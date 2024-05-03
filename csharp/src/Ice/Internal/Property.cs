51b008831e76e339139eb728f89910720d351904
Joe George <joe@zeroc.com>
2024-05-03 13:49:30 -0400

    Cleanup Property and PropertyValue to use records (#2113)
    Closes #2104

--- csharp/src/Ice/Internal/Property.cs
+++ csharp/src/Ice/Internal/Property.cs
@@ -2,42 +2,4 @@
 
 namespace Ice.Internal;
 
-public sealed class Property
-{
-    public Property(string pattern, string defaultValue, bool deprecated, string deprecatedBy)
-    {
-        _pattern = pattern;
-        _defaultValue = defaultValue;
-        _deprecated = deprecated;
-        _deprecatedBy = deprecatedBy;
-    }
-
-    public string
-    pattern()
-    {
-        return _pattern;
-    }
-
-    public string
-    defaultValue()
-    {
-        return _defaultValue;
-    }
-
-    public bool
-    deprecated()
-    {
-        return _deprecated;
-    }
-
-    public string
-    deprecatedBy()
-    {
-        return _deprecatedBy;
-    }
-
-    private string _pattern;
-    private string _defaultValue;
-    private bool _deprecated;
-    private string _deprecatedBy;
-}
+public sealed record class Property(string pattern, string defaultValue, bool deprecated, string deprecatedBy);
