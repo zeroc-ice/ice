458531f5057908a8fc9557eb320fde32fdd63e60
Joe George <joe@zeroc.com>
2024-05-07 09:07:19 -0400

    Formatting

--- csharp/src/Ice/Internal/Property.cs
+++ csharp/src/Ice/Internal/Property.cs
@@ -2,4 +2,9 @@
 
 namespace Ice.Internal;
 
-public sealed record class Property(string pattern, bool usesRegex, string defaultValue, bool deprecated, string deprecatedBy);
+public sealed record class Property(
+    string pattern,
+    bool usesRegex,
+    string defaultValue,
+    bool deprecated,
+    string deprecatedBy);
