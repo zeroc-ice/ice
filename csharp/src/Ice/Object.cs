61fff7e8a9a3b21569ffeadd10f28a12b147bc8d
Bernard Normier <bernard@zeroc.com>
2024-05-10 19:32:03 -0400

    Update csharp/src/Ice/Object.cs
    Co-authored-by: Joe George <joe@externl.com>

--- csharp/src/Ice/Object.cs
+++ csharp/src/Ice/Object.cs
@@ -55,7 +55,7 @@ public interface Object : ICloneable
     string ice_id(Current current);
 
     /// <summary>
-    /// Dispatches a dispatch to a servant. This method is used by dispatch interceptors to forward a dispatch
+    /// Dispatches a request to a servant. This method is used by dispatch interceptors to forward a dispatch
     /// to a servant (or to another interceptor).
     /// </summary>
     /// <param name="request">The details of the dispatch.</param>
