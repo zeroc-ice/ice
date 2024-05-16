d9e23295a267057b5474c8f4fa3f903c55f8e6bf
Bernard Normier <bernard@zeroc.com>
2024-05-16 11:05:32 -0400

    Fix more review comments

--- csharp/src/Ice/OutgoingResponse.cs
+++ csharp/src/Ice/OutgoingResponse.cs
@@ -69,18 +69,3 @@ public sealed class OutgoingResponse
     {
     }
 }
-
-/// <summary>
-/// Represents the status of a reply.
-/// </summary>
-public enum ReplyStatus : byte
-{
-    Ok = 0,
-    UserException = 1,
-    ObjectNotExist = 2,
-    FacetNotExist = 3,
-    OperationNotExist = 4,
-    UnknownLocalException = 5,
-    UnknownUserException = 6,
-    UnknownException = 7
-};
