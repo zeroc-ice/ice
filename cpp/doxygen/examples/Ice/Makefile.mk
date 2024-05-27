462d9c007d420efd34f217e4270585a17afaebbc
Jose <jose@zeroc.com>
2024-05-27 18:10:57 +0200

    Doxygen examples build fixes

--- cpp/doxygen/examples/Ice/Makefile.mk
+++ cpp/doxygen/examples/Ice/Makefile.mk
@@ -4,13 +4,15 @@
 
 $(project)_libraries    = IceExamples
 
-IceExamples_dependencies        := Ice
-IceExamples__targetdir        := doxygen/lib
+IceExamples_dependencies     := Ice
+IceExamples_targetdir        := doxygen/examples/lib
 
 ifeq ($(os),Darwin)
 IceExamples_extra_sources += $(filter-out doxygen/examples/Ice/SSL/OpenSSL%.cpp doxygen/examples/Ice/SSL/Schannel%.cpp, $(wildcard doxygen/examples/Ice/SSL/*.cpp))
+IceExamples_ldflags       += -framework Security -framework CoreFoundation
 else
 IceExamples_extra_sources += $(filter-out doxygen/examples/Ice/SSL/SecureTransport%.cpp doxygen/examples/Ice/SSL/Schannel%.cpp, $(wildcard doxygen/examples/Ice/SSL/*.cpp))
+IceExamples_ldflags       += -lssl -lcrypto
 endif
 
 projects += $(project)
