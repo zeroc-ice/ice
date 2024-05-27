78b3004b1de35cbd87ed70a73b7014cc8e27b26e
Jose <jose@zeroc.com>
2024-05-27 10:15:39 +0200

    Review fixes

--- cpp/doxygen/examples/Ice/SSL/OpenSSLServerAuthenticationOptions.cpp
+++ cpp/doxygen/examples/Ice/SSL/OpenSSLServerAuthenticationOptions.cpp
@@ -4,15 +4,19 @@
 
 #include <Ice/Ice.h>
 
-void serverCertificateSelectionCallbackExample() {
+void
+serverCertificateSelectionCallbackExample()
+{
     Ice::CommunicatorHolder communicator = Ice::initialize();
     //! [serverSSLContextSelectionCallback]
-    SSL_CTX *sslContext = SSL_CTX_new(TLS_method());
+    SSL_CTX* sslContext = SSL_CTX_new(TLS_method());
     // Load the server certificate chain from the keychain using SecureTransport APIs.
     communicator->createObjectAdapterWithEndpoints(
-        "Hello", "ssl -h 127.0.0.1 -p 10000",
+        "Hello",
+        "ssl -h 127.0.0.1 -p 10000",
         Ice::SSL::ServerAuthenticationOptions{
-            .serverSSLContextSelectionCallback = [sslContext](const std::string &) {
+            .serverSSLContextSelectionCallback = [sslContext](const std::string&)
+            {
                 // Keep the SSLContext alive for the lifetime of the connection.
                 SSL_CTX_up_ref(sslContext);
                 return sslContext;
@@ -23,26 +27,33 @@ void serverCertificateSelectionCallbackExample() {
     //! [serverSSLContextSelectionCallback]
 }
 
-void serverSetNewSessionCallbackExample() {
+void
+serverSetNewSessionCallbackExample()
+{
     Ice::CommunicatorHolder communicator = Ice::initialize();
     //! [sslNewSessionCallback]
     communicator->createObjectAdapterWithEndpoints(
-        "Hello", "ssl -h 127.0.0.1 -p 10000",
+        "Hello",
+        "ssl -h 127.0.0.1 -p 10000",
         Ice::SSL::ServerAuthenticationOptions{
-            .sslNewSessionCallback = [](SSL *ssl, const std::string &host) {
+            .sslNewSessionCallback = [](SSL* ssl, const std::string& host) {
                 SSL_set_verify(ssl, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, 0);
             }});
     //! [sslNewSessionCallback]
 }
 
-void clientCertificateValidationCallbackExample() {
+void
+clientCertificateValidationCallbackExample()
+{
     Ice::CommunicatorHolder communicator = Ice::initialize();
     //! [clientCertificateValidationCallback]
     communicator->createObjectAdapterWithEndpoints(
-        "Hello", "ssl -h 127.0.0.1 -p 10000",
+        "Hello",
+        "ssl -h 127.0.0.1 -p 10000",
         Ice::SSL::ServerAuthenticationOptions{
             .clientCertificateValidationCallback =
-                [](bool verified, X509_STORE_CTX *ctx,
-                   const Ice::SSL::ConnectionInfoPtr &info) { return verified; }});
+                [](bool verified,
+                   X509_STORE_CTX* ctx,
+                   const Ice::SSL::ConnectionInfoPtr& info) { return verified; }});
     //! [clientCertificateValidationCallback]
 }
