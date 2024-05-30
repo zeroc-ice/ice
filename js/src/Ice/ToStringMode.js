47b1872ee0deb98e390e605b8aae5fb36bad9f8d
Jose <jose@zeroc.com>
2024-05-30 18:42:50 +0200

    JavaScript cleanup

--- js/src/Ice/ToStringMode.js
+++ js/src/Ice/ToStringMode.js
@@ -3,6 +3,11 @@
 //
 
 const Ice = require("../Ice/EnumBase").Ice;
+/**
+ *  The output mode for xxxToString method such as identityToString and proxyToString. The actual encoding format for
+ *  the string is the same for all modes: you don't need to specify an encoding format or mode when reading such a
+ *  string.
+ **/
 Ice.ToStringMode = Ice.Slice.defineEnum(
     [
         ['Unicode', 0],
