59fccb4c13eb532c40dbd255bcea6d12b6219b9b
Bernard Normier <bernard@zeroc.com>
2024-06-10 11:44:46 -0400

    Clear list/stack after building pipeline

--- swift/src/Ice/ObjectAdapterI.swift
+++ swift/src/Ice/ObjectAdapterI.swift
@@ -12,6 +12,7 @@ class ObjectAdapterI: LocalObject<ICEObjectAdapter>, ObjectAdapter, ICEDispatchA
                 for factory in middlewareFactoryList.reversed() {
                     value = factory(value)
                 }
+                middlewareFactoryList.removeAll() // we're done with the factories, release their memory
                 dispatchPipelineValue = value
                 return value
             }
