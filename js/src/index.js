3981ecf256d1885b66174942848b08ad207bb9d6
Jose <jose@zeroc.com>
2024-06-11 12:07:37 +0200

    Aggregate and export IceMX definitions

--- js/src/index.js
+++ js/src/index.js
@@ -5,4 +5,5 @@
 export { Ice } from "./Ice/index.js";
 export { Glacier2 } from "./Glacier2/index.js";
 export { IceGrid } from "./IceGrid/index.js";
-export { IceStorm } from "./IceStorm/IceStorm.js";
+export { IceStorm } from "./IceStorm/index.js";
+export { IceMX } from "./IceMX/index.js";
