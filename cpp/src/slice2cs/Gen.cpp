3fa559807ce91444f1ebec0a174903eb6a808cf2
Austin Henriksen <austin@zeroc.com>
2024-06-13 11:47:39 -0400

    Random Cleanup (#2290)

--- cpp/src/slice2cs/Gen.cpp
+++ cpp/src/slice2cs/Gen.cpp
@@ -2878,21 +2878,15 @@ Slice::Gen::HelperVisitor::visitInterfaceDefStart(const InterfaceDefPtr& p)
 
     OperationList ops = p->allOperations();
 
-    for (OperationList::const_iterator r = ops.begin(); r != ops.end(); ++r)
+    for (const auto& op : ops)
     {
-        OperationPtr op = *r;
-        InterfaceDefPtr interface = op->interface();
         string opName = fixId(op->name(), DotNet::ICloneable, true);
         TypePtr ret = op->returnType();
         string retS = typeToString(ret, ns, op->returnIsOptional());
 
         vector<string> params = getParams(op, ns);
-        vector<string> args = getArgs(op);
         vector<string> argsAMI = getInArgs(op);
 
-        string deprecateReason = getDeprecationMessageForComment(op, "operation");
-
-        ParamDeclList inParams = op->inParameters();
         ParamDeclList outParams = op->outParameters();
 
         ExceptionList throws = op->throws();
@@ -2968,11 +2962,8 @@ Slice::Gen::HelperVisitor::visitInterfaceDefStart(const InterfaceDefPtr& p)
     }
 
     // Async invocation
-    for (OperationList::const_iterator r = ops.begin(); r != ops.end(); ++r)
+    for (const auto& op : ops)
     {
-        OperationPtr op = *r;
-
-        InterfaceDefPtr interface = op->interface();
         vector<string> paramsAMI = getInParams(op, ns);
         vector<string> argsAMI = getInArgs(op);
 
@@ -2986,8 +2977,6 @@ Slice::Gen::HelperVisitor::visitInterfaceDefStart(const InterfaceDefPtr& p)
         string progress = getEscapedParamName(op, "progress");
 
         TypePtr ret = op->returnType();
-
-        string retS = typeToString(ret, ns, op->returnIsOptional());
 
         string returnTypeS = resultType(op, ns);
 
@@ -3455,13 +3444,11 @@ Slice::Gen::DispatcherVisitor::visitInterfaceDefStart(const InterfaceDefPtr& p)
 
     _out << sb;
 
-    OperationList ops = p->operations();
-
-    for (OperationList::const_iterator i = ops.begin(); i != ops.end(); ++i)
+    for (const auto& op : p->operations())
     {
         string retS;
         vector<string> params, args;
-        string opName = getDispatchParams(*i, retS, params, args, ns);
+        string opName = getDispatchParams(op, retS, params, args, ns);
         _out << sp << nl << "public abstract " << retS << " " << opName << spar << params << epar << ';';
     }
 
