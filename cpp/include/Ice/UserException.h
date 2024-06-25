146b5e99bd870f9aee937e9865456129b85abf43
Bernard Normier <bernard@zeroc.com>
2024-06-24 21:44:46 -0400

    Reformat

--- cpp/include/Ice/UserException.h
+++ cpp/include/Ice/UserException.h
@@ -24,10 +24,7 @@ namespace Ice
         /**
          * Default constructor.
          */
-        UserException()
-            : Exception(nullptr, 0, nullptr)
-        {
-        }
+        UserException() : Exception(nullptr, 0, nullptr) {}
 
         /**
          * Obtains the Slice type ID of this exception.
