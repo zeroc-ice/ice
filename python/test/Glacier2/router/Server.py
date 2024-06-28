3ba16c8651095ee618f6782ea1b25ac6ef72916e
Jose <pepone@users.noreply.github.com>
2024-06-28 17:09:34 +0200

    Remove Python Ice.Application and Glacier2.Application classes (#2370)

--- python/test/Glacier2/router/Server.py
+++ python/test/Glacier2/router/Server.py
@@ -14,6 +14,9 @@ class CallbackI(Test.Callback):
     def initiateCallback(self, proxy, current):
         proxy.callback(current.ctx)
 
+    def initiateCallbackEx(self, proxy, current):
+        proxy.callbackEx(current.ctx)
+
     def shutdown(self, current):
         current.adapter.getCommunicator().shutdown()
 
@@ -22,14 +25,20 @@ class Server(TestHelper):
     def run(self, args):
         with self.initialize(args=args) as communicator:
             communicator.getProperties().setProperty(
-                "DeactivatedAdapter.Endpoints", self.getTestEndpoint(num=1)
-            )
-            communicator.createObjectAdapter("DeactivatedAdapter")
-
-            communicator.getProperties().setProperty(
-                "CallbackAdapter.Endpoints", self.getTestEndpoint()
+                "CallbackAdapter.Endpoints", self.getTestEndpoint(num=0)
             )
             adapter = communicator.createObjectAdapter("CallbackAdapter")
-            adapter.add(CallbackI(), Ice.stringToIdentity("callback"))
+
+            # The test allows "c1" as category.
+            adapter.add(CallbackI(), Ice.stringToIdentity("c1/callback"))
+
+            # The test allows "c2" as category.
+            adapter.add(CallbackI(), Ice.stringToIdentity("c2/callback"))
+
+            # The test rejects "c3" as category.
+            adapter.add(CallbackI(), Ice.stringToIdentity("c3/callback"))
+
+            # The test allows the prefixed userid.
+            adapter.add(CallbackI(), Ice.stringToIdentity("_userid/callback"))
             adapter.activate()
             communicator.waitForShutdown()
