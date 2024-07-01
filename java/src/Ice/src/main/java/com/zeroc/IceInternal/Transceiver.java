d247d5094d6999c7a157af39a352c2f652d690cb
Benoit Foucher <benoit@zeroc.com>
2024-07-01 13:07:06 +0200

    Added comment to the ready callback interface

--- java/src/Ice/src/main/java/com/zeroc/IceInternal/Transceiver.java
+++ java/src/Ice/src/main/java/com/zeroc/IceInternal/Transceiver.java
@@ -6,22 +6,22 @@ package com.zeroc.IceInternal;
 
 public interface Transceiver {
   /**
-   * Returns the selectable channel used by the thread pool's selector to wait for read or
-   * write readiness.
+   * Returns the selectable channel used by the thread pool's selector to wait for read or write
+   * readiness.
    *
-   * @return The selectable channel that will be registered with the thread pool's selector
-   * or null if the transceiver doesn't use a selectable channel.
+   * @return The selectable channel that will be registered with the thread pool's selector or null
+   *     if the transceiver doesn't use a selectable channel.
    */
   java.nio.channels.SelectableChannel fd();
 
   /**
-   * Sets the transceiver ready callback. This method is called by the thread pool to provide
-   * a callback object that the transceiver can use to notify the thread pool's selector when
-   * more data is ready to be read or written by this transceiver.
+   * Sets the transceiver ready callback. This method is called by the thread pool to provide a
+   * callback object that the transceiver can use to notify the thread pool's selector when more
+   * data is ready to be read or written by this transceiver.
    *
    * @param callback The ready callback provided by the thread pool's selector.
-   * @remark  The transceiver implementation typically uses this callback when it buffers
-   * data read from the selectable channel of if it doesn't use a selectable channel.
+   * @remark The transceiver implementation typically uses this callback when it buffers data read
+   *     from the selectable channel if it doesn't use a selectable channel.
    */
   void setReadyCallback(ReadyCallback callback);
 
