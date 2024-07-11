4b432e91cb280122bd5dada9bd807e0d090bd697
Bernard Normier <bernard@zeroc.com>
2024-07-11 16:07:37 -0400

    Remove IceGrid, IceBox etc for Ruby and PHP

--- ruby/ruby/IceBox.rb
+++
@@ -1,5 +0,0 @@
-#
-# Copyright (c) ZeroC, Inc. All rights reserved.
-#
-
-require_relative 'IceBox/ServiceManager.rb'
--- ruby/ruby/IceGrid.rb
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) ZeroC, Inc. All rights reserved.
-#
-
-require_relative 'IceGrid/Admin.rb'
-require_relative 'IceGrid/Descriptor.rb'
-require_relative 'IceGrid/FileParser.rb'
-require_relative 'IceGrid/Registry.rb'
-require_relative 'IceGrid/UserAccountMapper.rb'
