e648d24fe9659623e2fb4fdd87f45d801e5f0493
Bernard Normier <bernard@zeroc.com>
2024-07-11 17:19:24 -0400

    Matlab cleanup (#2464)

--- matlab/src/Util.h
+++ matlab/src/Util.h
@@ -3,34 +3,21 @@
 //
 
 #include "Ice/Ice.h"
-
-#if defined(__GNUC__)
-#    pragma GCC diagnostic ignored "-Wredundant-decls"
-#endif
 #include <mex.h>
-
-typedef struct mxArray_tag mxArray; // Forward declaration to avoid importing mex.h here
 
 namespace IceMatlab
 {
     mxArray* createStringFromUTF8(const std::string&);
     std::string getStringFromUTF16(mxArray*);
-    mxArray* createEmpty();
     mxArray* createBool(bool);
     mxArray* createByte(std::uint8_t);
-    mxArray* createShort(short);
     mxArray* createInt(int);
-    mxArray* createLong(long long);
-    mxArray* createFloat(float);
-    mxArray* createDouble(double);
-    mxArray* createEnumerator(const std::string&, int);
     int getEnumerator(mxArray*, const std::string&);
     mxArray* createIdentity(const Ice::Identity&);
     void getIdentity(mxArray*, Ice::Identity&);
     mxArray* createStringMap(const std::map<std::string, std::string>&);
     void getStringMap(mxArray*, std::map<std::string, std::string>&);
     mxArray* createProtocolVersion(const Ice::ProtocolVersion&);
-    void getProtocolVersion(mxArray*, Ice::ProtocolVersion&);
     mxArray* createEncodingVersion(const Ice::EncodingVersion&);
     void getEncodingVersion(mxArray*, Ice::EncodingVersion&);
     mxArray* convertException(std::exception_ptr);
@@ -40,7 +27,6 @@ namespace IceMatlab
     mxArray* createStringList(const std::vector<std::string>&);
     void getStringList(mxArray*, std::vector<std::string>&);
     mxArray* createByteArray(const std::byte*, const std::byte*);
-    mxArray* createByteList(const std::vector<std::byte>&);
 
     std::string idToClass(const std::string&);
 
