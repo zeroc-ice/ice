d3542d6e1e2474e4b29dfc796e2c9dd86c23a4ad
Jose <pepone@users.noreply.github.com>
2024-07-19 17:26:40 +0200

    Minor python cleanups (#2532)

--- python/python/Ice/UserException.py
+++ python/python/Ice/UserException.py
@@ -5,12 +5,4 @@ from .Exception import Exception
 class UserException(Exception):
     """The base class for all user-defined exceptions."""
 
-    def ice_getSlicedData(self):
-        """Returns the sliced data if the value has a preserved-slice base class and has been sliced during
-        un-marshaling of the value, null is returned otherwise.
-        Returns:
-            The sliced data or null."""
-        return getattr(self, "_ice_slicedData", None)
-
     __module__ = "Ice"
-    __class__ = "UserException"
