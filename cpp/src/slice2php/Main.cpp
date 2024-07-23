ab2eab1d2834100afc613ffd67a34e38f1c2de64
Bernard Normier <bernard@zeroc.com>
2024-07-23 09:35:12 -0400

    Fix checkedCast with context in PHP

--- cpp/src/slice2php/Main.cpp
+++ cpp/src/slice2php/Main.cpp
@@ -382,9 +382,9 @@ CodeVisitor::visitInterfaceDefStart(const InterfaceDefPtr& p)
     _out << nl << "return  $communicator->stringToProxy($proxyString, '" << scoped << "');";
     _out << eb;
 
-    _out << sp << nl << "public static function checkedCast($proxy, $facetOrContext=null, $context=null)";
+    _out << sp << nl << "public static function checkedCast($proxy, ...$args)";
     _out << sb;
-    _out << nl << "return $proxy->ice_checkedCast('" << scoped << "', $facetOrContext, $context);";
+    _out << nl << "return $proxy->ice_checkedCast('" << scoped << "', ...$args);";
     _out << eb;
 
     _out << sp << nl << "public static function uncheckedCast($proxy, $facet=null)";
