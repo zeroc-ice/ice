3e668ed3b68162a82fc3748a4fa795c19670f9f1
Jose <pepone@users.noreply.github.com>
2024-07-24 15:31:00 +0200

    Simplify Ice.Current implementation in Python extension (#2552)

---
+++ python/test/Ice/current/TestI.py
@@ -0,0 +1,37 @@
+#
+# Copyright (c) ZeroC, Inc. All rights reserved.
+#
+
+import Test
+
+class MyClassI(Test.TestIntf):
+
+    def getAdapterName(self, current):
+        return current.adapter.getName()
+
+    def getConnection(self, current):
+        return str(current.con) if current.con else ""
+
+    def getIdentity(self, current):
+        return current.id
+
+    def getFacet(self, current):
+        return current.facet
+
+    def getOperation(self, current):
+        return current.operation
+
+    def getMode(self, current):
+        return str(current.mode)
+
+    def getContext(self, current):
+        return current.ctx
+
+    def getRequestId(self, current):
+        return current.requestId
+
+    def getEncoding(self, current):
+        return str(current.encoding)
+
+    def shutdown(self, current=None):
+        current.adapter.getCommunicator().shutdown()
