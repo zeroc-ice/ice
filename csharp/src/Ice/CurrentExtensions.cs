8204de3fcfaf4901f161f980ca2c00f3aef3540c
Bernard Normier <bernard@zeroc.com>
2024-07-31 16:28:39 -0400

    Fix comment

--- csharp/src/Ice/CurrentExtensions.cs
+++ csharp/src/Ice/CurrentExtensions.cs
@@ -131,8 +131,7 @@ public static class CurrentExtensions
 
             if (current.requestId != 0)
             {
-                // The default class format doesn't matter since we always encode user exception
-                // in Sliced format.
+                // The default class format doesn't matter since we always encode user exceptions in Sliced format.
                 ostr = new OutputStream(Util.currentProtocolEncoding);
                 ostr.writeBlob(Protocol.replyHdr);
                 ostr.writeInt(current.requestId);
