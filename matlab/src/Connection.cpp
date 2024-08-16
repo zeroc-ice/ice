f4040a7ce04ca369843eac0967ae9c51024a9576
Bernard Normier <bernard@zeroc.com>
2024-08-16 17:12:30 -0400

    Reformat

--- matlab/src/Connection.cpp
+++ matlab/src/Connection.cpp
@@ -156,33 +156,34 @@ extern "C"
         auto connection = deref<Ice::Connection>(self);
 
         auto futurePtr = make_shared<SimpleFuture>();
-        connection->close([futurePtr](exception_ptr closeException)
-        {
-            try
+        connection->close(
+            [futurePtr](exception_ptr closeException)
             {
-                rethrow_exception(closeException);
-            }
-            catch (const Ice::ConnectionClosedException&)
-            {
-                futurePtr->done();
-            }
-            catch (const Ice::CloseConnectionException&)
-            {
-                futurePtr->done();
-            }
-            catch (const Ice::CommunicatorDestroyedException&)
-            {
-                futurePtr->done();
-            }
-            catch (const Ice::ObjectAdapterDeactivatedException&)
-            {
-                futurePtr->done();
-            }
-            catch (...)
-            {
-                futurePtr->exception(closeException);
-            }
-        });
+                try
+                {
+                    rethrow_exception(closeException);
+                }
+                catch (const Ice::ConnectionClosedException&)
+                {
+                    futurePtr->done();
+                }
+                catch (const Ice::CloseConnectionException&)
+                {
+                    futurePtr->done();
+                }
+                catch (const Ice::CommunicatorDestroyedException&)
+                {
+                    futurePtr->done();
+                }
+                catch (const Ice::ObjectAdapterDeactivatedException&)
+                {
+                    futurePtr->done();
+                }
+                catch (...)
+                {
+                    futurePtr->exception(closeException);
+                }
+            });
 
         *future = new shared_ptr<SimpleFuture>(move(futurePtr));
         return nullptr;
