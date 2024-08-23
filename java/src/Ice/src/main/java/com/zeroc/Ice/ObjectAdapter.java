01281a3b5f57f774bf0c3bb63f2f21d51a95e902
Bernard Normier <bernard@zeroc.com>
2024-08-23 14:13:09 -0400

    Thread pool fixes

--- java/src/Ice/src/main/java/com/zeroc/Ice/ObjectAdapter.java
+++ java/src/Ice/src/main/java/com/zeroc/Ice/ObjectAdapter.java
@@ -1626,11 +1626,13 @@ public final class ObjectAdapter {
     "Router.LocatorCacheTimeout",
     "Router.InvocationTimeout",
     "ProxyOptions",
+    "ThreadPool.Serialize",
     "ThreadPool.Size",
     "ThreadPool.SizeMax",
     "ThreadPool.SizeWarn",
     "ThreadPool.StackSize",
-    "ThreadPool.Serialize"
+    "ThreadPool.ThreadIdleTime",
+    "ThreadPool.ThreadPriority"
   };
 
   boolean filterProperties(List<String> unknownProps) {
