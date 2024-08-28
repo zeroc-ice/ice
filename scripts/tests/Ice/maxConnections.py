148affd4d2ce0a8d7a1e79ceb3f7903f91369a94
Bernard Normier <bernard@zeroc.com>
2024-08-28 14:27:31 -0400

    Add test script

---
+++ scripts/tests/Ice/maxConnections.py
@@ -0,0 +1,18 @@
+# Copyright (c) ZeroC, Inc.
+
+from Util import (
+    ClientServerTestCase,
+)
+
+# Enable some tracing to allow investigating test failures
+traceProps = {"Ice.Trace.Network": 2}
+testcases = [
+    ClientServerTestCase(traceProps=traceProps),
+]
+
+TestSuite(
+    __name__,
+    testcases,
+    options={"compress": [False], "protocol": ["ssl", "ws", "tcp"]},
+    multihost=False,
+)
