1567e2726c5d0aaf18d6c028c80b608c6a7116cc
Austin Henriksen <austin@zeroc.com>
2024-09-11 15:23:01 -0400

    Fixed formatting of Glacier2.

--- java/src/Glacier2/src/main/java/com/zeroc/Glacier2/SessionCallback.java
+++ java/src/Glacier2/src/main/java/com/zeroc/Glacier2/SessionCallback.java
@@ -3,11 +3,10 @@
 package com.zeroc.Glacier2;
 
 /**
- * A callback class to get notifications of status changes in the Glacier2 session.
- * All callbacks on the <code>Callback</code> interface occur in the main swing thread.
+ * A callback class to get notifications of status changes in the Glacier2 session. All callbacks on
+ * the <code>Callback</code> interface occur in the main swing thread.
  */
-public interface SessionCallback
-{
+public interface SessionCallback {
     /**
      * Notifies the application that the communicator was created.
      *
@@ -19,11 +18,9 @@ public interface SessionCallback
      * Notifies the application that the Glacier2 session has been established.
      *
      * @param session The established session.
-     *
      * @throws SessionNotExistException If the session no longer exists.
      */
-    void connected(SessionHelper session)
-        throws SessionNotExistException;
+    void connected(SessionHelper session) throws SessionNotExistException;
 
     /**
      * Notifies the application that the Glacier2 session has been disconnected.
