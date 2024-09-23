93d4e655ac57d504a2d4821b796be821f1e7a50f
Bernard Normier <bernard@zeroc.com>
2024-09-23 17:32:15 -0400

    Reformat

--- cpp/include/Glacier2/Glacier2.h
+++ cpp/include/Glacier2/Glacier2.h
@@ -9,7 +9,7 @@
 #include "Glacier2/Metrics.h"
 #include "Glacier2/PermissionsVerifier.h"
 #include "Glacier2/Router.h"
-#include "Glacier2/Session.h"
 #include "Glacier2/SSLInfo.h"
+#include "Glacier2/Session.h"
 
 #endif
