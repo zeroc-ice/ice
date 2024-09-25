57a0c2b0007ba33fd915de1863879c26666bbfe8
Bernard Normier <bernard@zeroc.com>
2024-09-25 11:55:45 -0400

    Rework comment

--- java/src/Ice/src/main/java/com/zeroc/Ice/ConnectionI.java
+++ java/src/Ice/src/main/java/com/zeroc/Ice/ConnectionI.java
@@ -2704,11 +2704,11 @@ public final class ConnectionI extends EventHandler implements Connection, Cance
     private int _readStreamPos;
     private int _writeStreamPos;
 
-    // An upcall corresponds to application code that the connection executes in a thread of its
-    // associated thread pool. While most upcalls complete synchronously, dispatches are upcalls
-    // that can complete asynchronously in another thread (typically not an Ice thread pool thread).
-    // Upcalls include dispatches, AMI (response) continuations, sent callbacks, and close
-    // callbacks.
+    // The upcall count keeps track of the number of dispatches, AMI (response) continuations,
+    // sent callbacks and connection establishment callbacks that have been started (or are about
+    // to be started) by a thread of the thread pool associated with this connection, and have
+    // not completed yet. All these operations except the connection establishment callbacks
+    // execute application code or code generated from Slice definitions.
     private int _upcallCount;
 
     // The number of outstanding dispatches. Maintained only while state is StateActive or
