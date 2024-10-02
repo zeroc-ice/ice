43dd17dc198703838063eaee9737686afe722f61
Jose <jose@zeroc.com>
2024-10-02 15:09:36 +0200

    Cleanup

--- csharp/test/Ice/unknownProperties/AllTests.cs
+++ csharp/test/Ice/unknownProperties/AllTests.cs
@@ -12,27 +12,27 @@ public class AllTests : global::Test.AllTests
         var communicator = helper.communicator();
         var output = helper.getWriter();
 
-        output.Write("Creating object adapter with unknown properties... ");
+        output.Write("Creating object adapter with unknown properties fails... ");
         communicator.getProperties().setProperty("Foo.Endpoints", "tcp -h 127.0.0.1");
         communicator.getProperties().setProperty("Foo.UnknownProperty", "bar");
         try
         {
             communicator.createObjectAdapter("Foo");
-            AllTests.test(false);
+            test(false);
         }
         catch(InitializationException)
         {
         }
         output.WriteLine("ok");
 
-        output.Write("Creating proxy with unknown properties... ");
+        output.Write("Creating proxy with unknown properties fails... ");
 
         communicator.getProperties().setProperty("Greeter", "hello:tcp -h 127.0.0.1 -p 10000");
         communicator.getProperties().setProperty("Greeter.UnknownProperty", "bar");
         try
         {
             communicator.propertyToProxy("Greeter");
-            AllTests.test(false);
+            test(false);
         }
         catch(InitializationException)
         {
