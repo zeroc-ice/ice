2d2b1d624d0be86f7b1f7fbd7b442808e8abbb07
Bernard Normier <bernard@zeroc.com>
2024-10-17 16:42:03 -0400

    Reformat

--- java/src/Ice/src/main/java/com/zeroc/Ice/ValueFactory.java
+++ java/src/Ice/src/main/java/com/zeroc/Ice/ValueFactory.java
@@ -2,20 +2,18 @@
 
 package com.zeroc.Ice;
 
-/**
- * A factory for class instances.
- */
+/** A factory for class instances. */
 @FunctionalInterface
 public interface ValueFactory {
     /**
-     * Create a new instance for a given Slice class. The type is the absolute Slice type id, i.e., the
-     * id relative to the unnamed top-level Slice module. For example, the absolute Slice type id
-     * for an interface <code>Bar</code> in the module <code>Foo</code> is <code>"::Foo::Bar"</code>
-     * . Note that the leading "<code>::</code>" is required.
+     * Create a new instance for a given Slice class. The type is the absolute Slice type id, i.e.,
+     * the id relative to the unnamed top-level Slice module. For example, the absolute Slice type
+     * id for an interface <code>Bar</code> in the module <code>Foo</code> is <code>"::Foo::Bar"
+     * </code> . Note that the leading "<code>::</code>" is required.
      *
      * @param type The Slice type ID.
-     * @return The class instance created for the given type, or nil if the factory is unable to create the
-     *     value.
+     * @return The class instance created for the given type, or nil if the factory is unable to
+     *     create the value.
      */
     Value create(String type);
 }
