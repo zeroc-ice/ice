60ce0c4fbecff2b96e135e496414526699741647
Jose <jose@zeroc.com>
2024-10-24 12:51:32 +0200

    clear debug tracing

--- cpp/test/DataStorm/types/test.py
+++ cpp/test/DataStorm/types/test.py
@@ -9,8 +9,6 @@ traceProps = {
     "DataStorm.Trace.Topic" : 1,
     "DataStorm.Trace.Session" : 3,
     "DataStorm.Trace.Data" : 2,
-    "Ice.Trace.Network" : 3,
-    "Ice.Trace.Protocol" : 1,
 }
 
 multicastProps = { "DataStorm.Node.Multicast.Enabled" : 1 }
