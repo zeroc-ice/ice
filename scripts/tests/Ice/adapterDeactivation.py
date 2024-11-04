92c68568edb1d68ea13e59f9787012d839969e13
Jose <jose@zeroc.com>
2024-11-04 18:24:07 +0100

    Disable IPv6 for C# Ice/adapterDeactivation

--- scripts/tests/Ice/adapterDeactivation.py
+++ scripts/tests/Ice/adapterDeactivation.py
@@ -3,7 +3,10 @@
 # Copyright (c) ZeroC, Inc. All rights reserved.
 #
 
-from Util import TestSuite
+from Util import CSharpMapping, Darwin, TestSuite, platform, Mapping
 
+# Disable IPv6 for macOS until https://github.com/dotnet/runtime/pull/108334 is merged in .NET 8
+# See https://github.com/zeroc-ice/ice/issues/2061
+ipv6 = not (isinstance(Mapping.getByPath(__name__), CSharpMapping) and isinstance(platform, Darwin))
 
-TestSuite(__name__, multihost=False)
+TestSuite(__name__, multihost=False, options={"ipv6": [ipv6]})
