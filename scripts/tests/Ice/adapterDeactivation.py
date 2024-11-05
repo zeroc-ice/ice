93ad157df510d603cc77b3fd896cd044d75855cc
Jose <jose@zeroc.com>
2024-11-05 09:56:27 +0100

    Review fixes

--- scripts/tests/Ice/adapterDeactivation.py
+++ scripts/tests/Ice/adapterDeactivation.py
@@ -5,8 +5,10 @@
 
 from Util import CSharpMapping, Darwin, TestSuite, platform, Mapping
 
-# Disable IPv6 for macOS until https://github.com/dotnet/runtime/pull/108334 is merged in .NET 8
+options = {}
+# Disable IPv6 for .NET on macOS until https://github.com/dotnet/runtime/pull/108334 is merged in .NET 8
 # See https://github.com/zeroc-ice/ice/issues/2061
-ipv6 = not (isinstance(Mapping.getByPath(__name__), CSharpMapping) and isinstance(platform, Darwin))
+if (isinstance(Mapping.getByPath(__name__), CSharpMapping) and isinstance(platform, Darwin)):
+    options = {"ipv6": [False]}
 
-TestSuite(__name__, multihost=False, options={"ipv6": [ipv6]})
+TestSuite(__name__, multihost=False, options=options)
