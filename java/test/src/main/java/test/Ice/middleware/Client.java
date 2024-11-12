75bd0d7e1dad6ea8bcb226739411201bce05d90b
Jose <jose@zeroc.com>
2024-11-12 18:37:20 +0100

    Fix error observer middleware test - #3138

--- java/test/src/main/java/test/Ice/middleware/Client.java
+++ java/test/src/main/java/test/Ice/middleware/Client.java
@@ -4,7 +4,12 @@ package test.Ice.middleware;
 
 public class Client extends test.TestHelper {
     public void run(String[] args) {
-        try (com.zeroc.Ice.Communicator communicator = initialize(args)) {
+        var properties = createTestProperties(args);
+
+        // Suppress dispatch exception logging; the error observer middleware
+        // tests throw exceptions during dispatch.
+        properties.setProperty("Ice.Warn.Dispatch", "0");
+        try (com.zeroc.Ice.Communicator communicator = initialize(properties)) {
             AllTests.allTests(this);
         }
     }
