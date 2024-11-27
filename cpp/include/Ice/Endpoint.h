75f146249b091722707ad3c3dabe1eebef977284
Bernard Normier <bernard@zeroc.com>
2024-11-27 09:33:31 -0500

    Move IAP EndpointInfo and ConnectionInfo (#3199)

--- cpp/include/Ice/Endpoint.h
+++ cpp/include/Ice/Endpoint.h
@@ -236,6 +236,65 @@ namespace Ice
     };
 
     /**
+     * Provides access to an IAP endpoint information.
+     * \headerfile Ice/Ice.h
+     */
+    class IAPEndpointInfo final : public EndpointInfo
+    {
+    public:
+        ~IAPEndpointInfo() final;
+        IAPEndpointInfo(const IAPEndpointInfo&) = delete;
+        IAPEndpointInfo& operator=(const IAPEndpointInfo&) = delete;
+
+        std::int16_t type() const noexcept final { return _type; }
+        bool secure() const noexcept final { return _secure; }
+
+        /**
+         * The accessory manufacturer or empty to not match against
+         * a manufacturer.
+         */
+        const std::string manufacturer;
+        /**
+         * The accessory model number or empty to not match against
+         * a model number.
+         */
+        const std::string modelNumber;
+        /**
+         * The accessory name or empty to not match against
+         * the accessory name.
+         */
+        const std::string name;
+        /**
+         * The protocol supported by the accessory.
+         */
+        const std::string protocol;
+
+        // internal constructor
+        IAPEndpointInfo(
+            int timeout,
+            bool compress,
+            std::string manufacturer,
+            std::string modelNumber,
+            std::string name,
+            std::string protocol,
+            std::int16_t type,
+            bool secure)
+            : EndpointInfo{timeout, compress},
+              manufacturer{std::move(manufacturer)},
+              modelNumber{std::move(modelNumber)},
+              name{std::move(name)},
+              protocol{std::move(protocol)},
+              _type{type},
+              _secure{secure}
+        {
+        }
+
+    private:
+        const std::int16_t _type;
+        const bool _secure;
+    };
+
+    /**
      * Provides access to the details of an opaque endpoint.
      * @see Endpoint
      * \headerfile Ice/Ice.h
