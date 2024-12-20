10f9350859a06119cc2b2e41203c286459ecfc2e
Jose <jose@zeroc.com>
2024-12-20 11:18:53 +0100

    Session reestablishment test

--- cpp/test/DataStorm/reliability/Reader.cpp
+++ cpp/test/DataStorm/reliability/Reader.cpp
@@ -57,7 +57,7 @@ void ::Reader::run(int argc, char* argv[])
                 auto connection = node.getSessionConnection(sample.getSession());
                 while (!connection)
                 {
-                    this_thread::sleep_for(chrono::milliseconds(200));
+                    this_thread::sleep_for(chrono::milliseconds(10));
                     connection = node.getSessionConnection(sample.getSession());
                 }
                 connection->close().get();
@@ -67,6 +67,71 @@ void ::Reader::run(int argc, char* argv[])
         writer.waitForReaders();
         writer.update(0);
         writer.waitForNoReaders();
+    }
+
+    {
+        Topic<string, int> topic(node, "int2");
+        auto reader = makeSingleKeyReader(topic, "element", "", config);
+        string session;
+
+        // Read 100 samples from the "element" key and close the connection.
+        for (int i = 0; i < 100; ++i)
+        {
+            auto sample = reader.getNextUnread();
+            if (sample.getValue() != i)
+            {
+                cerr << "unexpected sample: " << sample.getValue() << " expected:" << i << endl;
+                test(false);
+            }
+            session = sample.getSession();
+        }
+
+        auto connection = node.getSessionConnection(session);
+        while (!connection)
+        {
+            this_thread::sleep_for(chrono::milliseconds(10));
+            connection = node.getSessionConnection(session);
+        }
+        connection->close().get();
+
+        // Send a sample to the writer on "reader_barrier" to let it know that the connection was closed.
+        // The writer will read it after the session is reestablished.
+        auto writerB = makeSingleKeyWriter(topic, "reader_barrier");
+        writerB.waitForReaders();
+        writerB.update(0);
+
+        // Wait for the writer to acknowledge the sample send on "reader_barrier" and close the connection again.
+        auto readerB = makeSingleKeyReader(topic, "writer_barrier");
+        [[maybe_unused]] auto _ = readerB.getNextUnread();
+
+        // Session was reestablish close again
+        connection = node.getSessionConnection(session);
+        while (!connection)
+        {
+            this_thread::sleep_for(chrono::milliseconds(10));
+            connection = node.getSessionConnection(session);
+        }
+        connection->close().get();
+
+        // Let the writer know the connection was closed again, and that it can proceed with the second batch of
+        // samples.
+        writerB.update(0);
+
+        for (int i = 0; i < 100; ++i)
+        {
+            auto sample = reader.getNextUnread();
+            if (sample.getValue() != i + 100)
+            {
+                cerr << "unexpected sample: " << sample.getValue() << " expected:" << (i + 100) << endl;
+                test(false);
+            }
+            session = sample.getSession();
+        }
+
+        // Let the writer know we have processed all samples.
+        writerB.waitForReaders();
+        writerB.update(0);
+        writerB.waitForNoReaders();
     }
 }
 
