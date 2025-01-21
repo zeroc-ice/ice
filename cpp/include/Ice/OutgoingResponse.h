762c451a60194c209fca81ac8054cf85a87ac292
Bernard Normier <bernard@zeroc.com>
2025-01-20 19:08:54 -0500

    Update cpp/include/Ice/OutgoingResponse.h
    Co-authored-by: Austin Henriksen <austin.r.henriksen79@gmail.com>

--- cpp/include/Ice/OutgoingResponse.h
+++ cpp/include/Ice/OutgoingResponse.h
@@ -99,7 +99,7 @@ namespace Ice
 
         /**
          * Get the full details of the exception marshaled into the response.
-         * @return The exception details. For Ice exceptions, it's produced by streaming the exception into a
+         * @return The exception details. For Ice exceptions, it's produced by streaming the exception into an
          * std::ostringstream with operator<<. It's empty when replyStatus() is ReplyStatus::Ok.
          */
         [[nodiscard]] const std::string& exceptionDetails() const noexcept { return _exceptionDetails; }
