c25e9a1c11432df37f7233d9a13db830770e97a2
Jose <jose@zeroc.com>
2025-01-24 12:49:32 +0100

    Fixes to build and run the C++ iOS test controller

--- cpp/test/ios/controller/Makefile.mk
+++ cpp/test/ios/controller/Makefile.mk
@@ -2,7 +2,7 @@
 
 $(project)_type                 = xcode
 $(project)_platforms            = iphonesimulator iphoneos
-$(project)_configs              = xcodesdk
+$(project)_configs              = static
 $(project)_dependencies         = $(tests)
 $(project)_project              = C++ Test Controller.xcodeproj
 $(project)_scheme               = C++ Test Controller
