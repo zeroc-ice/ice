c8e67616f384c441b612b5ef4960743cdbe7af4f
Jose <pepone@users.noreply.github.com>
2025-01-28 20:30:44 +0100

    Test collocated invocations with normalized IPv6 addresses (#3436)
    Fix #3154

--- cpp/test/Ice/operations/Collocated.cpp
+++ cpp/test/Ice/operations/Collocated.cpp
@@ -4,6 +4,8 @@
 #include "TestHelper.h"
 #include "TestI.h"
 
+#include <iostream>
+
 using namespace std;
 
 class Collocated : public Test::TestHelper
@@ -12,6 +14,33 @@ public:
     void run(int, char**) override;
 };
 
+namespace
+{
+    void testCollocatedIPv6Invocation(Test::TestHelper* helper)
+    {
+        int port = helper->getTestPort(1);
+
+        string endpoint1 = "tcp -h \"::1\" -p " + to_string(port);
+        string endpoint2 = "tcp -h \"0:0:0:0:0:0:0:1\" -p " + to_string(port);
+
+        cout << "testing collocated invocation with normalized IPv6 address... " << flush;
+        Ice::CommunicatorHolder communicator = Ice::initialize();
+        communicator->getProperties()->setProperty("TestAdapter.Endpoints", endpoint1);
+        Ice::ObjectAdapterPtr adapter = communicator->createObjectAdapter("TestAdapter");
+        adapter->add(std::make_shared<MyDerivedClassI>(), Ice::stringToIdentity("test"));
+        // Don't activate OA to ensure collocation is used.
+
+        auto prx = Ice::ObjectPrx{communicator.communicator(), "test:" + endpoint1};
+        prx = prx->ice_invocationTimeout(10ms);
+        prx->ice_ping();
+
+        prx = Ice::ObjectPrx{communicator.communicator(), "test:" + endpoint2};
+        prx = prx->ice_invocationTimeout(10ms);
+        prx->ice_ping();
+        cout << "ok" << endl;
+    }
+}
+
 void
 Collocated::run(int argc, char** argv)
 {
@@ -19,7 +48,6 @@ Collocated::run(int argc, char** argv)
     properties->setProperty("Ice.BatchAutoFlushSize", "100");
     Ice::CommunicatorHolder communicator = initialize(argc, argv, properties);
     communicator->getProperties()->setProperty("TestAdapter.Endpoints", getTestEndpoint());
-    communicator->getProperties()->setProperty("TestAdapter.AdapterId", "test");
     Ice::ObjectAdapterPtr adapter = communicator->createObjectAdapter("TestAdapter");
     Ice::ObjectPrx prx = adapter->add(std::make_shared<MyDerivedClassI>(), Ice::stringToIdentity("test"));
     // adapter->activate(); // Don't activate OA to ensure collocation is used.
@@ -28,6 +56,8 @@ Collocated::run(int argc, char** argv)
 
     Test::MyClassPrx allTests(Test::TestHelper*);
     allTests(this);
+
+    testCollocatedIPv6Invocation(this);
 }
 
 DEFINE_TEST(Collocated)
--- csharp/test/Ice/operations/Collocated.cs
+++ csharp/test/Ice/operations/Collocated.cs
@@ -16,7 +16,6 @@ namespace Ice
                 initData.properties.setProperty("Ice.ThreadPool.Client.SizeWarn", "0");
                 initData.properties.setProperty("Ice.BatchAutoFlushSize", "100");
                 using var communicator = initialize(initData);
-                communicator.getProperties().setProperty("TestAdapter.AdapterId", "test");
                 communicator.getProperties().setProperty("TestAdapter.Endpoints", getTestEndpoint(0));
                 ObjectAdapter adapter = communicator.createObjectAdapter("TestAdapter");
                 ObjectPrx prx = adapter.add(new MyDerivedClassI(), Ice.Util.stringToIdentity("test"));
@@ -28,6 +27,29 @@ namespace Ice
                 }
 
                 await AllTests.allTests(this);
+
+                testCollocatedIPv6Invocation(this);
+            }
+
+            private static void testCollocatedIPv6Invocation(TestHelper helper)
+            {
+                TextWriter output = helper.getWriter();
+                int port = helper.getTestPort(1);
+                output.Write("testing collocated invocation with normalized IPv6 address... ");
+                output.Flush();
+                using var communicator = Ice.Util.initialize();
+                communicator.getProperties().setProperty("TestAdapter.Endpoints", $"tcp -h \"0:0:0:0:0:0:0:1\" -p {port}");
+                var adapter = communicator.createObjectAdapter("TestAdapter");
+                adapter.add(new MyDerivedClassI(), Ice.Util.stringToIdentity("test"));
+
+                var prx = Ice.ObjectPrxHelper.createProxy(communicator, $"test:tcp -h \"::1\" -p {port}");
+                prx = prx.ice_invocationTimeout(TimeSpan.FromMilliseconds(10));
+                prx.ice_ping();
+
+                prx = Ice.ObjectPrxHelper.createProxy(communicator, $"test:tcp -h \"0:0:0:0:0:0:0:1\" -p {port}");
+                prx = prx.ice_invocationTimeout(TimeSpan.FromMilliseconds(10));
+                prx.ice_ping();
+                output.WriteLine();
             }
 
             public static Task<int> Main(string[] args) =>
