7a349e13ca25ff76db438c28b525a84ea70b6ee2
Jose <jose@zeroc.com>
2025-01-30 12:51:49 +0100

    Enable tracing for Ice/inactivityTimeout to figure #3390

---
+++ scripts/tests/Ice/inactivityTimeout.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) ZeroC, Inc.
+
+from Util import ClientServerTestCase, TestSuite
+
+
+# Enable some tracing to allow investigating test failures
+traceProps = {"Ice.Trace.Network": 3, "Ice.Trace.Protocol": 1}
+
+TestSuite(__name__, [ClientServerTestCase(traceProps=traceProps)])
