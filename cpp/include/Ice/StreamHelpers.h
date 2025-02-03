1c5d4d4d22add852d11706d4370ab8e41efc6f93
Bernard Normier <bernard@zeroc.com>
2025-02-03 14:13:54 -0500

    Update comment

--- cpp/include/Ice/StreamHelpers.h
+++ cpp/include/Ice/StreamHelpers.h
@@ -10,7 +10,9 @@
 #include <iterator>
 #include <ostream>
 
-#if defined(_MSC_VER) // workaround for MSVC bug
+#if defined(_MSC_VER)
+// With MSVC, __has_include(<span>) evaluates to 1 in c++17 mode and the <span> header then issues a warning. This
+// is a permissible but impractical behavior. As a result, we include <span> only in c++20 mode.
 #    if _MSVC_LANG >= 202002L
 #        include <span>
 #    endif
