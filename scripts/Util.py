5eae6d721f923b4ab4966d6c742417ec6c06f59a
Jose <jose@zeroc.com>
2025-02-08 10:57:31 +0100

    Fix MATLAB command

--- scripts/Util.py
+++ scripts/Util.py
@@ -4139,7 +4139,7 @@ class MatlabMapping(CppBasedClientMapping):
                 " -wait -log -minimize" if isinstance(platform, Windows) else "",
             )
 
-        return "{0} \"cd '{1}', runTest {2} {3} {4}\"".format(
+        return "{0} \"cd('{1}');runTest {2} {3} {4}\"".format(
             matlabCmd,
             os.path.abspath(
                 os.path.join(os.path.dirname(__file__), "..", "matlab", "test", "lib")
