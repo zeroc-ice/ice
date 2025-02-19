8d6d47c1d52e17d45fc3efc35e01fe6d66f6f41a
Austin Henriksen <austin@zeroc.com>
2025-02-18 21:54:48 -0500

    Can move these variables to a better spot.

--- cpp/src/slice2js/Gen.cpp
+++ cpp/src/slice2js/Gen.cpp
@@ -2457,9 +2457,6 @@ Slice::Gen::TypeScriptVisitor::writeOpDocSummary(Output& out, const OperationPtr
         }
     }
 
-    ParameterList outParams = op->outParameters();
-    TypePtr ret = op->returnType();
-
     if (forDispatch)
     {
         const string currentParam = escapeParam(op->inParameters(), "current");
@@ -2486,13 +2483,14 @@ Slice::Gen::TypeScriptVisitor::writeOpDocSummary(Output& out, const OperationPtr
         out << ".";
     }
 
+    const TypePtr ret = op->returnType();
     if (comment && ret)
     {
         out << nl << " * - " << typeToTsString(ret, true, false, op->returnIsOptional()) << " : ";
         writeDocLines(out, comment->returns(), false, "   ");
     }
 
-    for (const auto& param : outParams)
+    for (const auto& param : op->outParameters())
     {
         auto q = paramDoc.find(param->name());
         if (q != paramDoc.end())
