161e4e9dfad5ccef9a109c9e07b545170e69839e
Joe George <joe@zeroc.com>
2025-03-07 10:51:57 -0500

    fixes

--- cpp/config/IceConfig.cmake
+++ cpp/config/IceConfig.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) ZeroC, Inc.
 
 if(WIN32)
-    # On Windows Ice ships was a NuGet package, so we need to find the package prefix directory.
+    # On Windows Ice ships as a NuGet package, so we need to find the package prefix directory.
     # This config file is located at <ice_nuget_package>/cmake/IceConfig.cmake
     get_filename_component(Ice_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)
 else()
