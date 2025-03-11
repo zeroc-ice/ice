b470552046685f9a4102dd0a89d48c50d6727239
Jose <pepone@users.noreply.github.com>
2025-03-11 18:00:01 +0100

    Add Slice Tools Gradle plugin to build Java and Android projects (#3650)

---
+++ java/tools/slice-tools/BUILDING.md
@@ -0,0 +1,50 @@
+# Building Slice Tools for Ice
+
+This document describes how to build and publish the Slice Tools for Ice Gradle Plugin.
+
+## Prerequisites
+
+- JDK 17 or greater
+
+## Build
+
+The plugin can be built using the following Gradle command:
+
+```shell
+../../gradlew build
+```
+
+This will:
+
+- Compile the source code
+- Run tests
+- Package the plugin
+
+### Bundled Compilers and Ice Slice Files
+
+To ensure that the Slice-to-Java compiler (slice2java) and the Ice Slice files are included with the plugin and
+available at runtime, they must be copied to the plugin’s resources directory.
+
+The plugin expects the following structure within src/main/resources/:
+
+```shell
+src/main/resources/resources/slice         # Ice Slice files
+src/main/resources/resources/macos-arm64   # slice2java compiler for macOS (Apple Silicon)
+src/main/resources/resources/linux-arm64   # slice2java compiler for Linux (ARM64)
+src/main/resources/resources/linux-x64     # slice2java compiler for Linux (x86_64)
+src/main/resources/resources/windows-x64   # slice2java compiler for Windows (x64)
+```
+
+## Publish
+
+The plugin can be published to **Maven Local** for testing.
+
+### Publish to Maven Local (For Development)
+
+To publish the plugin to your local Maven repository (`~/.m2/repository`), run:
+
+```shell
+./gradlew publishToMavenLocal
+```
+
+This allows other projects on your machine to use the locally published version.
---
+++ java/tools/slice-tools/src/main/kotlin/com/zeroc/ice/slice/tools/SliceExtension.kt
@@ -0,0 +1,51 @@
+// Copyright (c) ZeroC, Inc.
+
+package com.zeroc.ice.slice.tools
+
+import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.api.Project
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Property
+import javax.inject.Inject
+
+/**
+ * Gradle extension for configuring the Slice Tools plugin.
+ *
+ * This extension allows configuring the Slice-to-Java compiler (`slice2java`),
+ * defining Slice source sets, and setting default compilation options.
+ */
+abstract class SliceExtension @Inject constructor(project: Project, objects: ObjectFactory) {
+    /**
+     * Default compiler arguments applied to all Slice source sets.
+     * These arguments are passed to `slice2java` during compilation.
+     */
+    val compilerArgs: ListProperty<String> = objects.listProperty(String::class.java)
+
+    /**
+     * Default directories for the Slice include search path.
+     *
+     * These directories are passed to the Slice compiler using the `-I` option
+     * when compiling Slice files. Applied to all source sets.
+     */
+    val includeSearchPath: ConfigurableFileCollection = objects.fileCollection()
+
+    /**
+     * A container for managing Slice source sets.
+     *
+     * This allows defining multiple named Slice source sets within the project.
+     */
+    val sourceSets: NamedDomainObjectContainer<SliceSourceSet> =
+        project.objects.domainObjectContainer(SliceSourceSet::class.java) { name ->
+            project.objects.newInstance(DefaultSliceSourceSet::class.java, name, project.objects)
+        }
+
+    /**
+     * The path to the directory containing the Slice-to-Java compiler (`slice2java`).
+     *
+     * If not set, the plugin will attempt to locate `slice2java` and Slice files
+     * from the plugin's bundled resources.
+     */
+    val toolsPath: Property<String> = objects.property(String::class.java)
+}
