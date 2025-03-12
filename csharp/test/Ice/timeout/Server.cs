7cc7d993363ae91607c45d420ba20c169ba5fad4
Lilith <lilith@zeroc.com>
2025-03-12 11:51:21 -0400

    ice.sln dotnet formatting with file-scoped namespace rule applied

--- csharp/test/Ice/timeout/Server.cs
+++ csharp/test/Ice/timeout/Server.cs
@@ -1,32 +1,31 @@
 // Copyright (c) ZeroC, Inc.
 
-namespace Ice.timeout
+namespace Ice.timeout;
+
+public class Server : global::Test.TestHelper
 {
-    public class Server : global::Test.TestHelper
+    public override void run(string[] args)
     {
-        public override void run(string[] args)
+        var properties = createTestProperties(ref args);
+
+        using (var communicator = initialize(properties))
         {
-            var properties = createTestProperties(ref args);
+            communicator.getProperties().setProperty("TestAdapter.Endpoints", getTestEndpoint(0));
+            communicator.getProperties().setProperty("ControllerAdapter.Endpoints", getTestEndpoint(1));
+            communicator.getProperties().setProperty("ControllerAdapter.ThreadPool.Size", "1");
 
-            using (var communicator = initialize(properties))
-            {
-                communicator.getProperties().setProperty("TestAdapter.Endpoints", getTestEndpoint(0));
-                communicator.getProperties().setProperty("ControllerAdapter.Endpoints", getTestEndpoint(1));
-                communicator.getProperties().setProperty("ControllerAdapter.ThreadPool.Size", "1");
+            var adapter = communicator.createObjectAdapter("TestAdapter");
+            adapter.add(new TimeoutI(), Ice.Util.stringToIdentity("timeout"));
+            adapter.activate();
 
-                var adapter = communicator.createObjectAdapter("TestAdapter");
-                adapter.add(new TimeoutI(), Ice.Util.stringToIdentity("timeout"));
-                adapter.activate();
-
-                var controllerAdapter = communicator.createObjectAdapter("ControllerAdapter");
-                controllerAdapter.add(new ControllerI(adapter), Ice.Util.stringToIdentity("controller"));
-                controllerAdapter.activate();
-                serverReady();
-                communicator.waitForShutdown();
-            }
+            var controllerAdapter = communicator.createObjectAdapter("ControllerAdapter");
+            controllerAdapter.add(new ControllerI(adapter), Ice.Util.stringToIdentity("controller"));
+            controllerAdapter.activate();
+            serverReady();
+            communicator.waitForShutdown();
         }
-
-        public static Task<int> Main(string[] args) =>
-            global::Test.TestDriver.runTestAsync<Server>(args);
     }
+
+    public static Task<int> Main(string[] args) =>
+        global::Test.TestDriver.runTestAsync<Server>(args);
 }
