b4d9ffb53ca38820d08d9d2ddc4457fada45bb34
Austin Henriksen <austin.r.henriksen79@gmail.com>
2025-03-24 14:47:08 -0400

    Trailing whitespace in suggestion.

--- php/src/Makefile.mk
+++ php/src/Makefile.mk
@@ -9,7 +9,7 @@ IcePHP_installdir       := $(install_phplibdir)
 IcePHP_cppflags         := -I$(top_srcdir)/cpp/src $(ice_cpp_cppflags) $(php_cppflags)
 IcePHP_ldflags          := $(php_ldflags)
 IcePHP_dependencies     := IceDiscovery IceLocatorDiscovery Ice
-IcePHP_extra_sources    := $(wildcard $(top_srcdir)/cpp/src/Slice/*.cpp) 
+IcePHP_extra_sources    := $(wildcard $(top_srcdir)/cpp/src/Slice/*.cpp)
 
 projects += $(project)
 srcs:: $(project)
