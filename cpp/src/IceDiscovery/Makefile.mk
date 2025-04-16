9698cdcc542385005cb62184a12b726792ad46c8
Bernard Normier <bernard@zeroc.com>
2025-04-16 19:02:37 -0400

    Fix make install

--- cpp/src/IceDiscovery/Makefile.mk
+++ cpp/src/IceDiscovery/Makefile.mk
@@ -2,9 +2,9 @@
 
 $(project)_libraries := IceDiscovery
 
-# Tell the slice2cpp rule to ignore the top-level IceDiscovery include directory.
-IceDiscovery_includedir                 := $(project)/generated/IceDiscovery
+IceDiscovery_do_not_install_generated_headers := yes
 
+IceDiscovery_sliceflags                 := --include-dir IceDiscovery
 IceDiscovery_targetdir                  := $(libdir)
 IceDiscovery_dependencies               := Ice
 IceDiscovery_cppflags                   := -DICE_DISCOVERY_API_EXPORTS $(api_exports_cppflags)
--- cpp/src/IceLocatorDiscovery/Makefile.mk
+++ cpp/src/IceLocatorDiscovery/Makefile.mk
@@ -2,9 +2,9 @@
 
 $(project)_libraries := IceLocatorDiscovery
 
-# Tell the slice2cpp rule to ignore the top-level IceLocatorDiscovery include directory.
-IceLocatorDiscovery_includedir                  := $(project)/generated/IceLocatorDiscovery
+IceLocatorDiscovery_do_not_install_generated_headers := yes
 
+IceLocatorDiscovery_sliceflags                  := --include-dir IceLocatorDiscovery
 IceLocatorDiscovery_targetdir                   := $(libdir)
 IceLocatorDiscovery_dependencies                := Ice
 IceLocatorDiscovery_cppflags                    := -DICE_LOCATOR_DISCOVERY_API_EXPORTS $(api_exports_cppflags)
