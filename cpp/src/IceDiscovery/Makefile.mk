893ea2163540e64e75464de141470dd28eab2e79
Bernard Normier <bernard@zeroc.com>
2025-04-16 19:21:14 -0400

    Cleanup

--- cpp/src/IceDiscovery/Makefile.mk
+++ cpp/src/IceDiscovery/Makefile.mk
@@ -2,11 +2,11 @@
 
 $(project)_libraries := IceDiscovery
 
-IceDiscovery_do_not_install_generated_headers := yes
-
 IceDiscovery_sliceflags                 := --include-dir IceDiscovery
 IceDiscovery_targetdir                  := $(libdir)
 IceDiscovery_dependencies               := Ice
 IceDiscovery_cppflags                   := -DICE_DISCOVERY_API_EXPORTS $(api_exports_cppflags)
+
+IceDiscovery_install_generated_headers  := no
 
 projects += $(project)
--- cpp/src/IceLocatorDiscovery/Makefile.mk
+++ cpp/src/IceLocatorDiscovery/Makefile.mk
@@ -2,11 +2,11 @@
 
 $(project)_libraries := IceLocatorDiscovery
 
-IceLocatorDiscovery_do_not_install_generated_headers := yes
-
 IceLocatorDiscovery_sliceflags                  := --include-dir IceLocatorDiscovery
 IceLocatorDiscovery_targetdir                   := $(libdir)
 IceLocatorDiscovery_dependencies                := Ice
 IceLocatorDiscovery_cppflags                    := -DICE_LOCATOR_DISCOVERY_API_EXPORTS $(api_exports_cppflags)
+
+IceLocatorDiscovery_install_generated_headers   := no
 
 projects += $(project)
