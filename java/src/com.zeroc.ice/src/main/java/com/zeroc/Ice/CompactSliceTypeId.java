95526589e159246c6f34d6a7ec4fb0db5b2f950e
Bernard Normier <bernard@zeroc.com>
2025-04-17 13:46:08 -0400

    Add SliceTypeId annotations in Java (#3870)

---
+++ java/src/com.zeroc.ice/src/main/java/com/zeroc/Ice/CompactSliceTypeId.java
@@ -0,0 +1,21 @@
+// Copyright (c) ZeroC, Inc.
+
+package com.zeroc.Ice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Assigns a compact Slice type ID to a class.
+ *
+ * The Slice compiler assigns both a Slice type ID and a compact Slice type ID to the mapped class of a Slice class that
+ * specifies a compact type ID.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface CompactSliceTypeId {
+    /** The compact ID of this class.*/
+    int value();
+}
