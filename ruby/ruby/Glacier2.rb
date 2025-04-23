b1c7c53585af41e5657397ce179a3ecf903ef8d1
Lilith <lilith@zeroc.com>
2025-04-23 15:02:22 -0400

    Making suggested changes

--- ruby/ruby/Glacier2.rb
+++ ruby/ruby/Glacier2.rb
@@ -1,6 +1,4 @@
-#
 # Copyright (c) ZeroC, Inc. All rights reserved.
-#
 
 require 'Glacier2/Router'
 require 'Glacier2/PermissionsVerifier'
--- ruby/ruby/IceGrid.rb
+++ ruby/ruby/IceGrid.rb
@@ -1,9 +1,9 @@
-#
 # Copyright (c) ZeroC, Inc. All rights reserved.
-#
 
 require 'IceGrid/Admin.rb'
 require 'IceGrid/Descriptor.rb'
+require 'IceGrid/Exception.rb'
 require 'IceGrid/FileParser.rb'
 require 'IceGrid/Registry.rb'
+require 'IceGrid/Session.rb'
 require 'IceGrid/UserAccountMapper.rb'
