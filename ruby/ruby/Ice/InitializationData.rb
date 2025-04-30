6b8acce8474c9d4d9029b1fa1c5c6ae15c4ea8ad
Jose <jose@zeroc.com>
2025-04-30 13:28:15 +0200

    Remove Ruby InitializationData logger

--- ruby/ruby/Ice/InitializationData.rb
+++ ruby/ruby/Ice/InitializationData.rb
@@ -2,12 +2,11 @@
 
 module Ice
     class InitializationData
-        def initialize(properties=nil, logger=nil, sliceLoader=nil)
+        def initialize(properties=nil, sliceLoader=nil)
             @properties = properties
-            @logger = logger
             @sliceLoader = sliceLoader
         end
 
-        attr_accessor :properties, :logger, :sliceLoader
+        attr_accessor :properties, :sliceLoader
     end
 end
