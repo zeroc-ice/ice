9b8a4837ac6c1b313ef47be9134ec30870c94cbe
Austin Henriksen <austin@zeroc.com>
2025-05-05 15:04:11 -0400

    Updated BOM tests.

--- cpp/test/Slice/utf8BOM/Test.ice
+++ cpp/test/Slice/utf8BOM/Test.ice
@@ -1,9 +1,10 @@
 ﻿// Copyright (c) ZeroC, Inc.
 
-// This file is encoded in UTF8 with BOM
+// This file is encoded in 'UTF-8 with BOM'
 
 #pragma once
 
 module foo
 {
+﻿    // There is an illegal UTF-8 BOM at the start of this line.
 }
--- cpp/test/Slice/utf8BOM/test.py
+++ cpp/test/Slice/utf8BOM/test.py
@@ -18,7 +18,7 @@ class SliceUtf8BomTestCase(ClientTestCase):
         os.mkdir(outdir)
 
         try:
-            # Make sure the BOM marker wasn't accidentally deleted by over-zealous reformatting.
+            # Make sure the leading BOM marker wasn't accidentally deleted by over-zealous reformatting.
             current.write("Checking for UTF8-BOM marker... ")
             with open(test_file_name, 'rb') as test_file:
                 BOM = test_file.read(3)
@@ -26,13 +26,23 @@ class SliceUtf8BomTestCase(ClientTestCase):
                     raise RuntimeError("BOM marker is missing from 'Slice/utf8BOM/Test.ice'!")
             current.writeln("ok")
 
-            # Run 'slice2cpp' over the file to make sure the parser can correctly handle the BOM.
-            # We expect a '0' exit code (indicating success) and nothing to be output to the console.
+            # Run 'slice2cpp' over the file and collect its output so we can make sure the BOMs were properly handled.
             current.write("Compiling Slice file with UTF8-BOM marker... ")
             args = [test_file_name, "--output-dir", "tmp"]
-            slice2cpp.run(current, args, exitstatus=0)
-            if slice2cpp.getOutput(current):
-                raise RuntimeError("Unexpected output when compiling 'Slice/utf8BOM/Test.ice'!")
+            slice2cpp.run(current, args, exitstatus=1)
+            output = slice2cpp.getOutput(current)
+            lines = output.strip().splitlines()
+
+            # There is a known issue with MCPP on certain Linux platforms which causes the Slice compiler to erroneously
+            # report valid UTF-8 BOMS. See https://github.com/zeroc-ice/ice/issues/3940
+            if len(lines) == 2 and isinstance(Util.platform, Util.Linux):
+                if line[0].endswith("Test.ice:2: encountered unexpected UTF-8 BOM in input; BOMs can only appear at the beginning of files"):
+                    lines.pop(0)
+
+            # Other than the above-mentioned MCPP issue, we only expect 1 error for the BOM in the middle of the file.
+            if len(lines) != 1 or not lines[0].endswith("Test.ice:9: encountered unexpected UTF-8 BOM in input; BOMs can only appear at the beginning of files"):
+                raise RuntimeError("Unexpected output from BOM test: {0}".format(lines))
+
             current.writeln("ok")
         finally:
             if os.path.exists(outdir):
