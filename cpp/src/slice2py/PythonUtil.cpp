73a4c8b387e30e4b24530c7a07d9008a5d83b3f1
Jose <jose@zeroc.com>
2025-05-09 16:44:15 +0200

    Fix clang-format errors

--- cpp/src/slice2py/PythonUtil.cpp
+++ cpp/src/slice2py/PythonUtil.cpp
@@ -218,20 +218,20 @@ namespace
             return "";
         }
 
-	ostringstream os;
-	bool first = true;
-	os << "{Ice.Util.format_fields(";
-	for (const auto& dataMember : members)
-	{
-	  if (!first)
-	  {
-	    os << ", ";
-	  }
-	  first = false;
-	  os << dataMember->mappedName() << "=self." << dataMember->mappedName();
-	}
-	os << ")}";
-	return os.str();
+        ostringstream os;
+        bool first = true;
+        os << "{Ice.Util.format_fields(";
+        for (const auto& dataMember : members)
+        {
+            if (!first)
+            {
+                os << ", ";
+            }
+            first = false;
+            os << dataMember->mappedName() << "=self." << dataMember->mappedName();
+        }
+        os << ")}";
+        return os.str();
     }
 }
 
