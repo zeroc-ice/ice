7545047786bb2436f4ae3e34f0387e7cf21260a0
Jose <jose@zeroc.com>
2025-06-03 18:55:47 +0200

    Add back Ice services generated files to PHP

---
+++ php/lib/IceGrid.php
@@ -0,0 +1,9 @@
+<?php
+// Copyright (c) ZeroC, Inc.
+
+require_once 'IceGrid/Admin.php';
+require_once 'IceGrid/Descriptor.php';
+require_once 'IceGrid/FileParser.php';
+require_once 'IceGrid/Registry.php';
+require_once 'IceGrid/UserAccountMapper.php';
+?>
---
+++ php/lib/IceStorm.php
@@ -0,0 +1,6 @@
+<?php
+// Copyright (c) ZeroC, Inc.
+
+require_once 'IceStorm/IceStorm.php';
+require_once 'IceStorm/Metrics.php';
+?>
