e89fc3a6649289c35409bf4ce08fe53aecc5e20c
Jose <pepone@users.noreply.github.com>
2025-06-03 21:04:49 +0200

    Add back Ice services generated files to PHP (#4047)

---
+++ php/lib/Glacier2.php
@@ -0,0 +1,7 @@
+<?php
+// Copyright (c) ZeroC, Inc.
+
+require_once 'Glacier2/Router.php';
+require_once 'Glacier2/PermissionsVerifier.php';
+require_once 'Glacier2/Metrics.php';
+?>
---
+++ php/lib/IceBox.php
@@ -0,0 +1,5 @@
+<?php
+// Copyright (c) ZeroC, Inc.
+
+require_once 'IceBox/IceBox.php';
+?>
