79b91f4972bde08f611a98c0e0c6b44422689023
Bernard Normier <bernard@zeroc.com>
2025-06-06 19:19:48 -0400

    Convert CtrlCHandler into a struct (Swift) (#4068)

--- swift/src/Ice/CtrlCHandler.swift
+++ swift/src/Ice/CtrlCHandler.swift
@@ -3,11 +3,11 @@
 import IceImpl
 
 /// Helps applications handle Ctrl+C (SIGINT) and similar signals (SIGHUP and SIGTERM). Only available on macOS.
-@available(macOS 14, *)
-public final class CtrlCHandler {
+@available(macOS 15, *)
+public struct CtrlCHandler {
     private let handle = ICECtrlCHandler()
 
-    /// Creates a CtrlCHandler. Only one instance of this class can exist in a program at any point in time.
+    /// Creates a CtrlCHandler. Only one instance of this struct can exist in a program at any point in time.
     /// This instance must be created before starting any thread.
     public init() {}
 
