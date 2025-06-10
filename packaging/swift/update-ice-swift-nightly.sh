1042693b72ab8212be453e3a479d33cc63ca25aa
Jose <jose@zeroc.com>
2025-06-10 19:20:56 +0200

    Use authentication token to access ice-swift-nightly repository

--- packaging/swift/update-ice-swift-nightly.sh
+++ packaging/swift/update-ice-swift-nightly.sh
@@ -6,26 +6,29 @@ usage() {
 }
 
 ice_version=${1:-}
-
 if [ -z "$ice_version" ]; then
     usage
     exit 1
 fi
 
 repository_url=${2:-}
-
 if [ -z "$repository_url" ]; then
     usage
     exit 1
 fi
 
+if [ -z "${ICE_SWIFT_NIGHTLY_TOKEN:-}" ]; then
+    echo "Error: ICE_SWIFT_NIGHTLY_TOKEN environment variable is not set"
+    exit 1
+fi
+
 # Get Git repository root directory
 root_dir=$(git rev-parse --show-toplevel)
-cd "$root_dir"/packaging/swift
+cd "$root_dir/packaging/swift"
 
 # Clone the ice-swift-nightly repository and add the Ice for Swift sources
 [ -d ice-swift-nightly ] && rm -rf ice-swift-nightly
-git clone git@github.com:zeroc-ice/ice-swift-nightly -b main
+git clone "https://x-access-token:${ICE_SWIFT_NIGHTLY_TOKEN}@github.com/zeroc-ice/ice-swift-nightly.git" -b main
 cd ice-swift-nightly
 
 cp -rfv ../../../cpp .
@@ -33,16 +36,16 @@ cp -rfv ../../../slice .
 cp -rfv ../../../swift .
 cp -v ../../../Package.swift .
 
-# Download each XCFamework, compute its SHA256, and update Package.swift
+# Download each XCFramework, compute its SHA256, and update Package.swift
 for name in Ice IceDiscovery IceLocatorDiscovery; do
     zip_name=$name-$ice_version.xcframework.zip
     zip_url=$repository_url/$zip_name
 
-    curl -fsSL -o "${zip_name}" "$repository_url/$zip_name"
+    curl -fsSL -o "${zip_name}" "$zip_url"
     checksum=$(shasum -a 256 "${zip_name}" | cut -d ' ' -f 1)
     indent=$(printf "%12s" "") # indentation for the checksum line
 
-    # replace 'path: "cpp/lib/XCFrameworks/$name.xcframework"' with 'url: "$zip_url", checksum: "$checksum"'
+    # Replace path: "..." with url/checksum lines
     sed -i '' -e "s|path: \".*$name\.xcframework\"|url: \"${zip_url}\",\n${indent}checksum: \"${checksum}\"|" Package.swift
 done
 
@@ -51,4 +54,4 @@ git config user.name "ZeroC"
 git config user.email "git@zeroc.com"
 git commit -m "ice: $ice_version Nightly build"
 git tag -a "$ice_version" -m "ice: $ice_version"
-git push origin main
+git push "https://x-access-token:${ICE_SWIFT_NIGHTLY_TOKEN}@github.com/zeroc-ice/ice-swift-nightly.git" main --tags
