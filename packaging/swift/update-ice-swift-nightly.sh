7789a4258c0426414ae33c669e195d1e74757dc2
Jose <jose@zeroc.com>
2025-06-10 11:46:59 +0200

    Move Ice for Swift nightly builds to GitHub

--- packaging/swift/update-ice-swift-nightly.sh
+++ packaging/swift/update-ice-swift-nightly.sh
@@ -23,28 +23,15 @@ fi
 root_dir=$(git rev-parse --show-toplevel)
 cd "$root_dir"/packaging/swift
 
-git_hash=$(git rev-parse HEAD)
+# Clone the ice-swift-nightly repository and add the Ice for Swift sources
+[ -d ice-swift-nightly ] && rm -rf ice-swift-nightly
+git clone git@github.com:zeroc-ice/ice-swift-nightly -b main
+cd ice-swift-nightly
 
-[ -d ice-swift ] && rm -rf ice-swift
-mkdir ice-swift
-pushd ice-swift
-git init .
-git remote add origin https://github.com/zeroc-ice/ice
-git fetch --depth=1 origin "$git_hash"
-git sparse-checkout set cpp/src/Ice cpp/src/Slice cpp/src/slice2swift cpp/include/Ice swift slice Package.swift
-git checkout FETCH_HEAD
-rm -rf .git
-popd
-
-#
-# Perform cleanup on the Package.swift file:
-# - Remove exclude paths for cpp build dirs. "src/.../build"
-# - Replace XCFramework paths with the URL and checksum
-#
-
-# Remove exclude paths for cpp build dirs
-sed -i '' -e '/\/build",/d' ice-swift/Package.swift
-sed -i '' -e '/"test",/d' ice-swift/Package.swift
+cp -rfv ../../../cpp .
+cp -rfv ../../../slice .
+cp -rfv ../../../swift .
+cp -v ../../../Package.swift .
 
 # Download each XCFamework, compute its SHA256, and update Package.swift
 for name in Ice IceDiscovery IceLocatorDiscovery; do
@@ -56,13 +43,12 @@ for name in Ice IceDiscovery IceLocatorDiscovery; do
     indent=$(printf "%12s" "") # indentation for the checksum line
 
     # replace 'path: "cpp/lib/XCFrameworks/$name.xcframework"' with 'url: "$zip_url", checksum: "$checksum"'
-    sed -i '' -e "s|path: \".*$name\.xcframework\"|url: \"${zip_url}\",\n${indent}checksum: \"${checksum}\"|" ice-swift/Package.swift
+    sed -i '' -e "s|path: \".*$name\.xcframework\"|url: \"${zip_url}\",\n${indent}checksum: \"${checksum}\"|" Package.swift
 done
 
-cd ice-swift
-git init --initial-branch=main .
-git add .
+git add Package.swift cpp swift README.md
 git config user.name "ZeroC"
 git config user.email "git@zeroc.com"
-git commit -m "ice: $ice_version"
+git commit -m "ice: $ice_version Nightly build"
 git tag -a "$ice_version" -m "ice: $ice_version"
+git push origin main
