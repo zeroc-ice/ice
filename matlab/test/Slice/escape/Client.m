9fc0ddc4737aa50d881b9847a9604860621a5a3d
Bernard Normier <bernard@zeroc.com>
2025-06-17 09:41:15 -0400

    Improve the InitializationData API in MATLAB (#4117)

--- matlab/test/Slice/escape/Client.m
+++ matlab/test/Slice/escape/Client.m
@@ -7,9 +7,11 @@ function client(args)
     end
 
     helper = TestHelper();
-    initData = Ice.InitializationData();
-    initData.properties_ = helper.createTestProperties(args);
-    initData.sliceLoader = Ice.ClassSliceLoader([?classdef_.logical_, ?classdef_.escaped_xor, ?classdef_.Base, ?classdef_.Derived, ?classdef_.bitand_, ?classdef_.escaped_bitor]);
+
+    % Can even use deprecated properties_ name in constructor (not recommended obviously).
+    initData = Ice.InitializationData(properties_ = helper.createTestProperties(args));
+    initData.SliceLoader = Ice.ClassSliceLoader([?classdef_.logical_, ?classdef_.escaped_xor, ?classdef_.Base, ...
+        ?classdef_.Derived, ?classdef_.bitand_, ?classdef_.escaped_bitor]);
 
     communicator = helper.initialize(initData);
     cleanup = onCleanup(@() communicator.destroy());
