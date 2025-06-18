cdd7c8e5f257c00c7df271667b68ca51d8ca064c
Austin Henriksen <austin@zeroc.com>
2025-06-18 12:10:56 -0400

    Add support for '@remarks' to 'slice2py' and fix enum doc-comments.

--- python/python/Ice/ToStringMode.py
+++ python/python/Ice/ToStringMode.py
@@ -10,16 +10,19 @@ class ToStringMode(EnumBase):
 
     Enumerators:
 
-    - Unicode:  Characters with ordinal values greater than 127 are kept as-is in the resulting string. Non-printable ASCII
-      characters with ordinal values 127 and below are encoded as \\t, \\n (etc.) or \\unnnn.
+    - Unicode:
+          Characters with ordinal values greater than 127 are kept as-is in the resulting string. Non-printable ASCII
+          characters with ordinal values 127 and below are encoded as \\t, \\n (etc.) or \\unnnn.
 
-    - ASCII:  Characters with ordinal values greater than 127 are encoded as universal character names in the resulting
-      string \\unnnn for BMP characters and \\Unnnnnnnn for non-BMP characters. Non-printable ASCII characters
-      with ordinal values 127 and below are encoded as \\t, \\n (etc.) or \\unnnn.
+    - ASCII:
+          Characters with ordinal values greater than 127 are encoded as universal character names in the resulting
+          string \\unnnn for BMP characters and \\Unnnnnnnn for non-BMP characters. Non-printable ASCII characters
+          with ordinal values 127 and below are encoded as \\t, \\n (etc.) or \\unnnn.
 
-    - Compat: Characters with ordinal values greater than 127 are encoded as a sequence of UTF-8 bytes using octal escapes.
-      Characters with ordinal values 127 and below are encoded as \\t, \\n (etc.) or an octal escape. Use this mode
-      to generate strings compatible with Ice 3.6 and earlier.
+    - Compat:
+          Characters with ordinal values greater than 127 are encoded as a sequence of UTF-8 bytes using octal escapes.
+          Characters with ordinal values 127 and below are encoded as \\t, \\n (etc.) or an octal escape. Use this mode
+          to generate strings compatible with Ice 3.6 and earlier.
     """
 
     def __init__(self, _n, _v):
