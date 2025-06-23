4c74412fd8b715a10b0b07d2997600ab92f73b66
Joe George <joe@zeroc.com>
2025-06-23 15:40:24 -0400

    fixes

--- java/src/com.zeroc.ice/src/main/java/com/zeroc/Ice/SyscallException.java
+++ java/src/com.zeroc.ice/src/main/java/com/zeroc/Ice/SyscallException.java
@@ -5,7 +5,7 @@ package com.zeroc.Ice;
 /** This exception is raised if a system error occurred in the server or client process. */
 public class SyscallException extends LocalException {
     /**
-     * Constructs a SyscallException} with a {@code null detail message and the specified cause.
+     * Constructs a SyscallException with a {@code null} detail message and the specified cause.
      *
      * @param cause the cause of this exception
      */
