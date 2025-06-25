951730a92005b36819c78f0bc69bbd8ef70b7208
Joe George <joe@zeroc.com>
2025-06-25 13:32:55 -0400

    Swift doc comment fixes

--- swift/src/Ice/Properties.swift
+++ swift/src/Ice/Properties.swift
@@ -18,8 +18,6 @@ public protocol Properties: AnyObject {
     /// - parameter key: `String` The property key.
     ///
     /// - returns: `String` - The property value or the default value.
-    ///
-    /// - throws: `PropertyException` when the property is not a known Ice property.
     func getIceProperty(_ key: String) -> String
 
     /// Get a property by key. If the property is not set, the given default value is returned.
@@ -46,8 +44,7 @@ public protocol Properties: AnyObject {
     ///
     /// - returns: `Int32` - The property value interpreted as an integer, or the default value.
     ///
-    /// - throws: `PropertyException` when the property is not a known Ice property or if the
-    /// property value is not a valid integer.
+    /// - throws: `PropertyException` when the property value is not a valid integer.
     func getIcePropertyAsInt(_ key: String) throws -> Int32
 
     /// Get a property as an integer. If the property is not set, the given default value is returned.
@@ -81,8 +78,6 @@ public protocol Properties: AnyObject {
     /// - parameter key: `String` The property key.
     ///
     /// - returns: `StringSeq` - The property value interpreted as list of strings, or the default value.
-    ///
-    /// - throws: `PropertyException` when the property is not a known Ice property.
     func getIcePropertyAsList(_ key: String) -> StringSeq
 
     /// Get a property as a list of strings.  The strings must be separated by whitespace or comma. If the property is
