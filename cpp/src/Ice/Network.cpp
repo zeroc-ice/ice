91216c39d9e1bf27ec5b7b0acbd8946abfcc986c
Bernard Normier <bernard@zeroc.com>
2025-08-27 14:03:38 -0400

    Reformat

--- cpp/src/Ice/Network.cpp
+++ cpp/src/Ice/Network.cpp
@@ -25,9 +25,9 @@
 #    include <winsock2.h>
 #    include <ws2tcpip.h>
 #else
+#    include <ifaddrs.h>
 #    include <net/if.h>
 #    include <sys/ioctl.h>
-#    include <ifaddrs.h>
 #endif
 
 using namespace std;
