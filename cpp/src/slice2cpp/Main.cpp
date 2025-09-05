9eec0627acec7a4a419f79a815019138dcaef37c
Austin Henriksen <austin@zeroc.com>
2025-09-05 17:25:31 -0400

    Fix Slice compiler hang when preprocessor returns non-zero status.

--- cpp/src/slice2cpp/Main.cpp
+++ cpp/src/slice2cpp/Main.cpp
@@ -196,7 +196,10 @@ compile(const vector<string>& argv)
         {
             preprocessor = Preprocessor::create(argv[0], fileName, preprocessorArgs);
             FILE* preprocessedHandle = preprocessor->preprocess("-D__SLICE2CPP__");
-            assert(preprocessedHandle);
+            if (preprocessedHandle == nullptr)
+            {
+                return EXIT_FAILURE;
+            }
 
             unit = Unit::createUnit("cpp", false);
             int parseStatus = unit->parse(fileName, preprocessedHandle, debug);
