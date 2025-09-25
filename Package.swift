2296a0fe40dd437fc95c0b151d8a462ad9e98c3a
Joe George <joe@zeroc.com>
2025-09-25 15:27:16 -0400

    Small fix to RPM README

--- Package.swift
+++ Package.swift
@@ -33,7 +33,6 @@ let package = Package(
         .plugin(name: "CompileSlice", targets: ["CompileSlice"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zeroc-ice/mcpp.git", branch: "master"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3"),
     ],
     targets: [
