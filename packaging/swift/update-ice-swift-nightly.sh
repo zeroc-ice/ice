a759df0e2db4a541fb0aa4560d736aefd7a0ca8d
Jose <jose@zeroc.com>
2025-09-26 10:22:57 +0200

    Another fix

--- packaging/swift/update-ice-swift-nightly.sh
+++ packaging/swift/update-ice-swift-nightly.sh
@@ -24,6 +24,10 @@ cd "$root_dir/packaging/swift"
 git clone "https://x-access-token:${ICE_NIGHTLY_PUBLISH_TOKEN}@github.com/zeroc-ice/ice-swift-nightly.git" -b main
 cd ice-swift-nightly
 
+# Clean up the repository to ensure files removed from ice are not kept in ice-swift-nightly
+rm -rfv *
+
+# Now copy the Ice for Swift sources
 cp -rfv ../../../cpp .
 cp -rfv ../../../slice .
 cp -rfv ../../../swift .
