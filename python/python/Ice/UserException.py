cdd7df7c5529e6376f2687be4edd902dbc41ec54
Jose <pepone@users.noreply.github.com>
2025-09-29 22:00:26 +0200

    Fix bogus __str__ for unmarshalled UserException  (#4487)

--- python/python/Ice/UserException.py
+++ python/python/Ice/UserException.py
@@ -8,5 +8,11 @@ class UserException(IceException):
     The base class for all user-defined exceptions.
     """
 
+    def __str__(self):
+        # Types derived from UserException are data classes. We override __str__ to delegate to the generated __repr__,
+        # since the built-in __str__ from BaseException only works when all arguments are passed to the constructor,
+        # which is not the case for exceptions unmarshalled from the wire.
+        return self.__repr__()
+
 
 __all__ = ["UserException"]
