a129427ad83b2faf11f3a081fb6ccfbd5e642401
Bernard Normier <bernard@zeroc.com>
2025-10-06 13:43:44 -0400

    Run idleTimeout on main thread

--- scripts/tests/Ice/idleTimeout.py
+++ scripts/tests/Ice/idleTimeout.py
@@ -6,4 +6,9 @@ from Util import ClientServerTestCase, TestSuite
 
 traceProps = {"Ice.Trace.Network": 3, "Ice.Trace.Protocol": 1}
 
-TestSuite(__name__, [ClientServerTestCase(traceProps=traceProps)], options={"compress": [False], "serialize": [False]})
+TestSuite(
+    __name__,
+    [ClientServerTestCase(traceProps=traceProps)],
+    runOnMainThread=True,
+    options={"compress": [False], "serialize": [False]}
+)
