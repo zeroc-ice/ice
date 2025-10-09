c90527078c36bb0a26da9226d0064d1e3bc8c49d
Bernard Normier <bernard@zeroc.com>
2025-10-09 14:59:36 -0400

    Add test(false)

--- java/test/src/main/java/test/Ice/udp/AllTests.java
+++ java/test/src/main/java/test/Ice/udp/AllTests.java
@@ -185,6 +185,10 @@ public class AllTests {
             }
 
             test(ret);
+            if ("1".equals(communicator.getProperties().getIceProperty("Ice.IPv6"))) {
+                out.print("aborting to get the log files");
+                test(false);
+            }
             out.println("ok");
 
             out.print("testing udp bi-dir connection... ");
