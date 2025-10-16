907a6f04596e858058a698af8ca51c0e7f3f92b5
Bernard Normier <bernard@zeroc.com>
2025-10-16 10:35:14 -0400

    Apply suggestion from @externl
    Co-authored-by: Joe George <joe@externl.com>

--- cpp/test/Glacier2/dynamicFiltering/Server.cpp
+++ cpp/test/Glacier2/dynamicFiltering/Server.cpp
@@ -38,7 +38,7 @@ public:
     {
     }
 
-    [[nodiscard]] std::optional<Ice::ObjectPrx> findObjectById(Identity id, const Current&) const override
+    [[nodiscard]] optional<Ice::ObjectPrx> findObjectById(Identity id, const Current&) const override
     {
         return _adapter->createProxy(id);
     }
