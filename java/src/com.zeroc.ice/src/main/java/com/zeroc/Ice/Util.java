73558266608025e92c2ce4df94a77f34a90528d2
Bernard Normier <bernard@zeroc.com>
2025-10-20 12:35:58 -0400

    Remove bad import

--- java/src/com.zeroc.ice/src/main/java/com/zeroc/Ice/Util.java
+++ java/src/com.zeroc.ice/src/main/java/com/zeroc/Ice/Util.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.Identity;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadFactory;
