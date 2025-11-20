7f8249575a827b1f7a734e65a02fd1e204ebec59
Austin Henriksen <austin@zeroc.com>
2025-11-20 11:41:03 -0500

    Revert "TEMP"
    This reverts commit ed56f227a10ca67907cb6d9bf1b1a75842ced907.

--- java/src/com.zeroc.ice/src/main/java/com/zeroc/Ice/SliceTypeId.java
+++ java/src/com.zeroc.ice/src/main/java/com/zeroc/Ice/SliceTypeId.java
@@ -11,15 +11,15 @@ import java.lang.annotation.Target;
  * Assigns a Slice type ID to a class or to an interface.
  *
  * <p>The Slice compiler assigns Slice type IDs to the classes and interfaces it generates from Slice classes,
- * exceptions, and interfaces.
+ * exceptions, and interfaces.</p>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface SliceTypeId {
     /**
-     * Returns the Slice type ID assigned to this class or interface.
+     * The Slice type ID of this class or interface.
      *
-     * @return the Slice type ID 
+     * @return the Slice type ID of this class or interface
     */
     String value();
 }
