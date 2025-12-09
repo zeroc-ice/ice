f6657f9b61b90029c759c4cbe1b613cb42d42cd7
Bernard Normier <bernard@zeroc.com>
2025-12-09 16:04:19 -0500

    Fix test

--- cpp/test/IceStorm/createOrRetrieve/test.py
+++ cpp/test/IceStorm/createOrRetrieve/test.py
@@ -1,6 +1,6 @@
 # Copyright (c) ZeroC, Inc.
 
-from IceStormUtil import IceStorm, IceStormTestCase
+from IceStormUtil import IceStorm, IceStormTestCase, IceStormProcess
 from Util import Client, ClientTestCase, TestSuite
 
 props = {}
@@ -13,18 +13,26 @@ class CreateOrRetrieveTestCase(IceStormTestCase):
     def teardownClientSide(self, current, success):
         self.shutdown(current)
 
+class IceStormClient(IceStormProcess, Client):
+    processType = "client"
+
+    def __init__(self, instanceName=None, instance=None, *args, **kargs):
+        Client.__init__(self, *args, **kargs)
+        IceStormProcess.__init__(self, instanceName, instance)
+
+    getParentProps = Client.getProps  # Used by IceStormProcess to get the client properties
 
 TestSuite(
     __file__,
     [
         CreateOrRetrieveTestCase(
-            "persistent", icestorm=persistent, client=ClientTestCase(client=Client())
+            "persistent", icestorm=persistent, client=ClientTestCase(client=IceStormClient())
         ),
         CreateOrRetrieveTestCase(
-            "transient", icestorm=transient, client=ClientTestCase(client=Client())
+            "transient", icestorm=transient, client=ClientTestCase(client=IceStormClient())
         ),
         CreateOrRetrieveTestCase(
-            "replicated", icestorm=replicated, client=ClientTestCase(client=Client())
+            "replicated", icestorm=replicated, client=ClientTestCase(client=IceStormClient())
         ),
     ],
     multihost=False,
