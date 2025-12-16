234b4c632f98787ed7e4bd67d4f465f611599264
Jose <jose@zeroc.com>
2025-12-16 18:10:55 +0100

    Update mcpp deps

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         .plugin(name: "CompileSlice", targets: ["CompileSlice"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zeroc-ice/mcpp.git", .upToNextMinor(from: "2.7.3")),
+        .package(url: "https://github.com/zeroc-ice/mcpp.git", revision: "4137e39e62bd22c232b470c9516669fc3d1a1353"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3"),
     ],
     targets: [
