e108b1e202c74e0dabb5365cb49e7514aaef942d
Jose <pepone@users.noreply.github.com>
2025-12-17 16:43:01 +0100

    Refactor MSI workflow (#4779)

--- packaging/release/prepare-release-assets.sh
+++ packaging/release/prepare-release-assets.sh
@@ -56,6 +56,9 @@ cp -v "${STAGING_DIR}/pip-packages-macos-26-3.13"/zeroc_ice-*.whl .
 cp -v "${STAGING_DIR}/pip-packages-macos-26-3.14"/zeroc_ice-*.whl .
 cp -v "${STAGING_DIR}/pip-packages-macos-26-3.14"/zeroc_ice-*.tar.gz .
 
+# IceGridGUI JAR package
+cp -v "${STAGING_DIR}/icegridgui-jar/icegridgui.jar" .
+
 # Create RPM package archives (.zip and .tar.gz) for each distribution/arch
 for dir in "${STAGING_DIR}"/rpm-packages-*; do
   name=$(basename "$dir")
