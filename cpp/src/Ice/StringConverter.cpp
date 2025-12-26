a83ab26a4df237a8c18293377d1ee5a533f37e3f
Bernard Normier <bernard@zeroc.com>
2025-12-26 17:32:05 -0500

    Fix clang build for c++23 and c++26

--- cpp/src/Ice/StringConverter.cpp
+++ cpp/src/Ice/StringConverter.cpp
@@ -6,6 +6,10 @@
 #    define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
 #elif (__cplusplus >= 201703L)
 #    include "DisableWarnings.h"
+#    if defined(__clang__) // for clang in C++26 mode
+#       define _LIBCPP_ENABLE_CXX26_REMOVED_CODECVT
+#       define _LIBCPP_ENABLE_CXX26_REMOVED_WSTRING_CONVERT
+#    endif
 #endif
 
 #include "Ice/StringConverter.h"
--- cpp/src/Slice/FileTracker.h
+++ cpp/src/Slice/FileTracker.h
@@ -7,6 +7,7 @@
 #include <map>
 #include <memory>
 #include <stdexcept>
+#include <string>
 
 namespace Slice
 {
