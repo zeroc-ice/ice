6b52d2323e014b0a39e502ed7e447164f8856ee2
Jose <jose@zeroc.com>
2026-02-04 12:01:26 +0100

    Refactor TcpTransceiver.js error handling and code structure
    - Add constructor validation for address parameter
    - Extract addressToString helper function for consistent address formatting
    - Simplify initialize() error handling with targeted try-catch for socket creation
    - Refactor socketClosed() to properly handle the hadError parameter
    - Simplify close() by removing unnecessary try-catch around fd.destroy()
    - Update translateError() to expect pre-formatted address string

--- js/src/Ice/TcpTransceiver.js
+++ js/src/Ice/TcpTransceiver.js
@@ -7,6 +7,7 @@ import {
     ConnectionLostException,
     ConnectionRefusedException,
     ConnectFailedException,
+    InitializationException,
     SocketException,
 } from "./LocalExceptions.js";
 
@@ -18,6 +19,9 @@ const StateConnected = 2;
 
 class TcpTransceiver {
     constructor(instance, addr, sourceAddr) {
+        if (addr == null || addr.host === undefined || addr.port === undefined) {
+            throw new InitializationException("invalid address provided to TcpTransceiver constructor");
+        }
         this._logger = instance.logger();
         this._readBuffers = [];
         this._readPosition = 0;
@@ -25,11 +29,12 @@ class TcpTransceiver {
         this._fd = null;
         this._addr = addr;
         this._sourceAddr = sourceAddr;
-        this._desc = "local address = <not connected>\nremote address = " + addr.host + ":" + addr.port;
+        this._desc = "local address = <not connected>\nremote address = " + addressToString(this._addr);
         this._state = StateNeedConnect;
         this._registered = false;
         this._exception = null;
     }
+
 
     setCallbacks(connectedCallback, bytesAvailableCallback, bytesWrittenCallback) {
         this._connectedCallback = connectedCallback;
@@ -41,45 +46,35 @@ class TcpTransceiver {
     // Returns SocketOperation.None when initialization is complete.
     //
     initialize() {
-        try {
-            if (this._exception) {
-                throw this._exception;
-            }
-
-            if (this._state === StateNeedConnect) {
-                this._state = StateConnectPending;
+        if (this._exception) {
+            throw this._exception;
+        }
+        if (this._state === StateNeedConnect) {
+            this._state = StateConnectPending;
+            try {
                 this._fd = net.createConnection({
                     port: this._addr.port,
                     host: this._addr.host,
                     localAddress: this._sourceAddr,
                 });
-
-                this._fd.on("connect", () => this.socketConnected());
-                this._fd.on("data", buf => this.socketBytesAvailable(buf));
-
-                //
-                // The error callback can be triggered from the socket
-                // write(). We don't want it to be dispatched right away
-                // from within the write() so we delay the call with
-                // setImmediate. We do the same for close as a
-                // precaution. See also issue #6226.
-                //
-                this._fd.on("close", err => Timer.setImmediate(() => this.socketClosed(err)));
-                this._fd.on("error", err => Timer.setImmediate(() => this.socketError(err)));
-
-                return SocketOperation.Connect; // Waiting for connect to complete.
-            } else if (this._state === StateConnectPending) {
-                //
-                // Socket is connected.
-                //
-                this._desc = fdToString(this._fd, this._addr);
-                this._state = StateConnected;
+            } catch (err) {
+                this._exception = translateError(this._state, err, addressToString(this._addr));
+                throw this._exception;
             }
-        } catch (err) {
-            if (!this._exception) {
-                this._exception = translateError(this._state, err, this._addr);
-            }
-            throw this._exception;
+
+            this._fd.on("connect", () => this.socketConnected());
+            this._fd.on("data", buf => this.socketBytesAvailable(buf));
+
+            // The error callback can be triggered from the socket write(). We don't want it to be dispatched right
+            // away from within the write() so we delay the call with setImmediate. We do the same for close as a
+            // precaution. See also issue #6226.
+            this._fd.on("close", hadError => Timer.setImmediate(() => this.socketClosed(hadError)));
+            this._fd.on("error", err => Timer.setImmediate(() => this.socketError(err)));
+
+            return SocketOperation.Connect; // Waiting for connect to complete.
+        } else if (this._state === StateConnectPending) {
+            this._desc = fdToString(this._fd, this._addr);
+            this._state = StateConnected;
         }
 
         console.assert(this._state === StateConnected);
@@ -119,14 +114,8 @@ class TcpTransceiver {
             // before calling transceiver initialize.
             return;
         }
-
-        try {
-            this._fd.destroy();
-        } catch (ex) {
-            throw translateError(this._state, ex, this._addr);
-        } finally {
-            this._fd = null;
-        }
+        this._fd.destroy();
+        this._fd = null;
     }
 
     /**
@@ -236,18 +225,22 @@ class TcpTransceiver {
         }
     }
 
-    socketClosed(err) {
-        //
-        // Don't call the closed callback if an error occurred; the error callback
-        // will be called.
-        //
-        if (!err) {
-            this.socketError(null);
+    socketClosed(hadError) {
+        if (hadError) {
+            console.assert(this._exception !== null && this._exception !== undefined);
+            return; // Already handled by the error callback.
+        }
+
+        this._exception = new ConnectionLostException(addressToString(this._addr));
+        if (this._state < StateConnected) {
+            this._connectedCallback();
+        } else if (this._registered) {
+            this._bytesAvailableCallback();
         }
     }
 
     socketError(err) {
-        this._exception = translateError(this._state, err, this._addr);
+        this._exception = translateError(this._state, err, addressToString(this._addr));
         if (this._state < StateConnected) {
             this._connectedCallback();
         } else if (this._registered) {
@@ -260,15 +253,15 @@ function fdToString(fd, targetAddr) {
     if (fd === null) {
         return "<closed>";
     }
-
     return addressesToString(fd.localAddress, fd.localPort, fd.remoteAddress, fd.remotePort, targetAddr);
 }
 
-function translateError(state, err, addr) {
-    const address = addr ? `${addr.host}:${addr.port}` : null;
-    if (!err) {
-        return new ConnectionLostException(address);
-    } else if (state < StateConnected) {
+function addressToString(addr) {
+    return `${addr.host}:${addr.port}`;
+}
+
+function translateError(state, err, address) {
+    if (state < StateConnected) {
         if (connectionRefused(err.code)) {
             return new ConnectionRefusedException(address, { cause: err });
         } else if (connectionFailed(err.code)) {
@@ -277,7 +270,7 @@ function translateError(state, err, addr) {
     } else if (connectionLost(err.code)) {
         return new ConnectionLostException(address, { cause: err });
     }
-    return new SocketException(address ? `Socket error on ${address}.` : "socket exception", { cause: err });
+    return new SocketException(`Socket error on ${address}.`, { cause: err });
 }
 
 function addressesToString(localHost, localPort, remoteHost, remotePort, targetAddr) {
