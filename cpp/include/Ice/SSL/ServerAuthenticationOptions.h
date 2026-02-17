305c6c69dc22361d538521ed1926cd78c02c4d60
Jose <jose@zeroc.com>
2026-02-17 15:24:17 +0100

    Clarify that hRootStore from credentials callback is not used for validation

--- cpp/include/Ice/SSL/ServerAuthenticationOptions.h
+++ cpp/include/Ice/SSL/ServerAuthenticationOptions.h
@@ -34,7 +34,9 @@ namespace Ice::SSL
         /// This callback is invoked by the SSL transport for each new incoming connection before starting the SSL
         /// handshake to determine the appropriate server credentials. The callback must return a `SCH_CREDENTIALS` that
         /// represents the server's credentials. The SSL transport takes ownership of the credentials' `paCred` and
-        /// `hRootStore` members and releases them when the connection is closed.
+        /// `hRootStore` members and releases them when the connection is closed. Note that the `hRootStore` from the
+        /// returned credentials is not used for certificate validation; use `trustedRootCertificates` or
+        /// `clientCertificateValidationCallback` instead.
         ///
         /// @param adapterName The name of the object adapter that accepted the connection.
         /// @return The server SSL credentials.
