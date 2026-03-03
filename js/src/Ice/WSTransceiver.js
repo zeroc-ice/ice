c7d37ca3ab163325f1d8bac86341abc50641d9a4
Jose <pepone@users.noreply.github.com>
2026-03-03 15:21:58 +0100

    Fix WebSocket URL construction for IPv6 and wss default port (#5083, #5084) (#5086) (#5200)
    Wrap IPv6 literal addresses in brackets per RFC 3986/6455, and use 443 as the
    default port for wss instead of 80.

--- js/src/Ice/WSTransceiver.js
+++ js/src/Ice/WSTransceiver.js
@@ -390,8 +390,9 @@ if (typeof WebSocket !== 'undefined')
         {
             const transceiver = new WSTransceiver(instance);
             let url = secure ? "wss" : "ws";
-            url += "://" + addr.host;
-            if(addr.port !== 80)
+            const isIPv6 = addr.host.indexOf(":") !== -1;
+            url += "://" + (isIPv6 ? `[${addr.host}]` : addr.host);
+            if(addr.port !== (secure ? 443 : 80))
             {
                 url += ":" + addr.port;
             }
