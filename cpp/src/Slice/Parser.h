30f571e58d2baa81f42fe3cbeb57d60716a5a7f0
Austin Henriksen <austin.r.henriksen79@gmail.com>
2026-04-01 13:40:25 -0400

    Break up some of the functions.

--- cpp/src/Slice/Parser.h
+++ cpp/src/Slice/Parser.h
@@ -743,12 +743,16 @@ namespace Slice
 
         /// Returns true if this operation uses class types in any of its in parameters.
         [[nodiscard]] bool sendsClasses() const;
+
         /// Returns true if this operation uses class types in its return type, or any of its out parameters.
         [[nodiscard]] bool returnsClasses() const;
+
         /// Returns true if this operation has any out parameters, a non-void return type, or can throw any exceptions.
         [[nodiscard]] bool returnsData() const;
+
         /// Returns true if this operation has any out parameters or a non-void return type.
         [[nodiscard]] bool returnsAnyValues() const;
+
         /// Returns true if this operation's output parameters, plus any non-void return type, is greater than 1.
         [[nodiscard]] bool returnsMultipleValues() const;
 
