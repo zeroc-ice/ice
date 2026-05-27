620cc45048075293ef58d00e0220ad2c6d235d12
Jose <jose@zeroc.com>
2026-05-27 13:15:09 +0200

    Consume --TestAdapter.* args in C# and Java TestHelper
    Brings the C# and Java test helpers into parity with C++
    (cpp/test/Common/TestHelper.cpp:286-293), which has long consumed both
    --Test.* and --TestAdapter.* arguments during createTestProperties.
    Without this, the framework's Server(props={"TestAdapter.X": ...})
    mechanism silently dropped adapter-prefixed properties on the C# and
    Java sides. The wsAllowedOrigins Server.cs and Server.java no longer
    need a workaround.

--- csharp/test/Ice/wsAllowedOrigins/Server.cs
+++ csharp/test/Ice/wsAllowedOrigins/Server.cs
@@ -15,9 +15,6 @@ public class Server : Test.TestHelper
     public override void run(string[] args)
     {
         using Communicator communicator = initialize(ref args);
-        // The C# test framework's TestHelper only consumes --Test.* args; this test relies on the framework passing
-        // --TestAdapter.AllowedOrigins=..., so we consume the TestAdapter prefix explicitly here.
-        args = communicator.getProperties().parseCommandLineOptions("TestAdapter", args);
         communicator.getProperties().setProperty("TestAdapter.Endpoints", getTestEndpoint(0, "ws"));
         Ice.ObjectAdapter adapter = communicator.createObjectAdapter("TestAdapter");
         adapter.activate();
--- java/test/src/main/java/test/Ice/wsAllowedOrigins/Server.java
+++ java/test/src/main/java/test/Ice/wsAllowedOrigins/Server.java
@@ -10,9 +10,6 @@ import test.TestHelper;
 public class Server extends TestHelper {
     public void run(String[] args) {
         try (Communicator communicator = initialize(args)) {
-            // The Java test framework's TestHelper only consumes --Test.* args; this test relies on the framework
-            // passing --TestAdapter.AllowedOrigins=..., so we consume the TestAdapter prefix explicitly here.
-            communicator.getProperties().parseCommandLineOptions("TestAdapter", args);
             communicator.getProperties().setProperty("TestAdapter.Endpoints", getTestEndpoint(0, "ws"));
             ObjectAdapter adapter = communicator.createObjectAdapter("TestAdapter");
             adapter.activate();
