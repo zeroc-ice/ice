3f6d56333d4f359bbd16bf77979c1b594386026e
Jose <pepone@users.noreply.github.com>
2026-06-23 18:06:10 +0200

    Delete copy/move on IceRuby classes that register a GC root (#5674)

--- ruby/src/IceRuby/RubySliceLoader.h
+++ ruby/src/IceRuby/RubySliceLoader.h
@@ -15,6 +15,12 @@ namespace IceRuby
         explicit RubySliceLoader(VALUE sliceLoader);
         ~RubySliceLoader() final;
 
+        // Registers a GC root tied to the address of _sliceLoader, so it's neither copyable nor movable.
+        RubySliceLoader(const RubySliceLoader&) = delete;
+        RubySliceLoader(RubySliceLoader&&) = delete;
+        RubySliceLoader& operator=(const RubySliceLoader&) = delete;
+        RubySliceLoader& operator=(RubySliceLoader&&) = delete;
+
         [[nodiscard]] Ice::ValuePtr newClassInstance(std::string_view typeId) const final;
 
         // TODO: we currently don't support loading of custom Slice exceptions from Ruby.
