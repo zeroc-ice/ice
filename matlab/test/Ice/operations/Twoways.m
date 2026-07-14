df9c4a364789e0a92649df8e34ee66f467a22fa4
Jose <pepone@users.noreply.github.com>
2026-07-14 20:18:58 +0200

    Fix MATLAB Communicator.getImplicitContext crash under the default configuration (#5975)

--- matlab/test/Ice/operations/Twoways.m
+++ matlab/test/Ice/operations/Twoways.m
@@ -1295,6 +1295,13 @@ classdef Twoways
             assert(isequal(r, ctx));
 
             %
+            % Under the default configuration (Ice.ImplicitContext=None), getImplicitContext returns an empty array.
+            %
+            icNone = helper.initialize(Properties = helper.communicator().getProperties().clone());
+            assert(isempty(icNone.getImplicitContext()));
+            icNone.destroy();
+
+            %
             % Test implicit context propagation
             %
             impls = {'Shared', 'PerThread'};
