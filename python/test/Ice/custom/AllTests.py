51e3bc087bfc1f4166c13242b6793bd2c52de17d
Austin Henriksen <austin@zeroc.com>
2026-08-03 15:14:04 -0400

    Changelog fragment and small fix.

--- python/test/Ice/custom/AllTests.py
+++ python/test/Ice/custom/AllTests.py
@@ -76,7 +76,7 @@ def allTests(helper: TestHelper, communicator: Ice.Communicator) -> Test.CustomP
     test(r == stringList)
     test(b2 == stringList)
 
-    s = Test.S() # Must use the default-constructor, which relies on imports to construct its fields.
+    s = Test.S() # Must use the default constructor, which relies on imports to construct its fields.
     s.b1 = bytes(byteList)
     s.b2 = byteList
     s.b4 = byteList
@@ -86,7 +86,7 @@ def allTests(helper: TestHelper, communicator: Ice.Communicator) -> Test.CustomP
     s.a1 = array.array("b", byteList)
     custom.sendS(s)
 
-    c = Test.C() # Must use the default-constructor, which relies on imports to construct its fields.
+    c = Test.C() # Must use the default constructor, which relies on imports to construct its fields.
     c.b1 = bytes(byteList)
     c.b2 = byteList
     c.b4 = byteList
