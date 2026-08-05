f76911b1b50e8a5bc9a2163d2e6ede93b48120ee
dev_Hakaze <af.nawfal@gmail.com>
2026-08-05 20:21:46 +0700

    Reject invalid discovery timeout properties at initialization (#6186)
    (cherry picked from commit 2a8f89b22ae7ff3b03f6f2169e4fc513a9b25d67)

--- cpp/src/IceDiscovery/LookupI.cpp
+++ cpp/src/IceDiscovery/LookupI.cpp
@@ -193,6 +193,10 @@ LookupI::LookupI(LocatorRegistryIPtr registry, const LookupPrx& lookup, const Ic
       _domainId(properties->getIceProperty("IceDiscovery.DomainId")),
       _timer(IceInternal::getInstanceTimer(lookup->ice_getCommunicator()))
 {
+    if (_timeout <= chrono::milliseconds::zero())
+    {
+        throw PropertyException{__FILE__, __LINE__, "property 'IceDiscovery.Timeout' must be greater than 0"};
+    }
     if (_latencyMultiplier < 1)
     {
         throw PropertyException{
