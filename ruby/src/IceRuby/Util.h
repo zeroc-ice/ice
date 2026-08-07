1ba0bc2675e40e214f53b43780b9600c0a89bad4
Austin Henriksen <austin@zeroc.com>
2026-08-07 14:07:52 -0400

    Remove more dead code from Ruby.

--- ruby/src/IceRuby/Util.h
+++ ruby/src/IceRuby/Util.h
@@ -86,12 +86,6 @@ namespace IceRuby
     VALUE stringSeqToArray(const std::vector<std::string>&);
 
     //
-    // Convert a vector of std::byte into a Ruby array of numbers.
-    // May raise RubyException.
-    //
-    VALUE createNumSeq(const std::vector<std::byte>&);
-
-    //
     // Convert a Ruby hash to Ice::Context. Returns true on success
     // and false if the value is not a hash. May raise RubyException.
     //
