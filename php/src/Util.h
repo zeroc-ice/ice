69faed1fde5b4ca84cbb28170f2cf4f5564335a5
Bernard Normier <bernard@zeroc.com>
2026-08-07 10:20:40 -0400

    Raise RuntimeException when an Ice class lookup fails in IcePHP (#6433)
    (cherry picked from commit 11a048066492bf4a786c69ad5399b478e6c47d39)

--- php/src/Util.h
+++ php/src/Util.h
@@ -51,6 +51,11 @@ namespace IcePHP
 
     zend_class_entry* nameToClass(const std::string&);
 
+    // Like nameToClass, but fails when the class is not defined - typically because the application didn't load the
+    // Ice library files. On failure, this function raises a RuntimeException in the PHP interpreter and returns
+    // nullptr; the caller must check for nullptr and propagate the failure.
+    zend_class_entry* lookupClass(const std::string&);
+
     bool createIdentity(zval*, const Ice::Identity&);
     bool extractIdentity(zval*, Ice::Identity&);
 
