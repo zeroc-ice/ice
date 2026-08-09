55da1549519e915ae26d8a257dbd0dfb9d9f6f0f
Bernard Normier <bernard@zeroc.com>
2026-08-09 16:12:54 -0400

    Fix Python ice_invokeAsync result for oneway, datagram, and batch proxies (#6508)
    (cherry picked from commit 356e3822b23c26461d367c3c60c7fc7c51b25c9f)

--- python/test/Ice/blobject/RouterI.py
+++ python/test/Ice/blobject/RouterI.py
@@ -21,20 +21,10 @@ class BlobjectCall(object):
 
         if "_fwd" in self._curr.ctx and self._curr.ctx["_fwd"] == "o":
             proxy = proxy.ice_oneway()
-            try:
-                ok, out = proxy.ice_invoke(
-                    self._curr.operation,
-                    self._curr.mode,
-                    self._inParams,
-                    self._curr.ctx,
-                )
-                self._future.set_result((ok, out))
-            except Ice.Exception as e:
-                self._future.set_exception(e)
-        else:
-            f = proxy.ice_invokeAsync(self._curr.operation, self._curr.mode, self._inParams, self._curr.ctx)
-            assert isinstance(f, Ice.Future)
-            f.add_done_callback(self.done)
+
+        f = proxy.ice_invokeAsync(self._curr.operation, self._curr.mode, self._inParams, self._curr.ctx)
+        assert isinstance(f, Ice.Future)
+        f.add_done_callback(self.done)
 
     def done(self, future: Ice.Future):
         try:
@@ -119,14 +109,9 @@ class BlobjectI(Ice.Blobject):
         if len(current.facet) > 0:
             proxy = proxy.ice_facet(current.facet)
 
-        try:
-            if "_fwd" in current.ctx and current.ctx["_fwd"] == "o":
-                proxy = proxy.ice_oneway()
-                return proxy.ice_invoke(current.operation, current.mode, bytes, current.ctx)
-            else:
-                return proxy.ice_invoke(current.operation, current.mode, bytes, current.ctx)
-        except Ice.Exception:
-            raise
+        if "_fwd" in current.ctx and current.ctx["_fwd"] == "o":
+            proxy = proxy.ice_oneway()
+        return proxy.ice_invoke(current.operation, current.mode, bytes, current.ctx)
 
     def add(self, proxy: Ice.ObjectPrx):
         with self._lock:
