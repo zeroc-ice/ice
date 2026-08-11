389b6791c9bd2efcc25fb2f8359862d3e6c699a4
Jose <jose@zeroc.com>
2026-08-11 10:40:41 +0200

    Untie the IcePy background waits from the lifetime of the Python object

--- python/modules/IcePy/Thread.h
+++ python/modules/IcePy/Thread.h
@@ -7,8 +7,42 @@
 #include "Ice/Ice.h"
 #include "Util.h"
 
+#include <future>
+#include <memory>
+#include <thread>
+
 namespace IcePy
 {
+    /// Executes a blocking wait in a new detached thread, and returns a future that completes when this wait
+    /// completes.
+    ///
+    /// The wait must not use any Python state: it keeps running - and the returned future remains safe to destroy -
+    /// after the Python object that started this wait is deallocated. This is why we don't use std::async here: the
+    /// destructor of a future returned by std::async waits for the completion of the task.
+    ///
+    /// Throws when the thread cannot be started, in which case no wait is in progress.
+    template<typename Wait> std::future<void> waitInThread(Wait wait)
+    {
+        auto promise{std::make_shared<std::promise<void>>()};
+        std::future<void> future{promise->get_future()};
+
+        std::thread{[wait = std::move(wait), promise]
+                    {
+                        try
+                        {
+                            wait();
+                            promise->set_value();
+                        }
+                        catch (...)
+                        {
+                            promise->set_exception(std::current_exception());
+                        }
+                    }}
+            .detach();
+
+        return future;
+    }
+
     /// Release Python's Global Interpreter Lock during potentially time-consuming (and non-Python related) work.
     class AllowThreads
     {
